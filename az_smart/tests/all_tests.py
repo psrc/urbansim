2c7c7102e385880623bb93383802a756ea1fc664
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-02-02 00:00:23 +0000

    Initial import.
    git-svn-id: https://svn.urbansim.org/trunk@10293 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ az_smart/tests/all_tests.py
@@ -0,0 +1,17 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from opus_core.tests.utils.package_tester import PackageTester
+
+PackageTester().run_all_tests_for_package('az_smart')
