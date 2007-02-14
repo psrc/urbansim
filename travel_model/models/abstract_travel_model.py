a53c1f68637d1ca5d46c143a53c01c638594679d
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-02-14 01:15:50 +0000

    (lmwang)check in travel model package
    git-svn-id: https://svn.urbansim.org/trunk@10307 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ travel_model/models/abstract_travel_model.py
@@ -0,0 +1,41 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+import os
+from opus_core.model import Model
+from opus_core.logger import logger
+
+class AbstractTravelModel(Model):
+    """Basic functionality used by all of the transcad models.
+    Must be subclassed before use.
+    """
+    
+    def get_travel_model_data_dir(self, config, *args, **kwargs):
+        """Returns the full path to the directory for travel model-urbansim data exchange,
+        """
+        if os.environ.has_key("TRAVELMODELDATAROOT"):
+            drive = os.environ['TRAVELMODELDATAROOT']
+        else:
+            logger.log_warning("TRAVELMODELDATAROOT is not set; will try to use the current drive")
+            drive = '\\'
+        if config['travel_model_configuration'].has_key('directory'):
+            return os.path.join(drive, config['travel_model_configuration']['directory'])
+        else:
+            return drive
+            
+    def run_travel_model_macro(self):
+        pass
+    
+    def prepare_for_run(self, config, year):
+        pass
