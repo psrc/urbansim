cfd6abd3ec96be9331a663d9a1c3ea6d567a4196
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-04-18 11:04:31 +0000

    (hana) unit test fixed
    git-svn-id: https://svn.urbansim.org/trunk@10495 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/development_project_proposal/units_proposed.py
+++ psrc_parcel/development_project_proposal/units_proposed.py
@@ -18,7 +18,7 @@ from numpy import where
 
 class units_proposed(Variable):
     """total units proposed (residential units, and/or non residential sqft) for the proposed developmentt project,
-    depending on whether the proposed projects will be prorated or not, part or all of the 
+    depending on whether the proposed projects will be prorated or not, part or all of the
     units will be available
     """
     _return_type = "int32"
@@ -26,11 +26,11 @@ class units_proposed(Variable):
     def dependencies(self):
         return ["vacant_land_area = development_project_proposal.disaggregate(psrc_parcel.parcel.vacant_land_area)",
                 "land_area_min = development_project_proposal.disaggregate(development_template.land_area_min)",
-                "land_area_max = development_project_proposal.disaggregate(development_template.land_area_max)",                
+                "land_area_max = development_project_proposal.disaggregate(development_template.land_area_max)",
                 "density = development_project_proposal.disaggregate(psrc_parcel.development_template.density)",
-                "density_convertor = development_template.disaggregate(building_type.density_convertor)",                
+                "density_convertor = development_template.disaggregate(building_type.density_convertor)",
                 "density_convertor = development_project_proposal.disaggregate(development_template.density_convertor)",
-                "usable_ratio = 1- development_project_proposal.disaggregate(development_template.percent_land_overhead) / 100.0",                
+                "usable_ratio = 1- development_project_proposal.disaggregate(development_template.percent_land_overhead) / 100.0",
                 "possible_units = development_project_proposal.vacant_land_area * development_project_proposal.usable_ratio * development_project_proposal.density / development_project_proposal.density_convertor",
                 "min_units = development_project_proposal.land_area_min * development_project_proposal.usable_ratio * development_project_proposal.density / development_project_proposal.density_convertor",
                 "max_units = development_project_proposal.land_area_max * development_project_proposal.usable_ratio * development_project_proposal.density / development_project_proposal.density_convertor",
@@ -41,16 +41,16 @@ class units_proposed(Variable):
         templates = dataset_pool.get_dataset("development_template")
         #template_index = templates.get_id_index(proposals.get_attribute("template_id"))
         #density = zeros(templates.size(), dtype=Float)
-        
+
         #for density_name in unique_values(templates.get_attribute("density_name")):
             #templates.compute_variables("%s.%s" % density_name)
             #itemplates_using_this_density_name = where(template.get_attribute("density_name")==density_name)[0]
             #density[itemplates_using_this_density_name]=templates.get_attribute(density_name)[itemplates_using_this_density_name]
-            
+
         #proposal_density = density[template_index]
         #possbile_units = proposals.get_attribute("vacant_land_area") * proposal_density
         possible_units = proposals.get_attribute("possible_units")
-        
+
         min_units = proposals.get_attribute("min_units")
         max_units = proposals.get_attribute("max_units")
         results = possible_units
@@ -58,7 +58,7 @@ class units_proposed(Variable):
         w_min = where(results<min_units)
         results[w_max] = max_units[w_max]
         results[w_min] = 0 #min_units[w_min], there should not be any such cases; filter takes care of them
-        
+
         return results
 
     def post_check(self, values, dataset_pool):
@@ -73,7 +73,7 @@ class Tests(opus_unittest.OpusTestCase):
     def test_my_inputs(self):
         tester = VariableTester(
             __file__,
-            package_order=['psrc_parcel'],
+            package_order=['psrc_parcel', 'urbansim'],
             test_data={
             'development_template':
             {
@@ -102,12 +102,11 @@ class Tests(opus_unittest.OpusTestCase):
                 "template_id":array([1,  2, 3, 4,  2,  3, 4, 1,  2, 3, 4])
             }
         })
-        should_be = array([0, 0,  0, 0,  
-                              36, 80*self.ACRE, 200, 
+        should_be = array([0, 0,  0, 0,
+                              36, 80*self.ACRE, 200,
                            1, 36, 80*self.ACRE, 400])
-        
+
         tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
 
 if __name__=='__main__':
     opus_unittest.main()
-    
