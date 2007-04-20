743612dc771870abe7f4d8ac2383b691f5fb4762
pwaddell <pwaddell@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-04-20 22:37:13 +0000

    (pwaddell) updated specification of repm
    git-svn-id: https://svn.urbansim.org/trunk@10506 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/estimation/repm_specification.py
+++ psrc_parcel/estimation/repm_specification.py
@@ -36,6 +36,28 @@ specification = {
 ##    "building.stories",
 ##    "building.structure_value",    
 #    ],
+
+    4:   #multi-family condominium
+            [
+    "constant",
+    "building.year_built",
+    "beds=building.number_of_bedrooms/building.residential_units",
+    "sqft_bed=building.building_sqft/building.number_of_bedrooms",
+#    "baths=building.number_of_bathrooms",
+#    "building.stories",        
+    "lnsqft=ln(building.building_sqft/building.residential_units)",
+#    "building:opus_core.func.disaggregate(psrc_parcel.zone.number_of_households,[parcel]) as households_in_zone",
+    "far=building.building_sqft/building.disaggregate(parcel.parcel_sqft)",
+    "gc_da_to_cbd=building.disaggregate(psrc.zone.generalized_cost_hbw_am_drive_alone_to_cbd)",
+    "lnemp10wa=ln(building.disaggregate(psrc.zone.employment_within_10_minutes_travel_time_hbw_am_walk))",
+#    "lnemp45tw=ln(building.disaggregate(psrc.zone.employment_within_45_minutes_travel_time_hbw_am_transit_walk))",
+#    "lnemp30da=ln(building.disaggregate(psrc.zone.employment_within_30_minutes_travel_time_hbw_am_drive_alone))",
+    "empden=building.disaggregate(psrc.zone.number_of_jobs_per_acre)",
+#    "popden=building.disaggregate(psrc.zone.population_per_acre)",
+    ],
+    
+
+
 #    8:   #industrial
 #            [
 #    "constant",
@@ -58,33 +80,39 @@ specification = {
 ###    "building.stories",
 ###    "building.structure_value",    
 #    ],
-    12:   #mulit-family residential
+    12:   #multi-family residential
             [
     "constant",
-    "building.year_built",
-#    
-    "building.number_of_bedrooms",
-    "building.number_of_bathrooms",
-    "building.stories",        
-#        
-    "building.building_sqft",
+    "building.year_built",    "beds=building.number_of_bedrooms/building.residential_units",
+    "sqft_bed=building.building_sqft/building.number_of_bedrooms",
+#    "baths=building.number_of_bathrooms",
+#    "building.stories",        
+    "lnsqft=ln(building.building_sqft/building.residential_units)",
 #    "building:opus_core.func.disaggregate(psrc_parcel.zone.number_of_households,[parcel]) as households_in_zone",
-    "parcel_sqft=building.disaggregate(parcel.parcel_sqft)",
-    "gc_da_to_cbd=building.disaggregate(psrc.zone.generalized_cost_hbw_am_drive_alone_to_cbd, intermediates=[parcel])",
+    "far=building.building_sqft/building.disaggregate(parcel.parcel_sqft)",
+    "gc_da_to_cbd=building.disaggregate(psrc.zone.generalized_cost_hbw_am_drive_alone_to_cbd)",
+    "lnemp10wa=ln(building.disaggregate(psrc.zone.employment_within_10_minutes_travel_time_hbw_am_walk))",
+#    "lnemp45tw=ln(building.disaggregate(psrc.zone.employment_within_45_minutes_travel_time_hbw_am_transit_walk))",
+#    "lnemp30da=ln(building.disaggregate(psrc.zone.employment_within_30_minutes_travel_time_hbw_am_drive_alone))",
+    "empden=building.disaggregate(psrc.zone.number_of_jobs_per_acre)",
+#    "popden=building.disaggregate(psrc.zone.population_per_acre)",
     ],
     
     19:   #SINGLE family residential
             [
     "constant",
     "building.year_built",
-#    
-    "building.number_of_bedrooms",
-    "building.number_of_bathrooms",
-    "building.stories",        
-#        
-    "building.building_sqft",
+    "new=building.year_built>2000",
+    "preww2=building.year_built<1945",
+    "beds=building.number_of_bedrooms/building.residential_units",
+    "sqft_bed=building.building_sqft/building.number_of_bedrooms",#    "baths=building.number_of_bathrooms",
+    "lnsqft=ln(building.building_sqft/building.residential_units)",
+    "far=building.building_sqft/building.disaggregate(parcel.parcel_sqft)",
+#    "building.stories",        #    "lnsqft=ln(building.building_sqft)",
 #    "building:opus_core.func.disaggregate(psrc_parcel.zone.number_of_households,[parcel]) as households_in_zone",
-    "parcel_sqft=building.disaggregate(parcel.parcel_sqft)",
-    "gc_da_to_cbd=building.disaggregate(psrc.zone.generalized_cost_hbw_am_drive_alone_to_cbd, intermediates=[parcel])",
+#    "ln_parcel_sqft=ln(building.disaggregate(parcel.parcel_sqft))",
+    "lnemp10wa=ln(building.disaggregate(psrc.zone.employment_within_10_minutes_travel_time_hbw_am_walk))",
+#    "lnemp40da=ln(building.disaggregate(psrc.zone.employment_within_40_minutes_travel_time_hbw_am_drive_alone))",
+    "lngcdacbd=ln(building.disaggregate(psrc.zone.generalized_cost_hbw_am_drive_alone_to_cbd))",
     ],
 }            
