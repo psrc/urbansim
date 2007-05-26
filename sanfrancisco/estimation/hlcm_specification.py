7237395c96499e8af147ef466f0997eced0ff3c7
pwaddell <pwaddell@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-05-26 22:20:20 +0000

    (pwaddell) hlcm specification
    git-svn-id: https://svn.urbansim.org/trunk@10666 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/estimation/hlcm_specification.py
+++ sanfrancisco/estimation/hlcm_specification.py
@@ -15,9 +15,13 @@
 specification ={} 
 specification = {   
  
--2:   #submodel 
+1:   #submodel 
           [         
-             "ln_residential_units=ln(building.residential_units)", 
+             "ln_residential_units=ln(building.residential_units)",
+#             "cost_income = household.income*1000 / building.unit_price / 10",
+#             "building.unit_price",
+             "condo = (building.building_use_id == 2)",
+             "apartment = (building.building_use_id == 1)",             
 #             "building.year_built", 
 #             "building.bedrooms", 
 #             "sanfrancisco.building.building_sqft",               
@@ -26,38 +30,38 @@ specification = {
 #             "lot_area=building.disaggregate(parcel.area)", 
              "employment_within_60_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_60_minutes_bus_travel_time,intermediates=[parcel])", 
              "employment_within_20_minutes_hwy_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_20_minutes_hwy_travel_time,intermediates=[parcel])", 
-#             "sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",    
+             "sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",    
 #             "employment_of_building_use_cie_in_zone=building.disaggregate(sanfrancisco.zone.employment_of_building_use_cie,intermediates=[parcel])",              
             ], 
-#2:   #submodel 
-          #[         
-             #"ln_residential_units=ln(building.residential_units)", 
-##             "building.year_built", 
-##             "building.bedrooms", 
-##             "sanfrancisco.building.building_sqft",               
-##             "building_sqft_per_unit=sanfrancisco.building.building_sqft / sanfrancisco.building.residential_units", 
-             #"households_in_zone=building.disaggregate(sanfrancisco.zone.number_of_households,intermediates=[parcel])", 
-##             "employment_within_10_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_10_minutes_bus_travel_time,intermediates=[parcel])", 
-             #"lot_area=building.disaggregate(parcel.area)",              
-             #"employment_within_60_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_60_minutes_bus_travel_time,intermediates=[parcel])", 
-##             "employment_within_20_minutes_hwy_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_20_minutes_hwy_travel_time,intermediates=[parcel])"              
-              #"sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",    
-##             "employment_of_building_use_cie_in_zone=building.disaggregate(sanfrancisco.zone.employment_of_building_use_cie,intermediates=[parcel])",
-            #], 
-#3:   #submodel 
-          #[         
-             #"ln_residential_units=ln(building.residential_units)," 
-             #"building.year_built", 
-##             "building.bedrooms", 
-##             "sanfrancisco.building.building_sqft",               
-##             "building_sqft_per_unit=sanfrancisco.building.building_sqft / sanfrancisco.building.residential_units", 
-             #"households_in_zone=building.disaggregate(sanfrancisco.zone.number_of_households,intermediates=[parcel])", 
-##             "employment_within_10_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_10_minutes_bus_travel_time,intermediates=[parcel])", 
-             #"lot_area=building.disaggregate(parcel.area)", 
-##             "employment_within_60_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_60_minutes_bus_travel_time,intermediates=[parcel])", 
-             #"employment_within_20_minutes_hwy_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_20_minutes_hwy_travel_time,intermediates=[parcel])", 
-             #"sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",                 
-##             "employment_of_building_use_cie_in_zone=building.disaggregate(sanfrancisco.zone.employment_of_building_use_cie,intermediates=[parcel])", 
-            #], 
+2:   #submodel 
+          [         
+             "ln_residential_units=ln(building.residential_units)", 
+#             "building.year_built", 
+#             "building.bedrooms", 
+#             "sanfrancisco.building.building_sqft",               
+#             "building_sqft_per_unit=sanfrancisco.building.building_sqft / sanfrancisco.building.residential_units", 
+             "households_in_zone=building.disaggregate(sanfrancisco.zone.number_of_households,intermediates=[parcel])", 
+#             "employment_within_10_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_10_minutes_bus_travel_time,intermediates=[parcel])", 
+#             "lot_area=building.disaggregate(parcel.area)",              
+             "employment_within_60_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_60_minutes_bus_travel_time,intermediates=[parcel])", 
+#             "employment_within_20_minutes_hwy_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_20_minutes_hwy_travel_time,intermediates=[parcel])"              
+    "sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",    
+#             "employment_of_building_use_cie_in_zone=building.disaggregate(sanfrancisco.zone.employment_of_building_use_cie,intermediates=[parcel])",
+            ], 
+3:   #submodel 
+          [         
+             "ln_residential_units=ln(building.residential_units)," 
+             "building.year_built", 
+#             "building.bedrooms", 
+#             "sanfrancisco.building.building_sqft",               
+#             "building_sqft_per_unit=sanfrancisco.building.building_sqft / sanfrancisco.building.residential_units", 
+             "households_in_zone=building.disaggregate(sanfrancisco.zone.number_of_households,intermediates=[parcel])", 
+#             "employment_within_10_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_10_minutes_bus_travel_time,intermediates=[parcel])", 
+#             "lot_area=building.disaggregate(parcel.area)", 
+#             "employment_within_60_minutes_bus_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_60_minutes_bus_travel_time,intermediates=[parcel])", 
+             "employment_within_20_minutes_hwy_travel_time=building.disaggregate(sanfrancisco.zone.employment_within_20_minutes_hwy_travel_time,intermediates=[parcel])", 
+             "sector_3_employment_in_zone=building.disaggregate(sanfrancisco.zone.aggregate_employment_of_sector_3_from_building,intermediates=[parcel])",                 
+#             "employment_of_building_use_cie_in_zone=building.disaggregate(sanfrancisco.zone.employment_of_building_use_cie,intermediates=[parcel])", 
+            ], 
  
 }             
