dfa94f529b3a59798aba8dbc6fb56a1268b47777
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-05-27 03:06:37 +0000

    (hana) new variables
    git-svn-id: https://svn.urbansim.org/trunk@10672 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/development_project_proposal/building_sqft.py
@@ -0,0 +1,30 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from opus_core.variables.variable import Variable
+
+class building_sqft(Variable):
+    """ 
+    sums building_sqft over proposal components
+    """
+
+    def dependencies(self):
+        return ["_building_sqft = development_project_proposal.aggregate(psrc_parcel.development_project_proposal_component.building_sqft)",
+                 ]
+
+    def compute(self, dataset_pool):
+        return self.get_dataset().get_attribute("_building_sqft")
+
+    def post_check(self, values, dataset_pool):
+        self.do_check("x >= 0", values)
