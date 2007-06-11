161be2051943083d9b0917f5a3b19c606051b05f
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-11 22:50:30 +0000

    (pappert) template for new urbansim projects with source code
    git-svn-id: https://svn.urbansim.org/trunk@10836 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ z_template_project/tests/all_tests.py
@@ -0,0 +1,19 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+raise "Please chance the name of the package in tests.all_tests" #remove this line after changing the package name
+
+from opus_core.tests.utils.package_tester import PackageTester
+
+PackageTester().run_all_tests_for_package('z_template_project') 
