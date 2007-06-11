fc81c576e3c49e65a81ae057b6755dce74cf8a36
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-11 22:08:07 +0000

    (pappert) copy of psrc_parcel
    git-svn-id: https://svn.urbansim.org/trunk@10817 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim_parcel/parcel/existing_units.py
@@ -0,0 +1,80 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hunits_proposed.pyope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from opus_core.variables.variable import Variable
+#from variable_functions import my_attribute_label
+from numpy import where, zeros
+from opus_core.misc import unique_values
+
+class existing_units(Variable):
+    """total number of units (residential units or sqft), which are defined by building_types
+    """
+    _return_type = "int32"
+    package_name = "psrc_parcel"
+    
+    def dependencies(self):
+        return ["unit_name = parcel.disaggregate(land_use_type.unit_name)",
+                "building_sqft = parcel.aggregate(psrc_parcel.building.building_sqft)",
+                "residential_units = parcel.aggregate(building.residential_units)",
+                ]
+
+    def compute(self, dataset_pool):
+        parcels = self.get_dataset()
+        unit_name = parcels.get_attribute("unit_name")
+        results = zeros(parcels.size())
+        for name in unique_values(unit_name):
+            if name not in parcels.get_known_attribute_names():
+                parcels.compute_variables("%s.parcel.%s" % (self.package_name, name), dataset_pool)
+            w = where(unit_name == name)[0]
+            results[w] = parcels.get_attribute(name)[w]
+        return results
+
+    def post_check(self, values, dataset_pool):
+        self.do_check("x >= 0", values)
+
+from opus_core.tests import opus_unittest
+from numpy import array, int32
+from opus_core.tests.utils.variable_tester import VariableTester
+
+class Tests(opus_unittest.OpusTestCase):
+    def test_my_inputs(self):
+        tester = VariableTester(
+            __file__,
+            package_order=['psrc_parcel', 'urbansim'],
+            test_data={
+            'parcel':
+            {
+                "parcel_id":        array([1,   2,    3]),
+                "land_use_type_id": array([1,   2,    3 ]),
+                "parcel_sqft": array([1, 50,  200],dtype=int32),
+            },
+            'building':
+            {
+                "building_id":array([1, 2, 3, 4]),
+                "parcel_id":  array([1, 1, 2, 3]),
+                "building_sqft": array([1000, 0, 2000, 3000]),
+                "residential_units": array([0, 7, 4,   2]),
+            },
+            'land_use_type':
+            {
+                "land_use_type_id":array([1,  2, 3]),
+                "unit_name":  array(["residential_units", "parcel_sqft", "building_sqft"])
+            }
+        })
+        should_be = array([7, 50, 3000])
+
+        tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
+
+if __name__=='__main__':
+    opus_unittest.main()
---
+++ urbansim_parcel/parcel/min_units_capacity_for_building_type_DDD.py
@@ -0,0 +1,63 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from opus_core.variables.variable import Variable
+from variable_functions import my_attribute_label
+
+class min_units_capacity_for_building_type_DDD(Variable):
+    """ minimum number of units allowed by development constraints  """
+
+    def __init__(self, id_number):
+        Variable.__init__(self)
+        self.id_number = id_number
+
+    def dependencies(self):
+        return ["development_constraint.",
+             my_attribute_label(self.residential_units)]
+
+    def compute(self,  dataset_pool):
+        return self.get_dataset().get_attribute(self.residential_units) == self.tnumber
+
+    def post_check(self,  values, dataset_pool=None):
+        self.do_check("x == True or x == False", values)
+
+if __name__=='__main__':
+    import unittest
+    from urbansim.variable_test_toolbox import VariableTestToolbox
+    from numpy import array
+    from numpy import ma
+    from opus_core.resources import Resources    
+    from psrc_parcel.datasets.parcels import ParcelSet
+    
+    class Tests(unittest.TestCase):
+        variable_name = "psrc_parcel.parcel.has_2_units"
+
+        def test_my_inputs(self):
+            residential_units = array([1, 2, 5])
+            resources = Resources({'data':
+                                   {"parcel_id":array([1,2,3]),
+                                    "residential_units":residential_units,
+                                    },
+                                  })
+            parcels = ParcelSet(resources=resources, in_storage_type="RAM")
+
+            values = VariableTestToolbox().compute_variable(self.variable_name, \
+                {"parcel":parcels}, 
+                dataset = "parcel")
+            should_be = array([False, True, False])
+            
+            self.assertEqual(ma.allequal(values, should_be), \
+                             True, msg = "Error in " + self.variable_name)
+
+    unittest.main()
