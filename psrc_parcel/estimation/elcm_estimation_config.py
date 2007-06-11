a8f426355de56abdd5960ed948fb968abfbc115c
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-11 22:27:18 +0000

    (hana) config for elcm estimation
    git-svn-id: https://svn.urbansim.org/trunk@10832 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/estimation/elcm_estimation_config.py
@@ -0,0 +1,18 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from urbansim.configs.elcm_estimation_config import elcm_configuration as config
+
+class elcm_configuration(config):
+    pass
