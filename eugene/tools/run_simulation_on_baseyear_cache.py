ddd218370c6ea0a652350aac086ac5286aa97aa4
borning <borning@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-18 20:40:07 +0000

    (borning) changed enthought imports to use .api form
    git-svn-id: https://svn.urbansim.org/trunk@10905 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- eugene/tools/run_simulation_on_baseyear_cache.py
+++ eugene/tools/run_simulation_on_baseyear_cache.py
@@ -17,8 +17,9 @@ import sys
 
 from time import strftime, localtime
 
-from enthought.traits import HasTraits, Directory, Str, Event, Int
-from enthought.traits.ui import View, Group, Item
+from enthought.traits.api import HasTraits, Directory, Str, Event, Int
+from enthought.traits.api import View, Group, Item
+# later update the ui import to:   from enthought.traits.ui.api import View, Group, Item
 from enthought.traits.ui.menu import Action, NoButtons
 
 from opus_core.fork_process import ForkProcess
