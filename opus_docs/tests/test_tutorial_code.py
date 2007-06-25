cc6749b675e6e80811f5b1489d441f4fe546bd5d
borning <borning@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-25 05:22:26 +0000

    (borning) put urbansim 3 to 4 conversion information at top level
    git-svn-id: https://svn.urbansim.org/trunk@10996 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_docs/tests/test_tutorial_code.py
+++ opus_docs/tests/test_tutorial_code.py
@@ -22,9 +22,9 @@ class TestTutorialCode(opus_unittest.OpusTestCase):
     """Fails if tutorial_code.py fails.
     """
     def test_tutorial_code(self):
-        opus_manual_path = OpusPackage().get_path_for_package('opus_docs')
+        opus_docs_path = OpusPackage().get_path_for_package('opus_docs')
         error_code = os.system('python "%s"'
-            % os.path.join(opus_manual_path, 'docs', 'latex', 'tutorial_code.py'))
+            % os.path.join(opus_docs_path, 'manual', 'tutorial_code.py'))
         
         self.assert_(not error_code)
 
