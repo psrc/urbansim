1dcf0dd9cdf88eeb3f901fde64c452ad8b35b069
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-06-28 07:35:23 +0000

    (lmwang)script to run developer model at zonal level
    git-svn-id: https://svn.urbansim.org/trunk@11068 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/configs/data_preparation_buildings.py
@@ -0,0 +1,67 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+#from urbansim.estimation.config import config
+from opus_core.configurations.database_configuration import DatabaseConfiguration
+from urbansim_parcel.configs.controller_config import UrbansimParcelConfiguration
+from urbansim.configs.general_configuration import GeneralConfiguration
+from urbansim.configs.base_configuration import AbstractUrbansimConfiguration
+from urbansim.configurations.creating_baseyear_cache_configuration import CreatingBaseyearCacheConfiguration
+from opus_core.configurations.baseyear_cache_configuration import BaseyearCacheConfiguration
+from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
+from psrc_parcel.configs.household_location_choice_model_by_zones_configuration_creator import HouseholdLocationChoiceModelByZonesConfigurationCreator
+from urbansim.configurations.household_relocation_model_configuration_creator import HouseholdRelocationModelConfigurationCreator
+from opus_core.resources import merge_resources_with_defaults
+from numpy import array
+import os
+
+class DataPreparationBuildings(UrbansimParcelConfiguration):
+    def __init__(self):
+        config = UrbansimParcelConfiguration()
+
+        config_changes = {
+            'description':'data preparation for PSRC parcel (buildings)',
+            'cache_directory': None,
+            'creating_baseyear_cache_configuration': CreatingBaseyearCacheConfiguration(
+                cache_directory_root = r'/Users/hana/urbansim_cache/psrc/parcel',
+                #cache_directory_root = r'/workspace/urbansim_cache/psrc_parcel',
+                cache_from_mysql = False,
+                baseyear_cache = BaseyearCacheConfiguration(
+                    existing_cache_to_copy = r'/Users/hana/urbansim_cache/psrc/cache_source_parcel',
+                    #existing_cache_to_copy = r'/workspace/urbansim_cache/psrc_parcel/estimation',
+                    ),
+                cache_mysql_data = 'urbansim.model_coordinators.cache_mysql_data',
+                ),
+            'dataset_pool_configuration': DatasetPoolConfiguration(
+                package_order=['psrc_parcel', 'urbansim_parcel', 'urbansim', 'opus_core'],
+                package_order_exceptions={},
+                ),
+            'base_year':2005,
+            'years':(2006, 2006),
+            'models':[ # models are executed in the same order as in this list
+                 "expected_sale_price_model",
+                 "development_proposal_choice_model",
+                ],
+            "datasets_to_preload":{
+                    'zone':{},
+                    'household':{},
+                    'building': {}
+                }
+        }
+        #use configuration in config as defaults and merge with config_changes
+        config.replace(config_changes)
+        self.merge(config)
+#        self['models_configuration']['development_proposal_choice_model'] = {}
+        self['models_configuration']['development_proposal_choice_model']['controller']['run']['arguments']["zones"] = 'zone'
+        self['models_configuration']['expected_sale_price_model']['controller']  #no change needed 
---
+++ psrc_parcel/models/development_proposal_sampling_model_by_zones.py
@@ -0,0 +1,62 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from numpy import arange, zeros, logical_and, where
+from opus_core.logger import logger
+from urbansim_parcel.models.development_proposal_sampling_model import DevelopmentProposalSamplingModel
+from opus_core.datasets.dataset import DatasetSubset
+
+class DevelopmentProposalSamplingModelByZones(DevelopmentProjectProposalSamplingModel):
+        
+    def run(self, zones, *args, **kwargs):
+        #if agents_index is None:
+            #agents_index = arange(agent_set.size())
+        #cond_array = zeros(agent_set.size(), dtype="bool8")
+        #cond_array[agents_index] = True
+        zone_ids = zones.get_id_attribute()
+        #agents_zones = agent_set.get_attribute(zones.get_id_name()[0])
+        for zone_id in zone_ids:
+            self.zone = zone_id
+            self.proposal_set.compute_variables("zone_id = development_project_proposal.disaggregate(parcel.zone_id)", 
+                                                dataset_pool=self.dataset_pool)
+            self.proposal_set = DatasetSubset(self.proposal_set, 
+                                              index = where(self.proposal_set.get_attribute("zone_id") == zone_id)[0])
+            logger.log_status("DPSM for zone %s" % zone_id)
+            DevelopmentProposalSamplingModel.run(self, *args, **kwargs)
+
+    def check_vacancy_rates(self, target_vacancy):
+        for index in arange(target_vacancy.size()):
+            ##TODO allow target vacancies to vary across zones/sub region geographies
+            #zone_id = target_vacancy.get_attribute_by_index("zone_id", index)
+            #if zone_id != self.zone: continue
+
+            type_id = target_vacancy.get_attribute_by_index("generic_building_type_id", index)
+            type_name = target_vacancy.get_attribute_by_index("type_name", index)
+            unit_name = target_vacancy.get_attribute_by_index("unit_name", index)  #vacancy by type, could be residential, non-residential, or by building_type
+            target = target_vacancy.get_attribute_by_index("target_vacancy_rate", index)
+            buildings = self.dataset_pool.get_dataset("building")
+            is_matched_type = buildings.get_attribute("generic_building_type_id") == type_id
+            is_in_right_zone = buildings.get_attribute("zone_id") == self.zone
+            existing_units = buildings.get_attribute(unit_name)[is_matched_type*is_in_right_zone]
+            occupied_units = buildings.get_attribute("occupied_%s" % unit_name)[is_matched_type*is_in_right_zone]
+
+            self.existing_units[type_id] = existing_units.astype("float32").sum()
+            self.occupied_units[type_id] = occupied_units.astype("float32").sum()
+            self.proposed_units[type_id] = 0
+            self.demolished_units[type_id] = 0
+            vr = (self.existing_units[type_id] - self.occupied_units[type_id]) / float(self.existing_units[type_id])
+            if vr < target:
+                self.accepting_proposals[type_id] = True
+            else:
+                self.accepting_proposals[type_id] = False
