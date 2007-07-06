2760de6e124fd861a8d3bf50dccf07dc944ff406
borning <borning@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-07-06 06:48:09 +0000

    added support for a file of aliases in different variable directories
    git-svn-id: https://svn.urbansim.org/trunk@11157 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_core/test_agent/aliases.py
@@ -0,0 +1,21 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+    'income_times_5 = 5*opus_core.test_agent.income',
+    'income_times_10 = 5*opus_core.test_agent.income_times_2',
+    ]
---
+++ opus_core/variables/expression_tests/aliases_file.py
@@ -0,0 +1,48 @@
+#
+# Opus software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from opus_core.variables.variable_name import VariableName
+from opus_core.tests import opus_unittest
+from opus_core.datasets.dataset import Dataset
+from opus_core.storage_factory import StorageFactory
+from numpy import array, ma
+
+
+class Tests(opus_unittest.OpusTestCase):
+
+    def test_alias_file(self):
+        # this tests aliases in the file 'aliases.py'
+        # expr1 and expr2 are aliases, while expr3 is an ordinary variable, 
+        # just to make sure that aliases and ordinary variables interoperate correctly
+        expr1 = "opus_core.test_agent.income_times_10"
+        expr2 = "opus_core.test_agent.income_times_5"
+        expr3 = "opus_core.test_agent.income_times_2"
+        storage = StorageFactory().get_storage('dict_storage')
+        storage.write_table(
+            table_name='test_agents',
+            table_data={
+                "income":array([1,5,10]),
+                "id":array([1,3,4])
+                }
+            )
+        dataset = Dataset(in_storage=storage, in_table_name='test_agents', id_name="id", dataset_name="test_agent")
+        result1 = dataset.compute_variables([expr1])
+        self.assert_(ma.allclose(result1, array([10, 50, 100]), rtol=1e-6), "Error in test_alias_file")
+        result2 = dataset.compute_variables([expr2])
+        self.assert_(ma.allclose(result2, array([5, 25, 50]), rtol=1e-6), "Error in test_alias_file")
+        result3 = dataset.compute_variables([expr3])
+        self.assert_(ma.allclose(result3, array([2, 10, 20]), rtol=1e-6), "Error in test_alias_file")
+         
+if __name__=='__main__':
+    opus_unittest.main()
