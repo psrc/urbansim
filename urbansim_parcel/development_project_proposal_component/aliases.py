68e44963851123e77cfdd79f530ac0e25fc71fab
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-07-08 23:51:08 +0000

    (hana) missing aliases file added
    git-svn-id: https://svn.urbansim.org/trunk@11180 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim_parcel/development_project_proposal_component/aliases.py
@@ -0,0 +1,20 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+   "building_sqft_per_unit = development_project_proposal_component.disaggregate(development_template_component.building_sqft_per_unit)"
+   ]
