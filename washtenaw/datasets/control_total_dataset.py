29ad5cf01be7b1f37dfa63f96cf0abe731197beb
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-07-16 19:55:04 +0000

    (hana) regional HTM implemented
    git-svn-id: https://svn.urbansim.org/trunk@11245 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ washtenaw/datasets/control_total_dataset.py
@@ -0,0 +1,19 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from urbansim.datasets.control_total_dataset import ControlTotalDataset as UrbansimControlTotalDataset
+
+class ControlTotalDataset(UrbansimControlTotalDataset):
+    id_name_default = {"household":["year", "large_area_id"],
+                        "employment":["year", "sector_id", "large_area_id"]}
