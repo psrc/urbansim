23b2565c433a0eb8103e2098a0d6865093b76161
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-07-17 14:23:22 +0000

    (hana) a minor change
    git-svn-id: https://svn.urbansim.org/trunk@11259 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/models/development_project_proposal_regression_model.py
+++ urbansim_parcel/models/development_project_proposal_regression_model.py
@@ -121,7 +121,6 @@ class DevelopmentProjectProposalRegressionModel(RegressionModel):
                                                               resources = kwargs.get("resources", None))
         
             if existing_proposal_set is not None: # add existing proposals to the created ones
-                proposal_set.delete_computed_attributes()
                 proposal_set.join_by_rows(existing_proposal_set, require_all_attributes=False, change_ids_if_not_unique=True)
         else:
             proposal_set = existing_proposal_set
