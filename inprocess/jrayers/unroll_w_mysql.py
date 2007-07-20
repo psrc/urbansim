7840c5e5235f3b36890dfde05822a9ff8f5cfd7f
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-07-20 22:57:31 +0000

    added GPL header
    git-svn-id: https://svn.urbansim.org/trunk@11287 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/jrayers/unroll_w_mysql.py
+++ inprocess/jrayers/unroll_w_mysql.py
@@ -1,3 +1,16 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
 # This is the fastest one yet, runs on mysql and inserts 1.14 million records in ~4min
 
 import sys, os, time, datetime
