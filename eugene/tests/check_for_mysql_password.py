a6c7bfdfc4d664483e034cff8c6d4d7440d96617
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-03 22:32:48 +0000

    adding check for mysql password in files
    git-svn-id: https://svn.urbansim.org/trunk@11371 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ eugene/tests/check_for_mysql_password.py
@@ -0,0 +1,20 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+from opus_core.tests import opus_unittest
+from opus_core.tests.check_for_mysql_password import TestForMySQLPassword_abstract
+class TestForMySQLPassword(TestForMySQLPassword_abstract):
+    modul = "eugene"
+            
+if __name__ == "__main__":
+    opus_unittest.main()
