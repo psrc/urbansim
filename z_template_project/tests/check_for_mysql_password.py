34bc8392905fd4a86bd975bdf92e93609c65cd34
pappert <pappert@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-03 23:17:03 +0000

    adding check for syntax, gpl and mysql password to project template
    git-svn-id: https://svn.urbansim.org/trunk@11381 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ z_template_project/tests/check_for_mysql_password.py
@@ -0,0 +1,22 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+raise "Please chance the name of the package in tests.all_tests" #remove this line after changing the package name
+
+from opus_core.tests import opus_unittest
+from opus_core.tests.check_for_mysql_password import TestForMySQLPassword_abstract
+class TestForMySQLPassword(TestForMySQLPassword_abstract):
+    modul = "z_template_project"
+            
+if __name__ == "__main__":
+    opus_unittest.main()
