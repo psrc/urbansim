a194e3fe30415e323675d72e81131abfbe67f252
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-09 12:40:01 +0000

    (hana) one variable changed; unused models eliminated
    git-svn-id: https://svn.urbansim.org/trunk@11427 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/estimation/run_estimation.py
+++ psrc_parcel/estimation/run_estimation.py
@@ -73,13 +73,11 @@ class EstimationRunner(object):
         plot_correlation_diagnose('correlation_submodel_%s' % submodel)
 
 if __name__ == '__main__':
-#    model = ("REPM", "real_estate_price_model")
+    model = ("REPM", "real_estate_price_model")
 #    model = ("HLCM", "household_location_choice_model")
-#    model = ("BLCM", "business_location_choice_model")
 #    model = ("ELCM", "employment_location_choice_model", "home_based", True)
-    model = ("ELCM", "employment_location_choice_model", "non_home_based", False)
-#    model = ("BLDGLCM", "building_location_choice_model", "nonresidential", False)
-#    model = ("BLDGLCM", "building_location_choice_model", "residential", False)
+#    model = ("ELCM", "employment_location_choice_model", "non_home_based", False)
+
 
     from my_estimation_config import my_configuration
     er = EstimationRunner()
