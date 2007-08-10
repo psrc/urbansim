327d64612fcefd6f17f1b3a8c23700a96fc96c62
borning <borning@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-10 22:56:31 +0000

    added a utility to create a dictionary of replacements for generating cache file extensions, which depend on the processor architecture
    git-svn-id: https://svn.urbansim.org/trunk@11453 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_core/tests/utils/cache_extension_replacements.py
@@ -0,0 +1,34 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+
+import platform, sys
+
+"""
+Replacements is a dictionary to be used in % formatting expressions, to generate
+file extensions for file caches.  These will depend on whether the architecture is
+32 or 64 bit, and whether it is little-endian or big-endian.
+"""
+   
+replacements = {}
+
+if sys.byteorder=='little':
+    replacements['endian'] = 'l'
+else:
+    replacements['endian'] = 'b'
+    
+if platform.architecture()[0]=='64bit':
+    replacements['bytes'] = 8
+else:
+    replacements['bytes'] = 4
