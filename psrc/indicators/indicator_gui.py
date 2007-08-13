318fed9740bf8862b695a054b5fdf6033e365368
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-13 20:43:53 +0000

    (travis) eliminated imports from opus_core.indicator_framework.traits.__init__
    git-svn-id: https://svn.urbansim.org/trunk@11474 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc/indicators/indicator_gui.py
+++ psrc/indicators/indicator_gui.py
@@ -14,7 +14,8 @@
 
 # open a traits-based GUI for editing PSRC indicator requests
 
-from opus_core.indicator_framework.traits.traits_indicator_handler import TraitsIndicatorHandler
+from opus_core.indicator_framework.traits.traits_indicator_handler \
+    import TraitsIndicatorHandler
 
 handler = TraitsIndicatorHandler()
 handler.open_editor(package_order = ['psrc','urbansim','opus_core'])
