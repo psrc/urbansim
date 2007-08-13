0ae3f18486d525c1c1f4ca562a4bfe3c7b2be8f0
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-13 22:17:37 +0000

    (travis) updated imports
    git-svn-id: https://svn.urbansim.org/trunk@11494 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/indicators/make_indicators.py
+++ sanfrancisco/indicators/make_indicators.py
@@ -16,10 +16,14 @@
 # this illustrates using traits-based configurations programatically
 
 from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
-from opus_core.indicator_framework.core import SourceData
+from opus_core.indicator_framework.core.source_data import SourceData
 
-from opus_core.indicator_framework.image_types import Map, Chart, GeotiffMap, ArcGeotiffMap, LorenzCurve
-from opus_core.indicator_framework.image_types import Table, DatasetTable
+from opus_core.indicator_framework.image_types.matplotlib_map import Map
+from opus_core.indicator_framework.image_types.matplotlib_chart import Chart
+from opus_core.indicator_framework.image_types.table import Table
+from opus_core.indicator_framework.image_types.geotiff_map import GeotiffMap
+from opus_core.indicator_framework.image_types.dataset_table import DatasetTable
+from opus_core.indicator_framework.image_types.matplotlib_lorenzcurve import LorenzCurve
 
 
 run_description = '(baseline 06/28/2007)'
@@ -111,7 +115,7 @@ multi_year_requests = [
     ]
 
 if __name__ == '__main__':
-    from opus_core.indicator_framework.core import IndicatorFactory
+    from opus_core.indicator_framework.core.indicator_factory import IndicatorFactory
 
     IndicatorFactory().create_indicators(
         indicators = single_year_requests,
