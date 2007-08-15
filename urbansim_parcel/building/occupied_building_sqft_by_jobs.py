4440c612748ffaed82629c0bb89e12bf3b1f1ab7
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-15 23:02:12 +0000

    (hana) job.sqft is updated by building_sqft_per_job table for the variable occupied_building_sqft_by_jobs
    git-svn-id: https://svn.urbansim.org/trunk@11528 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/building/occupied_building_sqft_by_jobs.py
+++ urbansim_parcel/building/occupied_building_sqft_by_jobs.py
@@ -12,8 +12,8 @@
 # other acknowledgments.
 #
 
+from numpy import where, concatenate, newaxis
 from opus_core.variables.variable import Variable
-from variable_functions import my_attribute_label
 
 class occupied_building_sqft_by_jobs(Variable):
     """sum of jobs sqft per building"""
@@ -26,7 +26,27 @@ class occupied_building_sqft_by_jobs(Variable):
 
     def compute(self,  dataset_pool):
         jobs = dataset_pool.get_dataset("job")
-        return self.get_dataset().sum_dataset_over_ids(jobs, "sqft")
+        buildings = self.get_dataset()
+        sqft = jobs.get_attribute('sqft')
+        where_zero = where(sqft <= 0)[0]
+        if where_zero.size == 0:
+            return buildings.sum_over_ids(jobs.get_attribute('building_id'), sqft)
+        self.add_and_solve_dependencies(["urbansim_parcel.job.zone_id",
+                "bldgs_building_type_id = job.disaggregate(building.building_type_id)", 
+                "building_sqft_per_job.building_sqft_per_job"], dataset_pool=dataset_pool)
+        zones = jobs.get_attribute("zone_id")
+        type_ids = jobs.get_attribute("bldgs_building_type_id")
+        try:
+            sqft_per_job = dataset_pool.get_dataset("building_sqft_per_job")
+        except:
+            return buildings.sum_over_ids(jobs.get_attribute('building_id'), sqft)
+        
+        ids = concatenate((zones[:,newaxis],type_ids[:,newaxis]), axis=1)
+        index = sqft_per_job.try_get_id_index(ids)
+        building_sqft_per_job = sqft_per_job.get_attribute("building_sqft_per_job")[index]
+        building_sqft_per_job[where(index==-1)] = 0
+        sqft[where_zero] = building_sqft_per_job[where_zero].astype(sqft.dtype)
+        return buildings.sum_over_ids(jobs.get_attribute('building_id'), sqft)
 
     def post_check(self,  values, dataset_pool=None):
         size = dataset_pool.get_dataset("job").get_attribute("sqft").sum()
@@ -36,43 +56,31 @@ if __name__=='__main__':
     import unittest
     from numpy import array
     from numpy import ma
-    from opus_core.resources import Resources
-    from urbansim_parcel.datasets.building_dataset import BuildingDataset
-    from urbansim.datasets.job_dataset import JobDataset
-    from opus_core.storage_factory import StorageFactory
+    from opus_core.tests.utils.variable_tester import VariableTester
 
     class Tests(unittest.TestCase):
         variable_name = "urbansim_parcel.building.occupied_building_sqft_by_jobs"
         def test(self):
-            storage = StorageFactory().get_storage('dict_storage')
-            bs_table_name = 'job'
-
-            storage.write_table(
-                    table_name=bs_table_name,
-                    table_data={"job_id": array([1,2,3,4,5,6]),
-                               "sqft":        array([0,1,4,0,2,5]),
-                               "building_id": array([2,1,3,2,1,2])
+            tester = VariableTester(
+            __file__,
+            package_order=['urbansim_parcel','urbansim'],
+            test_data={
+            "building":{"building_id":         array([1,2,3]),
+                       "zone_id":              array([1,2,3]),
+                       "building_type_id":     array([1,3,1]),
+                },
+            "building_sqft_per_job":{
+                       "zone_id":              array([1,  1, 1,  2, 2, 2,  3, 3]),
+                       "building_type_id":     array([1,  2, 3,  1, 2, 3,  1, 3]),
+                       "building_sqft_per_job":array([100,50,200,80,60,500,20,10]),
+                },  
+             "job": {"job_id":      array([1,2,3,4, 5, 6]),
+                     "sqft":        array([0,1,4,0, 2, 5]),
+                     "building_id": array([2,1,3,2, 1, 2])
                                },
-                )
+                       }
+                                    )
+            should_be = array([1+2,500+500+5,4])
+            tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
 
-            bs = JobDataset(in_storage=storage,
-                                          in_table_name=bs_table_name)
-
-            storage = StorageFactory().get_storage('dict_storage')
-            building_table_name='building'
-            storage.write_table(
-                    table_name=building_table_name,
-                    table_data={"building_id": array([1,2,3]),},
-            )
-
-            buildings = BuildingDataset(in_storage=storage,
-                                        in_table_name=building_table_name)
-
-            buildings.compute_variables(self.variable_name,
-                                        resources=Resources({'job':bs}))
-            values = buildings.get_attribute(self.variable_name)
-            should_be = array([3,5,4])
-
-            self.assertEqual(ma.allclose(values, should_be, rtol=1e-20), \
-                             True, msg = "Error in " + self.variable_name)
     unittest.main()
