06196b1893198ab782d994fc159e9e0de5a16db5
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-16 21:34:47 +0000

    (hana) variables using job.sqft modified in order to use job.sqft_imputed
    git-svn-id: https://svn.urbansim.org/trunk@11545 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/job/sqft_imputed.py
+++ urbansim_parcel/job/sqft_imputed.py
@@ -12,30 +12,44 @@
 # other acknowledgments.
 #
 
-from numpy import clip
+from numpy import where, concatenate, newaxis
 from opus_core.variables.variable import Variable
 
-class occupied_building_sqft_by_jobs(Variable):
-    """Sum of jobs sqft per building. If job sqft is <= 0, it is replaced by zone-building_type average.
-        Result is clipped between 0 and building.building_sqft.
+class sqft_imputed(Variable):
+    """jobs sqft imputed by zone-building_type average for jobs where sqft is <= 0.
     """
 
     _return_type="int32"
 
     def dependencies(self):
-        return ["urbansim_parcel.job.building_id",
-                "urbansim_parcel.job.sqft_imputed",
-                "urbansim_parcel.building.building_sqft"]
+        return ["urbansim_parcel.job.sqft"]
 
     def compute(self,  dataset_pool):
-        jobs = dataset_pool.get_dataset("job")
-        buildings = self.get_dataset()
-        sqft = jobs.get_attribute('sqft_imputed')
-        return clip(buildings.sum_over_ids(jobs.get_attribute('building_id'), sqft), 0,
-                        buildings.get_attribute("building_sqft"))
+        jobs = self.get_dataset()
+        sqft = jobs.get_attribute('sqft').copy()
+        where_zero = where(sqft <= 0)[0]
+        if where_zero.size == 0:
+            return sqft
+        self.add_and_solve_dependencies(["urbansim_parcel.job.zone_id",
+                "bldgs_building_type_id = job.disaggregate(building.building_type_id)", 
+                "urbansim_parcel.job.building_id",
+                "building_sqft_per_job.building_sqft_per_job"], dataset_pool=dataset_pool)
+        buildings = dataset_pool.get_dataset("building")
+        zones = jobs.get_attribute("zone_id")
+        type_ids = jobs.get_attribute("bldgs_building_type_id")
+        try:
+            sqft_per_job = dataset_pool.get_dataset("building_sqft_per_job")
+        except:
+            return sqft
+        ids = concatenate((zones[:,newaxis],type_ids[:,newaxis]), axis=1)
+        index = sqft_per_job.try_get_id_index(ids)
+        building_sqft_per_job = sqft_per_job.get_attribute("building_sqft_per_job")[index]
+        building_sqft_per_job[where(index==-1)] = 0
+        sqft[where_zero] = building_sqft_per_job[where_zero].astype(sqft.dtype)
+        return sqft
 
     def post_check(self,  values, dataset_pool=None):
-        size = dataset_pool.get_dataset("job").get_attribute("sqft").sum()
+        size = self.get_dataset().get_attribute("sqft").sum()
         self.do_check("x >= 0 and x <= " + str(size), values)
 
 if __name__=='__main__':
@@ -45,7 +59,6 @@ if __name__=='__main__':
     from opus_core.tests.utils.variable_tester import VariableTester
 
     class Tests(unittest.TestCase):
-        variable_name = "urbansim_parcel.building.occupied_building_sqft_by_jobs"
         def test(self):
             tester = VariableTester(
             __file__,
@@ -63,11 +76,11 @@ if __name__=='__main__':
                 },  
              "job": {"job_id":      array([1,2,3,4, 5, 6, 7]),
                      "sqft":        array([0,1,4,0, 2, 5, 0]),
-                     "building_id": array([2,1,3,2, 1, 2, 3])
+                     "building_id": array([2,1,3,1, 1, 2, 3])
                                },
                        }
                                     )
-            should_be = array([1+2,900,4+20])
-            tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
+            should_be = array([500, 1, 4, 100, 2, 5, 20])
+            tester.test_is_equal_for_variable_defined_by_this_module(self, should_be)
 
     unittest.main()
