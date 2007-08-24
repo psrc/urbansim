53d946038e188518a25bf5b32cb99b83fbaa4e17
pwaddell <pwaddell@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-08-24 01:58:54 +0000

    patch to fix zone_id
    git-svn-id: https://svn.urbansim.org/trunk@11633 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/sql/update development_events_exogenous.sql
+++ sanfrancisco/sql/update development_events_exogenous.sql
@@ -1,2 +1,6 @@
 alter table development_events_exogenous add blklot varchar(9);
 update development_events_exogenous set blklot = mapblklot;
+
+#One loose end to take care of: adding zone_id to parcels
+alter table parcels add zone_id int(32);
+update parcels set zone_id = taz;
