7441a7d3af20529d20bc59c5c5ba68a11b7e8789
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-04 05:41:42 +0000

    Started to add code to handle switching of which tabs are available when each left toolbox item is selected
    git-svn-id: https://svn.urbansim.org/trunk@11679 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/Main/opusGui.py
+++ opus_gui/Main/opusGui.py
@@ -40,3 +40,47 @@ class OpusGui(QMainWindow, Ui_MainWindow):
 
     # We need to initialize the window sizes
     self.splitter.setSizes([150,700])
+
+    debugString = QString("Startup Done...")
+    self.statusbar.showMessage(debugString)
+
+    # Build a list of the default list of tabs
+    self.tabWidgetList = {}
+    for tabIndex in range(self.tabWidget.count()):
+      self.tabWidgetList[str(self.tabWidget.widget(tabIndex).objectName())] = \
+      self.tabWidget.widget(tabIndex)
+    # Init to the first in toolbox by default
+    self.toolBoxChanged(0)
+    
+    QObject.connect(self.toolBox, SIGNAL("currentChanged(int)"),
+                    self.toolBoxChanged)
+  
+  def updateTabs(self, listOfTabs):
+    # Here we can update to show current tabs
+    for index in range(self.tabWidget.count()):
+      self.tabWidget.removeTab(0)      
+    for tab in listOfTabs:
+      if tab in self.tabWidgetList:
+        self.tabWidget.addTab(self.tabWidgetList.get(tab),tab)
+    
+  def toolBoxChanged(self, index):
+    # Here we can add and remove tabs from the right side gui as the toolbox
+    # items are changed
+    # Switch on the toolbox item
+    item = self.toolBox.widget(index)
+    if (item.objectName() == "datamanager_page"):
+      # Data Manager
+      self.updateTabs(("tab_detailView","tab_mapView"))
+    elif (item.objectName() == "modelmanager_page"):
+      # Model Manager
+      self.updateTabs(("tab_detailView","tab_mapView","tab_modelFlow"))
+    elif (item.objectName() == "runmanager_page"):
+      # Run Manager
+      self.updateTabs(("tab_detailView","tab_mapView","tab_runStatus"))
+    elif (item.objectName() == "resultsmanager_page"):
+      # Result Manager
+      self.updateTabs(("tab_detailView","tab_mapView","tab_trendView"))
+
+    debugString = QString("toolBoxChanged signal captured - Name = " + item.objectName())
+    self.statusbar.showMessage(debugString)
+    
