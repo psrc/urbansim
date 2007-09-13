dbc01f446d56f3df49fa7e68232b2ca550e2cf72
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-13 21:54:25 +0000

    (hana) agent_event_model, its configuration and datasets added
    git-svn-id: https://svn.urbansim.org/trunk@11733 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ washtenaw/configurations/households_event_model_configuration_creator.py
@@ -0,0 +1,74 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from enthought.traits.api import HasStrictTraits, Str
+
+from opus_core.configuration import Configuration
+
+
+class HouseholdsEventModelConfigurationCreator(HasStrictTraits):
+    location_set = Str('gridcell')
+    agent_event_set = Str('households_event')
+    
+    _model_name = 'agent_event_model'
+    
+    def execute(self):        
+        return Configuration({
+            'import': {
+                'washtenaw.models.%s' % self._model_name: 'AgentEventModel'
+                },
+            'init': {'name': 'AgentEventModel'},
+            'run': {
+                'arguments': {
+                    'location_set': self.location_set,
+                    'agent_event_set': self.agent_event_set,
+                    'current_year': 'year',
+                    'dataset_pool': 'dataset_pool'
+                    }
+                }
+            })
+            
+
+from opus_core.tests import opus_unittest 
+
+
+class TestDeletionEventModelConfigurationCreator(opus_unittest.OpusTestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    def test_defaults(self):
+        creator = HouseholdsEventModelConfigurationCreator()
+        
+        expected = Configuration({
+           'import': {
+                'washtenaw.models.agent_event_model': 'AgentEventModel'
+                },
+            'init': {'name': 'AgentEventModel'},
+            'run': {
+                'arguments': {
+                    'location_set': 'gridcell',
+                    'agent_event_set': 'households_event',
+                    'current_year': 'year',
+                    'dataset_pool': 'dataset_pool'
+                    }
+                }
+            })
+        result = creator.execute()
+        self.assertDictsEqual(result, expected)           
+            
+if __name__ == '__main__':
+    opus_unittest.main()
---
+++ washtenaw/datasets/households_event_dataset.py
@@ -0,0 +1,26 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from urbansim.datasets.dataset import Dataset as UrbansimDataset
+
+class HouseholdsEventDataset(UrbansimDataset):
+    """Set of events that represent deletion/adding/replacing households from/to specific locations.
+    """
+
+    id_name_default = ["grid_id", "scheduled_year"]
+    in_table_name_default = "households_events"
+    out_table_name_default = "households_events"
+    dataset_name = "households_event"
+    
+    
