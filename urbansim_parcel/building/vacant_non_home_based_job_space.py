af990c459a32850839def85e078a9c5d6c8658aa
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-14 12:38:38 +0000

    (hana) variable implementation changed
    git-svn-id: https://svn.urbansim.org/trunk@11737 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/building/vacant_non_home_based_job_space.py
+++ urbansim_parcel/building/vacant_non_home_based_job_space.py
@@ -14,21 +14,33 @@
 
 from opus_core.variables.variable import Variable
 from variable_functions import my_attribute_label
-from opus_core.misc import clip_to_zero_if_needed
+from numpy import ma, clip
 
 class vacant_non_home_based_job_space(Variable):
     """ number of job spaces that is vacant/unoccupied"""
     
+    _return_type = "int32"
+    
     def dependencies(self):
-        return ["urbansim_parcel.building.total_non_home_based_job_space",
-                "urbansim_parcel.building.number_of_non_home_based_jobs"
+        return ["urbansim_parcel.building.non_residential_sqft",
+                "urbansim_parcel.building.occupied_building_sqft_by_jobs",
+                "building_sqft_per_job.building_sqft_per_job",
+                "urbansim_parcel.building.non_residential_sqft",
+                "urbansim_parcel.building.zone_id",
+                "building.building_type_id",
                 ]
 
     def compute(self,  dataset_pool):
-        buildings = self.get_dataset()        
-        return clip_to_zero_if_needed(buildings.get_attribute("total_non_home_based_job_space") - \
-                                      buildings.get_attribute("number_of_non_home_based_jobs"), 
-                                      "vacant_non_home_based_job_space")
+        buildings = self.get_dataset()  
+        sqft_per_job = dataset_pool.get_dataset("building_sqft_per_job")
+        zones = buildings.get_attribute("zone_id")
+        type_ids = buildings.get_attribute("building_type_id")
+        non_residential_sqft = buildings.get_attribute("non_residential_sqft")
+        building_sqft_per_job_table = sqft_per_job.get_building_sqft_as_table(zones.max(), type_ids.max())
+        building_sqft_per_job = building_sqft_per_job_table[zones, type_ids]
+        remaining_space = clip(non_residential_sqft - buildings.get_attribute("occupied_building_sqft_by_jobs"),
+                               0, non_residential_sqft)
+        return ma.filled(ma.masked_where(building_sqft_per_job==0, remaining_space / building_sqft_per_job), 0)
 
     def post_check(self,  values, dataset_pool=None):
         self.do_check("x >= 0", values)
@@ -46,13 +58,21 @@ class Tests(opus_unittest.OpusTestCase):
             package_order=['urbansim_parcel','urbansim'],
             test_data={
             "building":{"building_id":         array([1,2,3,4,5,6,7,8,9,10]),
-                       "total_non_home_based_job_space": array([1,1,2,2,1,3,3,3,2,2]),
-                       "number_of_non_home_based_jobs":  array([1,3,1,2,2,1,2,3,2,4])
-                },                
+                       "zone_id":              array([1,1,2,2,1,3,3,3,2,2]),
+                       "building_type_id":     array([1,3,1,2,2,1,2,3,2,4]),
+                       "non_residential_sqft": array([1,2,2,1,7,0,3,5,4,6])*1000,
+             "occupied_building_sqft_by_jobs": array([500, 2100, 0, 0, 6000, 100, 3000, 1000, 0, 5999])
+                                       #vacant sqft:  500, 0,  2000, 1000, 1000, 0, 0, 4000, 4000, 1  
+                },
+            "building_sqft_per_job":{
+                       "zone_id":              array([1,  1, 1,  2, 2, 2,  3, 3]),
+                       "building_type_id":     array([1,  2, 3,  1, 2, 3,  1, 3]),
+                       "building_sqft_per_job":array([100,50,200,80,60,500,20,10]),
+                },                          
         }
         )
         
-        should_be = array([0, 0, 1, 0, 0, 2, 1, 0, 0, 0])
+        should_be = array([5, 0, 2000/80, 1000/60, 1000/50, 0, 0, 4000/10, 4000/60, 0])
         
         tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
 
