f4d7b1c38958917d95cb1f8e151be408d3d8a2b2
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-18 23:24:23 +0000

    (lmwang)added handling for agent addition events
    git-svn-id: https://svn.urbansim.org/trunk@11747 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- washtenaw/models/agent_event_model.py
+++ washtenaw/models/agent_event_model.py
@@ -12,7 +12,7 @@
 # other acknowledgments.
 # 
 
-from numpy import array, where, resize, logical_and, zeros
+from numpy import array, where, resize, logical_and, zeros, arange
 from opus_core.model import Model
 from opus_core.sampling_toolbox import sample_noreplace
 from opus_core.logger import logger
@@ -83,23 +83,24 @@ class AgentEventModel(Model):
             # compute agents locations
             agent_set.compute_one_variable_with_unknown_package(location_id_name, self.dataset_pool)
                     
-        agent_ids = agent_set.get_attribute(location_id_name)
         # iterate over locations
         for ilocation_id in range(location_ids_in_event_set.size):
+            agent_ids = agent_set.get_attribute(location_id_name)
             location_id = location_ids_in_event_set[ilocation_id]
             change_type = types_of_change[ilocation_id]
+
+            agents_to_consider = agent_ids == location_id
+            for characteristics in other_characteristics:
+                characteristics_value = eval("agent_event_set.get_data_element_by_id((location_id, current_year)).%s" % 
+                                             characteristics)
+                agents_to_consider = logical_and(agents_to_consider, 
+                                                 agent_set.get_attribute(characteristics) == characteristics_value)
+            number_of_agents = totals[ilocation_id]
+            agent_index = where(agents_to_consider)[0]  
+            if  is_percentage[ilocation_id]: # number_of_agents means percentage; convert to absolute number
+                number_of_agents = agent_index.size*number_of_agents/100.0
+            number_of_agents = int(number_of_agents)
             if change_type == 'D':
-                agents_to_consider = agent_ids == location_id
-                for characteristics in other_characteristics:
-                    characteristics_value = eval("agent_event_set.get_data_element_by_id((location_id, current_year)).%s" % 
-                                                 characteristics)
-                    agents_to_consider = logical_and(agents_to_consider, 
-                                                     agent_set.get_attribute(characteristics) == characteristics_value)
-                number_of_agents = totals[ilocation_id]
-                agent_index = where(agents_to_consider)[0]  
-                if  is_percentage[ilocation_id]: # number_of_agents means percentage; convert to absolute number
-                    number_of_agents = agent_index.size*number_of_agents/100.0
-                number_of_agents = int(number_of_agents)
                 if number_of_agents > 0:
                     if agent_index.size <= number_of_agents: # unplace all agents
                         unplace_index = agent_index
@@ -108,6 +109,18 @@ class AgentEventModel(Model):
                     agent_set.modify_attribute(name=location_id_name, 
                                            data=resize(array([-1], dtype=agent_ids.dtype), unplace_index.size),
                                            index = unplace_index)
+            elif change_type == 'A':
+                if number_of_agents > 0:
+                    data = {agent_set.get_id_name()[0]: arange(1, number_of_agents+1, 1) + agent_set.get_id_attribute().max()}
+                    data[location_id_name] = array([location_id] * number_of_agents)
+                
+                    for characteristics in other_characteristics:
+                        characteristics_value = eval("agent_event_set.get_data_element_by_id((location_id, current_year)).%s" % 
+                                                     characteristics)
+                        data[characteristics] = array([characteristics_value] * number_of_agents)
+                
+                    agent_set.add_elements(data, require_all_attributes=False)
+                    
                             
 from opus_core.tests import opus_unittest
 from numpy import ma, arange
@@ -155,6 +168,20 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
                 }
             )
         return JobsEventDataset(in_storage=storage, in_table_name='events')
+
+    def _create_simple_job_addition_event_set(self):
+        storage = StorageFactory().get_storage('dict_storage')
+        
+        storage.write_table(table_name='events', 
+               table_data = {
+                "scheduled_year": array([2000, 2000, 2001, 2001]),
+                "grid_id": array([10, 5, 3, 5]),
+                "total_number": array([20, 5, 0, 3]),
+                "change_type": array(['A', 'A', 'A', 'A']),
+                
+                }
+            )
+        return JobsEventDataset(in_storage=storage, in_table_name='events')
         
     def _create_household_deletion_event_set(self):
         storage = StorageFactory().get_storage('dict_storage')
@@ -165,6 +192,20 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
                 "grid_id": array([1, 5, 3, 5, 2, 1]),
                 "total_number": array([6, 5, 25, 0, 0, 50]),
                 "is_percentage": array([0,0, 0,  0, 0,  1], dtype="bool8"),
+                }
+            )
+        return HouseholdsEventDataset(in_storage=storage, in_table_name='events')
+
+    def _create_household_addition_event_set(self):
+        storage = StorageFactory().get_storage('dict_storage')
+        
+        storage.write_table(table_name='events', 
+               table_data = {
+                "scheduled_year": array([2000, 2000, 2001, 2001, 2001, 2001]),
+                "grid_id": array([1, 5, 3, 5, 2, 1]),
+                "total_number": array([6, 5, 25, 0, 0, 50]),
+                "is_percentage": array([0,0, 0,  0, 0,  1], dtype="bool8"),
+                "change_type":array(['A','A','A','A','A','A'])
                 }
             )
         return HouseholdsEventDataset(in_storage=storage, in_table_name='events')
@@ -179,6 +220,21 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
                 "total_number": array([2,    5,  70,  2,    100,    1]),
                 "is_percentage": array([0,   0,   1,  0,     1,     0], dtype="bool8"),
                 "sector_id":        array([1,    1,    2,   2,     2,    1])
+                }
+            )
+        return JobsEventDataset(in_storage=storage, in_table_name='events')
+
+    def _create_job_addition_event_set_with_characteristics(self):
+        storage = StorageFactory().get_storage('dict_storage')
+        
+        storage.write_table(table_name='events', 
+               table_data = {
+                "scheduled_year": array([2000, 2000, 2001, 2001, 2001, 2001]),
+                "grid_id":          array([1,    5,    3,   5,     2,    1]),
+                "total_number": array([2,    5,  70,  2,    100,    1]),
+                "is_percentage": array([0,   0,   1,  0,     1,     0], dtype="bool8"),
+                "sector_id":        array([1,    1,    2,   2,     2,    1]),
+                "change_type":array(['A','A','A','A','A','A'])                
                 }
             )
         return JobsEventDataset(in_storage=storage, in_table_name='events')
@@ -197,6 +253,21 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
         number_of_jobs = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs", dataset_pool=dataset_pool)
         # the model should remove another 3 jobs from gridcell 5
         self.assert_(ma.allclose(number_of_jobs, array( [10,10,10,10,2,10,10,10,10,0])))
+
+    def test_addition_of_jobs(self):
+        dataset_pool = DatasetPool(storage=self.storage, package_order=["washtenaw","urbansim", "opus_core"])
+        gridcell_set = dataset_pool.get_dataset('gridcell')
+        event_set = self._create_simple_job_addition_event_set()
+        jobs = dataset_pool.get_dataset("job")
+        AgentEventModel().run(gridcell_set, event_set, jobs, 2000, dataset_pool)
+        number_of_jobs = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs", dataset_pool=dataset_pool)
+        # the model should add 5 jobs to gridcell 5 and 20 jobs to gridcell 10
+        self.assert_(ma.allclose(number_of_jobs, array( [10,10,10,10,15,10,10,10,10,30]))) 
+
+        AgentEventModel().run(gridcell_set, event_set, jobs, 2001, dataset_pool)
+        number_of_jobs = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs", dataset_pool=dataset_pool)
+        # the model should add another 3 jobs to gridcell 5
+        self.assert_(ma.allclose(number_of_jobs, array( [10,10,10,10,18,10,10,10,10,30])))
         
     def test_deletion_of_households(self):
         dataset_pool = DatasetPool(storage=self.storage, package_order=["washtenaw","urbansim", "opus_core"])
@@ -214,6 +285,23 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
                                                               dataset_pool=dataset_pool)
         # the model should remove 50% from gridcell 1 (2) and 25 households from gridcell 3
         self.assert_(ma.allclose(number_of_households, array( [2,0,5,0,0,0,0,0,0,0])))
+
+    def test_addition_of_households(self):
+        dataset_pool = DatasetPool(storage=self.storage, package_order=["washtenaw","urbansim", "opus_core"])
+        gridcell_set = dataset_pool.get_dataset('gridcell')
+        event_set = self._create_household_addition_event_set()
+        households = dataset_pool.get_dataset("household")
+        AgentEventModel().run(gridcell_set, event_set, households, 2000, dataset_pool)
+        number_of_households = gridcell_set.compute_variables("urbansim.gridcell.number_of_households", 
+                                                              dataset_pool=dataset_pool)
+        # the model should add 6 households to gridcell 1, 
+        self.assert_(ma.allclose(number_of_households, array( [16,0,30,0,5,0,0,0,0,0]))) 
+
+        AgentEventModel().run(gridcell_set, event_set, households, 2001, dataset_pool)
+        number_of_households = gridcell_set.compute_variables("urbansim.gridcell.number_of_households", 
+                                                              dataset_pool=dataset_pool)
+        # the model should add 50% from gridcell 1 (8) and 25 households to gridcell 3
+        self.assert_(ma.allclose(number_of_households, array( [24,0,55,0,5,0,0,0,0,0])))
         
     def test_deletion_of_jobs_with_one_characteristics(self):
         dataset_pool = DatasetPool(storage=self.storage, package_order=["washtenaw","urbansim", "opus_core"])
@@ -248,7 +336,42 @@ class AgentEventsTests(opus_unittest.OpusTestCase):
         self.assert_(ma.allclose(number_of_jobs_of_sector_1, array( [1,4,4,4,0,4,4,4,4,4]))) 
         self.assert_(ma.allclose(number_of_jobs_of_sector_2, array( [3, 0, 1, 3, 1, 3, 3, 3, 3, 3]))) 
         # sector 4 does not change
+
+    def test_addition_of_jobs_with_one_characteristics(self):
+        dataset_pool = DatasetPool(storage=self.storage, package_order=["washtenaw","urbansim", "opus_core"])
+        gridcell_set = dataset_pool.get_dataset('gridcell')
+        event_set = self._create_job_addition_event_set_with_characteristics()
+        jobs = dataset_pool.get_dataset("job")
+        AgentEventModel().run(gridcell_set, event_set, jobs, 2000, dataset_pool)
+        number_of_jobs_of_sector_1 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_1", 
+                                                                    dataset_pool=dataset_pool)
+        number_of_jobs_of_sector_2 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_2", 
+                                                                    dataset_pool=dataset_pool)
+        number_of_jobs_of_sector_4 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_4", 
+                                                                    dataset_pool=dataset_pool)
+
+        # the model should add 2 jobs of sector 1 to gridcell 1, 
+        #                      5 jobs of sector 1 to gridcell 5
+        self.assert_(ma.allclose(number_of_jobs_of_sector_1, array( [6,4,4,4,9,4,4,4,4,4]))) 
+        # other sectors don't change
+        self.assert_(ma.allclose(number_of_jobs_of_sector_2, array( 10 * [3]))) 
+        self.assert_(ma.allclose(number_of_jobs_of_sector_4, array( 10 * [3]))) 
+
+        AgentEventModel().run(gridcell_set, event_set, jobs, 2001, dataset_pool)
+        number_of_jobs_of_sector_1 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_1", 
+                                                                    dataset_pool=dataset_pool)
+        number_of_jobs_of_sector_2 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_2", 
+                                                                    dataset_pool=dataset_pool)
+        number_of_jobs_of_sector_4 = gridcell_set.compute_variables("urbansim.gridcell.number_of_jobs_of_sector_4", 
+                                                                    dataset_pool=dataset_pool)
+        # the model should add 2 jobs of sector 2 to gridcell 5, 
+        #                      1 job of sector 1 to gridcell 1,
+        #                      4 jobs of sector 2 to gridcell 2
+        #                      70% jobs of sector 2 to gridcell 3
+        self.assert_(ma.allclose(number_of_jobs_of_sector_1, array( [7, 4, 4, 4, 9, 4, 4, 4, 4, 4]))) 
+        self.assert_(ma.allclose(number_of_jobs_of_sector_2, array( [3, 6, 5, 3, 5, 3, 3, 3, 3, 3]))) 
+        # sector 4 does not change
         self.assert_(ma.allclose(number_of_jobs_of_sector_4, array( 10 * [3])))
-        
+                
 if __name__=="__main__":
     opus_unittest.main()
