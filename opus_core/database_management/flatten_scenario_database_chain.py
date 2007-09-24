bad64c18da123041a67f7d270b9a9d1024ddcfa3
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-24 23:53:20 +0000

    (travis) implemented sqlalchemy version of flatten scenario database
    git-svn-id: https://svn.urbansim.org/trunk@11796 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/database_management/flatten_scenario_database_chain.py
+++ opus_core/database_management/flatten_scenario_database_chain.py
@@ -12,177 +12,94 @@
 # other acknowledgments.
 #
 
-from opus_core.store.opus_database import OpusDatabase
-from opus_core.store.scenario_database import ScenarioDatabase
-from opus_core.store.mysql_database_server import MysqlDatabaseServer
-from opus_core.configurations.database_server_configuration import DatabaseServerConfiguration
-
-from urbansim.store.database_tools import DatabaseTools
+from opus_core.database_management.database_server import DatabaseServer
+from opus_core.database_management.database_server_configuration import DatabaseServerConfiguration
+from opus_core.database_management.cross_database_operations import CrossDatabaseOperations
+from opus_core.database_management.scenario_database_manager import ScenarioDatabaseManager
+ 
+    
 from opus_core.logger import logger
 
 class FlattenScenarioDatabaseChain(object):
-    def _create_db_from_chain_via_sql(self, 
-                                      to_host_name,
-                                      to_database_name, 
-                                      to_user_name, 
-                                      to_password, 
-                                      table_mapping, 
-                                      tables_to_copy=[]
-                                     ):
-        """Use SQL commands to copy an UrbanSim style database chain from
-        from one MySQL to a new databsae on the same MySQL.  This method
-        does the copy via SQL - it does not bring the data into Python.
-        
-        Given a dictionary that maps tables to their database location 
-        will create a database that contains all the tables in this dictionary. 
-        Example use would be to aggregate all tables in an UrbanSim database-chain (Scenario Database $$) 
-        into one database"""
-        db_server_config = DatabaseServerConfiguration(host_name=to_host_name,
-                                                       user_name=to_user_name,
-                                                       password=to_password
-                                                   )
-        db_server = MysqlDatabaseServer(db_server_config)
-        db_server.drop_database(to_database_name)
-        db_server.create_database(to_database_name)
-        new_database = db_server.get_database(to_database_name)
 
-        for table_name, table_database_name in table_mapping.iteritems():
-            if table_name not in tables_to_copy:
-                continue
-            logger.start_block("Copying table '%s' from database '%s' to '%s'" 
-                               % (table_name, table_database_name, to_database_name))
-            try:
-                sql = "create table %(new_database_name)s.%(table_name)s select * from %(old_database_name)s.%(table_name)s" % {
-                    'table_name':table_name,
-                    'old_database_name':table_database_name,
-                    'new_database_name':to_database_name,
-                    }
-                new_database.DoQuery(sql)
-                if table_name == 'scenario_information':
-                    self._fix_scenario_information_table(table_database_name, new_database, to_database_name)
-            finally:
-                logger.end_block()
-        
-    def _fix_scenario_information_table(self, from_database_name, new_database, to_database_name):
+    def _fix_scenario_information_table(self, new_database):
         """Ensure that the scenario_information table doesn't point to any
         more databases.
         """
-        new_database.DoQuery("update %s.scenario_information set parent_database_url=''" % to_database_name)
+        new_database.metadata.reflect()
+        scenario_information_table = new_database.get_table('scenario_information')
+        qry = scenario_information_table.update(values = {'parent_database_url':''})
+        new_database.engine.execute(qry)
             
     def _create_db_from_chain_via_python(self, 
-                                        from_host_name, 
-                                        from_database_name,
-                                        from_user_name,
-                                        from_password,
-                                        to_host_name,
-                                        to_database_name, 
-                                        to_user_name,
-                                        to_password, 
-                                        table_mapping, 
-                                        tables_to_copy=[]
+                                         db_server_config_from,
+                                         from_database_name,
+                                         db_server_config_to,
+                                         to_database_name, 
+                                         tables_to_copy=[]
                                         ):
-        """Use Python to copy a UrbanSim sytle database chain from 
-        one MySQL server to another MySQL server.  This method does the copy
-        by reading the data to memory.
-        
-        Given a dictionary that maps tables to their database location 
-        will create a database that contains all the tables in this dictionary. 
-        Example use would be to aggregate all tables in an UrbanSim database-chain (Scenario Database $$) 
-        into one database"""
-        db_server_config = DatabaseServerConfiguration(host_name=to_host_name,
-                                                       user_name=to_user_name,
-                                                       password=to_password
-                                                   )
-        db_server = MysqlDatabaseServer(db_server_config)
-        db_server.drop_database(to_database_name)
+        """Copy a UrbanSim sytle database chain from 
+        one MySQL server to another MySQL server."""
 
-        # TODO: move DatabaseTools functionality into OpusDatabase.
-        db_tool = DatabaseTools()
-        new_database = db_tool.create_db(name=to_database_name,
-                                         hostname=to_host_name,
-                                         username=to_user_name,
-                                         password=to_password)
+                
+        db_server_from = DatabaseServer(db_server_config_from)
+        db_server_to = DatabaseServer(db_server_config_to)
+        db_server_to.drop_database(to_database_name)
+        db_server_to.create_database(to_database_name)
         
-        for table_name, table_database_name in table_mapping.iteritems():
-            if table_name not in tables_to_copy:
-                continue
-            logger.start_block("Copying table '%s' from database '%s'" 
-                               % (table_name, table_database_name))
-            try:
-                from_database = OpusDatabase(hostname=from_host_name, 
-                                             username=from_user_name, 
-                                             password=from_password, 
-                                             database_name=table_database_name)
-                #GetResultsFromQuery returns a list, where first element is a list of field names, 
-                #and other elements are the values
-                table = from_database.GetResultsFromQuery("SELECT * FROM %s.%s;" % (table_database_name, table_name))
-                
-                table_schema = from_database.get_schema_from_table(table_name)
-                new_database.create_table(table_name, table_schema)
-                
-                field_names = table[0]
-                field_values = table[1:]
-                names_values_dict = []
-                #iterate through the rows of the original table
-                for v in field_values:
-                    row_dict = {}
-                    #create a dictionary that represents one row of data in this table
-                    #(mapping of field names to values)
-                    for (name, value) in zip(field_names, v):
-                        #add extra quotes around values that are strings
-                        if isinstance(value, str):
-                            value = "'%s'" % value
-                        row_dict[name] = value
+        database_out = db_server_to.get_database(to_database_name)
+        
+        scenario_db_manager = ScenarioDatabaseManager(
+            server_configuration = db_server_config_from, 
+            base_scenario_database_name = from_database_name)
+        table_mapping = scenario_db_manager.get_database_to_table_mapping()
     
-                    names_values_dict.append(({}, row_dict))
-                #copy original table's rows to new table
-                db_tool.populate_table('%s.%s' % (to_database_name, table_name), 
-                                       field_names, 
-                                       names_values_dict)
+        cross_db_operations = CrossDatabaseOperations()
+        
+        for database_name, tables in table_mapping.items():
+            database_in = db_server_from.get_database(database_name)
+            for table in tables:
+                if table not in tables_to_copy:
+                    continue
                 
-                from_database.close()
-                if table_name == 'scenario_information':
-                    self._fix_scenario_information_table(table_database_name, new_database, to_database_name)
-            finally:
-                logger.end_block()
+                logger.start_block("Copying table '%s' from database '%s'" 
+                                   % (table, database_name)) 
+                
+                try:               
+                    cross_db_operations.copy_table(table_to_copy = table, 
+                                                   database_in = database_in, 
+                                                   database_out = database_out, 
+                                                   use_chunking = True)
+                finally:
+                    logger.end_block()        
+            database_in.close()
             
-        new_database.close()
+        self._fix_scenario_information_table(database_out)            
+        database_out.close()
 
     def copy_scenario_database(self, config):
-        """If mode is 'sql' do the copy via SQL statements.
-        If mode is 'python' do the copy by reading data into Python.
-        """
         logger.start_block("Copying tables from database chain starting at '%s' on '%s'\nto database '%s' on '%s'"
                            % (config['to_database_name'], 
                               config['from_host_name'], 
                               config['to_database_name'], 
                               config['to_host_name']))
+        
+        db_server_config_to = DatabaseServerConfiguration(
+           host_name = config['to_host_name'],
+           user_name = config['to_user_name'],
+           password = config['to_password'])
+                                                             
+        db_server_config_from = DatabaseServerConfiguration(
+           host_name = config['from_host_name'],
+           user_name = config['from_user_name'],
+           password = config['from_password'])        
+        
         try:
-            scenario_db = ScenarioDatabase(hostname=config['from_host_name'], 
-                                           username=config['from_user_name'], 
-                                           password=config['from_password'],
-                                           database_name=config['from_database_name'])
-            table_mapping = scenario_db.get_table_mapping()
-            scenario_db.close()
-            if config['mode'] == 'python':
-                self._create_db_from_chain_via_python(to_host_name=config['to_host_name'],
-                                                     to_database_name=config['to_database_name'],
-                                                     to_user_name=config['to_user_name'],
-                                                     to_password=config['to_password'],
-                                                     from_host_name=config['from_host_name'],
-                                                     from_database_name=config['from_database_name'],
-                                                     from_user_name=config['from_user_name'],
-                                                     from_password=config['from_password'],
-                                                     table_mapping=table_mapping,
-                                                     tables_to_copy=config['tables_to_copy'])
-            elif config['mode'] == 'sql':
-                self._create_db_from_chain_via_sql(to_host_name=config['to_host_name'],
-                                                  to_database_name=config['to_database_name'],
-                                                  to_user_name=config['to_user_name'],
-                                                  to_password=config['to_password'],
-                                                  table_mapping=table_mapping,
-                                                  tables_to_copy=config['tables_to_copy'])
-            else:
-                raise StandardError("Mode most be one of 'sql' or 'python'")
+            self._create_db_from_chain_via_python(
+                 db_server_config_to = db_server_config_to,
+                 to_database_name=config['to_database_name'],
+                 db_server_config_from = db_server_config_from,
+                 from_database_name=config['from_database_name'],
+                 tables_to_copy=config['tables_to_copy'])
         finally:
             logger.end_block()
