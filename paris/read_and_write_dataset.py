7540c047bac11e14f13982bb8f0160f0d36cece9
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-09-27 20:07:04 +0000

    (travis) converted to use sql_storage
    git-svn-id: https://svn.urbansim.org/trunk@11837 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- paris/read_and_write_dataset.py
+++ paris/read_and_write_dataset.py
@@ -8,7 +8,6 @@ from urbansim.datasets.zone_dataset import ZoneDataset
 from urbansim.datasets.neighborhood_dataset import NeighborhoodDataset
 from urbansim.datasets.job_dataset import JobDataset
 from paris.paris_settings import ParisSettings
-from opus_core.storage_factory import StorageFactory
 #
 # Miscellaneous stuff for manually testing and for generating flt binary databases
 #
@@ -23,87 +22,94 @@ class ReadWriteADataset(object):
 class GridcellsReadMySQLWriteFlt(object):
     """Reads Gridcells from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(),
-                           password=settings.get_db_password(),
-                           database_name=settings.db)
-        gcs = GridcellDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con), nchunks=5)
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        
+        gcs = GridcellDataset(in_storage=in_storage, nchunks=5)
         print "Read and Write GridcellDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(gcs, out_storage=out_storage, out_table_name=settings.gcsubdir)
-        Con.close()
 
 class NeighborhoodsReadMySQLWriteFlt(object):
     """Reads Neighborhoods from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(),
-                           password=settings.get_db_password(),
-                           database_name=settings.db)
-        nbs = NeighborhoodDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con), nchunks=5)
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        nbs = NeighborhoodDataset(in_storage=in_storage, nchunks=5)
         print "Read and Write NeighborhoodDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(nbs, out_storage=out_storage, out_table_name=settings.nbsubdir)
-        Con.close()
 
 class HouseholdsReadMySQLWriteFlt(object):
     """Reads Households from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(), 
-                           password=settings.get_db_password(),
-                           database_name=settings.db)
-        hhs = HouseholdDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con), nchunks=3)
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        hhs = HouseholdDataset(in_storage=in_storage, nchunks=3)
         print "Read and Write HouseholdDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(hhs, out_storage=out_storage, out_table_name=settings.hhsubdir)
-        Con.close()
   
 
 class JobsReadMySQLWriteFlt(object):
     """Reads jobs from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(),
-                           password=settings.get_db_password(),
-                           database_name=settings.db)
-        jobs = JobDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con), nchunks=5)
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        jobs = JobDataset(in_storage=in_storage, nchunks=5)
         print "Read and Write JobDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(jobs, out_storage=out_storage, out_table_name=settings.jobsubdir)
-        Con.close()
 
 class TravelDataReadMySQLWriteFlt(object):
     """Reads travel data from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(), 
-                           password=settings.get_db_password(),
-                           database_name=settings.db)
-        traveldata = TravelDataDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con))
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        traveldata = TravelDataDataset(in_storage=in_storage)
         print "Read and Write TravelDataDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(traveldata, out_storage=out_storage, out_table_name=settings.traveldatasubdir)
-        Con.close()
               
 class ZonesReadMySQLWriteFlt(object):
     """Reads zones from MySQL and writes them to disk."""
     def __init__(self):
-        Con = OpusDatabase(hostname=settings.get_db_host_name(),
-                           username=settings.get_db_user_name(), 
-                           password=settings.get_db_password(),
-                           database_name=settings.db, )
-        zones = ZoneDataset(in_storage=StorageFactory().get_storage('mysql_storage', storage_location=Con))
+        in_storage = StorageFactory().get_storage(
+            'sql_storage',
+            hostname=settings.get_db_host_name(),
+            username=settings.get_db_user_name(),
+            password=settings.get_db_password(),
+            database_name=settings.db)
+        zones = ZoneDataset(in_storage=in_storage)
         print "Read and Write ZoneDataset."
         out_storage = StorageFactory().build_storage_for_dataset(type='flt_storage', 
             storage_location=settings.dir)
         ReadWriteADataset(zones, out_storage=out_storage, out_table_name=settings.zonesubdir)
-        Con.close()
               
 if __name__ == "__main__":
 #        GridcellsReadMySQLWriteFlt()
