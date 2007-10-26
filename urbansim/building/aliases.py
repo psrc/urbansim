4f991e3e8efa77e173a07743aee99e9d8653ad7f
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-10-26 21:38:45 +0000

    (hana) new alias
    git-svn-id: https://svn.urbansim.org/trunk@12093 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/building/aliases.py
+++ urbansim/building/aliases.py
@@ -16,6 +16,7 @@ aliases = [
            "is_condo_residential=building.disaggregate(building_type.building_type_name=='condo_residential')",
            "is_single_family_residential=building.disaggregate(building_type.building_type_name == 'single_family_residential')",
            "is_multi_family_residential=building.disaggregate(building_type.building_type_name =='multi_family_residential')",
+           "is_mobile_home=building.disaggregate(building_type.building_type_name=='mobile_home')",
            "is_commercial=building.disaggregate(building_type.building_type_name =='commercial')",
            "is_industrial=building.disaggregate(building_type.building_type_name =='industrial')",
            "is_office=building.disaggregate(building_type.building_type_name =='office')",
