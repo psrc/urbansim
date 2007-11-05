d37df3e3627f321d67c443bb22293e69e5f7eccc
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-11-05 22:33:11 +0000

    (hana) script updated
    git-svn-id: https://svn.urbansim.org/trunk@12195 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/hana/uncertainty/run_bm.py
+++ inprocess/hana/uncertainty/run_bm.py
@@ -15,6 +15,7 @@
 import sys
 from numpy import sort
 from opus_core.bayesian_melding import BayesianMelding
+from opus_core.bayesian_melding import ObservedData
 from opus_core.plot_functions import plot_histogram
 from opus_core.misc import unique_values
 from opus_core.storage_factory import StorageFactory
@@ -32,45 +33,45 @@ if __name__ == "__main__":
 
     # in what directory is the file 'cache_directories'
     #cache_directory = "/home/hana/urbansim_cache/psrc/parcel/run_3904.2007_10_19_15_01"
-    cache_directory = "/Users/hana/urbansim_cache/psrc/parcel/run_3764.2007_10_08_16_01"
+    cache_directory = "/Users/hana/urbansim_cache/psrc/parcel/run_3914.2007_10_19_18_55"
     # This is needed only if one of the runs was scaled, e.g. run on reduced set of gridcells. 
     # It gives the directory of the base year full set, in order to scale back.
 #    scaling = {1: "/scratch/urbbuild/urbansim_cache/psrc/cache_source_zone"}
 
     # where the true data (on a zone level) is stored in a table format 
-    true_data_dir = "/Users/hana/data/"
+    observed_data_dir = "/Users/hana/bm/observed_data/"
     #true_data_dir = "/home/hana/urbansim_cache/psrc/data"
 
-    true_data_file_name = "PSRC2005TAZData" # the physical file should have the ending '.tab'
+    observed_data_file_name = "PSRC2005TAZData" # the physical file should have the ending '.tab'
 
-    in_storage = StorageFactory().get_storage('tab_storage',
-                                              storage_location = true_data_dir)
-    zones = ZoneDataset(in_storage=in_storage, in_table_name=true_data_file_name)
-    
-    flt_storage = StorageFactory().get_storage('flt_storage',
-                                              storage_location = cache_directory + "/2000")
-    fazes = FazDataset(in_storage=flt_storage, in_table_name='fazes')
-    large_areas = LargeAreaDataset(in_storage=flt_storage, in_table_name='large_areas')
+    observed_data = ObservedData(observed_data_dir, 2002, 'tab_storage', 
+                                 package_order=['urbansim_parcel', 'urbansim', 'opus_core'])
 
+    known_output={"urbansim_parcel.zone.number_of_households": "PSRC2005TAZData", 
+                  "urbansim_parcel.zone.number_of_jobs": "PSRC2005TAZData",
+                  "urbansim_parcel.large_area_x_land_use_type.total_value_per_sqft": ("avg_total_value_per_unit_by_la", {"id_name": 
+                                                                                      ["large_area_id", "land_use_type_id"]})
+                  }
+                  
+    for var, file in known_output.iteritems():
+        if isinstance(file, tuple):
+            observed_data.add_quantity(var, file[0], **file[1])
+        else:
+            observed_data.add_quantity(var, file)
+        
     bm = BayesianMelding(cache_directory, 
-                         #cache_with_true_data=true_data_cache, 
-                         datasets_with_true_data={'zone':zones, 'faz':fazes, 'large_area': large_areas},
-                         year_with_true_data=2005,
-                         known_output=["urbansim_parcel.zone.number_of_households", 
-                        "urbansim_parcel.zone.number_of_jobs"
-                        ], 
+                         observed_data,                        
                          transformation = 'sqrt', base_year=2000, 
                          #scaling_parents = scaling,
                          package_order=['urbansim_parcel', 'urbansim', 'opus_core'])
     weights = bm.compute_weights()
     print weights
 
-#    posterior = bm.generate_posterior_distribution(year=2010, quantity_of_interest="urbansim.zone.number_of_households",
-#                                                   use_bias_and_variance_from = "urbansim.zone.number_of_households", 
-#                                                   replicates=1000)
+    posterior = bm.generate_posterior_distribution(year=2005, quantity_of_interest="urbansim_parcel.zone.number_of_households",
+                                                   replicates=10)
 
-    #bm.write_simulated_values("/Users/hana/simulated_values")
-    #bm.write_values_from_multiple_runs("/Users/hana/multiple_run_values")
+    bm.write_simulated_values("/Users/hana/simulated_values")
+    bm.write_values_from_multiple_runs("/Users/hana/multiple_run_values")
     #bm.write_simulated_values("/home/hana/BM/psrc_analysis/data/simulated_values")
     #bm.write_values_from_multiple_runs("/home/hana/BM/psrc_analysis/data/multiple_run_values")
 
