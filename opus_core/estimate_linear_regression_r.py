8729f7b2c833e800558907e9e0cf40a936caabf9
selimnairb <selimnairb@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-11-07 14:20:53 +0000

    - Updated to work with numpy - Made output functionally equivalent to estimate_linear_regression.py - Added AIC statistic
    git-svn-id: https://svn.urbansim.org/trunk@12226 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/estimate_linear_regression_r.py
+++ opus_core/estimate_linear_regression_r.py
@@ -12,14 +12,18 @@
 # other acknowledgments.
 #
 
+from opus_core.estimation_procedure import EstimationProcedure
 from rpy import r, set_default_mode, NO_CONVERSION, BASIC_CONVERSION
-from numpy import zeros, float32, swapaxes
+from numpy import array, zeros, float32, swapaxes
+from numpy import sqrt, log
+from opus_core.logger import logger
 from opus_core.misc import check_dimensions
 
-class estimate_linear_regression_r(object):
+class estimate_linear_regression_r(EstimationProcedure):
     """    Class for estimating linear regression using the R function lm (rpy required).        
     """
-    def run(self, data, coefficients, resources=None):
+    #def run(self, data, coefficients, resources=None):
+    def run(self, data, regression=None, resources=None):
         """
         Expects an entry 'outcome' in resources that provides the values of the dependent variable.
         'coefficients' is of type "SpecifiedCoefficientsFor1Submodel". 
@@ -29,11 +33,14 @@ class estimate_linear_regression_r(object):
         """
         if data.ndim < 2:
             raise StandardError, "Argument 'data' must be a 2D numpy array."
+        tags = ["estimate", "result"]
+        vl = 2
             
-        beta = coefficients.get_coefficient_values()[0,:] # we are interested only in one equation
+        #beta = coefficients.get_coefficient_values()[0,:] # we are interested only in one equation
         nobs = data.shape[0]
         nvar = data.shape[1]
-        constant_position = coefficients.get_constants_positions()
+        #constant_position = coefficients.get_constants_positions()
+        constant_position = resources.get("constant_position",  array([], dtype='int32')) #position for intercept
         if constant_position.size <=0: #position for intercept
             constant_position=-1
             nvalues = nvar
@@ -41,15 +48,21 @@ class estimate_linear_regression_r(object):
             constant_position=constant_position[0]
             nvalues = nvar+1
 
+        beta = zeros(nvalues).astype(float32)
+
         if beta.size <> nvalues:
             raise StandardError, "Mismatch in dimensions of 'data' and 'coefficients.beta'."
             
-        coef_names = coefficients.get_coefficient_names_without_constant()
-        rnames = array(map(lambda x: "x."+ str(x), range(1,nvar+1)))
-        coef_names_array = swapaxes(array([coef_names[0,:], rnames]),1,0)
-        logger.log_status(coef_names_array)
-        outcome = resources["outcome"].astype("float64") # Rpy doesn't like float32, but is okay with float64
+        #coef_names = coefficients.get_coefficient_names_without_constant()
+        coef_names = resources.get("coefficient_names",  nvar*[])
+        #rnames = array(map(lambda x: "x."+ str(x), range(1,nvar+1)))
+        #coef_names_array = swapaxes(array([coef_names[0,:], rnames]),1,0)
+        #logger.log_status(coef_names_array)
+        # Rpy doesn't like float32, but is okay with float64
+        outcome = resources["outcome"].astype("float64")
         set_default_mode(NO_CONVERSION)
+        
+        # Rpy doesn't like float32, but is okay with float64
         data_for_r = data.astype("float64")
         d = r.data_frame(x=data_for_r,y=outcome)
         expression = "y ~ x.1"
@@ -57,19 +70,33 @@ class estimate_linear_regression_r(object):
             expression=expression+" + x."+str(i)
         model = r(expression)
         fit = r.lm(model, data=d)
+        
+        # Begin, for AIC
+        # WARNING: Make sure this code appears just after the call to r.lm.
+        #          If it appears after the call to r.summary, the call to
+        #          r.AIC will crash
+        set_default_mode(BASIC_CONVERSION)
+        aic = r.AIC(fit)
+        set_default_mode(NO_CONVERSION)
+        # End, for AIC
+        
         summary = r.summary(fit)
         summary[0]=expression
-        r.print_(summary)
+        
+        #r.print_(summary)
         set_default_mode(BASIC_CONVERSION)
         new_coef = r.coefficients(fit)
         values = zeros((nvalues,), dtype=float32)
         se = zeros((nvalues,), dtype=float32)
         if constant_position>=0:
             values[constant_position] = new_coef["(Intercept)"]
+            start = 1
             try:
                 se[constant_position] = summary[3]["coefficients"][0,1]
             except:
                 pass
+        else:
+            start = 0
         j=0
         for i in range(1,nvar+1):
             if j == constant_position:
@@ -80,8 +107,45 @@ class estimate_linear_regression_r(object):
             except:
                 pass
             j+=1
-        coefficients.set_coefficient_values(values)
-        coefficients.set_standard_errors(se)
-        coefficients.set_measure("t-statistic", values/se)
-        return coefficients
+        #coefficients.set_coefficient_values(values)
+        #coefficients.set_standard_errors(se)
+        #coefficients.set_measure("t-statistic", values/se)
+        #return coefficients
+        
+        # r.summary(fit)['coefficients'] is a 2D array of the following structure:
+        # [ [estimate, standard error, t-value, p-value], ...]
+        # To get an array of the estimates for all the variables, slice off the first column:
+        estimates = r.summary(fit)['coefficients'][:,0]
+        # To get an array of the standard errors for all of the variables, slice off the second column:
+        standard_errors = r.summary(fit)['coefficients'][:,1]
+        
+        Rsquared = r.summary(fit)['r.squared']
+        Rsquared_adj = r.summary(fit)['adj.r.squared']
+        
+        tvalues = values/se
+        result = {"estimators":values, "standard_errors":se,
+                   "other_measures":{"t_statistic":tvalues},
+                    "other_info":{"R-Squared":Rsquared,
+            "Adjusted R-Squared":Rsquared_adj}}
+
+        logger.log_status("Akaike's Information Criterion (AIC): ", str(aic), tags=tags, verbosity=vl)
+
+        names = resources.get("coefficient_names",  nvar*[])
+        logger.log_status("R-Squared:            ", Rsquared, tags=tags, verbosity_level=vl)
+        logger.log_status("Adjusted R-Squared:   ", Rsquared_adj, tags=tags, verbosity_level=vl)
+        logger.log_status('Suggested |t-value| > ', sqrt(log(nobs)))
+        logger.log_status("-----------------------------------------------", tags=tags, verbosity_level=vl)
+        logger.log_status("Coeff_names\testimate\tSE\tt-values", tags=tags, verbosity_level=vl)
+        if constant_position>=0:
+            logger.log_status("%10s\t%8g\t%8g\t%8g" % ("constant", estimates[0], standard_errors[0],
+                                    estimates[0]/standard_errors[0]), tags=tags, verbosity_level=vl)
+        for i in range(nvar):
+            logger.log_status("%10s\t%8g\t%8g\t%8g" % (names[i],estimates[i+start],standard_errors[i+start],
+                                    estimates[i+start]/standard_errors[i+start]), tags=tags, verbosity_level=vl)
+        logger.log_status("===============================================", tags=tags, verbosity_level=vl)
+        logger.log_status(tags=tags, verbosity_level=vl)
+        
+        return result
+        
+
         
