74db1918c1457345583655ae12f31bc0f9de2e8c
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-11-07 01:10:46 +0000

    (hana) variables modified
    git-svn-id: https://svn.urbansim.org/trunk@12219 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/faz_x_land_use_type/total_value_per_sqft.py
+++ urbansim_parcel/faz_x_land_use_type/total_value_per_sqft.py
@@ -16,10 +16,13 @@ from numpy import zeros, where
 from scipy import ndimage
 
 class total_value_per_sqft(Variable):
+    
+    id_variable = "faz_id = parcel.disaggregate(zone.faz_id)"
+    
     def dependencies(self):
-        return ["faz_id = parcel.disaggregate(zone.faz_id)",
+        return [self.id_variable,
                 "parcel.land_use_type_id",
-                #"urbansim_parcel.parcel.total_value_per_sqft"
+                "urbansim_parcel.parcel.total_value_per_sqft",
                 "urbansim_parcel.parcel.unit_price"
                 ]
         
@@ -28,8 +31,10 @@ class total_value_per_sqft(Variable):
         parcels = dataset_pool.get_dataset('parcel')
         #up = parcels.get_attribute("total_value_per_sqft")
         up = parcels.get_attribute("unit_price")
+        where_zero = where(up == 0)[0]
+        up[where_zero] = parcels.get_attribute("total_value_per_sqft")[where_zero]
         where_positive = where(up > 0)[0]
-        faz = parcels.get_attribute("faz_id")
+        faz = parcels.get_attribute(self.id_variable)
         lut = parcels.get_attribute("land_use_type_id")
         fazids = ds.get_dataset(1).get_id_attribute()
         lutids = ds.get_dataset(2).get_id_attribute()
@@ -59,12 +64,13 @@ class Tests(opus_unittest.OpusTestCase):
              "parcel":{
                  "parcel_id": arange(1,21),
                  "unit_price":       array([10,5,3,100,2,1,4,2,68,400,3,12,14,59,24,0,0,24,5,2.5]),
+             "total_value_per_sqft": array([10,5,3,100,2,1,4,2,68,400,3,12,14,59,24,20,0,24,5,2.5]),
                  "land_use_type_id": array([1, 3,1, 5, 4,5,4,3,1,  1, 4,3, 1, 5, 5, 3,4, 1,1, 1]),
                  "faz_id":           array([1, 2,2, 1, 2,1,1,1,1,  2, 2,1, 2, 1, 2, 2,1, 1,1, 2])
                        }
              })
         should_be = array([[array([10,68,24,5]).mean(), 0, array([2,12]).mean(), 4, array([100,1,59]).mean()],
-                           [array([3,400,14,2.5]).mean(), 0, 5, array([2,3]).mean(), 24]])
+                           [array([3,400,14,2.5]).mean(), 0, array([5, 20]).mean(), array([2,3]).mean(), 24]])
                             
         tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
 
--- urbansim_parcel/large_area_x_land_use_type/total_value_per_sqft.py
+++ urbansim_parcel/large_area_x_land_use_type/total_value_per_sqft.py
@@ -11,30 +11,12 @@
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
 # 
-from opus_core.variables.variable import Variable
-from numpy import zeros, where
-from scipy import ndimage
 
-class total_value_per_sqft(Variable):
-    def dependencies(self):
-        return ["large_area_id = parcel.disaggregate(faz.large_area_id, intermediates=[zone])",
-                "parcel.land_use_type_id",
-                "urbansim_parcel.parcel.total_value_per_sqft"
-                ]
-        
-    def compute(self, dataset_pool):
-        ds = self.get_dataset()
-        parcels = dataset_pool.get_dataset('parcel')
-        up = parcels.get_attribute("total_value_per_sqft")
-        la = parcels.get_attribute("large_area_id")
-        lut = parcels.get_attribute("land_use_type_id")
-        laids = ds.get_dataset(1).get_id_attribute()
-        lutids = ds.get_dataset(2).get_id_attribute()
-        result = zeros(ds.size()[0])
-        for i in range(result.shape[0]): # iterate over large areas
-            labels = where(la == laids[i], lut, 0)
-            result[i,:] = ndimage.mean(up, labels=labels, index=lutids)
-        return result
+from urbansim_parcel.faz_x_land_use_type.total_value_per_sqft import total_value_per_sqft as faz_total_value_per_sqft
+
+class total_value_per_sqft(faz_total_value_per_sqft):
+    id_variable = "large_area_id = parcel.disaggregate(faz.large_area_id, intermediates=[zone])"
+
     
 from opus_core.tests import opus_unittest
 from opus_core.tests.utils.variable_tester import VariableTester
@@ -55,13 +37,14 @@ class Tests(opus_unittest.OpusTestCase):
                               },
              "parcel":{
                  "parcel_id": arange(1,21),
-                 "total_value_per_sqft":       array([10,5,3,100,2,1,4,2,68,400,3,12,14,59,24,0,0,24,5,2.5]),
+                 "unit_price":       array([10,5,3,100,2,1,4,2,68,400,3,12,14,59,24,0,0,24,5,2.5]),
+                "total_value_per_sqft": array([10,5,3,100,2,1,4,2,68,400,3,12,14,59,24,0,5,24,5,2.5]),
                  "land_use_type_id": array([1, 3,1, 5, 4,5,4,3,1,  1, 4,3, 1, 5, 5, 3,4, 1,1, 1]),
                  "large_area_id":    array([1, 2,2, 1, 2,1,1,1,1,  2, 2,1, 2, 1, 2, 2,1, 1,1, 2])
                        }
              })
-        should_be = array([[array([10,68,24,5]).mean(), 0, array([2,12]).mean(), array([4,0]).mean(), array([100,1,59]).mean()],
-                           [array([3,400,14,2.5]).mean(), 0, array([5,0]).mean(), array([2,3]).mean(), 24]])
+        should_be = array([[array([10,68,24,5]).mean(), 0, array([2,12]).mean(), array([4,5]).mean(), array([100,1,59]).mean()],
+                           [array([3,400,14,2.5]).mean(), 0, array([5]).mean(), array([2,3]).mean(), 24]])
                             
         tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
 
