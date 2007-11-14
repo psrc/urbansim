36edf1c8f4dbff4973f1bc590b58cae9f4bb11b5
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-11-14 06:49:18 +0000

    Added check for opus to allow the gui to still run without the ability to run a model
    git-svn-id: https://svn.urbansim.org/trunk@12286 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/Run/opusRunModel.py
+++ opus_gui/Run/opusRunModel.py
@@ -18,9 +18,14 @@ from PyQt4.QtGui import *
 from opusRunModel_ui import *
 import os, sys, time
 
-from opus_core.tools.start_run import StartRunOptionGroup
-from opus_core.services.run_server.run_manager import insert_auto_generated_cache_directory_if_needed, SimulationRunError
-from inprocess.configurations.xml_configuration import XMLConfiguration
+try:
+    from opus_core.tools.start_run import StartRunOptionGroup
+    from opus_core.services.run_server.run_manager import insert_auto_generated_cache_directory_if_needed, SimulationRunError
+    from inprocess.configurations.xml_configuration import XMLConfiguration
+    WithOpus = True
+except ImportError:
+    WithOpus = False
+    print "Unable to import opus core libs"
 
 class OpusModelTest(object):
     def __init__(self,parent):
@@ -29,66 +34,67 @@ class OpusModelTest(object):
         self.finishedCallback = parent.finishedCallback
 
     def run(self):
-        # Run the Eugene model using the XML version of the Eugene configuration.
-        # This code adapted from opus_core/tools/start_run.py
-        statusfile = None
-        try:
-            option_group = StartRunOptionGroup()
-            parser = option_group.parser
-            # simulate 0 command line arguments by passing in []
-            (options, args) = parser.parse_args([])
-            run_manager = option_group.get_run_manager(options)
-            # find the directory containing the eugene xml configurations
-            xml_dir = __import__('opus_gui').__path__[0]
-            xml_path = os.path.join(xml_dir, 'projects', 'eugene', 'baseline.xml')
-            # xml_dir = __import__('inprocess').__path__[0]
-            # xml_path = os.path.join(xml_dir, 'configurations', 'projects', 'eugene', 'baseline.xml')
-            config = XMLConfiguration(xml_path)
-            insert_auto_generated_cache_directory_if_needed(config)
-            (self.start_year, self.end_year) = config['years']
-            cache_dir = config['cache_directory']
-            statusfile = os.path.join(cache_dir, 'status.txt')
-            self.statusfile = statusfile
-            config['status_file_for_gui'] = statusfile
-            run_manager.run_run(config)
-            succeeded = True
-        except SimulationRunError:
-            succeeded = False
-        if statusfile is not None and os.path.exists(statusfile):
-            os.remove(statusfile)
-        self.parent.finishedCallback(succeeded)
-        
-        # statement to signal i % progress:
-        # self.parent.progressCallback(i)
+        if WithOpus:
+            # Run the Eugene model using the XML version of the Eugene configuration.
+            # This code adapted from opus_core/tools/start_run.py
+            statusfile = None
+            try:
+                option_group = StartRunOptionGroup()
+                parser = option_group.parser
+                # simulate 0 command line arguments by passing in []
+                (options, args) = parser.parse_args([])
+                run_manager = option_group.get_run_manager(options)
+                # find the directory containing the eugene xml configurations
+                xml_dir = __import__('opus_gui').__path__[0]
+                xml_path = os.path.join(xml_dir, 'projects', 'eugene', 'baseline.xml')
+                # xml_dir = __import__('inprocess').__path__[0]
+                # xml_path = os.path.join(xml_dir, 'configurations', 'projects', 'eugene', 'baseline.xml')
+                config = XMLConfiguration(xml_path)
+                insert_auto_generated_cache_directory_if_needed(config)
+                (self.start_year, self.end_year) = config['years']
+                cache_dir = config['cache_directory']
+                statusfile = os.path.join(cache_dir, 'status.txt')
+                self.statusfile = statusfile
+                config['status_file_for_gui'] = statusfile
+                run_manager.run_run(config)
+                succeeded = True
+            except SimulationRunError:
+                succeeded = False
+            if statusfile is not None and os.path.exists(statusfile):
+                os.remove(statusfile)
+            self.parent.finishedCallback(succeeded)
+        else:
+            pass
         
     def _compute_progress(self, statusfile):
-        # Compute percent progress for the progress bar.
-        # The statusfile is written by the _write_status_for_gui method
-        # in class ModelSystem in urbansim.model_coordinators.model_system
-        # The file is ascii, with the following format (1 item per line):
-        #   current year
-        #   total number of models
-        #   number of current model that is about to run (starting with 0)
-        #   message to display in the progress bar widget
-        try:
-            f = open(statusfile)
-            lines = f.readlines()
-            f.close()
-            # use float for all numbers to help with percent computation
-            current_year = float(lines[0])
-            total_models = float(lines[1])
-            current_model = float(lines[2])
-            message = lines[3].strip()
-            total_years = float(self.end_year - self.start_year + 1)
-            # For each year, we need to run all of the models.
-            # year_fraction_completed is the fraction completed (ignoring the currently running year)
-            # model_fraction_completed is the additional fraction completed for the current year
-            year_fraction_completed = (current_year - self.start_year) / total_years
-            model_fraction_completed = (current_model / total_models) / total_years
-            percentage = 100.0* (year_fraction_completed + model_fraction_completed)
-            return {"percentage":percentage,"message":message}
-        except IOError:
-            return {"percentage":0,"message":"Model initializing..."}
+        if WithOpus:
+            # Compute percent progress for the progress bar.
+            # The statusfile is written by the _write_status_for_gui method
+            # in class ModelSystem in urbansim.model_coordinators.model_system
+            # The file is ascii, with the following format (1 item per line):
+            #   current year
+            #   total number of models
+            #   number of current model that is about to run (starting with 0)
+            #   message to display in the progress bar widget
+            try:
+                f = open(statusfile)
+                lines = f.readlines()
+                f.close()
+                # use float for all numbers to help with percent computation
+                current_year = float(lines[0])
+                total_models = float(lines[1])
+                current_model = float(lines[2])
+                message = lines[3].strip()
+                total_years = float(self.end_year - self.start_year + 1)
+                # For each year, we need to run all of the models.
+                # year_fraction_completed is the fraction completed (ignoring the currently running year)
+                # model_fraction_completed is the additional fraction completed for the current year
+                year_fraction_completed = (current_year - self.start_year) / total_years
+                model_fraction_completed = (current_model / total_models) / total_years
+                percentage = 100.0* (year_fraction_completed + model_fraction_completed)
+                return {"percentage":percentage,"message":message}
+            except IOError:
+                return {"percentage":0,"message":"Model initializing..."}
 
 class RunModelThread(QThread):
     def __init__(self, parent):
