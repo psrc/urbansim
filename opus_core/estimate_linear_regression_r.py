7fa69ef60068d386594a10c3f3e1e082eb7dfe29
selimnairb <selimnairb@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-11-14 20:27:39 +0000

    - run method: now properly specifies models with only 1 indep. variable (R doesn't like "x.i" notation for such models, but instead wants "y ~ x" - Added basic unit test for a regression with a single independent variable
    git-svn-id: https://svn.urbansim.org/trunk@12289 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/estimate_linear_regression_r.py
+++ opus_core/estimate_linear_regression_r.py
@@ -22,7 +22,6 @@ from opus_core.misc import check_dimensions
 class estimate_linear_regression_r(EstimationProcedure):
     """    Class for estimating linear regression using the R function lm (rpy required).        
     """
-    #def run(self, data, coefficients, resources=None):
     def run(self, data, regression=None, resources=None):
         """
         Expects an entry 'outcome' in resources that provides the values of the dependent variable.
@@ -36,28 +35,23 @@ class estimate_linear_regression_r(EstimationProcedure):
         tags = ["estimate", "result"]
         vl = 2
             
-        #beta = coefficients.get_coefficient_values()[0,:] # we are interested only in one equation
         nobs = data.shape[0]
         nvar = data.shape[1]
-        #constant_position = coefficients.get_constants_positions()
         constant_position = resources.get("constant_position",  array([], dtype='int32')) #position for intercept
         if constant_position.size <=0: #position for intercept
             constant_position=-1
             nvalues = nvar
         else:
             constant_position=constant_position[0]
-            nvalues = nvar+1
+            nvalues = nvar+1        
 
-        beta = zeros(nvalues).astype(float32)
+        # R doesn't like "x.i" notation when there's a single independent variable
+        if nvar < 2:
+            expression = "y ~ x"
+        else:
+            expression = "y ~ x.1"
 
-        if beta.size <> nvalues:
-            raise StandardError, "Mismatch in dimensions of 'data' and 'coefficients.beta'."
-            
-        #coef_names = coefficients.get_coefficient_names_without_constant()
         coef_names = resources.get("coefficient_names",  nvar*[])
-        #rnames = array(map(lambda x: "x."+ str(x), range(1,nvar+1)))
-        #coef_names_array = swapaxes(array([coef_names[0,:], rnames]),1,0)
-        #logger.log_status(coef_names_array)
         # Rpy doesn't like float32, but is okay with float64
         outcome = resources["outcome"].astype("float64")
         set_default_mode(NO_CONVERSION)
@@ -65,7 +59,7 @@ class estimate_linear_regression_r(EstimationProcedure):
         # Rpy doesn't like float32, but is okay with float64
         data_for_r = data.astype("float64")
         d = r.data_frame(x=data_for_r,y=outcome)
-        expression = "y ~ x.1"
+        
         for i in range(2,nvar+1):
             expression=expression+" + x."+str(i)
         model = r(expression)
@@ -101,16 +95,15 @@ class estimate_linear_regression_r(EstimationProcedure):
         for i in range(1,nvar+1):
             if j == constant_position:
                 j+=1
-            values[j] = new_coef["x."+str(i)]
+            if constant_position == -1:
+                values[j] = new_coef["x"]
+            else:
+                values[j] = new_coef["x."+str(i)]
             try:
                 se[j] = summary[3]["coefficients"][i,1]
             except:
                 pass
             j+=1
-        #coefficients.set_coefficient_values(values)
-        #coefficients.set_standard_errors(se)
-        #coefficients.set_measure("t-statistic", values/se)
-        #return coefficients
         
         # r.summary(fit)['coefficients'] is a 2D array of the following structure:
         # [ [estimate, standard error, t-value, p-value], ...]
@@ -148,4 +141,70 @@ class estimate_linear_regression_r(EstimationProcedure):
         return result
         
 
+from opus_core.tests import opus_unittest
+from opus_core.datasets.dataset import Dataset
+from opus_core.datasets.dataset_pool import DatasetPool
+from opus_core.storage_factory import StorageFactory
+from opus_core.resources import Resources
+from numpy import array, resize
+
+
+class Tests(opus_unittest.OpusTestCase):
+    """ Unit tests for opus_core.estimate_linear_regression_r
+    """
+    def test_estimation_one_var(self):
+        """ Test a regression estimation for a model with one independent variable
+        """
         
+        # First, use scipy to get reference values to compare the results of our
+        # R-based regression to.
+        #print "using scipy to calculate reference regression..."
+        # Example regression from: http://www2.warwick.ac.uk/fac/sci/moac/currentstudents/peter_cock/python/lin_reg/
+        from scipy import stats
+        x = [5.05, 6.75, 3.21, 2.66]
+        y = [1.65, 26.5, -5.93, 7.96]
+        gradient, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+        r_squared = r_value**2
+        #print "Gradient and intercept", gradient, intercept
+        ##Gradient and intercept 5.3935773612 -16.2811279931
+        #print "R-squared", r_squared
+        ##R-squared 0.524806275136
+        #print "p-value", p_value
+        ##p-value 0.275564857882
+        
+        # Next, setup the call to estimate_linear_regression_r.run(...)
+        # Need to call run method on estimate_linear_regression_r, whose prototype is: 
+        #   def run(self, data, regression=None, resources=None):
+        #   regresion is not used by the run method
+        #   things I need to store in resources:
+        #     constant_position = resources.get("constant_position",  array([], dtype='int32')) #position for intercept
+        #     coef_names = resources.get("coefficient_names",  nvar*[])
+        #     outcome = resources["outcome"].astype("float64")
+        
+        # Create resources
+        coeff = array(['EX'])
+        resources = Resources()
+        # No constant
+        resources.add("constant_position", array([], dtype='int32'))
+        resources.add("coefficient_names", coeff)
+        resources.add("outcome", array(y))
+        
+        #data = array([x, y])
+        data = resize(array([x]), (len(x), 1))
+        
+        # run RPy-based regression
+        estimateR = estimate_linear_regression_r()
+        result = estimateR.run(data, resources=resources)
+        #print "results from RPy-base estimation: " + str(result)
+    
+        # Finally, compare the scipy-based regression to the R-based regression
+        # Compare gradient of the independent variable estimate (would like to compare intercept, but that's not returned by run() method
+        self.assertEqual(round(gradient, 4), round(result['estimators'][0], 4))
+        # Compare the R-Squared
+        self.assertEqual(round(r_squared, 6), round(result['other_info']['R-Squared'], 6))
+    
+if __name__=='__main__':
+    try: import wingdbstub
+    except: pass
+
+    opus_unittest.main()
