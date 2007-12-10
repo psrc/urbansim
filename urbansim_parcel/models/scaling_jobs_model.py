1e291f8f9e490513811dc90aff1c9a9e6af5f877
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-10 03:58:06 +0000

    (hana) parcel version of the SJM added
    git-svn-id: https://svn.urbansim.org/trunk@12413 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim_parcel/models/scaling_jobs_model.py
@@ -0,0 +1,22 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from urbansim.models.scaling_jobs_model import ScalingJobsModel as UrbansimScalingJobsModel
+
+class ScalingJobsModel(UrbansimScalingJobsModel):
+    """
+    This model works exactly as its parent. It uses different variable_package.
+    It is used for locating scalable jobs into buildings.  
+    """
+    variable_package = "urbansim_parcel"
