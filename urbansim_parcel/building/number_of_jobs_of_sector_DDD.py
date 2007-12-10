c41bfebd7fac87a0fb0f8d6c564d745ec30089e6
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-10 04:15:11 +0000

    (hana) variable added
    git-svn-id: https://svn.urbansim.org/trunk@12415 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim_parcel/building/number_of_jobs_of_sector_DDD.py
@@ -0,0 +1,24 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from urbansim.gridcell.number_of_jobs_of_sector_DDD import number_of_jobs_of_sector_DDD as us_number_of_jobs_of_sector_DDD
+from urbansim.functions import attribute_label
+
+class number_of_jobs_of_sector_DDD(us_number_of_jobs_of_sector_DDD):
+    """Sum the number of jobs for a given building that are in the employment sector specified by DDD.
+       (see code in the parent's class) 
+    """
+    def dependencies(self):
+        return [attribute_label("job", self.job_is_in_employment_sector),
+                'job.building_id']
