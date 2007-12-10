6189d1cce5ec9cb74a44f29731089bda136df89e
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-10 21:43:07 +0000

    (travis) replacing indicator_framework.storage_location.database with database_management.database_configuration
    git-svn-id: https://svn.urbansim.org/trunk@12426 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/indicators/make_indicators.py
+++ psrc_parcel/indicators/make_indicators.py
@@ -25,11 +25,11 @@ from opus_core.indicator_framework.image_types.table import Table
 from opus_core.indicator_framework.image_types.geotiff_map import GeotiffMap
 from opus_core.indicator_framework.image_types.dataset_table import DatasetTable
 from opus_core.indicator_framework.image_types.matplotlib_lorenzcurve import LorenzCurve
-from opus_core.indicator_framework.storage_location.database import Database
+from opus_core.database_management.database_configuration import DatabaseConfiguration
 
 
 run_description = '(baseline 11/26/2007)'
-cache_directory = r'D:\urbansim_cache\run_4168.2007_11_20_16_53'
+cache_directory = r'C:\cache\run_4168.2007_11_20_16_53'
 
 #run_description = '(no build 11/26/2007)'
 #cache_directory = r'D:\urbansim_cache\run_4169.2007_11_21_00_12'
@@ -37,7 +37,7 @@ cache_directory = r'D:\urbansim_cache\run_4168.2007_11_20_16_53'
 source_data = SourceData(
     cache_directory = cache_directory,
     run_description = run_description,
-    years = range(2000, 2031, 10),
+    years = range(2000, 2021, 10),
     dataset_pool_configuration = DatasetPoolConfiguration(
         package_order=['psrc_parcel','urbansim_parcel','psrc', 'urbansim','opus_core'],
         package_order_exceptions={},
@@ -60,31 +60,31 @@ indicators=[
 #        dataset_name = 'zone',
 #        source_data = source_data,
 #        ),
-    Map(
-        attribute = 'faz_employment=faz.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel,zone])',
-        dataset_name = 'faz',
-        source_data = source_data,
-        ),
-    Map(
-        name = 'population_change',
-        source_data = source_data,
-        attribute = 'population=faz.aggregate(urbansim_parcel.building.population, intermediates=[parcel,zone])',
-        operation = 'change',
-        dataset_name = 'faz',
-        ),
-    DatasetTable(
-        source_data = source_data,
-        dataset_name = 'building',
-        name =  'new_bldg',
-        attributes = [
-            'building.building_id',
-            'urbansim_parcel.building.unit_price',
-            'urbansim_parcel.building.residential_units',
-            'urbansim_parcel.building.vacant_residential_units',
-            'urbansim_parcel.building.year_built',
-        ],
-        exclude_condition = 'building.year_built<DDDD',
-    ),
+#    Map(
+#        attribute = 'faz_employment=faz.aggregate(urbansim_parcel.building.number_of_jobs, intermediates=[parcel,zone])',
+#        dataset_name = 'faz',
+#        source_data = source_data,
+#        ),
+#    Map(
+#        name = 'population_change',
+#        source_data = source_data,
+#        attribute = 'population=faz.aggregate(urbansim_parcel.building.population, intermediates=[parcel,zone])',
+#        operation = 'change',
+#        dataset_name = 'faz',
+#        ),
+#    DatasetTable(
+#        source_data = source_data,
+#        dataset_name = 'building',
+#        name =  'new_bldg',
+#        attributes = [
+#            'building.building_id',
+#            'urbansim_parcel.building.unit_price',
+#            'urbansim_parcel.building.residential_units',
+#            'urbansim_parcel.building.vacant_residential_units',
+#            'urbansim_parcel.building.year_built',
+#        ],
+#        exclude_condition = 'building.year_built<DDDD',
+#    ),
 
 ]
 from opus_core.indicator_framework.core.indicator_factory import IndicatorFactory
