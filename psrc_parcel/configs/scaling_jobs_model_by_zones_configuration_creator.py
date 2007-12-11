f56688d306092b5b08df54f39b52e672521c4cb3
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-11 18:28:15 +0000

    (hana) SJM by zones creator added
    git-svn-id: https://svn.urbansim.org/trunk@12434 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/configs/scaling_jobs_model_by_zones_configuration_creator.py
@@ -0,0 +1,26 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from urbansim.configurations.governmental_employment_location_choice_model_configuration_creator import GovernmentalEmploymentLocationChoiceModelConfigurationCreator
+
+class ScalingJobsModelByZonesConfigurationCreator(GovernmentalEmploymentLocationChoiceModelConfigurationCreator):
+    _model_name = 'scaling_jobs_model_by_zones'
+    def execute(self):
+        result = GovernmentalEmploymentLocationChoiceModelConfigurationCreator.execute(self)
+        result['import'] = {
+                'psrc_parcel.models.%s' % self._model_name: 'ScalingJobsModelByZones'
+                }
+        result['init']['name'] = 'ScalingJobsModelByZones'
+        result["run"]["arguments"]["zones"] = 'zone'
+        return result
