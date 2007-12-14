11f6f6f619069b16deba3c221946bac2061a94bc
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-14 20:33:53 +0000

    (lmwang)no build travel model scenario
    git-svn-id: https://svn.urbansim.org/trunk@12490 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/configs/baseline_travel_model_fast_no_build.py
@@ -0,0 +1,45 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from baseline import Baseline
+
+class BaselineTravelModelFastNoBuild(Baseline):
+    multiple_runs=True
+    def __init__(self):
+        config = Baseline()
+        
+        config_changes = {
+            'description':'baseline travel model fast (no build)',
+        }
+        config.replace(config_changes)
+        
+        from psrc.configs.create_travel_model_configuration import create_travel_model_configuration
+        travel_model_configuration = create_travel_model_configuration('baseline_travel_model_psrc_fast_no_build', 
+                                                                       emme2_batch_file='MODELUSim.BAT ..\\triptabs',
+                                                                       mode='full')
+        config['travel_model_configuration'] = travel_model_configuration
+        config['travel_model_configuration']['locations_to_disaggregate'] = ['parcel', 'building']
+        del config['travel_model_configuration'][2000]
+        
+        ##fast model doesn't have bank2 and bank3; disable macros using them
+        del config['travel_model_configuration']['emmission_emme2_macros']['tazvmt2.mac']
+        del config['travel_model_configuration']['emmission_emme2_macros']['tazvmt3.mac']
+
+        del config['travel_model_configuration']['matrix_variable_map']['bank2']
+        del config['travel_model_configuration']['matrix_variable_map']['bank3']
+        
+        self.merge(config)
+
+#if __name__ == "__main__":
+#    config = BaselineTravelModelFast()
