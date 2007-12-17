2c8c620ad1359ed7b1a4ea9cb618d74e471aa9c3
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-17 21:12:47 +0000

    (travis) reverting erroneous change
    git-svn-id: https://svn.urbansim.org/trunk@12518 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/indicator_framework/test_classes/abstract_indicator_test.py
+++ opus_core/indicator_framework/test_classes/abstract_indicator_test.py
@@ -13,8 +13,8 @@
 # 
 
 from opus_core.tests import opus_unittest
-from inprocess.travis.opus_core.indicator_framework.core.source_data import SourceData
-from inprocess.travis.opus_core.indicator_framework.test_classes.test_with_attribute_data import TestWithAttributeData
+from opus_core.indicator_framework.core.source_data import SourceData
+from opus_core.indicator_framework.test_classes.test_with_attribute_data import TestWithAttributeData
 from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
 
 class AbstractIndicatorTest(TestWithAttributeData):
