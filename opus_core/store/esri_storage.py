007422abb4daf89e7c68796e3230be431b7f77a8
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-18 23:58:06 +0000

    incremental checkin, added unit tests
    git-svn-id: https://svn.urbansim.org/trunk@12526 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/store/esri_storage.py
+++ opus_core/store/esri_storage.py
@@ -17,7 +17,7 @@ from opus_core.logger import logger
 try:
     import arcgisscripting, types, pywintypes, os
     from opus_core.store.storage import Storage
-    from numpy import empty, append
+    from numpy import empty, append, ma, array
     from string import count
     from random import randint
 
@@ -29,10 +29,10 @@ else:
         """
         Testing an ESRI storage object
         TODO: needs better error checking throughout
-            - check for storage_location (workspace) existence
             - deal with feature datasets in geodatabases
-             - right now this assumes that all tables/feature classes
-             are in the 'root' of the geodb
+             - right now this ignores all tables/feature classes that exist
+               in feature datasets unless the feature dataset is specified as
+               part of the storage_location path
             - when creating a table, deal with table name length restrictions
             - handle single and double float types better when reading tables
 
@@ -51,7 +51,6 @@ else:
                 db_connection_file = os.path.split(storage_location)[-1]
                 full_db_connection_file_path = os.path.join(os.environ['USERPROFILE'], 'Application Data\\ESRI\\ArcCatalog', db_connection_file)
                 storage_location_exists = os.path.exists(full_db_connection_file_path)
-                # TODO: check for more of these boolean value checks
                 if not storage_location_exists:
                     raise IOError, 'The ArcSDE geodatabase connection "%s" does not exist.' % (db_connection_file)
             else:
@@ -78,12 +77,14 @@ else:
             This method writes a dataset (table_data) to the specified table (table_name).
             Set overwrite_existing = True if the table should be overwritten.
             """
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
             # Get full path to table
             full_table_location = self.get_full_table_location(table_name)
 
-            if overwrite_existing == True:
+            if overwrite_existing:
                 self.gp.OverwriteOutput= 1
-                if self.table_exists(table_name) == True:
+                if self.table_exists(table_name):
                     logger.log_note('The table with the name "%s" already exists.' % (table_name))
                     logger.log_note('This table will be overwritten.')
                     self.gp.Delete(full_table_location)
@@ -92,7 +93,7 @@ else:
                     logger.log_note('This table will be written.')
             else:
                 self.gp.OverwriteOutput = 0
-                if self.table_exists(table_name) == True:
+                if self.table_exists(table_name):
                     logger.log_note('The table with the name "%s" already exists.' % (table_name))
                     logger.log_note('This table will not be overwritten.')
                     return None
@@ -109,7 +110,7 @@ else:
                 dbf = True
 
             # Create table
-            if dbf == True:
+            if dbf:
                 if table_name.find('.dbf') == -1:
                     table_name = table_name + '.dbf'
                     self.gp.CreateTable(storage_location, table_name)
@@ -125,7 +126,7 @@ else:
                 column_names.append(i)
             # Get shortened column names
             short_column_names = []
-            if dbf == True:
+            if dbf:
                 for i in column_names:
                     if len(i) <= 10:
                         short_column_names.append(i)
@@ -156,7 +157,7 @@ else:
                 self.gp.AddField(full_table_location, i, esri_column_types[x])
                 x += 1
             # Delete automatically added field if table of type .dbf
-            if dbf == True:
+            if dbf:
                 self.gp.DeleteField(full_table_location, 'Field1')
 
             # Insert records
@@ -194,8 +195,11 @@ else:
             representing the column names that should be loaded from the
             table.
             """
+
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
             # Check for table existence
-            if self.table_exists(table_name) == False:
+            if not self.table_exists(table_name):
                 logger.log_warning('Table with name "%s" does not exist.' % (table_name))
                 return None
 
@@ -260,14 +264,16 @@ else:
             SHAPE.area, SHAPE.len)
             """
 
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
             # Check for table existence
-            if self.table_exists(table_name) == False:
+            if not self.table_exists(table_name):
                 logger.log_warning('Table with name "%s" does not exist.' % (table_name))
                 return None
 
             # Create full path to table
             storage_location = self.get_storage_location()
-            if storage_location[-1] != '/':
+            if not storage_location[-1] == '/':
                 storage_location = storage_location + '/'
             full_table_name = storage_location + table_name
 
@@ -300,9 +306,11 @@ else:
             (e.g. SHAPE.area, SHAPE.len).
             """
 
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
             # Create full path to table
             storage_location = self.get_storage_location()
-            if storage_location[-1] != '/':
+            if not storage_location[-1] == '/':
                 storage_location = storage_location + '/'
             full_table_name = storage_location + table_name
 
@@ -336,7 +344,8 @@ else:
             ends in .txt, .csv, .tab, etc., it will return a .txt.dbf on the end.
             This method strips off the ending .dbf if it exists.
             """
-
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
             storage_location = self.get_storage_location()
 
             table_names = []
@@ -348,10 +357,10 @@ else:
             while table:
                 if count(str(table), '.') > 1:
                     substitute_table_name = str(table).replace('.dbf', '')
-                    if self._validate_table(substitute_table_name) == True:
+                    if self._validate_table(substitute_table_name):
                         table_names.append(substitute_table_name)
                 else:
-                    if self._validate_table(table) == True:
+                    if self._validate_table(table):
                         table_names.append(str(table))
                 table = tables.Next()
 
@@ -361,14 +370,16 @@ else:
             fcs.Reset()
             fc = fcs.Next()
             while fc:
-                if self._validate_table(fc) == True:
+                if self._validate_table(fc):
                     table_names.append(str(fc))
                 fc = fcs.Next()
 
             return table_names
 
         def table_exists(self, table_name):
-            if self.gp.Exists(table_name) == True:
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
+            if self.gp.Exists(table_name):
                 return True
             else:
                 return False
@@ -379,6 +390,8 @@ else:
             the ESRI Geoprocessor throws an exeption, the table is considered
             to be invalid.
             """
+            # Reset the workspace
+            self.gp.Workspace = self._storage_location
 
             try:
                 self.gp.SearchCursor(table_name)
@@ -426,3 +439,209 @@ else:
             rand = str(randint(0,99))
             short_name = column_name[0:length] + rand
             return short_name
+
+
+    from opus_core.tests import opus_unittest
+
+    class TestStorage(opus_unittest.OpusTestCase):
+
+        def setUp(self):
+            # Get the opus core directory
+            opus_core_directory = __import__('opus_core').__path__[0]
+            # Set up paths to different data sources
+            # TODO: figure out how to do this with SDE Geodatabases
+            self.test_data_location_files = os.path.join(opus_core_directory, 'store', 'data', 'esri')
+            self.test_data_location_file_geodb = os.path.join(opus_core_directory, 'store', 'data', 'esri', 'test_fgdb.gdb')
+            self.test_data_location_personal_geodb = os.path.join(opus_core_directory, 'store', 'data', 'esri', 'test_pgdb.mdb')
+            # Create different storage objects
+            self.file_path_storage_object = esri_storage(self.test_data_location_files)
+            self.file_geodb_storage_object = esri_storage(self.test_data_location_file_geodb)
+            self.personal_geodb_storage_object = esri_storage(self.test_data_location_personal_geodb)
+
+        def tearDown(self):
+            # Delete schema.ini created by the ESRI geoprocessor:
+            full_path_to_schema_ini = os.path.join(self.test_data_location_files, 'schema.ini')
+            if os.path.exists(full_path_to_schema_ini):
+                os.remove(full_path_to_schema_ini)
+            # Delete other tables created by write tests below:
+            # .dbf write test cleanup
+            full_path_to_test_dbf_table = os.path.join(self.test_data_location_files, 'test_dbf_table.dbf')
+            if os.path.exists(full_path_to_test_dbf_table):
+                self.file_path_storage_object.gp.Delete(full_path_to_test_dbf_table)
+            # file geodb write test cleanup
+            full_path_to_test_filegdb_table = os.path.join(self.test_data_location_file_geodb, 'test_filegeodb_table')
+            if self.file_geodb_storage_object.table_exists(full_path_to_test_filegdb_table):
+                self.file_geodb_storage_object.gp.Delete(full_path_to_test_filegdb_table)
+            # personal geodb write test cleanup
+            full_path_to_test_personalgeodb_table = os.path.join(self.test_data_location_personal_geodb, 'test_personalgeodb_table')
+            if self.personal_geodb_storage_object.table_exists(full_path_to_test_personalgeodb_table):
+                self.personal_geodb_storage_object.gp.Delete(full_path_to_test_personalgeodb_table)
+
+        def test_load_test_csv_tbl(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_csv_table = esri_storage_object.load_table('test_csv_tbl.csv')
+            test_data_table = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_csv_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_tab_tbl(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_tab_table = esri_storage_object.load_table('test_tab_tbl.tab')
+            test_data_table = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_tab_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_tbl_dbl_quoted_values(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_dbl_quoted_values = esri_storage_object.load_table('test_tbl_dbl_quoted_text_values.txt')
+            test_data_table = {'fld2': array(['val2', 'val5'], dtype='|S4'),
+                               'fld3': array(['val3', 'val6'], dtype='|S4'),
+                               'fld1': array(['val1', 'val4'], dtype='|S4')}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_dbl_quoted_values[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_tbl_single_quoted_text_values(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_single_quoted_values = esri_storage_object.load_table('test_tbl_single_quoted_text_values.txt')
+            test_data_table = {'fld2': array(["'val2'", "'val5'"], dtype='|S6'),
+                               'fld3': array(["'val3'", "'val6'"], dtype='|S6'),
+                               'fld1': array(["'val1'", "'val4'"], dtype='|S6')}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_single_quoted_values[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_txt_tbl(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_txt_table = esri_storage_object.load_table('test_txt_tbl.txt')
+            test_data_table = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_txt_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_dbf_tbl(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_dbf_table = esri_storage_object.load_table('test_dbf_tbl.dbf')
+            test_data_table = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_dbf_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_shapefile(self):
+            esri_storage_object = self.file_path_storage_object
+            loaded_shapefile_table = esri_storage_object.load_table('test_shapefile.shp')
+            test_data_table = esri_storage_object.load_table('test_shapefile.shp')
+            #test_data_table = {'date_fld': array(['1/1/2001 12:00:00 AM', '2/2/2002 12:00:00 AM', '3/3/2003 12:00:00 AM'], dtype='|S20'),
+            #                   'int': array([1000000, 2000000, 3000000]),
+            #                   'small_int': array([1, 2, 3]),
+            #                   'single_fld': array([ 1.11110997,  2.22221994,  3.33332992]),
+            #                   'string_fld': array(['string 1', 'string 2', 'string 3'], dtype='|S8'),
+            #                   'double_fld': array([ 1.11111111,  2.22222222,  2.22222222])}
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_shapefile_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_get_table_names_from_file_path(self):
+            esri_storage_object = self.file_path_storage_object
+            table_names_in_path = ['test_csv_tbl.csv', 'test_tab_tbl.tab',
+                                   'test_tbl_dbl_quoted_text_values.txt',
+                                   'test_tbl_single_quoted_text_values.txt',
+                                   'test_txt_tbl.txt', 'test_dbf_tbl.dbf',
+                                   'test_shapefile.shp']
+            test_table_names_in_path = esri_storage_object.get_table_names()
+            self.assertEqual(test_table_names_in_path, table_names_in_path)
+
+        def test_get_table_names_from_file_geodb(self):
+            esri_storage_object = self.file_geodb_storage_object
+            table_names_in_file_geodb = ['test_table', 'test_feature_class']
+            test_table_names_in_file_geodb = esri_storage_object.get_table_names()
+            self.assertEqual(table_names_in_file_geodb, test_table_names_in_file_geodb)
+
+        def test_get_table_names_from_personal_geodb(self):
+            esri_storage_object = self.personal_geodb_storage_object
+            table_names_in_personal_geodb = ['test_table', 'test_feature_class']
+            test_table_names_in_personal_geodb = esri_storage_object.get_table_names()
+            self.assertEqual(table_names_in_personal_geodb, test_table_names_in_personal_geodb)
+
+        def test_load_test_feature_class_from_file_geodb(self):
+            esri_storage_object = self.file_geodb_storage_object
+            loaded_feature_class = esri_storage_object.load_table('test_feature_class')
+            test_data_table = esri_storage_object.load_table('test_feature_class')
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_feature_class[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_table_from_file_geodb(self):
+            esri_storage_object = self.file_geodb_storage_object
+            loaded_data_table = esri_storage_object.load_table('test_table')
+            test_data_table = esri_storage_object.load_table('test_table')
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_data_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_feature_class_from_personal_geodb(self):
+            esri_storage_object = self.personal_geodb_storage_object
+            loaded_feature_class = esri_storage_object.load_table('test_feature_class')
+            test_data_table = esri_storage_object.load_table('test_feature_class')
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_feature_class[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_load_test_table_from_personal_geodb(self):
+            esri_storage_object = self.personal_geodb_storage_object
+            loaded_data_table = esri_storage_object.load_table('test_table')
+            test_data_table = esri_storage_object.load_table('test_table')
+            for i in test_data_table:
+                passed = False
+                if ma.allequal(test_data_table[i], loaded_data_table[i]):
+                    passed = True
+                self.assertEqual(passed, True)
+
+        def test_write_table_to_file_path(self):
+            esri_storage_object = self.file_path_storage_object
+            table_data = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            table_name = 'test_dbf_table.dbf'
+            esri_storage_object.write_table(table_name, table_data)
+            table_exists = esri_storage_object.table_exists(table_name)
+            self.assertEqual(table_exists, True)
+
+        def test_write_table_to_file_geodb(self):
+            esri_storage_object = self.file_geodb_storage_object
+            table_data = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            table_name = 'test_filegeodb_table'
+            esri_storage_object.write_table(table_name, table_data)
+            table_exists = esri_storage_object.table_exists(table_name)
+            self.assertEqual(table_exists, True)
+
+        def test_write_table_to_personal_geodb(self):
+            esri_storage_object = self.personal_geodb_storage_object
+            table_data = {'fld2': array([2, 5]), 'fld3': array([3, 6]), 'fld1': array([1, 4])}
+            table_name = 'test_personalgeodb_table'
+            esri_storage_object.write_table(table_name, table_data)
+            table_exists = esri_storage_object.table_exists(table_name)
+            self.assertEqual(table_exists, True)
+
+    if __name__ == '__main__':
+        opus_unittest.main()
