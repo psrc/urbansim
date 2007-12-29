ca95810a1dd2ced8dfd76b94efe659d847181efc
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-29 22:33:03 +0000

    Added the psrc_parcel.xml to the local projects area, moved the models_to_estimate section to the top, and then added code to have a right click option to run the estimation.
    git-svn-id: https://svn.urbansim.org/trunk@12602 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/config/opusXMLAction_Model.py
+++ opus_gui/config/opusXMLAction_Model.py
@@ -18,6 +18,11 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtXml import *
 
+import os
+from inprocess.configurations.xml_configuration import XMLConfiguration
+# from psrc_parcel.estimation.run_estimation import EstimationRunner
+from inprocess.configurations.gui_estimation_runner import EstimationRunner
+        
 
 class OpusXMLAction_Model(object):
     def __init__(self, parent):
@@ -32,10 +37,40 @@ class OpusXMLAction_Model(object):
         self.actPlaceHolder = QAction(self.applicationIcon, "Placeholder", self.xmlTreeObject.parent)
         QObject.connect(self.actPlaceHolder, SIGNAL("triggered()"), self.placeHolderAction)
 
+        self.actRunEstimation = QAction(self.applicationIcon, "Run Estimation", self.xmlTreeObject.parent)
+        QObject.connect(self.actRunEstimation, SIGNAL("triggered()"), self.runEstimationAction)
+
     def placeHolderAction(self):
         print "placeHolderAction pressed with column = %s and item = %s" % \
               (self.currentColumn, self.currentIndex.internalPointer().node().toElement().tagName())
     
+    def runEstimationAction(self):
+        print "runEstimationAction pressed with column = %s and item = %s" % \
+              (self.currentColumn, self.currentIndex.internalPointer().node().toElement().tagName())
+        # First confirm that the project file needs to be saved before running the estimation...
+        if not self.xmlTreeObject.model.dirty:
+            fileNameInfo = QFileInfo(self.xmlTreeObject.parentTool.xml_file)
+            parcelfile = fileNameInfo.absoluteFilePath().trimmed()
+            print parcelfile
+            xml_config = XMLConfiguration(parcelfile)
+            estimation_section = xml_config.get_estimation_section()
+            estimation_config = estimation_section['estimation_config']
+            
+            # TODO: put save_estimation results etc into config
+            for model_name in estimation_config['models_to_estimate']:
+                model_config = estimation_config['model_parameters'][model_name]
+                model = (model_config['abbreviation'], model_config['full_name'])
+                if 'location' in model_config:
+                    model = model , (model_config['location'], model_config['add_member_prefix'])
+                specification = xml_config.get_estimation_specification(model_config['full_name'])
+                er = EstimationRunner()
+                er.run_estimation(estimation_config, model, specification, save_estimation_results=True, diagnose=False)
+        else:
+            # Prompt the user to save...
+            QMessageBox.warning(self.xmlTreeObject.parent,
+                                "Warning",
+                                "Please save changes to project before running model")
+        
     def processCustomMenu(self, position):
         if self.xmlTreeObject.view.indexAt(position).isValid() and \
                self.xmlTreeObject.view.indexAt(position).column() == 0:
@@ -50,9 +85,9 @@ class OpusXMLAction_Model(object):
                 domElement = domNode.toElement()
                 if domElement.isNull():
                     return
-                if domElement.attribute(QString("type")) == QString("foo"):
+                if domElement.tagName() == QString("models_to_estimate"):
                     self.menu = QMenu(self.xmlTreeObject.parent)
-                    self.menu.addAction(self.actPlaceHolder)
+                    self.menu.addAction(self.actRunEstimation)
                     self.menu.exec_(QCursor.pos())
                 else:
                     self.menu = QMenu(self.xmlTreeObject.parent)
