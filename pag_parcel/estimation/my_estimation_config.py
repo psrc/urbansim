ed1365559f2d7d6252a9f3d78fe1b9e5cc9f07e1
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2007-12-31 21:40:42 +0000

    changes
    git-svn-id: https://svn.urbansim.org/trunk@12655 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- pag_parcel/estimation/my_estimation_config.py
+++ pag_parcel/estimation/my_estimation_config.py
@@ -1,16 +1,16 @@
 #
 # UrbanSim software. Copyright (C) 1998-2007 University of Washington
-# 
+#
 # You can redistribute this program and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation
 # (http://www.gnu.org/copyleft/gpl.html).
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
-# 
+#
 
 import os
 
@@ -22,16 +22,16 @@ from opus_core.configurations.baseyear_cache_configuration import BaseyearCacheC
 from urbansim.configurations.creating_baseyear_cache_configuration import CreatingBaseyearCacheConfiguration
 
 my_configuration = {
-    'cache_directory' : 'c:/urbansim_cache/pag_parcel/cache_source', # change or leave out
+    'cache_directory' : 'c:/urbansim_cache/pag_parcel/pag_2000_baseyear_cache', # change or leave out
     'output_configuration': DatabaseConfiguration(
-        database_name = 'pag_parcel_2000_estimation', #we want to write these to MSSQL
+        database_name = 'pag_parcel_2000_estimation',
         ),
     'dataset_pool_configuration': DatasetPoolConfiguration(
-        package_order=['pag_parcel','psrc', 'urbansim', 'opus_core'],
+        package_order=['pag_parcel','urbansim_parcel', 'urbansim', 'opus_core'],
         package_order_exceptions={},
         ),
     'datasets_to_cache_after_each_model':[],
     'low_memory_mode':False,
     'base_year': 2000,
-    'years': (2000,2000),                    
+    'years': (2000,2000),
     }
--- pag_parcel/estimation/repm_specification.py
+++ pag_parcel/estimation/repm_specification.py
@@ -1,81 +1,78 @@
 #
 # UrbanSim software. Copyright (C) 1998-2004 University of Washington
-# 
+#
 # You can redistribute this program and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation
 # (http://www.gnu.org/copyleft/gpl.html).
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
-# 
+#
 
 all_variables = [
     "lnsqft=ln(parcel.aggregate(urbansim_parcel.building.building_sqft))",
-    "lnsqftunit=ln(parcel.aggregate(building.building_sqft)/parcel.aggregate(building.residential_units))",
-    "lnlotsqft=ln(parcel.parcel_sqft)",
-    "lnlotsqftunit=ln(parcel.parcel_sqft/parcel.aggregate(building.residential_units))",
-    "ln_bldgage=ln(parcel.aggregate(urbansim_parcel.building.age_masked, function=mean))",
-    "far=parcel.aggregate(urbansim_parcel.building.building_sqft)/parcel.parcel_sqft",
+#    "lnsqftunit=ln(parcel.aggregate(building.building_sqft)/parcel.aggregate(building.residential_units))",
+#    "lnlotsqft=ln(parcel.parcel_sqft)",
+#    "lnlotsqftunit=ln(parcel.parcel_sqft/parcel.aggregate(building.residential_units))",
+#    "ln_bldgage=ln(parcel.aggregate(urbansim_parcel.building.age_masked, function=mean))",
+#    "far=parcel.aggregate(urbansim_parcel.building.building_sqft)/parcel.parcel_sqft",
                  ]
 
 specification = {
        "_definition_": all_variables,
-       
-    1:   #Single Family Low Density
+
+    101:   #Single Family Low Density
             [
     "constant",
     "lnsqft",
-    "lnlotsqft",
-    "ln_bldgage",
-    "far",    
+#    "lnlotsqft",
+#    "ln_bldgage",
     ],
 
-    3:   #Single Family High Density
+    165:   #Single Family High Density
             [
     "constant",
     "lnsqft",
-    "lnlotsqft",
-    ],
-
-
-    4:   #Multi Family Residential
-            [
-    "constant",
-    "lnsqft",
-    "lnlotsqft",
-    "lnlotsqftunit",
-    ],
-
-
-    7:   #Industrial 
-            [
-    "constant",
-    "lnsqft",
-    "lnlotsqft",
-    ],
-
-    8:   #Office
-            [
-    "constant",
-    "lnsqft",
-    "far",
-    "ln_bldgage",
 #    "lnlotsqft",
     ],
-
-    25:   #Mixed Use
-            [
-    "constant",
+#
+#
+#    4:   #Multi Family Residential
+#            [
+#    "constant",
 #    "lnsqft",
-    "lnlotsqft",
-    ], 
-
-    27:   #Vacant
-            [
-    "constant",
-    "lnlotsqft",
-    ], 
-}            
+#    "lnlotsqft",
+#    "lnlotsqftunit",
+#    ],
+#
+#
+#    7:   #Industrial
+#            [
+#    "constant",
+#    "lnsqft",
+#    "lnlotsqft",
+#    ],
+#
+#    8:   #Office
+#            [
+#    "constant",
+#    "lnsqft",
+#    "far",
+#    "ln_bldgage",
+#    ],
+#
+#    25:   #Mixed Use
+#            [
+#    "constant",
+#    "lnlotsqft",
+#    ],
+#
+#    27:   #Vacant
+#            [
+#    "constant",
+#    "lnlotsqft",
+#    ],
+}
