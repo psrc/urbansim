0055936e35a6027b42a0eb36290774b7f69a20d0
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-01-12 00:09:20 +0000

    (travis) detraitified
    git-svn-id: https://svn.urbansim.org/trunk@12714 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/configurations/baseyear_cache_configuration.py
+++ opus_core/configurations/baseyear_cache_configuration.py
@@ -12,44 +12,19 @@
 # other acknowledgments.
 #
 
-from enthought.traits.api import ListInt, Str, Trait, Constant, HasStrictTraits
-
-
-class BaseyearCacheConfiguration(HasStrictTraits):
+class BaseyearCacheConfiguration(object):
     """A BaseyearCacheConfiguration provides the configuration information 
     of the directory and years to cache."""
 
-#===============================================================================
-#   Constants
-#===============================================================================
     ALL_YEARS = 'all'
-
-#===============================================================================
-#   Traits
-#===============================================================================
-    existing_cache_to_copy = Str
-    years_to_cache = Trait(ALL_YEARS, ALL_YEARS, ListInt)
-    
-#===============================================================================
-#   Functionality
-#===============================================================================
 
     def __init__(self, existing_cache_to_copy, years_to_cache=ALL_YEARS):
         self.existing_cache_to_copy = existing_cache_to_copy
         self.years_to_cache = years_to_cache
         
         
-#===============================================================================
-#   Events
-#===============================================================================
-    pass
-        
-
 import os
 from opus_core.tests import opus_unittest
-
-from enthought.traits.api import TraitError
-
 
 class DatabaseConfigurationTests(opus_unittest.OpusTestCase):
     def setUp(self):
@@ -78,34 +53,6 @@ class DatabaseConfigurationTests(opus_unittest.OpusTestCase):
         
         self.assertEqual(bcc.existing_cache_to_copy, expected_existing_cache_to_copy)
         self.assertEqual(bcc.years_to_cache, expected_years_to_cache)
-        
-        try:
-            bcc.years_to_cache = 'a'
-        except TraitError:
-            pass
-        else:
-            self.fail("The years_to_cache accepted something other than a"
-                " list of integers or BaseyearCacheConfiguration.ALL_YEARS!")
-
-        try:
-            bcc.years_to_cache = BaseyearCacheConfiguration.ALL_YEARS
-        except TraitError:
-            self.fail("The years_to_cache did not accept "
-                "BaseyearCacheConfiguration.ALL_YEARS")
-            
-
-        try:
-            bcc.years_to_cache = ['a', 1900]
-        except TraitError:
-            pass
-        else:
-            self.fail('The years_to_cache accepted a list with '
-                'non-integers!')
-
-        try:
-            bcc.years_to_cache = [1, 1900]
-        except TraitError:
-            self.fail('The years_to_cache did not accept a list of integers!')
 
 
 if __name__=='__main__':
