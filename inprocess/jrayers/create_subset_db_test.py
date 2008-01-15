cda0f21ae16db24e96c37a86ab92a092ea56a5c2
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-01-15 18:24:11 +0000

    (hana) header added in order to turn the traffic light to green
    git-svn-id: https://svn.urbansim.org/trunk@12764 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/jrayers/create_subset_db_test.py
+++ inprocess/jrayers/create_subset_db_test.py
@@ -1,3 +1,17 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
 from opus_core.database_management.database_server import DatabaseServer
 from opus_core.database_management.database_server_configuration import DatabaseServerConfiguration
 dbconfig = DatabaseServerConfiguration()
@@ -89,5 +103,5 @@ for i in queries:
     print 'Finished:  %s' % (i)
 
 
-
+ 
 
