22299e1b2b77baf858995bf4b8f4da7e40f5927d
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-01-28 18:39:17 +0000

    (hana) config change
    git-svn-id: https://svn.urbansim.org/trunk@12905 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/configs/baseline_multiple_travel_models_2020.py
+++ psrc_parcel/configs/baseline_multiple_travel_models_2020.py
@@ -18,8 +18,8 @@ class BaselineMultipleTravelModels2020(Baseline):
     multiple_runs=True
     def __init__(self):
         config = Baseline()
-        config['number_of_runs'] = 10
-        #config['seed'] = 9999
+        config['number_of_runs'] = 2
+        config['seed'] = 1
         from psrc.configs.create_travel_model_configuration import create_travel_model_configuration
         travel_model_configuration = create_travel_model_configuration('baseline_travel_model_psrc_fast_hana', 
                                                                        emme2_batch_file='MODELUSim.BAT ..\\triptabs',
@@ -36,11 +36,9 @@ class BaselineMultipleTravelModels2020(Baseline):
         del config['travel_model_configuration']['matrix_variable_map']['bank2']
         del config['travel_model_configuration']['matrix_variable_map']['bank3']
         
-        config['travel_model_configuration']['bm_distribution_files'] = {}
-        config['travel_model_configuration']['bm_distribution_files'][2020] = \
-                {'household': '/Users/hana/bm/psrc_parcel/simulation_results/2020_urbansim_parcel.zone.number_of_households',
-                 'job': '/Users/hana/bm/psrc_parcel/simulation_results/2020_urbansim_parcel.zone.number_of_jobs'
-                 }
+        config['travel_model_configuration']['bm_distribution_file'] = \
+                '/Users/hana/bm/psrc_parcel/simulation_results/bm_parameters'
+                
         config['travel_model_configuration'][2020]['models'] = config['travel_model_configuration'][2020].get('models')
         config['travel_model_configuration'][2020]['models'].append('opus_emme2.models.restore_trip_tables')
         self.merge(config)
