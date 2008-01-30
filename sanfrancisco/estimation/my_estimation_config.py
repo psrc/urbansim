9563f73893c876f40d744234955dbe2ba5ab30f6
pwaddell <pwaddell@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-01-30 01:38:12 +0000

    updated specifications
    git-svn-id: https://svn.urbansim.org/trunk@12925 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/estimation/my_estimation_config.py
+++ sanfrancisco/estimation/my_estimation_config.py
@@ -19,7 +19,7 @@ from opus_core.configurations.baseyear_cache_configuration import BaseyearCacheC
 from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
 
 my_configuration = {
-     'cache_directory':r'/home/lmwang/work/sf/cache_source_0125', ### TODO: Set this cache_directory to something useful.
+     'cache_directory':r'e:/urbansim_cache/sanfrancisco/cache_source', ### TODO: Set this cache_directory to something useful.
     'input_configuration': DatabaseConfiguration(
         database_name = "sanfrancisco_baseyear",
         ),
@@ -29,7 +29,7 @@ my_configuration = {
         package_order_exceptions={},
         ),                          
     'output_configuration': DatabaseConfiguration(
-        database_name = "sanfrancisco_baseyear_change_20080115",
+        database_name = "sanfrancisco_baseyear_change_20080125",
         ),
     'dataset_pool_configuration': DatasetPoolConfiguration(
         package_order=['sanfrancisco', 'urbansim', 'opus_core'],
