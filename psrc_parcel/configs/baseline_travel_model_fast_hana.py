40f91d3d8227468e9850c40e474eac9c89ce150d
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-05 01:57:52 +0000

    (hana) config changes
    git-svn-id: https://svn.urbansim.org/trunk@12958 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/configs/baseline_travel_model_fast_hana.py
+++ psrc_parcel/configs/baseline_travel_model_fast_hana.py
@@ -32,7 +32,7 @@ class BaselineTravelModelFastHana(Baseline):
         from psrc.configs.create_travel_model_configuration import create_travel_model_configuration
         travel_model_configuration = create_travel_model_configuration('baseline_travel_model_psrc_fast_hana', 
                                                                        emme2_batch_file='MODELUSim.BAT ..\\triptabs',
-                                                                       mode='full', 
+                                                                       mode='skims', 
                                                                        years_to_run={2005: '2005_06', 2010: '2010_06', 2015: '2010_06'})
         config['travel_model_configuration'] = travel_model_configuration
         config['travel_model_configuration']['locations_to_disaggregate'] = ['parcel', 'building']
@@ -44,6 +44,8 @@ class BaselineTravelModelFastHana(Baseline):
         del config['travel_model_configuration']['matrix_variable_map']['bank2']
         del config['travel_model_configuration']['matrix_variable_map']['bank3']
         
+        config['travel_model_configuration']['export_macros']['get_link_attributes.mac'] = {'bank':'bank1', 'scenario':-1, 'path':'export_macros'}
+        config['travel_model_configuration']['node_matrix_variable_map'] = {"attr_on_links.rpt": {"timau": "am_pk_travel_time", "len": "distance"}}
         config['travel_model_configuration'][2015]['models'] = list(config['travel_model_configuration'][2015].get('models'))
         config['travel_model_configuration'][2015]['models'].append('opus_emme2.models.restore_trip_tables')
              
