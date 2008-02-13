3ecd0655ecf8952fe06584e28145b4493f0356c3
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-13 23:53:59 +0000

    (lmwang)constrained hlcm estimation for PSRC w/ submarket
    git-svn-id: https://svn.urbansim.org/trunk@13036 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ inprocess/lmwang/price/my_estimation_config.py
@@ -0,0 +1,37 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+import os
+
+from opus_core.database_management.database_configuration import DatabaseConfiguration
+from opus_core.services.run_server.generic_option_group import GenericOptionGroup
+from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
+from opus_core.configurations.baseyear_cache_configuration import BaseyearCacheConfiguration
+
+from urbansim.configurations.creating_baseyear_cache_configuration import CreatingBaseyearCacheConfiguration
+
+my_configuration = {
+    'cache_directory' : '/urbansim_cache/psrc_parcel/estimation/', # change or leave out
+    'output_configuration': DatabaseConfiguration(
+        database_name = 'psrc_2005_parcel_baseyear_change_20071011',
+        ),
+    'dataset_pool_configuration': DatasetPoolConfiguration(
+        package_order=['psrc_parcel', 'psrc', 'urbansim_parcel', 'urbansim', 'opus_core'],
+        package_order_exceptions={},
+        ),
+    'datasets_to_cache_after_each_model':[],
+    'low_memory_mode':False,
+    'base_year': 2000,
+    'years': (2000,2000),                    
+    }
