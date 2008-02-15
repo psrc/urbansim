5ca9cb477dc829a38de563bd2de778ddc84ed59f
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-15 22:38:43 +0000

    (hana) variables for commutes and nodes of travel data added
    git-svn-id: https://svn.urbansim.org/trunk@13059 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/node_travel_data/aliases.py
@@ -0,0 +1,19 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+                   
+aliases = [
+           "am_single_vehicle_to_work_travel_time = node_travel_data.am_pk_travel_time*1",
+           "single_vehicle_to_work_travel_distance = node_travel_data.distance*1"
+           ]
