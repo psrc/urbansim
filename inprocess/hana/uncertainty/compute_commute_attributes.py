f6345ae41034bc09e9366742169ce982dfca0057
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-16 02:10:25 +0000

    (hana) header added
    git-svn-id: https://svn.urbansim.org/trunk@13066 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/hana/uncertainty/compute_commute_attributes.py
+++ inprocess/hana/uncertainty/compute_commute_attributes.py
@@ -1,3 +1,18 @@
+#
+# Opus software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+
 directory = '/Users/hana/urbansim_cache/psrc/parcel/bm/emme2_on_observed_data/emme_run_1_2008_02_11_18_55/2006'
 #directory = '/Users/hana/urbansim_cache/psrc/parcel/bm/emme2_on_observed_data/full_tm/emme_run_1_2008_02_11_18_26/2006'
 export_directory = '/Users/hana/bm/psrc_parcel/simulation_results/fast_run/'
