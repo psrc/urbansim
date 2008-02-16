d725b51c85e5e4fbaf161b2b09e0db4360da2249
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-16 02:03:28 +0000

    (hana) header added and some changes
    git-svn-id: https://svn.urbansim.org/trunk@13065 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/hana/uncertainty/analyse_tt.py
+++ inprocess/hana/uncertainty/analyse_tt.py
@@ -18,8 +18,11 @@ if __name__ == "__main__":
     try: import wingdbstub
     except: pass
     
-    cache_directory = "/Users/hana/urbansim_cache/psrc/parcel/bm/runs_with_viaduct/run_5264_second_weight/emme_run_1_2008_02_14_12_22"
+    #cache_directory = "/Users/hana/urbansim_cache/psrc/parcel/bm/runs_with_viaduct/run_5264_second_weight/emme_run_1_2008_02_14_12_22"
+    cache_directory = "/Users/hana/urbansim_cache/psrc/parcel/bm/runs_without_viaduct/run_5263_highest_weight/emme_run_1_2008_02_15_13_00"
+    #prefix = 'viad_'
+    prefix = 'no_viad_'
     
     mr = MultipleRuns(cache_directory, package_order=['psrc_parcel', 'urbansim_parcel', 'urbansim', 'opus_core'])
     commutes = mr.get_datasets_from_multiple_runs(2021, ['commute_travel_data.sim_am_pk_travel_time', 'commute_travel_data.am_pk_travel_time'], 'commute_travel_data', name_attribute='name')
-    mr.export_values_from_mr('/Users/hana/bm/psrc_parcel/simulation_results/tt_2020')
+    mr.export_values_from_mr('/Users/hana/bm/psrc_parcel/simulation_results/tt_2020', prefix = prefix)
--- inprocess/hana/uncertainty/compute_and_flush_commute_travel_time.py
+++ inprocess/hana/uncertainty/compute_and_flush_commute_travel_time.py
@@ -1,3 +1,17 @@
+#
+# Opus software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
 import os
 from opus_core.storage_factory import StorageFactory
 from opus_core.variables.attribute_type import AttributeType
@@ -6,13 +20,24 @@ from numpy.random import normal
 from numpy import log, exp
 
 base_commute_directory = '/Users/hana/urbansim_cache/psrc/parcel/bm/emme2_on_observed_data'
-base_emme_dir = '/Users/hana/urbansim_cache/psrc/parcel/bm/runs_with_viaduct/run_5264_second_weight'
-emme_dirs = ['emme_run_1_2008_02_14_12_22', 'emme_run_2_2008_02_14_14_15', 'emme_run_3_2008_02_14_16_01',
-             'emme_run_4_2008_02_14_17_46', 'emme_run_5_2008_02_14_19_37', 'emme_run_6_2008_02_14_21_26',
-             'emme_run_7_2008_02_14_23_06', 'emme_run_8_2008_02_15_00_45', 'emme_run_9_2008_02_15_02_24', 
-             'emme_run_10_2008_02_15_04_02', 'emme_run_11_2008_02_15_05_44', 'emme_run_12_2008_02_15_07_23',
-             'emme_run_13_2008_02_15_09_02'
-             ]
+all_emme_dirs = {'with_viaduct': {'/Users/hana/urbansim_cache/psrc/parcel/bm/runs_with_viaduct/run_5264_second_weight':
+                                  ['emme_run_1_2008_02_14_12_22', 'emme_run_2_2008_02_14_14_15', 'emme_run_3_2008_02_14_16_01',
+                                   'emme_run_4_2008_02_14_17_46', 'emme_run_5_2008_02_14_19_37', 'emme_run_6_2008_02_14_21_26',
+                                   'emme_run_7_2008_02_14_23_06', 'emme_run_8_2008_02_15_00_45', 'emme_run_9_2008_02_15_02_24', 
+                                   'emme_run_10_2008_02_15_04_02', 'emme_run_11_2008_02_15_05_44', 'emme_run_12_2008_02_15_07_23',
+                                   'emme_run_13_2008_02_15_09_02'
+                                   ]
+                                  },
+             'without_viaduct': {
+                            '/Users/hana/urbansim_cache/psrc/parcel/bm/runs_without_viaduct/run_5263_highest_weight':
+                                [
+                                 'emme_run_1_2008_02_15_13_00', 'emme_run_1_2008_02_15_13_15'
+                                 ]
+                                 }
+             }
+which_dirs = 'without_viaduct'
+
+base_emme_dir, emme_dirs = all_emme_dirs[which_dirs].items()[0]
 for i in range(len(emme_dirs)):
     emme_dirs[i] = os.path.join(base_emme_dir, emme_dirs[i], str(2021))
 
