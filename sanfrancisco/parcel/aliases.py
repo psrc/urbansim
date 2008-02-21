9e4054d8421293be2f04056fcc7cbc4eb097ac6c
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-02-21 03:02:06 +0000

    (lmwang)added variables for parcel
    git-svn-id: https://svn.urbansim.org/trunk@13094 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/parcel/aliases.py
@@ -0,0 +1,23 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+       "number_of_businesses = parcel.aggregate(sanfrancisco.building.number_of_businesses)",
+       "employment = parcel.aggregate(sanfrancisco.building.employment)",
+       "number_of_households = parcel.aggregate(sanfrancisco.building.number_of_households)",
+       "population = parcel.aggregate(sanfrancisco.building.population)", 
+           ]
