fc10d72d6e0ab30e9fc7bf50f28218ee4977e358
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-01 01:47:38 +0000

    (hana) more examples
    git-svn-id: https://svn.urbansim.org/trunk@13163 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/examples/create_specification.py
+++ opus_core/examples/create_specification.py
@@ -34,5 +34,61 @@ def create_simple_regression_specification():
                                           )
     return specification
 
+def create_simple_specification_from_dictionary():
+    """Corresponds to the specification in 'create_simple_regression_specification'."""
+    spec = {
+       -2: [ # use always -2, if there are no submodels
+           ('constant', 'b_0'),
+           ('dataset.some_primary_attribute', 'b_1'),
+           ('my_variable = log(package.dataset.variable1*package.dataset.variable2)', 'b_2')
+           ]     
+            }
+    return EquationSpecification(specification_dict=spec)
+
+def create_specification_with_multiple_submodels():
+    """It has 2 submodels and a fixed value for cofficient 'bias'."""
+    spec = {
+       1: [
+           'constant',
+           ('dataset.some_primary_attribute', 'b_1'),
+           ('my_variable = log(package.dataset.variable1*package.dataset.variable2)', 'b_2')
+           ],
+       5: [
+           'constant',
+           'package.dataset.variable1',
+           ('bias', 'bias', 1) 
+           ]
+            }
+    return EquationSpecification(specification_dict=spec)
+
+def create_specification_with_definition():
+    """All variables are defined in a preamble. They are then further referenced only by aliases.
+    """
+    spec = {
+        '_definition_': [
+                         ('dataset.some_primary_attribute', 'b_1'),
+                         ('my_variable = log(package.dataset.variable1*package.dataset.variable2)', 'b_2'),
+                         'package.dataset.variable1',
+                         'var2 = log(package.dataset.variable2*1000)',
+                         ('bias', 'bias', 1)
+                         ],
+       1: [
+           'constant',
+           'some_primary_attribute',
+           'my_variable',
+            'var2'
+           ],
+       5: [
+           'constant',
+           'variable1',
+           'bias'
+           ]
+            }
+    return EquationSpecification(specification_dict=spec)
+
 if __name__ == "__main__":
     create_simple_regression_specification().summary()
+    create_simple_specification_from_dictionary().summary()
+    create_specification_with_multiple_submodels().summary()
+    create_specification_with_definition().summary()
+    
