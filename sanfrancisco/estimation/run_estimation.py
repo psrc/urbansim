cc6ffdbea93201ac36b9d35b0a0c197bd1ab0229
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-07 21:30:19 +0000

    (hana) estimation scripts adopted to the new Estimator
    git-svn-id: https://svn.urbansim.org/trunk@13204 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/estimation/run_estimation.py
+++ sanfrancisco/estimation/run_estimation.py
@@ -26,67 +26,38 @@
 # other acknowledgments.
 # 
 
-from urbansim.estimation.estimator import Estimator
-from urbansim.estimation.estimator import update_controller_by_specification_from_module
-from urbansim.estimation.estimator import plot_utility_diagnose
-from urbansim.estimation.estimator import plot_correlation_diagnose
+##!!!!!!!!!!!
+# Calling this script is equivalent to do the following from the command line (e.g. for nonresidential BLDGLCM):
+#
+# python urbansim/tools/start_estimation.py -c sanfrancisco.configs.baseline_estimation --model=building_location_choice_model --group=nonresidential -s sanfrancisco.estimation.bldglcm_specification --save-results
+#
+# Make you private changes in sanfrancisco.configs.baseline_estimation.
+#!!!!!!!!!!!!
 
-class EstimationRunner(object):
+from sanfrancisco.estimation.my_estimation_config import my_configuration
+from urbansim.estimation.estimation_runner import EstimationRunner
+from sanfrancisco.configs.baseline import Baseline
+from sanfrancisco.configs.config_changes_for_estimation import ConfigChangesForEstimation
 
-    def run_estimation(self, estimation_config, model, save_estimation_results=True, diagnose=False):
-        self.model = model
-        self.spec_file = "%s_specification" % model[0].lower()
-        ### TODO: Re-write this
-        type = None
-        add_member_prefix = False
-        if len(model) > 2:
-            type = model[2]
-            if len(model) > 3:
-                add_member_prefix = model[3]
-        if type is None:
-            exec("from sanfrancisco.estimation.%s_estimation_config import run_configuration" % model[0].lower())
-            run_configuration = update_controller_by_specification_from_module(
-                                run_configuration, model[1],
-                                "sanfrancisco.estimation.%s" % self.spec_file)
-        else:
-            exec("from sanfrancisco.estimation.%s_estimation_config import %s_configuration as config" % (model[0].lower(),
-                  model[0].lower()))
-            conf = config(type, add_member_prefix)
-            run_configuration = conf.get_configuration()
-            run_configuration = conf.get_updated_configuration_from_module(
-                                     run_configuration, "sanfrancisco.estimation.%s" % self.spec_file)
-
-        if diagnose and self.model[0] != 'REPM':  #only works for LCM and derivatives
-            model_name = model[1]
-            #if type is not None:
-                #model_name = "%s_%s" % (type, model_name)
-            run_configuration["models_configuration"][model_name]["controller"]["estimate"]["arguments"]["procedure"]=\
-                "'opus_core.bhhh_mnl_estimation_with_diagnose'"
-                
-        run_configuration.replace(estimation_config)
-        self.estimator = Estimator(run_configuration, save_estimation_results=save_estimation_results)
-        self.estimator.estimate()
-
-    def reestimate(self, submodels=None):
-        if len(self.model) > 2:
-            self.estimator.reestimate(self.spec_file, type=self.model[2], submodels=submodels)
-        else:
-            self.estimator.reestimate(self.spec_file, submodels=submodels)
-    
-    def plot_utility(self, submodel=-2):
-        plot_utility_diagnose('util_submodel_%s' % submodel)
-        
-    def plot_correlation(self, submodel=-2):
-        plot_correlation_diagnose('correlation_submodel_%s' % submodel)
+models = {
+          'hlcm': ['household_location_choice_model', 'sanfrancisco.estimation.hlcm_specification', None],
+          'blcm': ['business_location_choice_model', 'sanfrancisco.estimation.blcm_specification', None],
+          'repm': ['real_estate_price_model', 'sanfrancisco.estimation.repm_specification', None],
+          "BLDGLCM-nonres": ["building_location_choice_model", 'sanfrancisco.estimation.bldglcm_specification', "nonresidential"],
+          "BLDGLCM-res": ["building_location_choice_model", 'sanfrancisco.estimation.bldglcm_specification', "residential"],
+          }
 
 if __name__ == '__main__':
-#    model = ("REPM", "real_estate_price_model")
-#    model = ("HLCM", "household_location_choice_model")
-#    model = ("BLCM", "business_location_choice_model")
-#    model = ("BLDGLCM", "building_location_choice_model", "nonresidential", False)
-    model = ("BLDGLCM", "building_location_choice_model", "residential", False)
+#    model = 'repm'
+#    model = 'hlcm'
+#    model = 'blcm'
+#    model = "BLDGLCM-nonres"
+    model = "BLDGLCM-res"
 
-    from my_estimation_config import my_configuration
-    er = EstimationRunner()
-    er.run_estimation(my_configuration, model, save_estimation_results=True, diagnose=False)
+    config = Baseline()
+    config.merge(my_configuration)
+    config['config_changes_for_estimation'] = ConfigChangesForEstimation()
+    er = EstimationRunner(models[model][0], specification_module=models[model][1], model_group=models[model][2],
+                           configuration=config, save_estimation_results=True)
+    er.estimate()
     
