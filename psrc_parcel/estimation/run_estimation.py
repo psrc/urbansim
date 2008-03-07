c3aadbf760faba9120a82f772afb2e120b0ae09a
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-07 21:29:37 +0000

    (hana) estimation scripts adopted to the new Estimator
    git-svn-id: https://svn.urbansim.org/trunk@13203 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/estimation/run_estimation.py
+++ psrc_parcel/estimation/run_estimation.py
@@ -12,80 +12,45 @@
 # other acknowledgments.
 # 
 
-from urbansim.estimation.estimator import Estimator
-from urbansim.estimation.estimator import update_controller_by_specification_from_module
-from urbansim.estimation.estimator import plot_utility_diagnose
-from urbansim.estimation.estimator import plot_correlation_diagnose
+##!!!!!!!!!!!
+# Calling this script is equivalent to do the following from the command line (e.g. for non-home-based ELCM):
+#
+# python urbansim/tools/start_estimation.py -c psrc_parcel.configs.baseline_estimation --model=employment_location_choice_model --group=non_home_based -s psrc_parcel.estimation.elcm_specification
+#
+# Make you private changes in psrc_parcel.configs.baseline_estimation.
+#!!!!!!!!!!!!
+
+
+from urbansim.estimation.estimation_runner import EstimationRunner
 from psrc_parcel.configs.baseline import Baseline
+from urbansim_parcel.configs.config_changes_for_estimation import ConfigChangesForEstimation
+from psrc_parcel.estimation.my_estimation_config import my_configuration
 
-class EstimationRunner(object):
-
-    def run_estimation(self, estimation_config, model, save_estimation_results=True, diagnose=False):
-        self.model = model
-        self.spec_file = "%s_specification" % model[0].lower()
-        ### TODO: Re-write this
-        type = None
-        add_member_prefix = False
-        baseline_config = Baseline()
-        if len(model) > 2:
-            type = model[2]
-            if len(model) > 3:
-                add_member_prefix = model[3]
-        if type is None:
-            exec("from urbansim_parcel.estimation.%s_estimation_config import run_configuration" % model[0].lower())
-            run_configuration["models_configuration"][self.model[1]]["controller"].replace(baseline_config["models_configuration"][self.model[1]]["controller"])
-            run_configuration = update_controller_by_specification_from_module(
-                                run_configuration, model[1],
-                                "psrc_parcel.estimation.%s" % self.spec_file)
-        else:
-            exec("from urbansim_parcel.estimation.%s_estimation_config import %s_configuration as config" % (model[0].lower(),
-                  model[0].lower()))
-            conf = config(type, add_member_prefix)
-            run_configuration = conf.get_configuration()
-            run_configuration["models_configuration"][conf.model_name]["controller"].replace(
-                                                                 baseline_config["models_configuration"][conf.model_name]["controller"])
-            run_configuration = conf.get_updated_configuration_from_module(
-                                     run_configuration, "psrc_parcel.estimation.%s" % self.spec_file)
-
-        if diagnose and self.model[0] != 'REPM':  #only works for LCM and derivatives
-            model_name = model[1]
-            #if type is not None:
-                #model_name = "%s_%s" % (type, model_name)    
-            procedure_name = run_configuration["models_configuration"][model_name]["controller"]["estimate"]["arguments"].get('procedure',
-                                                                                                            "'opus_core.bhhh_mnl_estimation'")
-            run_configuration["models_configuration"][model_name]["controller"]["estimate"]["arguments"]["procedure"]= "'%s_with_diagnose'" % \
-                procedure_name[1:(len(procedure_name)-1)]
-                
-        run_configuration.replace(estimation_config)
-        self.estimator = Estimator(run_configuration, save_estimation_results=save_estimation_results)
-        self.estimator.estimate()
-
-    def reestimate(self, submodels=None):
-        if len(self.model) > 2:
-            self.estimator.reestimate(self.spec_file, type=self.model[2], submodels=submodels)
-        else:
-            self.estimator.reestimate(self.spec_file, submodels=submodels)
-    def save_results(self):
-        self.estimator.save_results()
-        
-    def plot_utility(self, submodel=-2):
-        plot_utility_diagnose('util_submodel_%s' % submodel)
-        
-    def plot_correlation(self, submodel=-2):
-        plot_correlation_diagnose('correlation_submodel_%s' % submodel)
+models = {
+          'hlcm': ['household_location_choice_model', 'psrc_parcel.estimation.hlcm_specification', None],
+          'elcm-non-home-based': ['employment_location_choice_model', 'psrc_parcel.estimation.elcm_specification', 'non_home_based'],
+          'elcm-home_based': ['employment_location_choice_model', 'psrc_parcel.estimation.elcm_specification', 'home_based'],
+          'repm': ['real_estate_price_model', 'psrc_parcel.estimation.repm_specification', None],
+          'wcm' : ['workplace_choice_model_for_resident', 'wcm_specification', None],
+          'hbjcm': ['home_based_job_choice_model', 'hbjcm_specification', None]
+          }
 
 if __name__ == '__main__':
     try: import wingdbstub
     except: pass
-    #model = ("REPM", "real_estate_price_model")
-    #model = ("HLCM", "household_location_choice_model")
-    #model = ("ELCM", "employment_location_choice_model", "home_based", True)
-    #model = ("ELCM", "employment_location_choice_model", "non_home_based", False)
-    model = ("WCM", "workplace_choice_model_for_resident")
-    #model = ("HBJCM", "home_based_job_choice_model")
+    model = 'hlcm'
+    #model = 'elcm-non-home-based'
+    #model = 'elcm-home_based'
+    #model = 'repm'
+    #model = 'wcm'
+    #model = 'hbjcm'
 
-
-    from my_estimation_config import my_configuration
-    er = EstimationRunner()
-    er.run_estimation(my_configuration, model, save_estimation_results=False, diagnose=False)
+    config = Baseline()
+    if 'models_in_year' in config.keys():
+        del config['models_in_year']    
+    config.merge(my_configuration)
+    config['config_changes_for_estimation'] = ConfigChangesForEstimation()
+    er = EstimationRunner(models[model][0], specification_module=models[model][1], model_group=models[model][2],
+                           configuration=config, save_estimation_results=False)
+    er.estimate()
     
