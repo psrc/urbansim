f610a8e2f1482644c6191ecac9999cdaf4bb6707
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-07 01:25:54 +0000

    (hana) files to be used with new version of estimator runner
    git-svn-id: https://svn.urbansim.org/trunk@13191 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/configs/baseline_estimation.py
@@ -0,0 +1,67 @@
+#
+# UrbanSim software. Copyright (C) 1998-2004 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from baseline import Baseline
+import os
+from opus_core.database_management.database_configuration import DatabaseConfiguration
+from urbansim.configurations.creating_baseyear_cache_configuration import CreatingBaseyearCacheConfiguration
+from opus_core.configurations.baseyear_cache_configuration import BaseyearCacheConfiguration
+from opus_core.configurations.dataset_pool_configuration import DatasetPoolConfiguration
+from sanfrancisco.configs.config_changes_for_estimation import ConfigChangesForEstimation
+
+class BaselineEstimation(Baseline):
+
+    def __init__(self):
+        Baseline.__init__(self)
+        self['config_changes_for_estimation'] = ConfigChangesForEstimation()
+        self.merge(self._get_estimation_changes())
+        
+    def _get_estimation_changes(self):
+        return {
+     'cache_directory':r'e:/urbansim_cache/sanfrancisco/cache_source', ### TODO: Set this cache_directory to something useful.
+    'input_configuration': DatabaseConfiguration(
+        database_name = "sanfrancisco_baseyear",
+        ),
+
+    'dataset_pool_configuration': DatasetPoolConfiguration(
+        package_order=['sanfrancisco', 'urbansim', 'opus_core'],
+        package_order_exceptions={},
+        ),                          
+    'output_configuration': DatabaseConfiguration(
+        database_name = "sanfrancisco_baseyear_change_20080125",
+        ),
+    'dataset_pool_configuration': DatasetPoolConfiguration(
+        package_order=['sanfrancisco', 'urbansim', 'opus_core'],
+        package_order_exceptions={},
+        ),     
+    'datasets_to_cache_after_each_model':[],
+    'base_year': 2001,
+    'years': (2001,2001), 
+    'datasets_to_cache_after_each_model':[],
+    'low_memory_mode':False,
+    "datasets_to_preload" : {
+        'zone':{},
+        'household':{},
+        'building':{},        
+        'parcel':{'package_name':'sanfrancisco'},
+        'business':{'package_name':'sanfrancisco'},
+        'person':{'package_name':'sanfrancisco'},        
+        "building_use":{'package_name':'sanfrancisco'},
+        "building_use_classification":{'package_name':'sanfrancisco'},
+        'travel_data':{},
+        'urbansim_constant':{}
+        }
+        }
+
+    
---
+++ sanfrancisco/configs/config_changes_for_estimation.py
@@ -0,0 +1,57 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from opus_core.configuration import Configuration
+
+class ConfigChangesForEstimation(Configuration):
+
+    def __init__(self):
+        Configuration.__init__(self)
+        estimation_changes = self._get_estimation_changes()
+        self.merge(estimation_changes)
+        
+    def _get_estimation_changes(self):
+        config = {
+            'household_location_choice_model': {
+                        'models': [                                
+                                   {"real_estate_price_model": ["run"]},
+                                   {"household_location_choice_model": ["estimate"]}
+                                   ],                  
+                    },
+            'business_location_choice_model': 
+                        {'models': [
+                                {'business_location_choice_model': ["estimate"]}
+                                                ],
+                        },
+            'real_estate_price_model': {'models': [
+                                {"real_estate_price_model": ["estimate"]}
+                                ]},
+            "building_location_choice_model": {
+                    'nonresidential': {
+                            'models': [
+                                {"building_location_choice_model": {
+                                     "group_members": [{'nonresidential': ["estimate"]}]
+                                         }
+                                }
+                                    ]},
+                    'residential': {
+                            'models': [
+                                {"building_location_choice_model": {
+                                     "group_members": [{'residential': ["estimate"]}]
+                                         }
+                                }
+                                    ]}
+                    }}
+                    
+        return config
