f248aa59baa0c0f58dc4bd4fa1cab982dd48be60
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-14 20:07:46 +0000

    (hana) example of changing controller; modification of LPM specification
    git-svn-id: https://svn.urbansim.org/trunk@13229 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- eugene/configs/baseline_estimation.py
+++ eugene/configs/baseline_estimation.py
@@ -15,6 +15,7 @@
 from baseline import Baseline
 from opus_core.database_management.database_configuration import DatabaseConfiguration
 from urbansim.configs.config_changes_for_estimation import ConfigChangesForEstimation
+from urbansim.configurations.land_price_model_configuration_creator import LandPriceModelConfigurationCreator
 
 class BaselineEstimation(Baseline):
     def __init__(self):
@@ -34,4 +35,7 @@ class BaselineEstimation(Baseline):
         self['base_year'] = 1980
         self['years'] = (1980,1980)
         self['seed'] = 10
-
+        #self['models_configuration']['land_price_model']['controller'] = LandPriceModelConfigurationCreator(
+                                                                                    #estimation_procedure='opus_core.bma_for_linear_regression_r',
+                                                                                    #estimate_config={'bma_imageplot_filename': 'bma_image.pdf'}
+        #                                                                            ).execute()
