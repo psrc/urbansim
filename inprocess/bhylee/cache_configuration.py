a8a2c09033815962a46cf6217fcf5e2f94f70198
bhylee <bhylee@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-31 21:06:33 +0000

    (bhylee) changed cache_configuration
    git-svn-id: https://svn.urbansim.org/trunk@13266 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/bhylee/cache_configuration.py
+++ inprocess/bhylee/cache_configuration.py
@@ -47,7 +47,6 @@ class CacheConfiguration(Configuration):
             'building_types',
             'urbansim_constants',
             'job_building_types',
-            'land_use_types',
             #'land_price_model_coefficients',
             #'land_price_model_specification',]
          ]
