f6382319b01e4af6e2347b56f787e53502c9c046
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-03-31 21:34:07 +0000

    (lmwang)fixed tab
    git-svn-id: https://svn.urbansim.org/trunk@13267 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/person/aliases.py
+++ urbansim_parcel/person/aliases.py
@@ -16,6 +16,6 @@ aliases = ['is_worker = person.employment_status > 0',
            'is_worker_without_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id <= 0)',
            'is_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id > 0)',
            'is_non_home_based_worker_with_job = numpy.logical_and( numpy.logical_and(urbansim_parcel.person.is_worker,  (person.work_at_home!=1)), person.job_id > 0)',
-	   'is_non_home_based_worker_without_job = numpy.logical_and( numpy.logical_and(urbansim_parcel.person.is_worker,  (person.work_at_home!=1)), person.job_id <= 0)',
+           'is_non_home_based_worker_without_job = numpy.logical_and( numpy.logical_and(urbansim_parcel.person.is_worker,  (person.work_at_home!=1)), person.job_id <= 0)',
            'zone_id=person.disaggregate(urbansim_parcel.household.zone_id)',                      
            ]
