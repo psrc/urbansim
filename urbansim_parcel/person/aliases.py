51f4d789613009d5b67fa161ec4143083069f02f
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-04-02 16:25:04 +0000

    removing tabs (?)
    git-svn-id: https://svn.urbansim.org/trunk@13279 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/person/aliases.py
+++ urbansim_parcel/person/aliases.py
@@ -11,11 +11,10 @@
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
 # 
-
 aliases = ['is_worker = person.employment_status > 0',
            'is_worker_without_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id <= 0)',
            'is_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id > 0)',
            'is_non_home_based_worker_with_job = numpy.logical_and( numpy.logical_and(urbansim_parcel.person.is_worker,  (person.work_at_home!=1)), person.job_id > 0)',
            'is_non_home_based_worker_without_job = numpy.logical_and( numpy.logical_and(urbansim_parcel.person.is_worker,  (person.work_at_home!=1)), person.job_id <= 0)',
-           'zone_id=person.disaggregate(urbansim_parcel.household.zone_id)',                      
+           'zone_id=person.disaggregate(urbansim_parcel.household.zone_id)',
            ]
