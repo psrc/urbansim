5c2af7e610c39df47238f8e864a166915d5bb8a5
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-04-03 15:53:55 +0000

    Resetting 2 space tabs to 4 space tabs
    git-svn-id: https://svn.urbansim.org/trunk@13292 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/opus.py
+++ opus_gui/opus.py
@@ -25,50 +25,48 @@ from opus_gui.main.opusgui import *
 
 # Main entry to program.  Set up the main app and create a new window.
 def main(argv):
-
-  # create Qt application
-  app = QApplication(argv,True)
-  
-  # Setting these items allows for saving application state via a QSettings object
-  app.setOrganizationName("CUSPA")
-  app.setOrganizationDomain("urbansim.org")
-  app.setApplicationName("OPUS")
-  
-  # Set the app style
-  app.setStyle(QString("plastique"))
-
-  # QGIS References are removed for the time being...
-  #try:
-  #  # QGIS bindings for mapping functions
-  #  import qgis.core
-  #  # Path to local QGIS install
-  #  qgis_prefix = "/usr/local/qgis_svn"
-  #  # initialize qgis libraries
-  #  qgis.core.QgsApplication.setPrefixPath(qgis_prefix, True)
-  #  qgis.core.QgsApplication.initQgis()
-  #except ImportError:
-  #    print "Unable to import QGIS"
-
-  # create main window
-  wnd = OpusGui()
-  wnd.show()
-
-  # Create signal for app finish
-  app.connect(app, SIGNAL("lastWindowClosed()"), app, SLOT("quit()"))
-
-  # Start the app up
-  retval = app.exec_()
-
-  # QGIS References are removed for the time being...
-  #try:
-  #  import qgis.core
-  #  # We got an exit signal so time to clean up
-  #  qgis.core.QgsApplication.exitQgis()
-  #except ImportError:
-  #  pass
-
-  sys.exit(retval)
+    # create Qt application
+    app = QApplication(argv,True)
+    
+    # Setting these items allows for saving application state via a QSettings object
+    app.setOrganizationName("CUSPA")
+    app.setOrganizationDomain("urbansim.org")
+    app.setApplicationName("OPUS")
+    
+    # Set the app style
+    app.setStyle(QString("plastique"))
+    
+    # QGIS References are removed for the time being...
+    #try:
+    #  # QGIS bindings for mapping functions
+    #  import qgis.core
+    #  # Path to local QGIS install
+    #  qgis_prefix = "/usr/local/qgis_svn"
+    #  # initialize qgis libraries
+    #  qgis.core.QgsApplication.setPrefixPath(qgis_prefix, True)
+    #  qgis.core.QgsApplication.initQgis()
+    #except ImportError:
+    #    print "Unable to import QGIS"
+    
+    # create main window
+    wnd = OpusGui()
+    wnd.show()
+    
+    # Create signal for app finish
+    app.connect(app, SIGNAL("lastWindowClosed()"), app, SLOT("quit()"))
+    
+    # Start the app up
+    retval = app.exec_()
+    
+    # QGIS References are removed for the time being...
+    #try:
+    #  import qgis.core
+    #  # We got an exit signal so time to clean up
+    #  qgis.core.QgsApplication.exitQgis()
+    #except ImportError:
+    #  pass
+    
+    sys.exit(retval)
 
 if __name__ == "__main__":
-  main(sys.argv)
-
+    main(sys.argv)
