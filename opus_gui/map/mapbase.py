1f1132c3cb71f15fc5ff83ef77b22b1bf4c46e87
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-04-09 04:56:50 +0000

    untabify and indent of files in map dir
    git-svn-id: https://svn.urbansim.org/trunk@13347 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/map/mapbase.py
+++ opus_gui/map/mapbase.py
@@ -22,74 +22,74 @@ from qgis.gui import *
 # General system includes
 import sys,string
 
-  
+
 # Main map class for all map related tools...
 class MapBase(object):
-  def __init__(self, parent):
-    self.parent = parent
+    def __init__(self, parent):
+        self.parent = parent
 
-    # create map canvas
-    self.canvas = QgsMapCanvas(self.parent.widgetMap)
-    self.canvas.setCanvasColor(Qt.yellow)
-    self.canvas.enableAntiAliasing(True)
-    self.canvas.useQImageToRender(False)
-    self.canvas.show()
-    self.canvas.parentWin = parent
-    debugString = QString("Finished Loading Canvas...")
-    self.parent.statusbar.showMessage(debugString)
-    
-    # lay our widgets out in the main window
-    self.layout = QVBoxLayout(self.parent.widgetMap)
-    self.layout.addWidget(self.canvas)
+        # create map canvas
+        self.canvas = QgsMapCanvas(self.parent.widgetMap)
+        self.canvas.setCanvasColor(Qt.yellow)
+        self.canvas.enableAntiAliasing(True)
+        self.canvas.useQImageToRender(False)
+        self.canvas.show()
+        self.canvas.parentWin = parent
+        debugString = QString("Finished Loading Canvas...")
+        self.parent.statusbar.showMessage(debugString)
 
-    # Link in the map tools
-    QObject.connect(self.parent.mpActionZoomIn, SIGNAL("triggered()"), self.zoomIn)
-    QObject.connect(self.parent.mpActionZoomOut, SIGNAL("triggered()"), self.zoomOut)
-    QObject.connect(self.parent.mpActionPan, SIGNAL("triggered()"), self.pan)
+        # lay our widgets out in the main window
+        self.layout = QVBoxLayout(self.parent.widgetMap)
+        self.layout.addWidget(self.canvas)
 
-    # Map tools
-    self.toolPan = QgsMapToolPan(self.canvas)
-    self.toolPan.setAction(self.parent.mpActionPan)
-    self.toolZoomIn = QgsMapToolZoom(self.canvas, False)
-    self.toolZoomIn.setAction(self.parent.mpActionZoomIn)
-    self.toolZoomOut = QgsMapToolZoom(self.canvas, True)
-    self.toolZoomOut.setAction(self.parent.mpActionZoomOut)
-    
-    ##### Lets test out a simple map
-    self.layers = []
-    f = "data/st99_d00.shp"
-    f_string = QString(f)
-    info = QFileInfo(QString(f))
-    
-    # create layer
-    layer = QgsVectorLayer(QString(f), info.completeBaseName(), "ogr")
-    
-    if not layer.isValid():
-      # Deal with the error
-      debugString = QString("Error Loading Layer...")
-      self.parent.statusbar.showMessage(debugString)
-      return
-    QgsMapLayerRegistry.instance().addMapLayer(layer)
-    
-    # set extent to the extent of our layer
-    self.canvas.setExtent(layer.extent())
-    
-    # Set the transparency for the layer
-    #layer.setTransparency(190)
-    
-    # set the map canvas layer set
-    cl = QgsMapCanvasLayer(layer)
-    self.layers.insert(0,cl)
-    self.canvas.setLayerSet(self.layers)
+        # Link in the map tools
+        QObject.connect(self.parent.mpActionZoomIn, SIGNAL("triggered()"), self.zoomIn)
+        QObject.connect(self.parent.mpActionZoomOut, SIGNAL("triggered()"), self.zoomOut)
+        QObject.connect(self.parent.mpActionPan, SIGNAL("triggered()"), self.pan)
 
-  # Signal handeler for zoom in button
-  def zoomIn(self):
-    self.canvas.setMapTool(self.toolZoomIn)
+        # Map tools
+        self.toolPan = QgsMapToolPan(self.canvas)
+        self.toolPan.setAction(self.parent.mpActionPan)
+        self.toolZoomIn = QgsMapToolZoom(self.canvas, False)
+        self.toolZoomIn.setAction(self.parent.mpActionZoomIn)
+        self.toolZoomOut = QgsMapToolZoom(self.canvas, True)
+        self.toolZoomOut.setAction(self.parent.mpActionZoomOut)
 
-  # Signal handeler for zoom out button
-  def zoomOut(self):
-    self.canvas.setMapTool(self.toolZoomOut)
+        ##### Lets test out a simple map
+        self.layers = []
+        f = "data/st99_d00.shp"
+        f_string = QString(f)
+        info = QFileInfo(QString(f))
 
-  # Signal handeler for pan button
-  def pan(self):
-    self.canvas.setMapTool(self.toolPan)
+        # create layer
+        layer = QgsVectorLayer(QString(f), info.completeBaseName(), "ogr")
+
+        if not layer.isValid():
+            # Deal with the error
+            debugString = QString("Error Loading Layer...")
+            self.parent.statusbar.showMessage(debugString)
+            return
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+
+        # set extent to the extent of our layer
+        self.canvas.setExtent(layer.extent())
+
+        # Set the transparency for the layer
+        #layer.setTransparency(190)
+
+        # set the map canvas layer set
+        cl = QgsMapCanvasLayer(layer)
+        self.layers.insert(0,cl)
+        self.canvas.setLayerSet(self.layers)
+
+    # Signal handeler for zoom in button
+    def zoomIn(self):
+        self.canvas.setMapTool(self.toolZoomIn)
+
+    # Signal handeler for zoom out button
+    def zoomOut(self):
+        self.canvas.setMapTool(self.toolZoomOut)
+
+    # Signal handeler for pan button
+    def pan(self):
+        self.canvas.setMapTool(self.toolPan)
