f47fa850ea8e34cfc00bf66a01608c3584ff85b6
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-04-23 16:41:46 +0000

    minor updates to allocation model
    git-svn-id: https://svn.urbansim.org/trunk@13502 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/models/allocation_model.py
+++ urbansim_parcel/models/allocation_model.py
@@ -16,36 +16,31 @@ from opus_core.allocation_model import AllocationModel as AM
 from inprocess.jrayers.excel_document import ExcelDocument
 from opus_core.storage_factory import StorageFactory
 from opus_core.datasets.dataset import Dataset
+from opus_core.logger import logger
 
 class AllocationModel(AM):
     def prepare_for_run(self, dataset=None, control_totals=None, excel_path=None, excel_data_info={}, esri_storage_location=None,
                         dataset_name=None, dataset_pool=None):
         
         if control_totals is None:
-            print "geting excel document data"
+            logger.log_status("Getting data from Excel for AllocationModel")
             excel_doc = ExcelDocument()
             excel_doc.open(excel_path)
-            
-            #list_of_column_names = ['year', 'persons']
-            #list_of_ranges = ['d22:d32', 'k22:k32']
+
             control_total_data = excel_doc.get_dict_table_from_column_names_and_ranges(excel_data_info)
             excel_doc.close()
             excel_doc.quit()
             del excel_doc
-            #print control_total_data
             
-            print "getting dict storage"
             dict_storage = StorageFactory().get_storage('dict_storage')
             dict_storage.write_table(table_name = 'control_totals', table_data = control_total_data)
 
-            print "getting control total data as 'dataset'"
             control_totals = Dataset(in_storage = dict_storage, in_table_name = 'control_totals', id_name='year')
 
         if dataset is None:
-            print "getting esri storage"
+            logger.log_status("Getting data from esri_storage for AllocationModel")
             esri_storage = StorageFactory().get_storage('esri_storage', storage_location=esri_storage_location)
                         
-            print "getting hotel data as 'dataset'"
             dataset = Dataset(in_storage=esri_storage, in_table_name=dataset_name, id_name=dataset_name+'_id', dataset_name=dataset_name)
         
         dataset_pool.add_datasets_if_not_included({dataset_name:dataset, control_totals.get_dataset_name():control_totals})
