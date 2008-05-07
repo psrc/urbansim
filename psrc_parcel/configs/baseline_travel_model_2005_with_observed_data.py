f9afdf46dcbc9b573639f467b59183944c27cf03
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-05-07 01:15:50 +0000

    (hana) config updated
    git-svn-id: https://svn.urbansim.org/trunk@13597 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/configs/baseline_travel_model_2005_with_observed_data.py
+++ psrc_parcel/configs/baseline_travel_model_2005_with_observed_data.py
@@ -15,35 +15,28 @@
 from baseline import Baseline
 
 class BaselineTravelModel2005WithObservedData(Baseline):
-    multiple_runs=True
+    multiple_runs=False
     def __init__(self):
         config = Baseline()
         config['number_of_runs'] = 1
         config['seed'] = 1
         from psrc.configs.create_travel_model_configuration import create_travel_model_configuration
-        travel_model_configuration = create_travel_model_configuration('baseline_travel_model_psrc_fast_hana', 
-                                                                       emme2_batch_file='MODELUSim.BAT ..\\triptabs',
-                                                                       mode='full', years_to_run={2005: '2005_06'})
+        travel_model_configuration = create_travel_model_configuration('baseline_travel_model_psrc_2008', 
+                                                                       emme2_batch_file='MODEL1-0.BAT',
+                                                                       mode='full', years_to_run={2005: '2006_v1.0aTG',
+                                                                                                  2010: '2010_v1.0aTG', 
+                                                                                                  2015: '2010_v1.0aTG_2015', 
+                                                                                                  2020: '2020_v1.0aTG'})
         config['travel_model_configuration'] = travel_model_configuration
         
         config['travel_model_configuration']['travel_model_input_file_writer'] = 'inprocess.hana.uncertainty.travel_model_input_file_writer_with_observed_values'
         config['travel_model_configuration']['locations_to_disaggregate'] = ['parcel', 'building']
         
-        ##fast model doesn't have bank2 and bank3; disable macros using them
-        del config['travel_model_configuration']['export_macros']['tazvmt2.mac']
-        del config['travel_model_configuration']['export_macros']['tazvmt3.mac']
-
-        del config['travel_model_configuration']['matrix_variable_map']['bank2']
-        del config['travel_model_configuration']['matrix_variable_map']['bank3']
-        
         config['travel_model_configuration']['export_macros']['get_link_attributes.mac'] = {'bank':'bank1', 'scenario':-1, 'path':'export_macros'}
         config['travel_model_configuration']['node_matrix_variable_map'] = {"bank1": {"attr_on_links.rpt": {"timau": "am_pk_travel_time", "len": "distance"},
                                                                                       "tveham.rpt": {"@tveh": "vehicle_volume"}
                                                                             }}
-                
-        config['travel_model_configuration'][2005]['models'] = config['travel_model_configuration'][2005].get('models')
-        config['travel_model_configuration'][2005]['models'].append('opus_emme2.models.restore_trip_tables')
         self.merge(config)
 
 if __name__ == "__main__":
-    config = BaselineMultipleTravelModels2020()
+    config = BaselineTravelModel2005WithObservedData()
