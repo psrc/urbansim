adf268b637f4a837a228a52a5122bf066b8fafd9
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-12 01:48:18 +0000

    (lmwang)refactored control total dataset
    git-svn-id: https://svn.urbansim.org/trunk@13781 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/datasets/household_control_total_dataset.py
+++ urbansim/datasets/household_control_total_dataset.py
@@ -15,6 +15,13 @@
 from control_total_dataset import ControlTotalDataset
 
 class HouseholdControlTotalDataset(ControlTotalDataset):
-
+    id_name_default = []  
+    #if both self.id_name_default and id_name argument in __init__ is unspecified, 
+    #ControlTotalDataset would use all attributes not beginning with "total"
+    #as id_name
+    in_table_name_default = "annual_household_control_totals"
+    out_table_name_default = "annual_household_control_totals"
+    dataset_name = "household_control_total"
+    
     def __init__(self, *args, **kwargs):
-        ControlTotalDataset(self, what='household', *args, **kwargs)
+        ControlTotalDataset(self, what='household', *args, **kwargs)        
