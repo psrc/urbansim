f77b27b18b288685ae6e7ba9f611763a96a22189
bhylee <bhylee@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-14 00:44:58 +0000

    (bhylee) created new individual worker logsum hbw am from home to work variables
    git-svn-id: https://svn.urbansim.org/trunk@13815 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/household_x_building/workerDDD_logsum_hbw_am_from_home_to_work.py
@@ -0,0 +1,40 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from psrc.abstract_variables.abstract_logsum_variable import abstract_logsum_variable
+from opus_core.misc import unique_values
+from numpy import where, repeat, ones, float32, resize, array
+from numpy import ma
+from opus_core.logger import logger
+
+class workerDDD_logsum_hbw_am_from_home_to_work(abstract_logsum_variable):
+    """logsum_hbw_am_time_from_home_to_work
+       logsum breaks by income:
+           Less than $25K;
+           $25K to $45K;
+           $45 to $75K;
+           More than $75K.
+    """
+    
+    def __init__(self, number):
+        self.default_value = -9
+        self.agent_zone_id = "work%s_workplace_zone_id = household.aggregate((person.worker%s == 1).astype(int32) * urbansim_parcel.person.workplace_zone_id)" % (number, number)
+        self.agent_category_attribute = "(psrc.household.logsum_income_break).astype(int32)"
+        self.location_zone_id = "urbansim_parcel.building.zone_id"
+        self.travel_data_attributes = {1: "travel_data.logsum_hbw_am_income_1", 
+                                       2: "travel_data.logsum_hbw_am_income_2", 
+                                       3: "travel_data.logsum_hbw_am_income_3", 
+                                       4: "travel_data.logsum_hbw_am_income_4" }
+        self.direction_from_home = False
+        abstract_logsum_variable.__init__(self)
