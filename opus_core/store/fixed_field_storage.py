4d2766e9c8127fe73716a7d234cf9ae6cde04003
braden <braden@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-17 21:31:33 +0000

    * Updated fixed field storage    - Writing seems to work, using basic unit test    - Uses printf-like formatting syntax    - Formatting info can be saved with the data, to a separate file, or not at all
    git-svn-id: https://svn.urbansim.org/trunk@13835 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/store/fixed_field_storage.py
+++ opus_core/store/fixed_field_storage.py
@@ -32,25 +32,42 @@ class fixed_field_storage(Storage):
     """
 
     #
-    # Constructor
+    # Static members
+    #
+    
+    class Location:
+        NONE = 0
+        HEADER = 1
+        FILE = 2
+
+
+    #
+    # Constructors
     #
 
     def __init__(self, 
                  storage_location, 
                  file_extension,
-                 write_format_to_header = true,
-                 header_prefix = '#',
-                 data_prefix = ''
-                 line_terminator = '\r\n',
-                 ):
+                 format_location = Location.HEADER,
+                 format_file_extension = 'fmt',
+                 format_prefix = '# ',
+                 data_prefix = '',
+                 line_terminator = '\n'
+                ):
 
         if not file_extension:
             raise ValueError('File extension must be a non-empty string.')
 
+        if not ( format_location == self.Location.NONE   or
+                 format_location == self.Location.HEADER or
+                 format_location == self.Location.FILE ) :
+            raise ValueError('format_location must be a fixed_field_storage.Location value.')
+
         self._output_directory = storage_location
         self._file_extension = file_extension
-        self._write_format_to_header = write_format_to_header
-        self._header_prefix = header_prefix
+        self._format_location = format_location
+        self._format_file_extension = format_file_extension
+        self._format_prefix = format_prefix
         self._data_prefix = data_prefix
         self._line_terminator = line_terminator
 
@@ -68,15 +85,10 @@ class fixed_field_storage(Storage):
                     table_data, 
                     mode = Storage.OVERWRITE,
                     format = None,
-#                    filename = None,
                    ):
 
         if not format:
             raise ValueError('No format supplied, and no default implemented yet.')
-        
-#        if not filename:
-#            filename = table_name
-#        file_path = _get_file_path_for_file(filename)
         
         if mode == Storage.APPEND:
             # Should this re-load the file and append at the data level,
@@ -86,23 +98,35 @@ class fixed_field_storage(Storage):
 
         column_size, column_names = self._get_column_size_and_names(table_data)
 
-        output = open(file_path, 'wb')
+        output = open(self._get_file_path_for_table(table_name), 'wb')
 
-        if self._write_format_to_header:
-            output.write(self._header_prefix + ' format: ' + format + self._line_terminator)
+        if self._format_location == self.Location.HEADER:
+            output.write(self._format_prefix
+                         + format
+                         + self._line_terminator)
+        elif self._format_location == self.Location.FILE:
+            format_output = open(self._get_format_file_path_for_table(table_name), 'wb')
+            format_output.write(format + self._line_terminator)
+            format_output.close()
 
         for row_index in range(column_size):
             row = {}
             for column_name, column_values in table_data.iteritems():
                 row[column_name] = column_values[row_index]
-            output.write(self._data_prefix + (format % row) + self._line_terminator)
+            python_format, target_size = self._convert_format_to_python_format_and_get_target_size(format)
+            formatted_row = python_format % row
+            if len(formatted_row) != target_size:
+                raise ValueError('Input data went over fixed field size.')
+            output.write(self._data_prefix
+                         + formatted_row
+                         + self._line_terminator)
 
         output.close()
 
 
     def load_table(self,
                    table_name,
-                   column_names = ALL_COLUMNS,
+                   column_names = Storage.ALL_COLUMNS,
                    lowercase = True
                   ):
 
@@ -118,9 +142,81 @@ class fixed_field_storage(Storage):
 
 
     #
-    # Private methods
+    # Private utility methods
     #
 
-    def _get_file_path_for_file(self, filename):
-        filename_with_extention = '%s.%s' % (filename, self._file_extension)
+    def _get_file_path_for_table(self, table_name):
+        filename_with_extention = '%s.%s' % (table_name, self._file_extension)
         return os.path.join(self._output_directory, filename_with_extention)
+
+    def _get_format_file_path_for_table(self, table_name):
+        filename_with_extention = '%s.%s' % (table_name, self._format_file_extension)
+        return os.path.join(self._output_directory, filename_with_extention)
+
+    def _convert_format_to_python_format_and_get_target_size(self, format):
+        field_spec_re = re.compile("^(?P<name>.*):(?P<format>[-#0 +]*(?P<length>[1-9]+[0-9]*)(?:\.[1-9]+[0-9]*)?[diouxXeEfFgGcrs])$")
+        python_format = ""
+        target_size = 0
+        field_specs = format.split(',')
+        for field_spec in field_specs:
+            match = field_spec_re.match(field_spec).groupdict()
+            if not match:
+                raise ValueError('Did not understand field spec "' + field_spec + '".')
+            python_format += "%%(%(name)s)%(format)s" % match
+            target_size += int(match['length'])
+        return python_format, target_size
+
+
+#
+# Unit Tests
+#
+
+from opus_core.tests import opus_unittest
+from opus_core.store.storage import TestStorageInterface
+
+import os
+
+from shutil import rmtree
+from tempfile import mkdtemp
+
+
+class TestFixedFieldStorage(TestStorageInterface):
+
+    format = "strings:5s,floats:6.2f,integers: 04i"
+    table_data = {
+                  'integers': array([1,2,-3]),
+                  'strings': array(['one', 'two', 'three']),
+                  'floats': array([1.11,22.2,3.3333])
+                 }
+
+
+    def setUp(self):
+        self.temp_dir = mkdtemp(prefix='opus_core_test_fixed_field_storage')
+        self.storage = fixed_field_storage(
+                            storage_location=self.temp_dir,
+                            file_extension='dat',
+                            format_location=fixed_field_storage.Location.FILE)
+    def tearDown(self):
+        if os.path.exists(self.temp_dir):
+            rmtree(self.temp_dir)
+
+    def test_fixed_field(self):
+        self.storage.write_table(
+            table_name = 'foo',
+            table_data = self.table_data,
+            format = self.format
+                 )
+
+        output = open(self.storage._get_file_path_for_table('foo'), 'r')
+        self.assertEqual(output.readline(), '  one  1.11 001\n')
+        self.assertEqual(output.readline(), '  two 22.20 002\n')
+        self.assertEqual(output.readline(), 'three  3.33-003\n')
+        output.close()
+
+        output = open(self.storage._get_format_file_path_for_table('foo'), 'r')
+        self.assertEqual(output.readline(), self.format + self.storage._line_terminator)
+        output.close()
+
+
+if __name__ == '__main__':
+    opus_unittest.main()
