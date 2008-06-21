23aaab86ae43a887601c00a70925c85de356fd4d
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-21 01:03:10 +0000

    (lmwang)new variables
    git-svn-id: https://svn.urbansim.org/trunk@13893 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim/gridcell/water_gridcells_within_walking_distance.py
@@ -0,0 +1,22 @@
+#
+# UrbanSim software. Copyright (C) 1998-2007 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+from abstract_within_walking_distance import abstract_within_walking_distance
+
+class water_gridcells_within_walking_distance(abstract_within_walking_distance):
+    """Number of gridcells that are fully in water within walking distance of this gridcell"""
+
+    _return_type = "int32"
+    dependent_variable = "is_fully_in_water"
+
