d2c27ff9cc06d7b7bf2c9b4090e8259261d0991a
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-25 23:43:18 +0000

    Some updates in edit_indicator_dialog to help get the update gui to work.  Changes revolve around traversing the dom elements in a more explicit manner, marking things as editable if they were not already editable, and if we mark something aditable making sure that inherited nodes are added back in if the edits force us to loose a node (like renaming an inherited node)
    git-svn-id: https://svn.urbansim.org/trunk@13994 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/results/forms/edit_indicator_dialog.py
+++ opus_gui/results/forms/edit_indicator_dialog.py
@@ -50,26 +50,75 @@ class EditIndicatorDialog(QDialog, Ui_dlgEditIndicator):
 #            package_name = QVariant(package_name)
 #            expression = QVariant(expression)
             
-            self.model.setData(self.selected_index,QVariant(indicator_name),Qt.EditRole)
+            # Keep track of any edits so we can mark the GUI as edited and force a save
+            # as well as make the node editable if it is not already...
+            dirty = False
+
+            # Grab the base node... this is a QDomNode
+            base_node = self.selected_index.internalPointer().node()
+            if not base_node.isNull():
+                # We only want to check out this node if it is of type "element"
+                if base_node.isElement():
+                    domElement = base_node.toElement()
+                    if not domElement.isNull():
+                        # Now we check to see if the tagname is the one we are looking for
+                        name = str(domElement.tagName())
+                        # and more importantly if it has changed... we only update on a changed value
+                        if name != indicator_name:
+                            # This path is to allow us to verify if the node being modified
+                            # is inherited and needs to be added back in
+                            domNodePath = self.model.domNodePath(base_node)
+                            # Actually update the tagname
+                            domElement.setTagName(QString(indicator_name))
+                            # Now search and check if inherited and needs to be added back in to tree
+                            self.model.checkIfInheritedAndAddBackToTree(domNodePath, self.selected_index.parent())
+                            # We have made updates so we need to do the "dirty stuff" later
+                            dirty = True
+
+            # Lets avoid calling setData directly... Will create a new method that will do the above
+            #self.model.setData(self.selected_index,QVariant(indicator_name),Qt.EditRole)
             
             vals = {
                     'package': package_name,
                     'expression': expression
             }
-            base_node = self.selected_index.internalPointer().node()
+
+            # Get the first child node (also a QDomNode) for traversal
             node = base_node.firstChild()
             
-            dirty = False
+            # Only march on if we have non-null nodes
             while not node.isNull():
-                name = str(node.nodeName())
-                if name in vals:
-                    if node.nodeValue() != vals[name]:
-                        node.setNodeValue(vals[name])
-                        print 'element.text: %s; node.nodeValue: %s'%(node.toElement().text(), node.nodeValue())
-                        dirty = True
+                # We only want to check out this node if it is of type "element"
+                if node.isElement():
+                    domElement = node.toElement()
+                    if not domElement.isNull():
+                        # Now we check to see if the tagname is the one we are looking for
+                        name = str(domElement.tagName())
+                        if name in vals:
+                            # We have a match se we need to grab the text node for the element
+                            elementText = str(domElement.text())
+                            # If the text node value has changed we need to update
+                            if elementText != vals[name]:
+                                # We need to grab the text node from the element
+                                if domElement.hasChildNodes():
+                                    children = domElement.childNodes()
+                                    for x in xrange(0,children.count(),1):
+                                        if children.item(x).isText():
+                                            textNode = children.item(x).toText()
+                                            # Finally set the text node value
+                                            textNode.setData(vals[name])
+                                            # We have made this element dirty so we need to mark it all dirty
+                                            dirty = True
+                # Continue to loop through children
                 node = node.nextSibling()
                 
+            # TODO: Should gather all of this into a method in the model to allow for bulk update
             if dirty:    
+                # If we have changed something we need to make sure the node we are editing is marked
+                # as editable since there was no check that the node was editable before allowing
+                # the right click edit option.
+                self.model.makeEditable(base_node)
+                # Flag the model as dirty to prompt for save
                 self.model.markAsDirty()
 
         else:
