96cb934d8e3cfb4db5b17db384774c7721c62b8c
aaronr <aaronr@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-26 05:46:57 +0000

    Ticket #243 - Adding a couple of new buttons for the expression library for adding and removing expressions.  These buttons are not enabled yet.
    git-svn-id: https://svn.urbansim.org/trunk@13996 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/config/generalmanager/all_variables_ui.py
+++ opus_gui/config/generalmanager/all_variables_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'all_variables.ui'
 #
-# Created: Mon Jun 23 21:56:40 2008
+# Created: Wed Jun 25 22:52:20 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -28,19 +28,36 @@ class Ui_AllVariablesGui(object):
         self.widget.setMaximumSize(QtCore.QSize(16777215,45))
         self.widget.setObjectName("widget")
 
-        self.gridlayout1 = QtGui.QGridLayout(self.widget)
-        self.gridlayout1.setObjectName("gridlayout1")
+        self.hboxlayout = QtGui.QHBoxLayout(self.widget)
+        self.hboxlayout.setObjectName("hboxlayout")
 
-        spacerItem = QtGui.QSpacerItem(40,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
-        self.gridlayout1.addItem(spacerItem,0,0,1,1)
+        spacerItem = QtGui.QSpacerItem(441,24,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.hboxlayout.addItem(spacerItem)
+
+        self.addNew = QtGui.QPushButton(self.widget)
+        self.addNew.setEnabled(False)
+        self.addNew.setObjectName("addNew")
+        self.hboxlayout.addWidget(self.addNew)
+
+        self.deleteRow = QtGui.QPushButton(self.widget)
+        self.deleteRow.setEnabled(False)
+        self.deleteRow.setObjectName("deleteRow")
+        self.hboxlayout.addWidget(self.deleteRow)
+
+        self.line = QtGui.QFrame(self.widget)
+        self.line.setFrameShape(QtGui.QFrame.VLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.hboxlayout.addWidget(self.line)
 
         self.saveChanges = QtGui.QPushButton(self.widget)
+        self.saveChanges.setEnabled(False)
         self.saveChanges.setObjectName("saveChanges")
-        self.gridlayout1.addWidget(self.saveChanges,0,1,1,1)
+        self.hboxlayout.addWidget(self.saveChanges)
 
         self.cancelWindow = QtGui.QPushButton(self.widget)
         self.cancelWindow.setObjectName("cancelWindow")
-        self.gridlayout1.addWidget(self.cancelWindow,0,2,1,1)
+        self.hboxlayout.addWidget(self.cancelWindow)
         self.vboxlayout.addWidget(self.widget)
 
         self.retranslateUi(AllVariablesGui)
@@ -49,6 +66,8 @@ class Ui_AllVariablesGui(object):
     def retranslateUi(self, AllVariablesGui):
         AllVariablesGui.setWindowTitle(QtGui.QApplication.translate("AllVariablesGui", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.variableBox.setTitle(QtGui.QApplication.translate("AllVariablesGui", "all_variables", None, QtGui.QApplication.UnicodeUTF8))
+        self.addNew.setText(QtGui.QApplication.translate("AllVariablesGui", "Add New Row", None, QtGui.QApplication.UnicodeUTF8))
+        self.deleteRow.setText(QtGui.QApplication.translate("AllVariablesGui", "Delete Selected Rows", None, QtGui.QApplication.UnicodeUTF8))
         self.saveChanges.setText(QtGui.QApplication.translate("AllVariablesGui", "Save Changes", None, QtGui.QApplication.UnicodeUTF8))
         self.cancelWindow.setText(QtGui.QApplication.translate("AllVariablesGui", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
 
