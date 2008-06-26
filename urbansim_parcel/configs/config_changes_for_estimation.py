1e79a436296e13f5beb128e046e6159cf99033bf
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-06-26 22:07:34 +0000

    (lmwang)added base_year change for hlcm estimation
    git-svn-id: https://svn.urbansim.org/trunk@14003 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/configs/config_changes_for_estimation.py
+++ urbansim_parcel/configs/config_changes_for_estimation.py
@@ -24,6 +24,7 @@ class ConfigChangesForEstimation(USConfigChangesForEstimation):
                                    #{"tour_schedule_model": ["run"]},
                                    {"household_location_choice_model": ["estimate"]},
                                    ],
+                                   'base_year': 2006,                                                   
                                    'years':(2006, 2006),
                                }
         self['employment_location_choice_model'] = {
