3d9784db5b12d948a8071fd209f02526c5ffcb11
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-08-03 06:45:06 +0000

    adding new run management related tests
    git-svn-id: https://svn.urbansim.org/trunk@14189 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- eugene/tests/test_run_manager.py
+++ eugene/tests/test_run_manager.py
@@ -42,7 +42,7 @@ config_template = """<opus_project>
   <scenario_manager>
     <Eugene_baseline copyable="True" executable="True" setexpanded="True" type="scenario">
       <years_to_run config_name="years" type="tuple">
-        <endyear type="integer">1983</endyear>
+        <endyear type="integer">1984</endyear>
       </years_to_run>
       <creating_baseyear_cache_configuration type="class">
         <cache_directory_root parser_action="prefix_with_opus_data_path" type="directory">%s/runs</cache_directory_root>
@@ -104,7 +104,7 @@ class RunManagerTests(opus_unittest.OpusIntegrationTestCase):
             rmtree(self.temp_dir)
         
     def test_restart_simple_run(self):
-        _do_run_simple_test_run(self, self.temp_dir, self.config)
+        _do_run_simple_test_run(self, self.temp_dir, self.config, end_year = 1983)
         runs_manager = RunManager(self.config)
         
         run_activity = runs_manager.services_db.get_table('run_activity')
