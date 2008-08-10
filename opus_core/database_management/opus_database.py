aac7ef0685067c2232b2ab5d3fdd6d39716df47d
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-08-10 00:47:38 +0000

    changing reflect to recurse
    git-svn-id: https://svn.urbansim.org/trunk@14255 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/database_management/opus_database.py
+++ opus_core/database_management/opus_database.py
@@ -72,7 +72,7 @@ class OpusDatabase(object):
             bind = self.engine
         )  
         
-        self.reflect(reflect = False)
+        self.reflect(recurse = False)
     
     def reflect(self, clear = True, recurse = True):
         try:
