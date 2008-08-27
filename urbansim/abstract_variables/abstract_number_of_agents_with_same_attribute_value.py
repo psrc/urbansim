3ad60ea7bfa28d108e55dbe30c1e65e52b48f761
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-08-27 17:01:33 +0000

    (lmwang)optimized abstract_number_of_agents_with_same_attribute_value;added instances of it
    git-svn-id: https://svn.urbansim.org/trunk@14402 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/abstract_variables/abstract_number_of_agents_with_same_attribute_value.py
+++ urbansim/abstract_variables/abstract_number_of_agents_with_same_attribute_value.py
@@ -15,7 +15,7 @@
 from opus_core.variables.variable import Variable
 from opus_core.variables.variable_name import VariableName
 from numpy import where, repeat, ones, float32, resize, array
-from numpy import ma, zeros, unique
+from numpy import ma, zeros, unique, arange
 from urbansim.functions import attribute_label
 from opus_core.logger import logger
 
@@ -33,10 +33,10 @@ class abstract_number_of_agents_with_same_attribute_value(Variable):
     #agent_attribute_unique_values = []  ##optional, specifying unique values in agent_attribute_name
     geography_dataset_name = 'to_be_defined'
     ## TODO: this expression must have an alias with pattern agents_of_attribute_%(agent_attribute_value)s
-    ## once Alan finishes refactoring opus expression to allow function definition, this can be simplied
+    ## once Alan finishes refactoring opus expression to allow function definition, this can be simplified
     expression_agents_of_attribute_by_geography = "'agents_of_attribute_%(agent_attribute_value)s = %(geography_dataset_name)s.aggregate(%(agent_attribute_name)s==%(agent_attribute_value)s)'"
     
-    ## this doesn't need to be change
+    ## this doesn't need to be changed
     expression_agents_of_attribute = "'agents_of_attribute_%(agent_attribute_value)s = %(choice_set_name)s.disaggregate(%(geography_dataset_name)s.agents_of_attribute_%(agent_attribute_value)s)'"
                            
     def dependencies(self):
@@ -45,10 +45,14 @@ class abstract_number_of_agents_with_same_attribute_value(Variable):
     def compute(self, dataset_pool):
         interaction_dataset = self.get_dataset()
         choice_set_name = interaction_dataset.get_dataset(2).get_dataset_name()
-        agent_category_attr = interaction_dataset.get_dataset(1).get_attribute_by_index(self.agent_attribute_name,
-                                                                      interaction_dataset.get_2d_index_of_dataset1())
+        agent_set = interaction_dataset.get_dataset(1)
+        ## TODO: this is a work-around to the problem that a 1d array indexed by None becomes a 2d array (numpy 1.0.4 - 1.1.1 at least)
+        agents_index = interaction_dataset.get_index(1)
+        if agents_index is None:
+            agents_index = arange( agent_set.size() )
+        agent_attr = agent_set.get_attribute_by_index(self.agent_attribute_name, agents_index)
         if not hasattr(self, 'agent_attribute_unique_values') or self.agent_attribute_unique_values is None or len(self.agent_attribute_unique_values)==0:
-            self.agent_attribute_unique_values = unique(agent_category_attr)
+            self.agent_attribute_unique_values = unique(agent_attr)
             
         agents_of_attribute_by_geography = [ eval( self.expression_agents_of_attribute_by_geography % 
                                                      {'agent_attribute_name':self.agent_attribute_name, 
@@ -68,12 +72,9 @@ class abstract_number_of_agents_with_same_attribute_value(Variable):
                                for i in self.agent_attribute_unique_values]
         self.add_and_solve_dependencies(agents_of_attribute, dataset_pool)
 
-        assert agent_category_attr.min() >= 0
-        results = zeros(agent_category_attr.shape, dtype=self._return_type)
-        choices = [results] * ( self.agent_attribute_unique_values.max() + 1 )
+        results = zeros(interaction_dataset.get_2d_index().shape, dtype=self._return_type)
         for i in self.agent_attribute_unique_values:
-            choices[i] = interaction_dataset.get_2d_dataset_attribute("agents_of_attribute_%s" % i)
-
-        results = agent_category_attr.choose(choices)
+            index_agents_of_this_value = where(agent_attr == i)[0]
+            results[index_agents_of_this_value, :] = interaction_dataset.get_2d_dataset_attribute("agents_of_attribute_%s" % i)[index_agents_of_this_value, :]
         
         return results
