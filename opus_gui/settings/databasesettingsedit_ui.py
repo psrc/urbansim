9c1241d5abc80c6a407408a531cdfa2c68002dbd
travis <travis@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-09-22 19:12:04 +0000

    appearance tweaks
    git-svn-id: https://svn.urbansim.org/trunk@14574 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/settings/databasesettingsedit_ui.py
+++ opus_gui/settings/databasesettingsedit_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'databasesettingsedit.ui'
 #
-# Created: Fri Aug 29 21:23:50 2008
+# Created: Mon Sep 22 11:27:21 2008
 #      by: PyQt4 UI code generator 4.3.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,34 +12,36 @@ from PyQt4 import QtCore, QtGui
 class Ui_DatabaseSettingsEditGui(object):
     def setupUi(self, DatabaseSettingsEditGui):
         DatabaseSettingsEditGui.setObjectName("DatabaseSettingsEditGui")
-        DatabaseSettingsEditGui.resize(QtCore.QSize(QtCore.QRect(0,0,576,730).size()).expandedTo(DatabaseSettingsEditGui.minimumSizeHint()))
-
+        DatabaseSettingsEditGui.resize(576, 730)
         self.vboxlayout = QtGui.QVBoxLayout(DatabaseSettingsEditGui)
         self.vboxlayout.setObjectName("vboxlayout")
-
         self.variableBox = QtGui.QGroupBox(DatabaseSettingsEditGui)
         self.variableBox.setObjectName("variableBox")
-
         self.gridlayout = QtGui.QGridLayout(self.variableBox)
         self.gridlayout.setObjectName("gridlayout")
         self.vboxlayout.addWidget(self.variableBox)
-
         self.widget = QtGui.QWidget(DatabaseSettingsEditGui)
-        self.widget.setMaximumSize(QtCore.QSize(16777215,45))
+        self.widget.setMaximumSize(QtCore.QSize(16777215, 45))
         self.widget.setObjectName("widget")
-
         self.hboxlayout = QtGui.QHBoxLayout(self.widget)
         self.hboxlayout.setObjectName("hboxlayout")
-
-        spacerItem = QtGui.QSpacerItem(441,24,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        spacerItem = QtGui.QSpacerItem(441, 24, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.hboxlayout.addItem(spacerItem)
-
         self.saveChanges = QtGui.QPushButton(self.widget)
         self.saveChanges.setEnabled(True)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.saveChanges.sizePolicy().hasHeightForWidth())
+        self.saveChanges.setSizePolicy(sizePolicy)
         self.saveChanges.setObjectName("saveChanges")
         self.hboxlayout.addWidget(self.saveChanges)
-
         self.cancelWindow = QtGui.QPushButton(self.widget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.cancelWindow.sizePolicy().hasHeightForWidth())
+        self.cancelWindow.setSizePolicy(sizePolicy)
         self.cancelWindow.setObjectName("cancelWindow")
         self.hboxlayout.addWidget(self.cancelWindow)
         self.vboxlayout.addWidget(self.widget)
