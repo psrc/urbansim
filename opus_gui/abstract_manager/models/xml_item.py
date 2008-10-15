cc18162f42eb517d3ad80c3506a89a107f3fced3
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-10-15 01:48:02 +0000

    removed the redundant 'row' parameter to init and some clean up
    git-svn-id: https://svn.urbansim.org/trunk@14746 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/abstract_manager/models/xml_item.py
+++ opus_gui/abstract_manager/models/xml_item.py
@@ -18,24 +18,23 @@ from PyQt4.QtXml import QDomNode
 
 
 class XmlItem:
-    def __init__(self,domDocument, node, row, parentOpusDataItem):
+    def __init__(self,domDocument, node, parentOpusDataItem):
         self.domDocument = domDocument
         self.domNode = node
         self.parentOpusDataItem = parentOpusDataItem
         self.childItems = []
 
     def initAsRootItem(self):
-        i = 0
-        for x in xrange(0,self.domNode.childNodes().count(),1):
-            current = self.domNode.childNodes().item(x)
-            if (current.attributes().namedItem(QString("hidden")).isNull() or \
-                current.attributes().namedItem(QString("hidden")).nodeValue() != QString("True")) and \
-                (current.nodeType() == QDomNode.ElementNode):
-                childNode = self.domNode.childNodes().item(x)
-                childItem = XmlItem(self.domDocument,childNode, i , self)
-                self.childItems.append(childItem)
-                i = i + 1
-                childItem.initAsRootItem()
+        '''use this element as a root item and selectivly add its children'''
+        for x in xrange(0, self.domNode.childNodes().count(), 1):
+            child_node = self.domNode.childNodes().item(x).toElement()
+            # do not add nodes (or subtrees) that should be hidden
+            if not child_node.isNull() and \
+                child_node.attribute('hidden').toLower() != QString('true'):
+                child_item = XmlItem(self.domDocument, child_node, self)
+                self.childItems.append(child_item)
+                # descend into childs subtree
+                child_item.initAsRootItem()
 
     def node(self):
         return self.domNode
@@ -43,23 +42,22 @@ class XmlItem:
     def parent(self):
         return self.parentOpusDataItem
 
-    def child(self,i):
+    def child(self, i):
         #print "DataItem.child ", i
         if len(self.childItems) > i:
             return self.childItems[i]
         tryToFind = i+1
         foundSoFar = 0
-        for x in xrange(0,self.domNode.childNodes().count(),1):
+        for x in xrange(0, self.domNode.childNodes().count(), 1):
             current = self.domNode.childNodes().item(x)
-            if (current.attributes().namedItem(QString("hidden")).isNull() or \
-                current.attributes().namedItem(QString("hidden")).nodeValue() != QString("True")) and \
-                (current.nodeType() == QDomNode.ElementNode):
+            if current.isElement() and not \
+                current.toElement().attribute('hidden').toLower() == QString('true'):
                 # We found one
                 foundSoFar = foundSoFar + 1
             if foundSoFar == tryToFind:
                 # We have the one we are looking for
                 childNode = self.domNode.childNodes().item(x)
-                childItem = XmlItem(self.domDocument,childNode, i , self)
+                childItem = XmlItem(self.domDocument, childNode, i , self)
                 self.childItems.append(childItem)
                 return childItem
         return None
