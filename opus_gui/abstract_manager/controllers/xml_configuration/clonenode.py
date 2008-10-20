79690efa28bfffe2433d2cc549f47e92b59e237a
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-10-20 19:08:55 +0000

    added RenameNodeGui based on CloneNodeGui
    git-svn-id: https://svn.urbansim.org/trunk@14803 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/abstract_manager/controllers/xml_configuration/clonenode.py
+++ opus_gui/abstract_manager/controllers/xml_configuration/clonenode.py
@@ -15,7 +15,7 @@
 
 # PyQt4 includes for python bindings to QT
 from PyQt4.QtCore import SIGNAL, QString, Qt
-from PyQt4.QtGui import QDialog
+from PyQt4.QtGui import QDialog, QMessageBox
 
 from opus_gui.abstract_manager.views.ui_clonenode import Ui_CloneNodeGui
 
@@ -49,4 +49,30 @@ class CloneNodeGui(QDialog, Ui_CloneNodeGui):
     def on_cancelXML_released(self):
         # Close things up...
         self.close()
-
+        
+class RenameNodeGui(CloneNodeGui):
+    def __init__(self, xml_controller, node):
+        flags = Qt.WindowTitleHint | Qt.WindowSystemMenuHint | Qt.WindowMaximizeButtonHint
+        CloneNodeGui.__init__(self, xml_controller, None, None, None)
+        
+        # check that we got a valid node
+        if not node.isElement():
+            QMessageBox.warning(self, 'Invalid node',
+                                'Ignoring rename request for non element node.')
+            self.close()
+        
+        # dialog is identical to clone node dialog except for some text strings
+        self.label.setText('Please select a new name')
+        self.createXML.setText('Rename')
+        
+        self.node = node
+        self.xml_controller = xml_controller
+        
+    def on_createXML_released(self):
+        # update the node tag name
+        name = self.newName.text()
+        name = name.replace(' ', '_')
+        element = self.node.toElement()
+        element.setTagName(name)
+        self.xml_controller.model.emit(SIGNAL('layoutChanged()'))
+        self.close()
