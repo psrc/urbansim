24a1148dadee74d1f07ff64f416bfda088b229e9
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-10-21 16:59:51 +0000

    removed unused build_tree_from_root()
    git-svn-id: https://svn.urbansim.org/trunk@14810 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/models_manager/models/xml_model_models.py
+++ opus_gui/models_manager/models/xml_model_models.py
@@ -47,21 +47,7 @@ class XmlModel_Models(XmlModel):
             if element_type == 'configuration':
                 return QVariant(QColor(Qt.darkMagenta))
         return QVariant()
-
-    def build_item_tree_from_root(self):
-        '''override the default tree building to make sure estimate_config 
-        is in top'''
-        child_nodes = [e.tagName() for e in self.child_elements(self.xmlRoot)]
-        # move estimation_config to first slot
-        if QString('estimation_config') in child_nodes:
-            idx = child_nodes.index('estimation_config')
-            child_nodes.insert(0, child_nodes.pop(child_nodes.index(idx)))
-
-        for e in child_nodes:
-            if not e.isNull():
-                item = XmlItem(self.domDocument, e, self._rootItem)
-                item.initAsRootItem()
-                self._rootItem.childItems.append(item)
+    
 
     def insert_model(self, model_element):
         '''inserts a dom element under model_system'''
