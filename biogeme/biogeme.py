bad6641be12bd92d1afb29ca70958913105dd8ad
jmeyer <jmeyer@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-10-24 01:59:25 +0000

    git-svn-id: https://svn.urbansim.org/trunk@14827 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- biogeme/biogeme.py
+++ biogeme/biogeme.py
@@ -1,3 +1,17 @@
+#
+# UrbanSim software. Copyright (C) 2005-2008 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
 # This file was created automatically by SWIG 1.3.29.
 # Don't modify this file, modify the SWIG interface instead.
 # This file is compatible with both classic and new-style classes.
