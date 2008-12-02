e7951ffd896fcab94edb70df51d262a03ecac972
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-12-02 19:10:00 +0000

    added script to autoupdate resource file based on Image folder contents. also added new image cog_missing.png
    git-svn-id: https://svn.urbansim.org/trunk@15083 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_gui/main/views/recompile_resources.py
@@ -0,0 +1,40 @@
+# UrbanSim software. Copyright (C) 2005-2008 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
+# ABOUT THIS SCRIPT
+# This scripts updates the resource files for UrbanSim based on the content
+# of the Images sub folder. It both generates a qrc files and runs pyrcc4 
+# on it. 
+
+import os, sys
+
+if __name__ == "__main__":
+
+    TMPFILE = '_tmp_resource.qrc'
+    OUTFILE = 'opusmain_rc.py'
+
+    files = os.listdir('./Images')
+    images = [f for f in files if f.endswith('.png')]
+    rcfile = open('./%s' %TMPFILE, 'w')
+    rcfile.write('<RCC>\n')
+    rcfile.write('    <qresource prefix="/Images" >\n')
+    for image in images:
+        rcfile.write('        <file>Images/%s</file>\n' %image)
+    rcfile.write('    </qresource>\n')
+    rcfile.write('</RCC>')
+    rcfile.close()
+    print 'Wrote %d images to %s' %(len(images), TMPFILE)
+    os.system('pyrcc4 %s -o %s' %(TMPFILE, OUTFILE))
+    print 'Compiled to %s' %OUTFILE
+    os.remove(TMPFILE)
+
