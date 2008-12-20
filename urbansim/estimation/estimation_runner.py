9d0be56ac566b2f2977451a059ed4ee39ecad20a
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-12-20 00:41:22 +0000

    whitespace cleanup
    git-svn-id: https://svn.urbansim.org/trunk@15241 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/estimation/estimation_runner.py
+++ urbansim/estimation/estimation_runner.py
@@ -1,16 +1,16 @@
 #
 # UrbanSim software. Copyright (C) 2005-2008 University of Washington
-# 
+#
 # You can redistribute this program and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation
 # (http://www.gnu.org/copyleft/gpl.html).
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
-# 
+#
 
 
 from opus_core.configuration import Configuration
@@ -19,28 +19,28 @@ from urbansim.estimation.estimator import plot_utility_diagnose
 from urbansim.estimation.estimator import update_controller_by_specification_from_module, update_controller_by_specification_from_dict
 
 class EstimationRunner(Estimator):
-     
+
     def __init__(self, model, specification_module=None, xml_configuration=None, model_group=None,
                   configuration={}, save_estimation_results=False):
         """
         If 'specification_module' is given, it contains the specification defined as a dictionary in a module.
-        Alternatively, the specification can be passed in an xml format in the 'xml_configuration' argument 
+        Alternatively, the specification can be passed in an xml format in the 'xml_configuration' argument
         (which should be an instance of XMLConfiguration).
         If both of those arguments are None, the specification is taken from the cache.
-        'configuration' is an Opus configuration. 
+        'configuration' is an Opus configuration.
         It can contain an entry 'config_changes_for_estimation' which is a dictionary
         where keys are model names and values are controller changes for that model.
         If 'configuration' is None, it is taken from 'xml_configuration'.
         If xml_configuration is used, and if it has a non-empty expression library, the dictionary representing
         the expression library is added to the configuration under the key 'expression_library'.
-        If save_estimation_results is True, the estimation results are saved in the output configuration 
+        If save_estimation_results is True, the estimation results are saved in the output configuration
         (if given in 'configuration') and in the cache.
         """
         self.specification_module = specification_module
         self.xml_configuration = xml_configuration
         self.model_group = model_group
         self.estimated_model = model
-                    
+
         if configuration is None:
             if self.xml_configuration is None:
                 raise StandardError, "Either dictionary based or XML based configuration must be given."
@@ -48,10 +48,10 @@ class EstimationRunner(Estimator):
         else:
             config = Configuration(configuration)
         config_changes = config.get('config_changes_for_estimation', {})
-        
+
         specification_dict=None
         if self.xml_configuration is not None:
-            specification_dict = self.xml_configuration.get_estimation_specification(model, model_group=model_group)
+            specification_dict = self.xml_configuration.get_estimation_specification(model, model_group)
 
         if model_group is None:
             if model in config_changes.keys():
@@ -81,16 +81,16 @@ class EstimationRunner(Estimator):
                     config["models_configuration"][model_name_in_configuration]["controller"]["prepare_for_estimate"]["arguments"]["specification_dict"] = "spec['%s']" % model_group
                 else:
                     config = update_controller_by_specification_from_dict(config, model, specification_dict[model_group])
-               
+
             config['model_name'] = '%s_%s' % (model_group, model)
-            
+
         Estimator.__init__(self, config, save_estimation_results=save_estimation_results)
 
     def reestimate(self, submodels=None, reload_xml_file=True):
         """Launch a re-estimation without recomputing all variables. 'submodels' is a list or single number of submodels to re-estimate.
         If it is None, all submodels are re-estimated.  If self.xml_configuration is not None and reload_xml_file is True, re-read
-        the contents of the xml configuration file in case it has changed.  This will usually be what you want to do when 
-        using reestimate from the command line -- but not from the GUI (since with the GUI, the XMLConfiguration might have been 
+        the contents of the xml configuration file in case it has changed.  This will usually be what you want to do when
+        using reestimate from the command line -- but not from the GUI (since with the GUI, the XMLConfiguration might have been
         edited but not yet saved back to the file).
         """
         specification_dict=None
@@ -99,9 +99,9 @@ class EstimationRunner(Estimator):
                 self.xml_configuration.initialize_from_xml_file()
             specification_dict = self.xml_configuration.get_estimation_specification(self.estimated_model)
         Estimator.reestimate(self, self.specification_module, specification_dict=specification_dict, type=self.model_group, submodels=submodels)
-        
+
     def plot_utility(self, submodel=-2):
-        """In order to use this method, the estimation must be run with a procedure that creates a file 'util_submodel_x' 
+        """In order to use this method, the estimation must be run with a procedure that creates a file 'util_submodel_x'
             (e.g. with bhhh_mnl_estimation_with_diagnose)
         """
         plot_utility_diagnose('util_submodel_%s' % submodel)
