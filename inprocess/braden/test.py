16a43356f8e212f9e08cdf3d138eaffc462a5875
braden <braden@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2008-12-29 20:22:20 +0000

    Work on xml schema for configs and test script
    git-svn-id: https://svn.urbansim.org/trunk@15284 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/braden/test.py
+++ inprocess/braden/test.py
@@ -24,23 +24,30 @@ class TestXMLSchema(opus_core.tests.opus_unittest.OpusTestCase):
     def setUp(self):
         pass
 
-    def test_test(self):
-        xml_file = open("test.xml","r")
-        xmlschema_file = open("opus_project.xsd","r")
-        self.validate_xml(xml_file, xmlschema_file)
+    def test_configs(self):
+        xmlschema_file = open("inprocess/braden/opus_project.xsd","r")
+        xml_files = []
+        xml_files.append(open("urbansim/configs/urbansim.xml","r"))
+        xml_files.append(open("urbansim_parcel/configs/urbansim_parcel.xml","r"))
+        xml_files.append(open("seattle_parcel/configs/seattle_parcel.xml","r"))
+        self.validate_xml(xml_files, xmlschema_file)
 
-    def validate_xml(self, xml_file, xmlschema_file):
+    def validate_xml(self, xml_files, xmlschema_file):
         if "lxml" in globals():
-            xml_doc = lxml.etree.parse(xml_file)
             xmlschema_doc = lxml.etree.parse(xmlschema_file)
             xmlschema = lxml.etree.XMLSchema(xmlschema_doc)
-            result = xmlschema.validate(xml_doc)
-            if not result:
-                error_log = xmlschema.error_log
-                message = "XML validation error:\n"
-                for error in error_log:
-                    message += "  %s:%s:%s:\n" % (error.filename, str(error.line), str(error.column))
-                    message += "    [%s] %s\n" % (error.level_name, error.message)
+            error = False
+            message = "XML validation error:\n"
+            for xml_file in xml_files:
+                xml_doc = lxml.etree.parse(xml_file)
+                result = xmlschema.validate(xml_doc)
+                if not result:
+                    error = True
+                    error_log = xmlschema.error_log
+                    for error in error_log:
+                        message += "  %s, line %s, col %s:\n" % (error.filename, str(error.line), str(error.column))
+                        message += "    [%s] %s\n" % (error.level_name, error.message)
+            if error:
                 raise Exception(message)
         else:
             sys.stderr.write("lxml not found; skipping xml schema validation test")
