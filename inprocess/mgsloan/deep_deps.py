c76eacbe1a910ca36a2834b9e4ff3311f77db022
mgsloan <mgsloan@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-01-16 00:14:52 +0000

    Added GNU header to dependency graph code
    git-svn-id: https://svn.urbansim.org/trunk@15482 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/mgsloan/deep_deps.py
+++ inprocess/mgsloan/deep_deps.py
@@ -1,3 +1,17 @@
+#
+# UrbanSim software. Copyright (C) 2005-2008 University of Washington
+#
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+#
+
 from opus_core.datasets.dataset import Dataset
 from opus_core.variables.attribute_box import AttributeBox
 from opus_core.storage_factory import StorageFactory
