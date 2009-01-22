da548fe7eb7575e4aa8535ef762b050537215eb7
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-01-22 00:44:50 +0000

    removed splitted import row
    git-svn-id: https://svn.urbansim.org/trunk@15554 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/results_manager/controllers/tabs/results_browser.py
+++ opus_gui/results_manager/controllers/tabs/results_browser.py
@@ -24,8 +24,7 @@ from opus_gui.results_manager.controllers.tabs.view_image_form import ViewImageF
 from opus_gui.results_manager.controllers.tabs.view_table_form import ViewTableForm
 from opus_core.logger import logger
 
-from opus_gui.general_manager.general_manager import get_available_indicator_nodes,\
-    get_available_indicator_names
+from opus_gui.general_manager.general_manager import get_available_indicator_nodes,get_available_indicator_names
 from opus_gui.results_manager.results_manager import get_simulation_runs,\
     get_years_for_simulation_run
 from opus_gui.main.controllers.mainwindow import get_mainwindow_instance
