30de309da728f6b61c92ad75de46e0584bc601e1
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-01-28 19:48:47 +0000

    added get_model_nodes and get_model_names
    git-svn-id: https://svn.urbansim.org/trunk@15596 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/models_manager/models_manager.py
+++ opus_gui/models_manager/models_manager.py
@@ -15,6 +15,28 @@ from opus_gui.models_manager.controllers.tabs.estimation_gui_element import Esti
 from opus_gui.abstract_manager.abstract_manager import AbstractManager
 from opus_gui.models_manager.controllers.xml_configuration.xml_controller_models import XmlController_Models
 
+def get_model_nodes(project):
+    '''
+    Get a list of all model nodes in the project.
+
+    @param project (OpusProject) the project to fetch values from
+    @return the list of available model nodes (list(Element))
+    '''
+    model_manager_node = project.find('model_manager/model_system')
+    return [node for node in (model_manager_node or []) if
+            node.get('type') == 'model']
+
+def get_model_names(project):
+    '''
+    Get a list of model names in the project.
+
+    @param project (OpusProject) the project to fetch values from
+    @return the list of available model nodes (list(Element))
+    '''
+    model_nodes = get_model_nodes(project)
+    return [node.tag for node in model_nodes]
+
+
 class ModelsManager(AbstractManager):
     def __init__(self, base_widget, tab_widget, project):
         AbstractManager.__init__(self, base_widget, tab_widget, project,
