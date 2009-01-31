98921b0dd85091e6ab7e5580c132ce8d39a73bbe
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-01-31 21:16:53 +0000

    fixed bug where Python would garbage collect the XmlModel used by self.tree_view added error messages and error handling for saving the changes
    git-svn-id: https://svn.urbansim.org/trunk@15614 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/main/controllers/dialogs/databasesettings.py
+++ opus_gui/main/controllers/dialogs/databasesettings.py
@@ -13,7 +13,9 @@
 
 from xml.etree.cElementTree import ElementTree
 
-from PyQt4.QtGui import QDialog, QTreeView
+from PyQt4.QtGui import QDialog, QMessageBox
+
+from opus_gui.main.controllers.dialogs.message_box import MessageBox
 from opus_gui.abstract_manager.models.xml_model import XmlModel
 from opus_gui.abstract_manager.controllers.xml_configuration.xml_controller import XmlItemDelegate
 from opus_gui.abstract_manager.views.xml_view import XmlView
@@ -31,25 +33,56 @@ class DatabaseSettingsEditGui(QDialog, Ui_DatabaseSettingsEditGui):
             os.path.join(settings_directory,
                          'database_server_configurations.xml')
         try:
-            self.xml_root = ElementTree(file=self._config_filename).getroot()
-            self.tree_view = XmlView(self)
-            self.tree_view.setModel(XmlModel(self.xml_root))
-            self.tree_view.setItemDelegate(XmlItemDelegate(self.tree_view))
-            self.tree_view.openDefaultItems()
-            self.gridlayout.addWidget(self.tree_view)
+            root = ElementTree(file=self._config_filename).getroot()
+            view = XmlView(self)
+            model = XmlModel(root)
+            delegate = XmlItemDelegate(view)
+            view.setModel(model)
+            view._model = model # HACK this is necessary so that Qt don't GC the model object
+            view._delegate = delegate # Same as above
+            view.setItemDelegate(delegate)
+            view.openDefaultItems()
+
+            self.gridlayout.addWidget(view)
+
+            self.tree_view = view
+            self.xml_root = root
+            return
 
         except IOError, ex:
-            print str(ex)
+            MessageBox.error(mainwindow = self,
+                          text = 'Could not initialize Database Settings',
+                          detailed_text = str(ex))
             self.xml_root = None
             self._config_filename = ''
             self.configFile = None
 
-    def on_saveChanges_released(self):
+    def on_buttonBox_accepted(self):
         try:
             ElementTree(self.xml_root).write(self._config_filename)
+        except IOError, ex:
+            MessageBox.error(self,
+                          text = 'A disk error occured when saving the ' +
+                          'settings.',
+                          detailed_text = str(ex))
+        except Exception, ex:
+            MessageBox.error(mainwindow = self,
+                          text = 'An unknown error occured when saving ' +
+                          'your changes.',
+                          detailed_text = str(ex))
         finally:
             self.close()
 
-    def on_cancelWindow_released(self):
-        self.close()
-
+    def on_buttonBox_rejected(self):
+        # If there are any changes to the data, ask the user to save them
+        if self.tree_view.model().dirty:
+            question = ('Changes have been made to the settings.\n'
+                        'Do you want to save or discard those changes?')
+            buttons = (QMessageBox.Discard, QMessageBox.Save, QMessageBox.Cancel)
+            doSave = QMessageBox.question(self, "Warning", question, *buttons)
+            if doSave == QMessageBox.Save:
+                self.on_buttonBox_accepted()
+            elif doSave == QMessageBox.Discard:
+                self.close()
+            else:
+                pass # Cancel
