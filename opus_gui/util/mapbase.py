9c2ef966bf99dd144ee8ae1d235a62feffef30a5
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-02-20 00:18:46 +0000

    added comment about star imports
    git-svn-id: https://svn.urbansim.org/trunk@15799 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/util/mapbase.py
+++ opus_gui/util/mapbase.py
@@ -1,15 +1,15 @@
 # UrbanSim software. Copyright (C) 2005-2008 University of Washington
-# 
+#
 # You can redistribute this program and/or modify it under the terms of the
 # GNU General Public License as published by the Free Software Foundation
 # (http://www.gnu.org/copyleft/gpl.html).
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
 # and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
 # other acknowledgments.
-# 
+#
 
 
 # PyQt4 includes for python bindings to QT
@@ -17,6 +17,7 @@ from PyQt4.QtCore import QString, Qt, QFileInfo, QObject, SIGNAL
 from PyQt4.QtGui import QVBoxLayout
 
 # QGIS bindings for mapping functions
+# CK: avoid star imports
 from qgis.core import *
 from qgis.gui import *
 
