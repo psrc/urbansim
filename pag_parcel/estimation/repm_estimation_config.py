1393f7cb5c49c30db4e445bf4ab7b84e0e62dde6
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-03-20 20:55:20 +0000

    removing the pag_parcel package
    git-svn-id: https://svn.urbansim.org/trunk@15926 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- pag_parcel/estimation/repm_estimation_config.py
+++
@@ -1,9 +0,0 @@
-# Opus/UrbanSim urban simulation software.
-# Copyright (C) 2005, 2006, 2007, 2008, 2009 University of Washington
-# See opus_docs/LICENSE 
-
-from pag_parcel.configs.controller_config import config
-
-run_configuration = config
-run_configuration['models'] = [
-    {'real_estate_price_model':['estimate']} ]
