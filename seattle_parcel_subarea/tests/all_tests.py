3a681439bb8e77b95708ddc6d88716fcfde316c9
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-03-20 20:54:42 +0000

    removing the seattle_parcel_subarea package
    git-svn-id: https://svn.urbansim.org/trunk@15925 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- seattle_parcel_subarea/tests/all_tests.py
+++
@@ -1,7 +0,0 @@
-# Opus/UrbanSim urban simulation software.
-# Copyright (C) 2005, 2006, 2007, 2008, 2009 University of Washington
-# See opus_docs/LICENSE 
-
-from opus_core.tests.utils.package_tester import PackageTester
-
-PackageTester().run_all_tests_for_package('seattle_parcel_subarea')
