ca5be67b37f82ddffe326bd6b2ed9da24ef11a05
pwaddell <pwaddell@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-05-29 02:16:46 +0000

    (lmwang & pwaddell)add new variables
    git-svn-id: https://svn.urbansim.org/trunk@16184 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim/travel_data/aliases.py
@@ -0,0 +1,11 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+    "aggregate_am_bike_to_work_travel_time = travel_data.am_bike_to_work_travel_time * travel_data.am_biking_person_trips",
+    
+    ]
