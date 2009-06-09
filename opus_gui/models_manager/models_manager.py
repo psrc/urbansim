1789b9e095e3246f576f9ca6d66f7c03619acaef
ckla <ckla@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-06-09 22:22:41 +0000

    moved external functions get_model_nodes() and get_model_names() to models_manager_functions
    git-svn-id: https://svn.urbansim.org/trunk@16237 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/models_manager/models_manager.py
+++ opus_gui/models_manager/models_manager.py
@@ -6,24 +6,6 @@ from opus_gui.models_manager.controllers.tabs.estimation_gui_element import Esti
 from opus_gui.abstract_manager.abstract_manager import AbstractManager
 from opus_gui.models_manager.controllers.xml_configuration.xml_controller_models import XmlController_Models
 
-def get_model_nodes(project):
-    '''
-    Get a list of all model nodes in the project.
-
-    @param project (OpusProject) the project to fetch values from
-    @return the list of available model nodes (list(Element))
-    '''
-    return project.findall('model_manager/models/model')
-
-def get_model_names(project):
-    '''
-    Get a list of model names in the project.
-
-    @param project (OpusProject) the project to fetch values from
-    @return the list of available model nodes (list(Element))
-    '''
-    return [node.get('name') for node in get_model_nodes(project)]
-
 class ModelsManager(AbstractManager):
     def __init__(self, base_widget, tab_widget, project):
         AbstractManager.__init__(self, base_widget, tab_widget, project, 'model_manager')
@@ -32,3 +14,4 @@ class ModelsManager(AbstractManager):
     def add_estimation_element(self, estimation):
         tab_widget = EstimationGuiElement(self.base_widget, self, estimation)
         self._attach_tab(tab_widget = tab_widget)
+
