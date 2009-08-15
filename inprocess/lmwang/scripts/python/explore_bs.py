dfa8a169dd8c2e7700716ccecc5e7e3bb12f08e9
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-08-15 01:50:59 +0000

    (lmwang)removed lmwang/scripts
    git-svn-id: https://svn.urbansim.org/trunk@16314 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/lmwang/scripts/python/explore_bs.py
+++
@@ -1,6 +0,0 @@
-import urllib
-
-usock = urllib.urlopen('http://www.blogger.com/redirect/next_blog.pyra?navBar=true')
-htmlSource = usock.read()
-usock.close()
-print htmlSource
--- inprocess/lmwang/scripts/python/gama.py
+++
@@ -1,3 +0,0 @@
-#capture keyboard input
-
-#create charts
