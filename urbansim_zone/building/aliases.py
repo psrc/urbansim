83a2a48cc0b574bdc7e3b11885622a4abc35da62
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-09-18 04:59:22 +0000

    (lmwang)new variables
    git-svn-id: https://svn.urbansim.org/trunk@16328 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_zone/building/aliases.py
+++ urbansim_zone/building/aliases.py
@@ -2,5 +2,8 @@
 # Copyright (C) 2005-2009 University of Washington
 # See opus_core/LICENSE
 aliases = [
-        "total_residential_units = building.residential_units",
+        "total_residential_units = building.sf_residential_units + mf_residential_units",
+        "number_of_households = building.number_of_agents(household)",
+        "vacant_residential_units = clip_to_zero(urbansim_zone.building.total_residential_units - urbansim_zone.building.number_of_households)",
+        
            ]
