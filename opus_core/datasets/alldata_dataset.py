f29b3ed4a138c4f77993b4524fb916097d8956ac
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2009-10-13 17:23:38 +0000

    (lmwang)change the id_name for alldata to alldata_id
    git-svn-id: https://svn.urbansim.org/trunk@16356 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/datasets/alldata_dataset.py
+++ opus_core/datasets/alldata_dataset.py
@@ -11,17 +11,17 @@ class AlldataDataset(Dataset):
     """Special dataset for summaries over all members of other datasets. It has only one member.
        (to be used with the built-in function aggregate_all)
     """
-    
+    id_name = 'alldata_id'
     def __init__(self, *args, **kwargs):
         storage = StorageFactory().get_storage('dict_storage')
         
         storage.write_table(table_name='alldata',
             table_data={
-                'id':array([1]),
+                self.id_name:array([1]),
                 }
             )
         
-        Dataset.__init__(self, in_storage=storage, in_table_name='alldata', id_name="id", dataset_name="alldata")
+        Dataset.__init__(self, in_storage=storage, in_table_name='alldata', id_name=self.id_name, dataset_name="alldata")
     
     def flush_dataset(self):
         # no flushing is allowed for this dataset
