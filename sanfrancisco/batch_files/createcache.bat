ac7c5e72402b7bed4e492a00fd95ef92c5a4a5d7
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-03-17 00:01:34 +0000

    xml-based cache
    git-svn-id: https://svn.urbansim.org/src/trunk@16487 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/batch_files/createcache.bat
+++ sanfrancisco/batch_files/createcache.bat
@@ -1,1 +1,7 @@
-python C:\opus_workspace\opus_core\tools\create_baseyear_cache.py sanfrancisco.configs.baseline --cache-directory=\urbansim_cache\sanfrancisco\cache_source
+python C:\opus\src\opus_core\tools\create_baseyear_cache.py ^
+::--configuration-path=sanfrancisco.configs.baseline ^
+--database-name=sanfrancisco_baseyear ^
+--xml-configuration=sanfrancisco/configs/sanfrancisco.xml ^
+--scenario_name=sanfrancisco_baseline ^
+--cache-directory=C:\opus\data\sanfrancisco\base_year_data ^
+--database-configuration=estimation_database_server
