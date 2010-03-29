249fe311516a0af2a7188ce779689eab30619eb9
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-03-29 21:57:10 +0000

    (lmwang)updated GPL text
    git-svn-id: https://svn.urbansim.org/src/trunk@16513 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_gui/data_manager/run/tools/od_run_from_configuration.py
+++ opus_gui/data_manager/run/tools/od_run_from_configuration.py
@@ -1,16 +1,7 @@
-#
-# UrbanSim software. Copyright (C) 2005-2008 University of Washington
-#
-# You can redistribute this program and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation
-# (http://www.gnu.org/copyleft/gpl.html).
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
-# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
-# other acknowledgments.
-#
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
+
 from data_mining.outlier_det import run_test
 import os, sys, subprocess, time
 from xml.dom import minidom
