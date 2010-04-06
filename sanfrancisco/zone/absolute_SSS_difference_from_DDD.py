08651cb68ad735254fa93d62f5f65f382620c067
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-06 22:37:50 +0000

    (lmwang)difference indicators for sanfrancisco project
    git-svn-id: https://svn.urbansim.org/src/trunk@16528 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/zone/absolute_SSS_difference_from_DDD.py
@@ -0,0 +1,24 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from urbansim.abstract_variables.abstract_absolute_SSS_difference_from_DDD \
+    import abstract_absolute_SSS_difference_from_DDD
+
+
+class absolute_SSS_difference_from_DDD(abstract_absolute_SSS_difference_from_DDD):
+    """difference of variable SSS (current year - baseyear)"""
+
+    def __init__(self, *args, **kwargs):
+        abstract_absolute_SSS_difference_from_DDD.__init__(self, 'zone', package_name='sanfrancisco', *args, **kwargs)
+
+
+from opus_core.tests import opus_unittest
+from urbansim.abstract_variables.abstract_absolute_SSS_difference_from_DDD import TestFactory
+
+# Need to assign to the same name as the class defined in TestFactory.
+__MyTests = TestFactory().get_test_case_for_dataset('zone', 'zones', 'zone_id')
+
+
+if __name__ == '__main__':
+    opus_unittest.main()
