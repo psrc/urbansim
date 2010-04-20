27268b6fec3e887a60314a5e860fd3efc5daf8fe
lmzlmz <lmzlmz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-20 00:31:00 +0000

    Added quick unit test
    git-svn-id: https://svn.urbansim.org/src/trunk@16572 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/zone/average_income.py
+++ sanfrancisco/zone/average_income.py
@@ -22,3 +22,35 @@ class average_income(Variable):
         imin = dataset_pool.get_dataset("household").get_attribute("income").min()
         imax = dataset_pool.get_dataset("household").get_attribute("income").max()
         self.do_check("x >= %s and x <= %s" % (imin, imax), values)
+
+from opus_core.tests import opus_unittest
+from opus_core.datasets.dataset_pool import DatasetPool
+from opus_core.storage_factory import StorageFactory
+from numpy import array
+from opus_core.tests.utils.variable_tester import VariableTester
+
+class Tests(opus_unittest.OpusTestCase):
+    def test_my_inputs(self):
+        tester = VariableTester(
+            __file__,
+            package_order=['sanfrancisco','urbansim'],
+            test_data={
+                "household":{
+                    "household_id":array([1,2,3,4,5,6,7,8,9,10]),
+                    "income":array([11,12,13,14,15,16,17,18,19,20]),
+                    "zone_id":array([1,2,1,2,1,2,1,2,1,3]),
+                    },
+                "zone":{
+                    "zone_id":array([1,2,3,4])
+                    }
+                # zone 1: 11,13,15,17,19 = 15
+                # zone 2: 12,14,16,18 = 15
+                # zone 3: 20 = 20
+           }
+        )
+        
+        should_be = array([15, 15, 20, 0])
+
+        tester.test_is_close_for_variable_defined_by_this_module(self, should_be)
+if __name__=='__main__':
+    opus_unittest.main()
