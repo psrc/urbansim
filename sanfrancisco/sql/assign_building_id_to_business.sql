51e37d22ae4e19641e0063ab926546043f6cfdd7
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-22 00:40:09 +0000

    added constraint on employment and sqft (larger than 0)
    git-svn-id: https://svn.urbansim.org/src/trunk@16580 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/sql/assign_building_id_to_business.sql
+++ sanfrancisco/sql/assign_building_id_to_business.sql
@@ -11,31 +11,31 @@ drop table if exists business_for_estimation;
 
 create table business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 1
+	where building_id > 0 and sector_id = 1 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;
 
 insert into business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 2
+	where building_id > 0 and sector_id = 2 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;
 	
 insert into business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 3
+	where building_id > 0 and sector_id = 3 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;	
 
 insert into business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 4
+	where building_id > 0 and sector_id = 4 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;	
 
 insert into business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 5
+	where building_id > 0 and sector_id = 5 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;	
 
 insert into business_for_estimation
 select * from business 
-	where building_id > 0 and sector_id = 6
+	where building_id > 0 and sector_id = 6 and b.employment>0 and b.sqft>0
 	order by rand() limit 5000;
 				
