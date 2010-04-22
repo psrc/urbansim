9f28ca4caba54bb04557bfe9750e48be14bf6439
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-22 00:43:36 +0000

    sampled sector-wise (in effect over-sampling smaller sectors).
    git-svn-id: https://svn.urbansim.org/src/trunk@16581 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/sql/create_business_for_estimation.sql
+++ sanfrancisco/sql/create_business_for_estimation.sql
@@ -1,8 +1,33 @@
-##create business_for_estimation using random sampling from business table. 
-##Not sure what original critieria were.
+##create business_for_estimation using random sampling from business table.
 
 drop table if exists business_for_estimation;
+
 create table business_for_estimation
-SELECT * FROM business b
-ORDER BY RAND()
-LIMIT 21000;
+select * from business b
+	where building_id > 0 and sector_id = 1 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;
+
+insert into business_for_estimation
+select * from business b
+	where building_id > 0 and sector_id = 2 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;
+	
+insert into business_for_estimation
+select * from business b
+	where building_id > 0 and sector_id = 3 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;	
+
+insert into business_for_estimation
+select * from business b
+	where building_id > 0 and sector_id = 4 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;	
+
+insert into business_for_estimation
+select * from business b
+	where building_id > 0 and sector_id = 5 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;
+
+insert into business_for_estimation
+select * from business b
+	where building_id > 0 and sector_id = 6 and b.employment>0 and b.sqft>0
+	order by rand() limit 5000;
