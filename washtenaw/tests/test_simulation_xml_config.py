79af1560a447ab18416bfe48b77c26e315dc1a66
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-24 18:40:36 +0000

    (lmwang)integration test for semcog
    git-svn-id: https://svn.urbansim.org/src/trunk@16591 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- washtenaw/tests/test_simulation_xml_config.py
+++ washtenaw/tests/test_simulation_xml_config.py
@@ -15,11 +15,20 @@ class TestSimulation(opus_unittest.OpusIntegrationTestCase):
     """ this integration test checks and downloads a zipped cache from semcog ftp,
         unzip, and run washtenaw_parcel for 3 years
     """
+    def setUp(self):
+        self.opus_home = os.environ["OPUS_HOME"]
+        self.data_path = os.path.join(self.opus_home, 'data', 'washtenaw_parcel')
+    
+    def tearDown(self):
+        runs_path = os.path.join(self.data_path, 'runs')
+        stderr = Popen( "rm -rf %s" % runs_path,
+                        stderr=PIPE ).communicate()[0]
+        if stderr:
+            raise RuntimeError( "Error when deleting runs directory %s: %s" % (runs_path, stderr) )
     
     def test_simulation(self):
-        
-        opus_home = os.environ["OPUS_HOME"]
-        washtenaw_data_path = os.path.join(opus_home, 'data', 'washtenaw_parcel', 'base_year_data')
+        base_year_data_path = os.path.join(self.data_path, 'base_year_data')        
+
         ftp_url = os.environ["FTP_URL"]
         file_name = os.path.split(ftp_url)[1]
         ftp_user = os.environ["FTP_USERNAME"]
@@ -30,14 +39,14 @@ class TestSimulation(opus_unittest.OpusIntegrationTestCase):
                         wget --timestamping %s --ftp-user=%s --ftp-password=%s;
                         rm -rf 2008;
                         unzip -o %s
-                        """ % (washtenaw_data_path, ftp_url, ftp_user, ftp_password, file_name),
+                        """ % (base_year_data_path, ftp_url, ftp_user, ftp_password, file_name),
                         stderr=PIPE ).communicate()[0]
         if stderr:
             raise RuntimeError( "Error when downloading and unzipping file from %s: %s" % (ftp_url, stderr) )
                 
         run_manager = RunManager()
         run_as_multiprocess = True
-        xml_config = XMLConfiguration(os.path.join(opus_home, 'project_configs', 'washtenaw_parcel.xml'))
+        xml_config = XMLConfiguration(os.path.join(self.opus_home, 'project_configs', 'washtenaw_parcel.xml'))
         for scenario_name in ['washtenaw_parcel']:
             config = xml_config.get_run_configuration(scenario_name)
             insert_auto_generated_cache_directory_if_needed(config)
