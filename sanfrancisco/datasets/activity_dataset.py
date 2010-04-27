995d91ea18244f2c1d7b79e392f706240579ecfc
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-04-27 00:45:29 +0000

    (lmwang)added building_types and activities to tables_to_cache; followed naming convention for table_name
    git-svn-id: https://svn.urbansim.org/src/trunk@16596 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/datasets/activity_dataset.py
@@ -0,0 +1,12 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from numpy import where
+from urbansim.datasets.dataset import Dataset as UrbansimDataset
+
+class ActivityDataset(UrbansimDataset):
+    id_name_default = "activity_id"
+    in_table_name_default = "activities"
+    out_table_name_default = "activities"
+    dataset_name = "activity"
