f6f03a7f5c8c52985adb9201453b7fb681019942
mmarsik <mmarsik@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-07-08 05:16:38 +0000

    git-svn-id: https://svn.urbansim.org/src/trunk@16768 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- biocomplexity/examples/lccm_estimator_multirun.py
+++ biocomplexity/examples/lccm_estimator_multirun.py
@@ -1,16 +1,6 @@
-#
-# Opus software. Copyright (C) 1998-2004 University of Washington
-# 
-# You can redistribute this program and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation
-# (http://www.gnu.org/copyleft/gpl.html).
-# 
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
-# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
-# other acknowledgments.
-#
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
 
 from biocomplexity.datasets.land_cover_dataset import LandCoverDataset
 from biocomplexity.models.land_cover_change_model import LandCoverChangeModel
--- biocomplexity/examples/lccm_runner_sample.py
+++ biocomplexity/examples/lccm_runner_sample.py
@@ -1,16 +1,6 @@
-#
-# Opus software. Copyright (C) 2005-2008 University of Washington
-# 
-# You can redistribute this program and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation
-# (http://www.gnu.org/copyleft/gpl.html).
-# 
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
-# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
-# other acknowledgments.
-#
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
 
 """ This module is a sample configuration of the Land Change Cover Model (LCCM).
 It is intended to be modified to specify parameters to the model, or to
