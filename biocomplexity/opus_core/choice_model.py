863111191295e4a168de4c75e42b235777c6c574
mmarsik <mmarsik@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-07-10 05:06:58 +0000

    git-svn-id: https://svn.urbansim.org/src/trunk@16770 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- biocomplexity/opus_core/choice_model.py
+++ biocomplexity/opus_core/choice_model.py
@@ -1,16 +1,6 @@
-#
-# UrbanSim software. Copyright (C) 2005-2008 University of Washington
-#
-# You can redistribute this program and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation
-# (http://www.gnu.org/copyleft/gpl.html).
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
-# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
-# other acknowledgments.
-#
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
 
 import gc
 from opus_core.datasets.dataset_factory import DatasetFactory
