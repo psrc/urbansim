d3d4e3242fa0a68b9368a34267d89160bdae4df0
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-08-10 17:52:44 +0000

    added a few aliases for use with target_vacancies
    git-svn-id: https://svn.urbansim.org/src/trunk@16811 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/development_event_history/aliases.py
+++ sanfrancisco/development_event_history/aliases.py
@@ -3,5 +3,9 @@
 # See opus_core/LICENSE 
 
 aliases = [
-       "is_residential = development_event_history.disaggregate(building_type_classification.is_residential, intermediates=[building_type])"
-           ]
+       "is_residential = development_event_history.disaggregate(building_type_classification.is_residential, intermediates=[building_type])",
+       "building_class_id = development_event_history.disaggregrate(building_type.class_id)",
+       "occupied_mixed_spaces = sanfrancisco.development_event_history.occupied_sqft + safe_array_divide(sanfrancisco.building.number_of_households, (building.residential_units).astype('float')) * building.residential_sqft",
+       "total_mixed_spaces = development_event_history.non_residential_sqft + building.residential_sqft",   
+       "building_group_id=development_event_history.disaggregate(building_type_classification.grouping_id, intermediates=[building_type])",
+       ]
