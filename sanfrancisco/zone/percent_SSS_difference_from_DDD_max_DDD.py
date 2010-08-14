e4c456ae1ccae44b1b76a19a678dbb5a4dade74c
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-08-14 06:22:24 +0000

    (lmwang)refactored abstract difference variables so that only a dumb child class is needed to implement them for new dataset
    git-svn-id: https://svn.urbansim.org/src/trunk@16827 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/zone/percent_SSS_difference_from_DDD_max_DDD.py
@@ -0,0 +1,12 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+
+from urbansim.abstract_variables.abstract_percent_SSS_difference_from_DDD_max_DDD \
+    import abstract_percent_SSS_difference_from_DDD_max_DDD
+
+
+class percent_SSS_difference_from_DDD_max_DDD(abstract_percent_SSS_difference_from_DDD_max_DDD):
+    """percent difference of variable SSS (current year - baseyear)"""
+    pass
