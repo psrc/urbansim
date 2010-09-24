798787a713689f7bb7db1131769116d24de62117
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-09-24 21:01:59 +0000

    re-enabled unittest test_run_simulation_all_chunks using zipped cache biocomplexity/data/sample_cache.zip
    git-svn-id: https://svn.urbansim.org/src/trunk@16882 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- biocomplexity/tests/test_run_simulation_all_chunks.py
+++ biocomplexity/tests/test_run_simulation_all_chunks.py
@@ -10,34 +10,48 @@ from opus_core.tests import opus_unittest
 from biocomplexity.examples.run_simulation_all_chunks import Simulation
 from biocomplexity.datasets.land_cover_dataset import LandCoverDataset
 from biocomplexity.opus_package_info import package
+from tempfile import mkdtemp
 
+import sys, zipfile, os, os.path
+
+def unzip_files_into_dir(file, dir):
+#    os.mkdir(dir, 0777)
+    zfobj = zipfile.ZipFile(file)
+    for name in zfobj.namelist():
+        if name.endswith('/'):
+            os.mkdir(os.path.join(dir, name))
+        else:
+            outfile = open(os.path.join(dir, name), 'wb')
+            outfile.write(zfobj.read(name))
+            outfile.close()
 
 class TestRunSimulationAllChunks(opus_unittest.OpusTestCase):
     """Test on running the simulation for multiple years, transition between
     the input and output steps."""
-    
-    def Mtest_run_simulation_all_chunks(self):
-        """ test if the simulation run without errors"""
-        
+
+    def setUp(self):
         package_dir_path = package().get_package_path()
-        parent_dir_path = package().get_package_parent_path()
-        base_directory = os.path.join(package_dir_path, "data",
+        self.base_directory = os.path.join(package_dir_path, "data",
                                       "small_test_set_opus", "1991")
         
-        urbansim_cache_directory = os.path.join(parent_dir_path, "urbansim_cache", 
-                                                "data", "2006_02_14__11_43")
+        zipped_cache = os.path.join(package_dir_path, "data", "sample_cache.zip")
+        self.cache_dir = mkdtemp(prefix='tmp_biocomplexity')
+        unzip_files_into_dir(zipped_cache, self.cache_dir)        
+        self.urbansim_cache_directory = os.path.join(self.cache_dir, "urbansim_cache", 
+                                                     "data", "2006_02_14__11_43")
+        
+    def tearDown(self):
+        shutil.rmtree(self.cache_dir)
+    
+    def test_run_simulation_all_chunks(self):
+        """ test if the simulation run without errors"""
+        
         years = [2003,2007]
-        coefficients = "land_cover_change_model_coefficients_small_test"
-        specification = "land_cover_change_model_specification_small_test"
-        Simulation().run(base_directory, urbansim_cache_directory, years,
+        coefficients = "lccm_coefficients_small_test"
+        specification = "lccm_specification_small_test"
+        Simulation().run(self.base_directory, self.urbansim_cache_directory, years,
                          None, None, coefficients, specification,
                          convert_flt=False, convert_input=False)
-        self._clean_up_land_cover_cache(urbansim_cache_directory, years)
-
-    def _clean_up_land_cover_cache(self, urbansim_cache_directory, years):
-        lc_in_table_name = LandCoverDataset.in_table_name_default # 'land_covers'
-        for year in years:
-            shutil.rmtree(os.path.join(urbansim_cache_directory, str(year), lc_in_table_name))
         
         
 if __name__ == "__main__":
