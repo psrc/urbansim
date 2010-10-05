3ffa1e2d6cf35b95978d2c13dfb0c59b37df132d
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-10-05 23:54:13 +0000

    filter modified to exclude zero prices
    git-svn-id: https://svn.urbansim.org/src/trunk@16900 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/building/ln_price_residual.py
+++ urbansim_parcel/building/ln_price_residual.py
@@ -1,11 +1,11 @@
 # Opus/UrbanSim urban simulation software.
 # Copyright (C) 2005-2009 University of Washington
-# See opus_core/LICENSE 
+# See opus_core/LICENSE
 
 from urbansim.abstract_variables.abstract_iv_residual import abstract_iv_residual
 
 class ln_price_residual(abstract_iv_residual):
     """"""
-    p = "ln_bounded(urbansim_parcel.building.price_per_unit)"
-    iv = "ln_bounded(urbansim_parcel.building.avg_price_per_unit_in_zone)"
-    filter = "urbansim_parcel.building.building_id>0"
+    p = "ln(urbansim_parcel.building.price_per_unit)"
+    iv = "ln(urbansim_parcel.building.avg_price_per_unit_in_zone)"
+    filter = "numpy.logical_and(urbansim_parcel.building.building_id>0, urbansim_parcel.building.price_per_unit > 0)"
