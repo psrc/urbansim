708ef966cc7af3e70237de59824975d4559c5ca0
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-11-08 21:23:42 +0000

    Variable distance_to_SSS_dataset made abstract and put into urbansim, in order to be able to use it with any geography.
    git-svn-id: https://svn.urbansim.org/src/trunk@16975 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ urbansim/abstract_variables/abstract_distance_to_SSS_dataset.py
@@ -0,0 +1,45 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from opus_core.variables.variable import Variable
+from scipy.spatial import KDTree
+from numpy import column_stack
+
+class abstract_distance_to_SSS_dataset(Variable):
+    """distance of a dataset centroid to nearest SSS dataset point.
+    """
+    _return_type = "float32"
+    
+    dataset_x_coord = "x_coord"
+    dataset_y_coord = "y_coord"
+    my_x_coord = "x_coord"
+    my_y_coord = "y_coord"
+    package = "urbansim"
+    from_dataset = "gridcell"
+    
+    def __init__(self, to_dataset):
+        self.to_dataset = to_dataset
+        Variable.__init__(self)
+    
+    def dependencies(self):
+        return ["%s.%s.%s" % (self.package, self.from_dataset, self.my_x_coord),
+                "%s.%s.%s" % (self.package, self.from_dataset, self.my_y_coord)]
+
+    def compute(self, dataset_pool):
+        loc = dataset_pool.get_dataset(self.to_dataset)
+        self.add_and_solve_dependencies(["%s.%s" % (self.to_dataset, self.dataset_x_coord), 
+                                         "%s.%s" % (self.to_dataset, self.dataset_y_coord)], 
+                                        dataset_pool=dataset_pool)
+        pgcoords = column_stack( (loc.get_attribute(self.dataset_x_coord), 
+                                  loc.get_attribute(self.dataset_y_coord)) )
+        ds = self.get_dataset()
+        coords = column_stack( (ds.get_attribute(self.my_x_coord), 
+                                ds.get_attribute(self.my_y_coord)) )
+        kd_tree = KDTree(pgcoords, 10)
+        distances, indices = kd_tree.query(coords)
+        return distances
+
+    def post_check(self, values, dataset_pool):
+        self.do_check("x >= 0", values)
+
--- urbansim_parcel/parcel/distance_to_SSS_dataset.py
+++ urbansim_parcel/parcel/distance_to_SSS_dataset.py
@@ -2,44 +2,24 @@
 # Copyright (C) 2005-2009 University of Washington
 # See opus_core/LICENSE
 
-from opus_core.variables.variable import Variable
-from variable_functions import my_attribute_label
-from numpy import zeros
-from numpy import array
-from scipy.spatial import KDTree
-from numpy import column_stack
+from urbansim.abstract_variables.abstract_distance_to_SSS_dataset import abstract_distance_to_SSS_dataset
 
-class distance_to_SSS_dataset(Variable):
+class distance_to_SSS_dataset(abstract_distance_to_SSS_dataset):
     """distance of parcel centroid to nearest SSS dataset point,
         id name = dataset name_id, e.g. for busstop dataset, busstop_id 
         x coordinate field name = point_x
         y coordinate field name = point_y"""
     _return_type = "int32"
+    dataset_x_coord = "point_x"
+    dataset_y_coord = "point_y"
+    my_x_coord = "x_coord_sp"
+    my_y_coord = "y_coord_sp"
+    package = "urbansim_parcel"
+    from_dataset = "parcel"
 
-    def __init__(self, points):
-        self.points = points
-        Variable.__init__(self)
-    
-    def dependencies(self):
-        return [my_attribute_label("x_coord_sp"),
-                my_attribute_label("y_coord_sp")]
-
-    def compute(self, dataset_pool):
-        loc = dataset_pool.get_dataset(self.points)
-        self.add_and_solve_dependencies([self.points+".point_x", self.points+".point_y"], 
-                                        dataset_pool=dataset_pool)
-        pgcoords = column_stack( (loc.get_attribute("point_x"), loc.get_attribute("point_y")) )
-        parcels = self.get_dataset()
-        coords = column_stack( (parcels.get_attribute("x_coord_sp"), parcels.get_attribute("y_coord_sp")) )
-        kd_tree = KDTree(pgcoords, 10)
-        distances, indices = kd_tree.query(coords)
-        return distances
-
-    def post_check(self, values, dataset_pool):
-        self.do_check("x >= 0", values)
 
 from opus_core.tests import opus_unittest
-from numpy import array, int32
+from numpy import array
 from opus_core.tests.utils.variable_tester import VariableTester
 
 class Tests(opus_unittest.OpusTestCase):
