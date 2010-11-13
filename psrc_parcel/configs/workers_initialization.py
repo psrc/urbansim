bba6d38fea29dd9081ad44289e0c8a071d36b93e
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-11-13 19:24:03 +0000

    new variables for workplace choice model and config for worker initialization
    git-svn-id: https://svn.urbansim.org/src/trunk@16981 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/configs/workers_initialization.py
+++ psrc_parcel/configs/workers_initialization.py
@@ -19,7 +19,11 @@ class WorkersInitialization(Baseline):
         config.replace(config_changes)
         
         self.merge(config)
-        self['models_configuration']['work_at_home_choice_model']['controller']['run']['arguments']['run_choice_model'] = False
-        self['models_configuration']['work_at_home_choice_model']['controller']['run']['arguments']['choose_job_only_in_residence_zone'] = False
-        
-
+        self['models_configuration']['work_at_home_choice_model']['controller']['run']['arguments']['run_choice_model'] = True
+        self['models_configuration']['work_at_home_choice_model']['controller']['run']['arguments']['choose_job_only_in_residence_zone'] = True
+        self['models_configuration']['work_at_home_choice_model']['controller']['init']['arguments']['filter'] = "'job.building_type==2'"
+        self['models_configuration']['work_at_home_choice_model']['controller']['prepare_for_run']['arguments']['agents_filter'] = "'urbansim_parcel.person.is_worker'"
+        self['models_configuration']['workplace_choice_model_for_resident']['controller']['init']['arguments']["run_config"].merge({"capacity_string":"job.building_type==1"})
+        self['models_configuration']['workplace_choice_model_for_resident']['controller']['init']['arguments']["filter"] = "'job.building_type==1'"
+        self['models_configuration']['workplace_choice_model_for_resident']['controller']['run']['arguments']["agents_filter"] = "'urbansim_parcel.person.is_non_home_based_worker'"
+        self['models_configuration']['workplace_choice_model_for_resident']['controller']['run']['arguments']["chunk_specification"] = "{'records_per_chunk':20000}"
--- psrc_parcel/person/aliases.py
+++ psrc_parcel/person/aliases.py
@@ -11,5 +11,11 @@ aliases = [
            "sector_id_of_worker1 = person.disaggregate(household.aggregate((person.worker1==1) * (person.disaggregate(job.sector_id))) )",
            "sector_id_of_worker2 = person.disaggregate(household.aggregate((person.worker2==1) * (person.disaggregate(job.sector_id))) )",           
            "worker_id = 1*(person.worker1==1) + 2*(person.worker2==1)",
-           "household_income = person.disaggregate(household.income)",           
+           "household_income = person.disaggregate(household.income)",
+           "edu_lths = numpy.in1d(person.edu, (1,))",
+           "edu_lehs = numpy.in1d(person.edu, (1,2))",
+           "edu_hspa = numpy.in1d(person.edu, (2,4,5))",
+           "edu_college = numpy.in1d(person.edu, (3,6,7))",
+           "edu_pg = numpy.in1d(person.edu, (7,))",
+           "edu_adv = numpy.in1d(person.edu, (6, 7))",
            ]
