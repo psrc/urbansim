601f2a53c12ea32e290a4e94665efdcb2752b37b
lmzlmz <lmzlmz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2010-12-04 21:32:39 +0000

    Revised income quartile definition to revolve around the one that PlanB ABAG uses, which is based on translating incomes to 1989 dollars and cutting the quartiles at 25k, 45k and 75k.  Also added 1% year-over-year income increases (so Q4 keeps growing).
    git-svn-id: https://svn.urbansim.org/src/trunk@17001 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/household/in_adjusted_income_quartile_DDD.py
@@ -0,0 +1,42 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from opus_core.variables.variable import Variable
+from variable_functions import my_attribute_label
+from urbansim.functions import attribute_label
+from numpy import where,logical_and
+
+class in_adjusted_income_quartile_DDD(Variable):
+    """1 if a household in in the given income quartile, 0 otherwise.  
+       Quartile should be 1,2,3, or 4.
+       
+       These are not true quartiles, but they're defined by the quartiles used by ABAG, which is
+       * incomes less than $25k,
+       * incomes between $25k and $45k
+       * incomes between $45k and $75k
+       * incomes greater than $75k
+       All in 1989 constant dollars.
+    """
+
+    def __init__(self, quartile):
+        self.quartile = quartile
+        Variable.__init__(self)
+    
+        self.scale = 0.7328  # 1999 dollars to 1989 dollars
+
+        
+    def dependencies(self):
+        return ["income_adjusted = sanfrancisco.household.income_adjusted"]
+            
+    def compute(self,  dataset_pool):
+        income_adj = self.get_dataset().get_attribute("income_adjusted")
+        if self.quartile==1:
+            return where(income_adj< (25/self.scale),1,0)
+        elif self.quartile==2:
+            return where(logical_and(income_adj>= (25/self.scale), income_adj< (45/self.scale)),1,0)
+        elif self.quartile==3:       
+            return where(logical_and(income_adj> (45/self.scale), income_adj< (75/self.scale)),1,0)
+        else:
+            return where(income_adj>= (75/self.scale),1,0)
+
---
+++ sanfrancisco/household/income_adjusted.py
@@ -0,0 +1,36 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from opus_core.variables.variable import Variable
+from variable_functions import my_attribute_label
+from urbansim.functions import attribute_label
+from math import pow
+from opus_core.simulation_state import SimulationState
+
+class income_adjusted(Variable):
+    """
+    Adjusts income up slightly, based on ABAG adjustments (assuming this is based on
+    inflation, cost of living adjustments, etc.)
+    
+    Not doing this on any global level because we're not adjusting up land values and other stuff;
+    the purpose of this is to export more reasonable incomes to SF-CHAMP.
+    """
+
+    def __init__(self):
+        
+        # This is mysterious and terrible.  But the avg weighted income is just too low!
+        self.initial_factor = 1.0
+        
+        self.year_over_year_factor = 1.01
+
+        Variable.__init__(self)
+        
+    def compute(self,  dataset_pool):
+        income = self.get_dataset().get_attribute("income")
+        
+        current_year = SimulationState().get_current_time()
+        if current_year < 2009: return income
+        
+        return income*self.initial_factor*pow(self.year_over_year_factor,current_year-2009)
+    
