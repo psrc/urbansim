7d1859d12310c0e09c5a37d930d77abd8070aa2f
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-01-12 01:38:06 +0000

    minor coding style improvement
    git-svn-id: https://svn.urbansim.org/src/trunk@17075 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- sanfrancisco/zone/mean_income_kDDD.py
+++ sanfrancisco/zone/mean_income_kDDD.py
@@ -4,8 +4,11 @@
 
 from opus_core.variables.variable import Variable
 from opus_core.misc import safe_array_divide
-from numpy import zeros, ones, diag, dot, newaxis
-import networkx as nx
+from numpy import zeros, dot, newaxis, array
+try:
+    import networkx as nx
+except ImportError:
+    nx = None
 
 class mean_income_kDDD(Variable):
     """mean income for DDD order expansion
@@ -13,7 +16,7 @@ class mean_income_kDDD(Variable):
     GNSI^2 = numpy.sqrt( alldata.aggregate_all( zone.number_of_agents(household) * numpy.square(sanfrancisco.zone.mean_income_k2 - zone.aggregate_all(household.income, function=mean)) ) / alldata.aggregate_all( numpy.square(household.income - household.aggregate_all(household.income, function=mean)) )) 
     """
 
-    _return_type="float32"
+    _return_type = "float32"
     
     def __init__(self, order):
         self.order = order
@@ -27,6 +30,10 @@ class mean_income_kDDD(Variable):
                 ]
 
     def compute(self,  dataset_pool):
+        """ use networkx to determine whether two geographies are adjacent with
+        k degree of expansion
+        """
+
         zones = self.get_dataset()
         adjacent_zones = dataset_pool.get_dataset('adjacent_zone')
         id_max = zones['zone_id'].max()+1
@@ -36,29 +43,30 @@ class mean_income_kDDD(Variable):
         sum_households = zeros(id_max, dtype=zones['sum_households'].dtype)
         sum_income[zones['zone_id']] = zones['sum_income']
         sum_households[zones['zone_id']] = zones['sum_households']
-        
+        if not nx: raise ImportError, "networkx module is required."
         G = nx.Graph()
         G.add_nodes_from(zones['zone_id'])
         G.add_edges_from(adjacent_zones.get_multiple_attributes(['zone_id', 'adjacent_zone_id']))
-        length=nx.all_pairs_shortest_path_length(G, cutoff=self.order)
+        length = nx.all_pairs_shortest_path_length(G, cutoff=self.order)
         
         for key, val in length.items():
             W[key][val.keys()] = 1
         
-        sum_income = dot(W, sum_income[:,newaxis])[:,0]
-        sum_households = dot(W, sum_households[:,newaxis])[:,0]
+        sum_income = dot(W, sum_income[:, newaxis])[:, 0]
+        sum_households = dot(W, sum_households[:, newaxis])[:, 0]
 
         results = safe_array_divide(sum_income, sum_households.astype('f'))
         return results[zones['zone_id']]
     
 from opus_core.tests import opus_unittest
-from opus_core.datasets.dataset_pool import DatasetPool
-from opus_core.storage_factory import StorageFactory
-from numpy import array
 from opus_core.tests.utils.variable_tester import VariableTester
 
 class Tests(opus_unittest.OpusTestCase):
+    """
+    """
     def test_my_inputs(self):
+        """ test k=0,1,2
+        """
         tester = VariableTester(
             __file__,
             package_order=['sanfrancisco','urbansim'],
@@ -85,18 +93,17 @@ class Tests(opus_unittest.OpusTestCase):
         )
         
         instance_name = 'sanfrancisco.zone.mean_income_k0'
-        tester.test_is_equal_for_family_variable(self, array([10, 32, 45, 85])*1000, instance_name)
+        should_be = array([10, 32, 45, 85])*1000 
+        tester.test_is_equal_for_family_variable(self, should_be, instance_name)
 
         instance_name = 'sanfrancisco.zone.mean_income_k1'
-        tester.test_is_close_for_family_variable(self, array([29, 30.142857, 35.714285, 45.142857])*1000, instance_name)
+        should_be = array([29, 30.142857, 35.714285, 45.142857])*1000
+        tester.test_is_close_for_family_variable(self, should_be, instance_name)
 
         instance_name = 'sanfrancisco.zone.mean_income_k2'
-        tester.test_is_close_for_family_variable(self, array([34.6, 34.6, 34.6, 34.6])*1000, instance_name)
+        should_be = array([34.6, 34.6, 34.6, 34.6])*1000
+        tester.test_is_close_for_family_variable(self, should_be, instance_name)
 
-if __name__=='__main__':
-    try:
-        import networkx        
-    except:
-        networkx = None
-    if networkx:
+if __name__ == '__main__':
+    if nx:
         opus_unittest.main()
