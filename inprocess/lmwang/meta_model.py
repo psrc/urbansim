a6b1888e99be8343dc557234c7925ed9cc349e42
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-03-20 23:24:36 +0000

    a new iteration of em_learning data, with sampling_prob and regularization on joint dims
    git-svn-id: https://svn.urbansim.org/src/trunk@17165 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- inprocess/lmwang/meta_model.py
+++ inprocess/lmwang/meta_model.py
@@ -13,41 +13,59 @@ from urbansim.model_coordinators.model_system import ModelSystem
 from opus_core.configurations.xml_configuration import XMLConfiguration
 from urbansim.estimation.estimation_runner import EstimationRunner
 from opus_core.variables.variable_name import VariableName
+from opus_core.sampling_toolbox import probsample_noreplace
 from numpy.random import randint
-from numpy import where
+from numpy import where, arange
 import cPickle
 
 class options(object):
-    meta_models = [['large_area_choice_model', 'residential_building_type_choice_model', 'auto_ownership_choice_model'],
-                   ['household_location_choice_model']
+    meta_models = [['residential_building_type_choice_model', 
+                    'tenure_choice_model', 
+                    'auto_ownership_choice_model', 
+                    'household_location_choice_model',
+                    'worker1_workplace_choice_model'],
                    ]
-    market_share = [['market_share=large_area.aggregate(building.residential_units)',
-                     'market_share=building_type.aggregate(building.residential_units)',
-                     'market_share=car.number_of_agents(household)'
-                     ],
-                     ['']
+    market_share = [['market_share=building_type.aggregate(building.residential_units)',
+                     '',
+                     'market_share=car.number_of_agents(household)',
+                     'market_share=district.aggregate(district_traffic_flow.share)',
+                     'market_share=district.aggregate(district_traffic_flow.share)'
+                    ] 
                     ]
+    choice_type = [['CLASSIFICATION', 'CLASSIFICATION', 'CLASSIFICATION', 'CLASSIFICATION', 'CLASSIFICATION']]
     #CLASSIFICATION = 1
     #RATING = 2
     #REGRESSION = 3
-    choice_type = [['CLASSIFICATION', 'CLASSIFICATION', 'CLASSIFICATION'], ['CLASSIFICATION']]
-    xml_configuration = '/workspace/opus/project_configs/psrc_parcel_test.xml'
+    xml_configuration = '/home/lmwang/opus/src/inprocess/lmwang/psrc_parcel_test.xml'
     scenario_name = 'psrc_baseline_test'
     year = 2000
     agent_set = 'household'
     sample_size = 5000 #for regularization
     agents_index = None
+    agents_filter = 'psrc_parcel.household.customized_run_filter'
     pickle_filename = 'em_data.pickle'
-    seed = None
+    seed = 100
     nchunks = 1 ##TODO
 
 if __name__ == '__main__':
+    try: import wingdbstub
+    except: pass
+    from psrc_parcel.household.aliases import aliases
+    aliases += ["customized_est_filter= (household.household_id > 6000000) * " + \
+                "numpy.logical_or(household.tenure==1, household.tenure==3) * " + \
+                #numpy.setmember1d(household.tenure, (1,3)) * \ ## buggy
+                "(( (psrc_parcel.household.building_type_id==4) + (psrc_parcel.household.building_type_id==11) + (psrc_parcel.household.building_type_id==12) + (psrc_parcel.household.building_type_id==19)) >= 1) * " + \
+                #numpy.setmember1d(psrc_parcel.household.building_type_id, (4, 11, 12, 19)) * \ ##ideally the above line can be replaced with this, but this is buggy
+                "(household.aggregate((person.worker1==1)*(urbansim_parcel.person.is_non_home_based_worker_with_job)))",
+               "customized_run_filter= household.aggregate(urbansim_parcel.person.is_non_home_based_worker)>=1"
+               ]
     xmlconfig = XMLConfiguration(options.xml_configuration)
     ## training data (start_estimation)
     training_data = []
     for h, hierarchy in enumerate(options.meta_models):
         model_data = []
         for i, model_name in enumerate(hierarchy):
+            logger.start_block('%s' % model_name)
             estimator = EstimationRunner(model=model_name, 
                                          specification_module=None, 
                                          xml_configuration=xmlconfig, 
@@ -59,10 +77,10 @@ if __name__ == '__main__':
                                          #save_estimation_results=False)
             
             estimator.estimate()
-            agents_size = estimator.get_agent_set().size()
             m = estimator.get_model()
             model_system = estimator.model_system
-            
+            dataset_pool=model_system.run_year_namespace["dataset_pool"]
+
             submodels = m.model_interaction.get_submodels()
             assert len(submodels) == 1
             submodel = submodels[0]
@@ -77,6 +95,9 @@ if __name__ == '__main__':
             index_chosen = where(is_chosen)[1]
             assert index_chosen.size == data.shape[0]
             
+            sampling_prob = mi.interaction_dataset['__sampling_probability'] \
+                          if '__sampling_probability' in mi.interaction_dataset.get_known_attribute_names() \
+                          else None            
             variable_names = m.model_interaction.get_variable_names(submodel)
             outputvar = model_system.run_year_namespace['outputvar']
             results_name = outputvar.strip('()').split(',')[1].strip()
@@ -84,6 +105,7 @@ if __name__ == '__main__':
             
             model_data.append({"data":data, 
                                'index_chosen':index_chosen,
+                               'sampling_probability': sampling_prob,
                                'variable_names':variable_names,
                                'estimation_results':estimation_results,
                                'model_name':model_name,
@@ -95,7 +117,6 @@ if __name__ == '__main__':
                 ms_variablename = VariableName(ms_expression)
         
                 dataset_name = ms_variablename.get_dataset_name()
-                dataset_pool=model_system.run_year_namespace["dataset_pool"]
                 ds = model_system.run_year_namespace[dataset_name] or model_system.run_year_namespace['datasets'][dataset_name]
                 id_name = ds.get_id_name()[0]
                 ds.compute_variables([ms_variablename], dataset_pool=dataset_pool)
@@ -105,18 +126,29 @@ if __name__ == '__main__':
                 market_ids_2d = market_ids[m.model_interaction.get_choice_index()]
                 model_data[i].update({'market_id':market_ids_2d, 'market_share':ms})
 
+            logger.end_block()
         training_data.append(model_data)
         
     config = xmlconfig.get_run_configuration(options.scenario_name)
-    if not options.agents_index:            
-        options.agents_index = randint(0, agents_size, size=options.sample_size).tolist()
+    if not options.agents_index:
+        agent_set = dataset_pool.get_dataset(options.agent_set)
+        agents_size = agent_set.size()
+        if options.agents_filter:
+            is_valid = agent_set.compute_variables(options.agents_filter)
+            options.agents_index = probsample_noreplace(arange(agents_size),
+                                                        options.sample_size,
+                                                        prob_array=is_valid
+                                                       ).tolist()
+        else:
+            options.agents_index = randint(0, agents_size, size=options.sample_size).tolist()
 
     ## regularization data
     population_data = []
     for h, hierarchy in enumerate(options.meta_models):
         model_data = []
         for i, model_name in enumerate(hierarchy):
-            
+            logger.start_block('%s' % model_name)
+
             config['models_configuration'][model_name]['controller']['run']['arguments']['agents_index'] = options.agents_index
             config['models'] = [{model_name:["run"]}]
             config['years'] = [options.year, options.year]
@@ -137,13 +169,18 @@ if __name__ == '__main__':
             model_system.run(config, write_datasets_to_cache_at_end_of_year=False)
             
             m = model_system.run_year_namespace["model"]
+            mi = m.model_interaction
             submodels = m.model_interaction.get_submodels()
             assert len(submodels) == 1
             submodel = submodels[0]
             
             data = m.get_all_data(submodel)
+            sampling_prob = mi.interaction_dataset['__sampling_probability'] \
+              if '__sampling_probability' in mi.interaction_dataset.get_known_attribute_names() \
+              else None
             model_data.append({'data':data, 
                                'model_name':model_name,
+                               'sampling_probability': sampling_prob,
                                'choice_type':options.choice_type[h][i]
                               })
             
@@ -153,15 +190,27 @@ if __name__ == '__main__':
         
                 dataset_name = ms_variablename.get_dataset_name()
                 dataset_pool=model_system.run_year_namespace["dataset_pool"]
-                ds = model_system.run_year_namespace[dataset_name] or model_system.run_year_namespace['datasets'][dataset_name]
-                id_name = ds.get_id_name()[0]
-                ds.compute_variables([ms_variablename], dataset_pool=dataset_pool)
-                ms = ds.get_multiple_attributes([id_name, ms_variablename.get_alias()])
+                from numpy import zeros
+                ## handle district_traffic_flow as a special case
+                if dataset_name == 'district_traffic_flow':
+                    id_name = 'district_id'  #fake id_name
+                    ds = dataset_pool.get_dataset('district_traffic_flow')
+                    ms = zeros( (ds['from_district_id'].max()+1, ds['to_district_id'].max()+1), 
+                                dtype=ds['share'].dtype)
+                    ms[ds['from_district_id'], ds['to_district_id']] = ds['share']
+                else:
+                    ds = model_system.run_year_namespace[dataset_name] \
+                       or model_system.run_year_namespace['datasets'][dataset_name]
+                    id_name = ds.get_id_name()[0]
+                    ds.compute_variables([ms_variablename], dataset_pool=dataset_pool)
+                    ms = ds.get_multiple_attributes([id_name, ms_variablename.get_alias()])
                 
                 market_ids = m.choice_set.compute_one_variable_with_unknown_package( id_name, dataset_pool=dataset_pool)
                 market_ids_2d = market_ids[m.model_interaction.get_choice_index()]
                 model_data[i].update({'market_id':market_ids_2d, 'market_share':ms})
                             
+            logger.end_block()
+
         population_data.append(model_data)
     
     ## pickle file   
