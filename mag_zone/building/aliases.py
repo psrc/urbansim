66f3db53e61b9b271605fc6c95ff519ca4588955
jrayers <jrayers@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-04-06 16:28:31 +0000

    new dataset and variables
    git-svn-id: https://svn.urbansim.org/src/trunk@17178 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ mag_zone/building/aliases.py
@@ -0,0 +1,22 @@
+#
+# UrbanSim software. Copyright (C) 2005-2008 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+    "skyharbor_enplanement_capacity = (building.other_spaces_name=='skyharbor_enplanement_capacity')*(building.other_spaces)",
+    "williamsgateway_enplanement_capacity = (building.other_spaces_name=='williamsgateway_enplanement_capacity')*(building.other_spaces)",
+    "hotel_motel_rooms = (building.other_spaces_name=='hotel_motel_rooms')*(building.other_spaces)",
+           ]
