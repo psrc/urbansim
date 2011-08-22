2689c1e8235bbc00eb96c41e8e05786c39ab3e92
tnicolai <tnicolai@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-08-22 11:35:24 +0000

    updates
    git-svn-id: https://svn.urbansim.org/src/trunk@17381 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_matsim/models/run_travel_model.py
+++ opus_matsim/models/run_travel_model.py
@@ -37,9 +37,18 @@ class RunTravelModel(AbstractTravelModel):
         config_obj = MATSimConfigObject(config, year, self.matsim_config_full)
         config_obj.marschall()
         
+        # change to destination directory opus_matsim
+        os.chdir(os.path.join(os.environ['OPUS_HOME'], 'opus_matsim'))
+        
         # calling travel model with cmd command
-        cmd = """cd %(opus_home)s/opus_matsim ; java %(vmargs)s -cp %(classpath)s %(javaclass)s %(matsim_config_file)s %(test_parameter)s""" % {
-                'opus_home': os.environ['OPUS_HOME'],
+        #cmd = """cd %(opus_home)s/opus_matsim ; java %(vmargs)s -cp %(classpath)s %(javaclass)s %(matsim_config_file)s %(test_parameter)s""" % {
+        #        'opus_home': os.environ['OPUS_HOME'],
+        #        'vmargs': "-Xmx2000m", # set to 8GB on math cluster and 2GB on Notebook
+        #        'classpath': "jar/matsim4urbansim.jar",
+        #        'javaclass': "playground.run.Matsim4Urbansim", # "playground.tnicolai.urbansim.cupum.MATSim4UrbansimCUPUM",
+        #        'matsim_config_file': self.matsim_config_full,
+        #        'test_parameter': self.test_parameter } 
+        cmd = """java %(vmargs)s -cp %(classpath)s %(javaclass)s %(matsim_config_file)s %(test_parameter)s""" % {
                 'vmargs': "-Xmx2000m", # set to 8GB on math cluster and 2GB on Notebook
                 'classpath': "jar/matsim4urbansim.jar",
                 'javaclass': "playground.run.Matsim4Urbansim", # "playground.tnicolai.urbansim.cupum.MATSim4UrbansimCUPUM",
