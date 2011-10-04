94cb256507d4a20de179b07d76dfee7700a7f3c4
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-10-04 15:39:45 +0000

    Adding sandbox for zurich_parcel project.
    git-svn-id: https://svn.urbansim.org/src/trunk@17494 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/opus_package_info.py
@@ -0,0 +1,10 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from opus_core.opus_package import OpusPackage
+
+class package(OpusPackage):
+    name = None
+    required_opus_packages = ["opus_core", "urbansim"]
+    version = "1.0.beta2"
---
+++ zurich_parcel/parcels/aliases.py
@@ -0,0 +1,20 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+# this is a test of the expression alias file
+# one of the aliases uses a primary attribute in the expression, the other a variable
+
+aliases = [
+       "number_of_businesses = parcel.aggregate(sanfrancisco.building.number_of_businesses)",
+       "employment = parcel.aggregate(sanfrancisco.building.employment)",
+       "number_of_households = parcel.aggregate(sanfrancisco.building.number_of_households)",
+       "population = parcel.aggregate(sanfrancisco.building.population)", 
+       "building_sqft_per_unit = safe_array_divide(parcel.building_sqft, parcel.residential_units)",
+       "building_sqft = parcel.aggregate(building.building_sqft)",
+       "housing_value = safe_array_divide((parcel.land_val + parcel.struc_val), sanfrancisco.parcel.residential_units)",
+       "lot_sqft_per_unit = safe_array_divide(parcel.lot_sf, parcel.residential_units)",
+       "residential_units = parcel.aggregate(building.residential_units)",
+       "remaining_non_residential_sqft_capacity = parcel.non_residential_sqft_capacity - parcel.aggregate(building.non_residential_sqft)",
+       "remaining_residential_units_capacity = parcel.residential_units_capacity - parcel.aggregate(building.residential_units)",
+           ]
