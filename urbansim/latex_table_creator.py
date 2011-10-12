a80a3db011653d497cea84c65e9b15841f721998
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-10-12 01:28:21 +0000

    methods moved to a parent class in opus_core
    git-svn-id: https://svn.urbansim.org/src/trunk@17566 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/latex_table_creator.py
+++ urbansim/latex_table_creator.py
@@ -4,12 +4,13 @@
 
 
 import os
+from opus_core.latex_table_creator import LatexTableCreator as CoreLatexTableCreator
 from urbansim.model_coordinators.model_system import ModelSystem
 from opus_core.storage_factory import StorageFactory
 from opus_core.model import prepare_specification_and_coefficients
 from opus_core.configuration import Configuration
 
-class LatexTableCreator(object):
+class LatexTableCreator(CoreLatexTableCreator):
     def create_latex_tables_for_model(self, config, model_name, dir):
         """Write to directory dir a file containing a LaTeX table describing this model's 
         coefficients, and a file containing a LaTeX table describing this model's specification.
@@ -37,28 +38,3 @@ class LatexTableCreator(object):
         self.create_latex_table_for_coefficients_for_model(coefficients, model_name, dir)
         self.create_latex_table_for_specifications_for_model(specification, model_name, dir)
         
-    def create_latex_table_for_specifications_for_model(self, specification, model_name, dir):
-        if specification is None:
-            return
-        from opus_core.latex import LaTeX
-        spec_table = specification.get_table_summary()
-        latex = LaTeX()
-        # Latex does not like underscores in file names, so use hyphens instead.
-        hyphenated_model_name = model_name.replace('_', '-')
-        label = 'table:%s-specification' % hyphenated_model_name
-        caption = '%s Specification' % model_name.replace('_', ' ').title()
-        latex.save_specification_table_to_tex_file(spec_table, os.path.join(dir, hyphenated_model_name + '-specification.tex'),
-                                                   label=label, caption=caption                                      )
-    
-    def create_latex_table_for_coefficients_for_model(self, coefficients, model_name, dir):
-        """Write this model's coefficients to a LaTeX table in directory dir.
-        Table is named as <model_name>_coefficients.tex.
-        """
-        if coefficients is None:
-            return
-        # Latex does not like underscores in file names, so use hyphens instead.
-        hyphenated_model_name = model_name.replace('_', '-')
-        label = 'table:%s-coefficients' % hyphenated_model_name
-        caption = '%s Coefficients' % model_name.replace('_', ' ').title()
-        coefficients.make_tex_table(os.path.join(dir, hyphenated_model_name + '-coefficients'),
-                                    label=label, caption=caption)
