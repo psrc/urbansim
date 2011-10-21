6f088601c3de9d6cf696e72324199f1e3a21ae16
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-10-21 17:10:15 +0000

    Adding variable to check for building type.
    git-svn-id: https://svn.urbansim.org/src/trunk@17603 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/building/variable_functions.py
@@ -0,0 +1,12 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from numpy import where, zeros
+from urbansim.functions import attribute_label
+  
+def my_attribute_label(attribute_name):
+    """Return a triple (package, dataset_name, attribute_name).
+    """
+    return "zurich_parcel.building." + attribute_name
+    
