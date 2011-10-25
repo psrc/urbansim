40d00e1bc16386440d0846608c4990855ace2c6f
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-10-25 15:04:23 +0000

    Remove San Francisco specific aliases.
    git-svn-id: https://svn.urbansim.org/src/trunk@17609 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- zurich_parcel/parcel/aliases.py
+++ zurich_parcel/parcel/aliases.py
@@ -6,13 +6,8 @@
 # one of the aliases uses a primary attribute in the expression, the other a variable
 
 aliases = [
-       "number_of_businesses = parcel.aggregate(sanfrancisco.building.number_of_businesses)",
-       "employment = parcel.aggregate(sanfrancisco.building.employment)",
-       "number_of_households = parcel.aggregate(sanfrancisco.building.number_of_households)",
-       "population = parcel.aggregate(sanfrancisco.building.population)", 
        "building_sqft_per_unit = safe_array_divide(parcel.building_sqft, parcel.residential_units)",
        "building_sqft = parcel.aggregate(building.building_sqft)",
-       "housing_value = safe_array_divide((parcel.land_val + parcel.struc_val), sanfrancisco.parcel.residential_units)",
        "lot_sqft_per_unit = safe_array_divide(parcel.lot_sf, parcel.residential_units)",
        "residential_units = parcel.aggregate(building.residential_units)",
        "remaining_non_residential_sqft_capacity = parcel.non_residential_sqft_capacity - parcel.aggregate(building.non_residential_sqft)",
