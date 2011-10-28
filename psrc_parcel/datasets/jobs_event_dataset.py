95fb7334f8ed6d7ebb167b2b1fd99b59eb5d95b2
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-10-28 01:20:38 +0000

    Agent event model added
    git-svn-id: https://svn.urbansim.org/src/trunk@17627 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ psrc_parcel/datasets/jobs_event_dataset.py
@@ -0,0 +1,14 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from urbansim.datasets.dataset import Dataset as UrbansimDataset
+
+class JobsEventDataset(UrbansimDataset):
+    """Set of events that represent deletion/adding/replacing of jobs from/to specific locations.
+    """
+
+    id_name_default = ["jobs_event_id"]
+    in_table_name_default = "jobs_events"
+    out_table_name_default = "jobs_events"
+    dataset_name = "jobs_event"
