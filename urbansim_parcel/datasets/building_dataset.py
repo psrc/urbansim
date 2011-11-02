7b558eea5011a5abc810435bc400f5f0673bb862
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-11-02 00:28:21 +0000

    added coordinate system
    git-svn-id: https://svn.urbansim.org/src/trunk@17657 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/datasets/building_dataset.py
+++ urbansim_parcel/datasets/building_dataset.py
@@ -14,3 +14,11 @@ class BuildingDataset(UrbansimDataset):
     def __init__(self, id_values=None, **kwargs):
         UrbansimDataset.__init__(self, **kwargs)
         
+    def compute_coordinate_system(self, dataset_pool):
+        parcels = dataset_pool.get_dataset('parcel')
+        pcl_coordsys = parcels.get_coordinate_system()
+        if pcl_coordsys is not None:
+            self.compute_variables(["%s = building.disaggregate(parcel.%s)" % (pcl_coordsys[0], pcl_coordsys[0]), 
+                                    "%s = building.disaggregate(parcel.%s)" % (pcl_coordsys[1], pcl_coordsys[1])],
+                               dataset_pool=dataset_pool)
+        return (pcl_coordsys)
