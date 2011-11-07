bc532c762835f81f1b3de6bf8349687b2d5df707
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-11-07 18:25:16 +0000

    Added a call of add_and_solve_dependencies for attributes that were removed from the dependencies list.
    git-svn-id: https://svn.urbansim.org/src/trunk@17669 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/building/job_capacity_computed_if_necessary.py
+++ psrc_parcel/building/job_capacity_computed_if_necessary.py
@@ -16,8 +16,6 @@ class job_capacity_computed_if_necessary(Variable):
     def dependencies(self):
         return ["urbansim_parcel.building.non_residential_sqft",
                 "urbansim_parcel.building.building_sqft_per_job",
-                #"building.job_capacity", 
-                #"building.year_built",
                 ]
 
     def compute(self,  dataset_pool):
@@ -28,6 +26,8 @@ class job_capacity_computed_if_necessary(Variable):
         ## only do this when job_capacity and year_built are primary attributes of buildings
         known_names = buildings.get_known_attribute_names()
         if 'job_capacity' in known_names and 'year_built' in known_names:
+            self.add_and_solve_dependencies(["building.job_capacity", "building.year_built"],
+                                                dataset_pool=dataset_pool)
             base_year = SimulationState().get_start_time()
             job_spaces[buildings['year_built']<=base_year] = buildings['job_capacity'][buildings['year_built']<=base_year]
         return job_spaces
