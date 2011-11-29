656aeb8a710e0fe43e40f213f05207a9cb5624f4
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-11-29 21:26:09 +0000

    handled division by zero
    git-svn-id: https://svn.urbansim.org/src/trunk@17741 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/building/sampling_by_residence_large_area_DDD.py
+++ psrc_parcel/building/sampling_by_residence_large_area_DDD.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
 # See opus_core/LICENSE 
 
-from numpy import array, where, ones
+from numpy import where, zeros
 from opus_core.variables.variable import Variable
 
 class sampling_by_residence_large_area_DDD(Variable):
@@ -19,7 +19,10 @@ class sampling_by_residence_large_area_DDD(Variable):
         vacant = (ds["vacant_residential_units"]>0).sum()
         this_area_index = where(ds['large_area_id'] == self.large_area_id)[0]
         vacant_this_area = (ds["vacant_residential_units"][this_area_index]>0).sum()
-        result = 35.0/(vacant-vacant_this_area) * ones(ds.size()).astype('float32')
-        result[this_area_index] = 65.0/vacant_this_area
+        result = zeros(ds.size()).astype('float32')
+        if vacant-vacant_this_area > 0:
+            result[:] = 35.0/(vacant-vacant_this_area)
+        if vacant_this_area > 0:
+            result[this_area_index] = 65.0/vacant_this_area
         return result
         
