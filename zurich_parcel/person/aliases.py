ce54875cc6e006efba76ad93ff55d62febc40d1c
schirpat <schirpat@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-11-30 08:49:47 +0000

    correct mistake.... replace '' through ""
    git-svn-id: https://svn.urbansim.org/src/trunk@17749 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- zurich_parcel/person/aliases.py
+++ zurich_parcel/person/aliases.py
@@ -2,9 +2,9 @@
 # Copyright (C) 2010-2012 ETH Zurich
 # See project SustainCity
 
-aliases = ['is_worker = person.worker > 0',
-           'is_non_home_based_worker = numpy.logical_and(zurich_parcel.person.is_worker, person.work_at_home!=1)',
-          'is_non_home_based_worker_with_job = numpy.logical_and(zurich_parcel.person.is_non_home_based_worker, person.job_id > 0)'
+aliases = ["is_worker = person.worker > 0",
+           "is_non_home_based_worker = numpy.logical_and(zurich_parcel.person.is_worker, person.work_at_home!=1)",
+           "is_non_home_based_worker_with_job = numpy.logical_and(zurich_parcel.person.is_non_home_based_worker, person.job_id > 0)"
    #copy of urbansim_parcel       
 #           'is_worker_without_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id <= 0)',
  #          'is_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id > 0)',
