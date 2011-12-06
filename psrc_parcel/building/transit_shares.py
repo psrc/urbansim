306abccae309505c992bf6cdd8e859840eeec9ab
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-12-06 22:30:24 +0000

    minor change and doc string
    git-svn-id: https://svn.urbansim.org/src/trunk@17756 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/building/transit_shares.py
+++ psrc_parcel/building/transit_shares.py
@@ -6,22 +6,38 @@ from opus_core.variables.variable import Variable
 from numpy import arange, zeros, array, where
 
 class transit_shares(Variable):
+    """ This variable is to be used to compute correction weights in the WESML estimation procedure.
+    It computes shares W_g/H_g as defined in Ben-Akiva & Lermann (p.238, Eq 8.34) where g is a building.
+    It is using the expansion factor from the household survey which has different factors for
+    households living in five different areas each of which is divided into an out of transit area and 
+    in transit area. The numbers are taken from the PSRC 2006 Household Activity Survey Analysis Report, page E-3.
+    """
     _return_type = "float32"
 
-    la_array = zeros((5, 19), dtype='bool8')
-    la_array[0, 6] = True # Seattle
-    la_array[1, [5,7,8,9,10,11]] = True # rest King 
-    la_array[2, arange(1,5)] = True # Snohomish
-    la_array[3, arange(12,16)] = True # Pierce
-    la_array[4, arange(16,19)] = True # Kitsap
-    weights = array([[223.13, 404.58, 364.77, 460.91, 159.16], [187.38, 359.14, 303.71, 335.88, 160.12 ]])
-    number_hh_est = array([[134, 766, 566, 572, 593], [1275, 523, 189, 95, 26]])
-    number_hh_survey = array([[6917, 308692, 194788, 250733, 91674],[254457, 175978, 59223, 32244, 2722]])
+    # expansion factor from household survey 
+    #                Seattle  King    Snoh    Pierce  Kitsap
+    weights = array([[223.13, 404.58, 364.77, 460.91, 159.16], # out of transit area
+                     [187.38, 359.14, 303.71, 335.88, 160.12 ]]) # in transit area
+    # number of records in the estimation dataset per area
+    number_hh_est = array([[134, 766, 566, 572, 593], # out of transit area
+                           [1275, 523, 189, 95, 26]]) # in transit area
+    # number of records in the household survey per area
+    number_hh_survey = array([[6917, 308692, 194788, 250733, 91674], # out of transit area
+                              [254457, 175978, 59223, 32244, 2722]]) # in transit area
     total_transit_est = float(number_hh_est.sum())
     total_transit_observed = (weights*number_hh_survey).sum()
     W = (weights*number_hh_survey)/total_transit_observed
     H = number_hh_est/total_transit_est
     share_ratio = W/H
+    
+    # dictionary that assigns large area ids to the five areas above
+    la_area = {
+        0: [6],             # Seattle
+        1: [5,7,8,9,10,11], # rest King 
+        2: [1,2,3,4],       # Snohomish
+        3: [12,13,14,15],   # Pierce
+        4: [16,17,18]       # Kitsap
+    }
     def dependencies(self):
         return ["urbansim_parcel.building.large_area_id", 
                 "is_in_transit_zone = building.disaggregate(parcel.is_in_transit_zone)"]
@@ -32,10 +48,11 @@ class transit_shares(Variable):
 
         for group in arange(5):
             large_areas = zeros(ds.size(), dtype='bool8')
-            which_la = where(self.la_array[group,:])[0]
+            which_la = self.la_area[group]
             for la in which_la:
                 large_areas[ds["large_area_id"]==la] = True
             for inout in [0,1]:
                 idx = (ds["is_in_transit_zone"] == inout)*large_areas
                 result[where(idx)] = self.share_ratio[inout, group]
         return result
+    
