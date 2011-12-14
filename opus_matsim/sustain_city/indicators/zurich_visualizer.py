27233a4f901b65348ad9072649312e3dc743252a
tnicolai <tnicolai@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2011-12-14 10:31:34 +0000

    testing indicators
    git-svn-id: https://svn.urbansim.org/src/trunk@17782 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_matsim/sustain_city/indicators/zurich_visualizer.py
+++ opus_matsim/sustain_city/indicators/zurich_visualizer.py
@@ -9,14 +9,13 @@
 import os
 from opus_gui.results_manager.run.indicator_framework.representations.indicator import Indicator
 
-project_name = pn = 'zurich_parcel' #'seattle_parcel'
+project_name = pn = 'zurich_parcel'
 # setting project name to environment. this is needed for the visualizer
 os.environ['OPUSPROJECTNAME'] = pn
 
 years_arr = [2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010]
-
 # zurich scenario from 2000 to 2010
-cache_directory = r'/Users/sustaincity/Development/opus_home/data/zurich_parcel_from_20111014/runs/run_4.test_no_action_10pct_30it_2000-2010'
+cache_directory = r'/Users/sustaincity/Development/opus_home/data/zurich_parcel_from_20111014/runs/run_3.test_no_action_1pct_30it_2000-2010_warm-hot-start'
 
 types = ['mapnik_map']
 
@@ -26,20 +25,17 @@ indicators = {
     
     'population':Indicator(
         dataset_name = 'zone',
-        attribute = 'urbansim_parcel.zone.population' ),
+        attribute = 'zone.aggregate(urbansim.zone.population)' ),
               
     # No further division into single family or multi family residential!  (Wohngebaeude)
     'residential_units':Indicator(
         dataset_name = 'zone',
         attribute = 'zone.aggregate(urbansim_parcel.parcel.residential_units)' ),
-    'residential_unit_price':Indicator(
-        dataset_name = 'zone',
-        attribute = 'zone.aggregate(building.average_value_per_unit * building.residential_units * (building.building_type_id==1))' ),          
+    #'residential_unit_price':Indicator(
+    #    dataset_name = 'zone',
+    #    attribute = 'zone.aggregate(urbansim_zone.building.average_value_per_unit * building.residential_units)' ),          
      
      # Verwaltung
-    'amin_sqft':Indicator(
-        dataset_name = 'zone',
-        attribute = 'zone.aggregate(building.non_residential_sqft * (building.building_type_id==0))' ),
     'amin_sqft':Indicator(
         dataset_name = 'zone',
         attribute = 'zone.aggregate(building.non_residential_sqft * (building.building_type_id==0))' ),
@@ -51,23 +47,23 @@ indicators = {
     'number_of_households':Indicator(
         dataset_name = 'zone',
         attribute = 'urbansim_parcel.zone.number_of_households' ),
-    'average_income':Indicator(
-        dataset_name = 'zone',
-        attribute = 'urbansim_parcel.zone.average_income' ),
+    #'average_income':Indicator(
+    #    dataset_name = 'zone',
+    #    attribute = 'urbansim_parcel.zone.average_household_income' ),
               
     # travel time dependent indicators
-    'employment_within_10_minutes_travel_time_hbw_am_drive_alone':Indicator(
-        dataset_name = 'zone',
-        attribute = 'urbansim_parcel.zone.employment_within_10_minutes_travel_time_hbw_am_drive_alone' ),
-    'employment_within_30_minutes_travel_time_hbw_am_drive_alone':Indicator(
-        dataset_name = 'zone',
-        attribute = 'urbansim_parcel.zone.employment_within_30_minutes_travel_time_hbw_am_drive_alone' ),
-    'travel_time_to_611':Indicator( # in zone 611 is the main station 
-       dataset_name = 'zone',
-       attribute = 'psrc.zone.travel_time_hbw_am_drive_alone_to_611'),
-    'travel_time_accessibility':Indicator( # in zone 611 is the main station 
-       dataset_name = 'zone',
-       attribute = 'zone.travel_time_accessibility'),
+    #'employment_within_10_minutes_travel_time_hbw_am_drive_alone':Indicator(
+    #    dataset_name = 'zone',
+    #    attribute = 'urbansim_parcel.zone.employment_within_10_minutes_travel_time_hbw_am_drive_alone' ),
+    #'employment_within_30_minutes_travel_time_hbw_am_drive_alone':Indicator(
+    #    dataset_name = 'zone',
+    #    attribute = 'urbansim_parcel.zone.employment_within_30_minutes_travel_time_hbw_am_drive_alone' ),
+    #'travel_time_to_611':Indicator( # in zone 611 is the main station 
+    #   dataset_name = 'zone',
+    #   attribute = 'psrc.zone.travel_time_hbw_am_drive_alone_to_611'),
+    #'travel_time_accessibility':Indicator( # in zone 611 is the main station 
+    #   dataset_name = 'zone',
+    #   attribute = 'zone.travel_time_accessibility'),
            
 }
 print "... done."
@@ -87,9 +83,9 @@ source_data = SourceData(
    #comparison_cache_directory = cache_directory,
    years = years_arr,
    dataset_pool_configuration = DatasetPoolConfiguration(
-         package_order=['zurich_parcel','urbansim_parcel', 'urbansim', 'psrc', 'opus_core'],
+         package_order=['zurich_parcel','urbansim_parcel', 'urbansim_zone' 'urbansim', 'psrc', 'opus_core'],
          ),
-   name = project_name
+   #name = project_name
 )
 
 print "... done."
@@ -120,11 +116,80 @@ visualizations = []
 
 # View an indicator as a Map
 visualizations += visualizer.visualize(
-    indicators_to_visualize = ['population'], #override default indicators to visualize (all)
+    indicators_to_visualize = ['population'],
     computed_indicators = computed_indicators,
     visualization_type = 'mapnik_map',
     name = 'population'
     )
 
+visualizations += visualizer.visualize(
+    indicators_to_visualize = ['residential_units'], 
+    computed_indicators = computed_indicators,
+    visualization_type = 'mapnik_map',
+    name = 'residential_units'
+    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['residential_unit_price'], 
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'residential_unit_price'
+#    )
+
+visualizations += visualizer.visualize(
+    indicators_to_visualize = ['amin_sqft'], 
+    computed_indicators = computed_indicators,
+    visualization_type = 'mapnik_map',
+    name = 'amin_sqft'
+    )
+
+visualizations += visualizer.visualize(
+    indicators_to_visualize = ['number_of_jobs'], 
+    computed_indicators = computed_indicators,
+    visualization_type = 'mapnik_map',
+    name = 'number_of_jobs'
+    )
+
+visualizations += visualizer.visualize(
+    indicators_to_visualize = ['number_of_households'], 
+    computed_indicators = computed_indicators,
+    visualization_type = 'mapnik_map',
+    name = 'number_of_households'
+    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['average_income'],
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'average_income'
+#    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['employment_within_10_minutes_travel_time_hbw_am_drive_alone'], 
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'employment_within_10_minutes_travel_time_hbw_am_drive_alone'
+#    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['employment_within_30_minutes_travel_time_hbw_am_drive_alone'], 
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'employment_within_30_minutes_travel_time_hbw_am_drive_alone'
+#    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['travel_time_to_611'], 
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'travel_time_to_611'
+#    )
+
+#visualizations += visualizer.visualize(
+#    indicators_to_visualize = ['travel_time_accessibility'], 
+#    computed_indicators = computed_indicators,
+#    visualization_type = 'mapnik_map',
+#    name = 'travel_time_accessibility'
+#    )
 
 print 'Finished with creating indicators'
