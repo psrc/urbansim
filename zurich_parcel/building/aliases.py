ef616104245e0a0c4f8037841610f08a42f2cabb
schirpat <schirpat@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-01-20 16:03:54 +0000

    aliases for real estate price model
    git-svn-id: https://svn.urbansim.org/src/trunk@17839 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/building/aliases.py
@@ -0,0 +1,9 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2012 ETH Zurich, Switzerland
+# See opus_core/LICENSE 
+
+
+
+aliases = [
+            "building_cost = building.improvement_value"
+           ]
