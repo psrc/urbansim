a3c40c3c4c8df17365f51497d4b0955b414af17f
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-01-29 00:45:49 +0000

    sampling weights variable for HLCM
    git-svn-id: https://svn.urbansim.org/src/trunk@17871 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- paris/building/M_DDDjt.py
+++ paris/building/M_DDDjt.py
@@ -18,20 +18,26 @@ class M_DDDjt(Variable):
         current_year = SimulationState().get_current_time()
         lag = current_year - self.base_year
         
+        TM_hit = "TM_{0}t = alldata.aggregate_all(numpy.logical_and(household.hh_type=={0}, household.building_id<=0))".format(self.type_id) if lag > 0 else "TM_{0}t = alldata.aggregate_all(household.hh_type=={0})".format(self.type_id)
+
+        TM_hi0 = "TM_{0}0 = paris.alldata.number_of_households_of_type_{0}_lag{1}".format(self.type_id, lag) if lag > 0 else "TM_{0}0 = alldata.aggregate_all(household.hh_type=={0})".format(self.type_id)
+
+        M_hij0 = "M_{0}j0 = paris.building.number_of_households_of_type_{0}_lag{1}".format(self.type_id, lag) if lag > 0 else "M_{0}j0 = paris.building.number_of_households_of_type_{0}".format(self.type_id)
+
+        V_ij0 = "V_ij0 = urbansim_zone.building.vacant_residential_units_lag{0}".format(lag) if lag > 0 else "V_ij0 = urbansim_zone.building.vacant_residential_units"
         return [
-                "M_hij0 = paris.building.number_of_households_of_type_{0}_lag{1}".format(self.type_id, lag),
-                "TM_hit = alldata.aggregate_all(numpy.logical_and(household.hh_type=={0}, household.building_id<=0))".format(self.type_id),
-                "TM_hi0 = paris.alldata.number_of_households_of_type_{0}_lag{1}".format(self.type_id, lag),
+                M_hij0,
+                TM_hit,
+                TM_hi0,
                 "V_ijt = urbansim_zone.building.vacant_residential_units",
-                "V_ij0 = urbansim_zone.building.vacant_residential_units_lag{}".format(lag),
+                V_ij0
                 
                 ]
 
     def compute(self, dataset_pool):
-        import ipdb; ipdb.set_trace()
         ds = self.get_dataset()
         alldata = dataset_pool.get_dataset('alldata')
-        results = ds['M_hij0'] * safe_array_divide(alldata['TM_hit'], alldata['TM_hi0']) * safe_array_divide(ds['V_ijt'], ds['V_ij0'])
+        results = ds['M_{}j0'.format(self.type_id)] * safe_array_divide(alldata['TM_{}t'.format(self.type_id)], alldata['TM_{}0'.format(self.type_id)]) * safe_array_divide(ds['V_ijt'], ds['V_ij0'])
         return results
 
 
