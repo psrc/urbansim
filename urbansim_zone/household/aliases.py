1de098a9593c042e99236a215a9e0e86c00e609c
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-02-06 05:14:48 +0000

    added a household.lifestyle variable
    git-svn-id: https://svn.urbansim.org/src/trunk@17894 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_zone/household/aliases.py
+++ urbansim_zone/household/aliases.py
@@ -4,4 +4,10 @@
 aliases = [
         "building_type_id = household.disaggregate(building.building_type_id)",
         "zone_id = household.disaggregate(building.zone_id)",
+        "lifestyle = 1 * (household.family_type == 2) * (household.children == 0) + " + \
+                     "2 * (household.family_type == 2) * (household.children == 1) * (household.ownrent==1) + " +  \
+                     "3 * (household.family_type == 2) * (household.children == 1) * (household.ownrent==2) + " + \
+                     "4 * (household.family_type == 1)",
+        "lifestylev = 5 * (household.vehicles == 0) + (urbansim_zone.household.lifestyle) * (household.vehicles > 0)"
            ]
+
