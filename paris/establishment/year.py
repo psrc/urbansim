d19fb373bfd9584081963e55954b1cde08fce3fb
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-03-04 18:53:36 +0000

    fixing variables for paris_zone
    git-svn-id: https://svn.urbansim.org/src/trunk@17932 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ paris/establishment/year.py
@@ -0,0 +1,18 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from opus_core.variables.variable import Variable
+from numpy import ones
+from opus_core.simulation_state import SimulationState
+
+class year(Variable):
+    """"""
+    def dependencies(self):
+        return []
+
+    def compute(self, dataset_pool):
+        current_year = SimulationState().get_current_time()
+        dataset = self.get_dataset()
+        year = ones( dataset.size(), dtype='i4' ) + current_year
+        return year
