20db87b925df31d2f47a5da641404cd77ee2a2c3
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-03-04 18:53:09 +0000

    fixing variables for paris_zone
    git-svn-id: https://svn.urbansim.org/src/trunk@17931 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- paris/establishment/ln_emp_ratio.py
+++ paris/establishment/ln_emp_ratio.py
@@ -3,19 +3,27 @@
 # See opus_core/LICENSE
 
 from opus_core.variables.variable import Variable, ln_bounded
+from opus_core.simulation_state import SimulationState
 
 class ln_emp_ratio(Variable):
-    """Natural log of the inc for this gridcell"""
+    """"""
         
     _return_type="float32"
+    base_year = 1999
 
     def dependencies(self):
+        current_year = SimulationState().get_current_time()
+        if current_year == self.base_year: #for estimation
+            lag = 2
+        else: #for simulation
+            lag = 1
+
         return [
                "lnemp=ln_bounded(establishment.employment)",
-               "lnemp_pre1=ln_bounded(establishment.employment_lag1)",
+               "lnemp_pre=ln_bounded(establishment.employment_lag%s)" % lag,
                ]
 
     def compute(self, dataset_pool):
         dataset = self.get_dataset()
-        result = dataset['lnemp'] - dataset['lnemp_pre1']
+        result = dataset['lnemp'] - dataset['lnemp_pre']
         return result
