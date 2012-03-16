1c03bf13ed86c645ed2472e00b88446a0454c6d6
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-03-16 06:06:36 +0000

    added bayarea package that support on-the-fly submarket dataset
    git-svn-id: https://svn.urbansim.org/src/trunk@17956 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ bayarea/household/aliases.py
@@ -0,0 +1,20 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+"""
+       income breaks:
+          1 - Less than $25K;
+          2 - $25K to $45K;
+          3 - $45 to $75K;
+          4 - More than $75K.
+"""
+
+aliases = [
+    "income_4 = 1 * (household.income < 25000) +" + \
+                         " 2 * numpy.logical_and(household.income >= 25000, household.income < 45000) +" + \
+                         " 3 * numpy.logical_and(household.income >= 45000, household.income < 75000) +" + \
+                         " 4 * (household.income >= 75000)",
+    "submarket_id = household.disaggregate(bayarea.building.submarket_id)",
+
+           ]
