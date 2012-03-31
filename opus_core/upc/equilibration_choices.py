fd16e9a97ebdfca437b00600c9b0fc8791e2d452
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-03-31 01:21:07 +0000

    wired equilibration_choices into choice_model; added unittest
    git-svn-id: https://svn.urbansim.org/src/branches/upc@17996 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/upc/equilibration_choices.py
+++ opus_core/upc/equilibration_choices.py
@@ -6,40 +6,64 @@ from opus_core.upc.lottery_choices import lottery_choices
 from scipy.optimize import fmin_l_bfgs_b
 from opus_core.logger import logger
 from copy import copy
-import numexpr as ne
+import numpy as np
+try:
+    import numexpr as ne
+except:
+    ne = None
 
 class equilibration_choices(lottery_choices):
+
     def run(self, 
             probability=None, 
             utilities=None, 
             capacity=None,
             resources=None):
         """
+
         """
 
-        #resources.check_obligatory_keys(["capacity"])
-        self.capacity = resources.get("capacity", capacity)
+        resources.check_obligatory_keys(['price_coef_name'])
+        if utilities is None:
+            utilities = resources.get("utilities")
+        self.capacity = capacity
+        if self.capacity is None:
+            self.capacity = resources.get("capacity", None)
+        assert self.capacity is not None
+        nagents, nalts = utilities.shape
         ##price of shape (1, nalts)
-        self.price = resources.get("price")
-        ##price_beta of shape (nagents, 1)
-        self.price_beta = resources.get("price_beta")  
-        self.U_cached = utilities - np.dot(self.price_beta, self.price.T)
-        price0 = copy(self.price)
-        results =fmin_l_bfgs_b(self.target_func, price0, fprime=self.fprime, 
-                               args=(supply, beta), bounds=bounds0, factr=1e12, 
-                               iprint=10)
+        self.price = np.empty((1, nalts))
+        self.price[...] = resources.get("price")
+        assert self.price.shape == (1, nalts)
+        ## agent specific beta for price, of shape (nagents, 1)
+        self.beta = resources.get("price_beta")  
+        assert self.beta.shape == (nagents, 1)
+
+        self.U_cached = utilities - np.dot(self.beta, self.price)
+        price_init = copy(self.price)
+        bounds0 = None
+        kwargs = {'factr': 1e14, 'iprint':10}
+        kwargs.update(resources.get('bfgs_kwargs', {}))
+        results = fmin_l_bfgs_b(self.target_func, price_init, 
+                                fprime=self.fprime, args=(self.capacity, self.beta), 
+                                bounds=bounds0,
+                                **kwargs)
+
         price_converged = results[0]
+        resources.merge({'price_converged': price_converged})
         demand, prob = self.update_demand(price_converged)
-        lottery_choices.run(self, probability=prob,
-                           resources=resources)
-                          )
+        return lottery_choices.run(self, probability=prob,
+                                   resources=resources)
         
     def update_price(self, price):
-        self.price = price
+        """
+        this can be expanded to allow various function form of price
+        """
+        self.price[...] = price
 
-    def update_demand(price):
+    def update_demand(self, price):
         self.update_price(price)
-        addition = np.dot(self.price_beta, self.price.T)
+        addition = np.dot(self.beta, self.price)
         U_cached = self.U_cached
         out = ne.evaluate("exp(U_cached + addition)")
         sumV = ne.evaluate("sum(out, axis=1)")[:, np.newaxis]
@@ -48,31 +72,43 @@ class equilibration_choices(lottery_choices):
         return demand, out
 
     def target_func(self, price, supply, beta=None):
-        demand, _ = update_demand(price)
-        surplus = ne.evaluate("sum((demand - supply)**2)")
+        demand, _ = self.update_demand(price)
+        
+        if ne is not None:
+            surplus = ne.evaluate("sum((demand - supply)**2)")
+        else:
+            surplus = np.sum((demand - supply)**2)
         #print surplus
         return surplus
 
     def fprime(self, price, supply, beta):
-        demand, prob = update_demand(price)
-        cross = cross_deriv(prob, beta)
+        demand, prob = self.update_demand(price)
+        cross = self.cross_deriv(prob, beta)
         grad = np.sum(2 * (demand - supply) * cross, axis=1)
         return grad
 
     def own_deriv(self, prob, beta, _prob_beta=None):
-        if _prob_beta is None:
-            out = ne.evaluate("prob * (1 - prob) * beta")
-            #out = prob * (1 - prob) * beta
+        if ne is not None:
+            if _prob_beta is None:
+                out = ne.evaluate("prob * (1 - prob) * beta")
+            else:
+                out = ne.evaluate("_prob_beta * (prob - 1)")
+            out = ne.evaluate("sum(out, axis=0)")
         else:
-            out = ne.evaluate("_prob_beta * (prob - 1)")
-            #out = _prob_beta * (prob - 1)
-        out = ne.evaluate("sum(out, axis=0)")
+            if _prob_beta is None:
+                out = prob * (1 - prob) * beta
+            else:
+                out = _prob_beta * (prob - 1)
+            out = out.sum(axis=0)
         return out
 
     def cross_deriv(self, prob, beta):
-        _prob_beta = ne.evaluate("- prob * beta")
+        if ne is not None:
+            _prob_beta = ne.evaluate("- prob * beta")
+        else:
+            _prob_beta = - prob * beta
         out = np.dot( prob.T, _prob_beta )
-        own = own_deriv(prob, beta, _prob_beta)
+        own = self.own_deriv(prob, beta, _prob_beta)
         np.fill_diagonal(out, own)
         return out
 
