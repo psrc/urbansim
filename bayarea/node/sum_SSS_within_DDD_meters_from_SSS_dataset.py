5594e200d91ee897efe75120a7dbaf8110f3d478
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-06 07:46:04 +0000

    (ejanowicz) node-level accessibility variables
    git-svn-id: https://svn.urbansim.org/src/trunk@18043 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ bayarea/node/sum_SSS_within_DDD_meters_from_SSS_dataset.py
@@ -0,0 +1,20 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from opus_core.variables.variable import Variable
+
+class sum_SSS_within_DDD_meters_from_SSS_dataset(Variable):
+    """
+    """
+    
+    def __init__(self, attribute, distance, table):
+        Variable.__init__(self)
+        self.attribute = attribute
+        self.distance = distance
+        self.table = table
+        
+    def compute(self,  dataset_pool):
+        nodes = dataset_pool.get_dataset('node')
+        result = nodes.subbuilding_sum_query(self.distance, self.attribute, self.table)
+        return result
