08dd1fe2a575cf70526f39405790409e6e778a26
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-13 20:41:03 +0000

    (ejanowicz) variables for submarket hlcm
    git-svn-id: https://svn.urbansim.org/src/trunk@18070 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/building/aliases.py
+++ bayarea/building/aliases.py
@@ -4,5 +4,10 @@
 
 aliases = [
     "zone_id = building.disaggregate(parcel.zone_id)",
-
+    "superd = building.disaggregate(parcel.superd)",
+    "neighborhood_type = building.disaggregate(parcel.neighborhood_type)",
+    "building_type = 1*(building.building_type_id==1) + 2*(building.building_type_id==2) + 3*(building.building_type_id>2)",
+    "tenure_id = 1*(building.tenure<2) + 2*(building.tenure==2)",
+    "building_type_id = building.building_type_id"
            ]
+#"building_type = 1*(building.building_type_id==1) + 2*(building.building_type_id==2) + 3*(building.building_type_id>2)"
