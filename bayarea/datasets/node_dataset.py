81ecf32a013a3443aa9cb15d052cd2b2b9c5d285
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-23 18:22:25 +0000

    (ejanowicz) move initializePOIs to line 27
    git-svn-id: https://svn.urbansim.org/src/trunk@18138 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/datasets/node_dataset.py
+++ bayarea/datasets/node_dataset.py
@@ -24,6 +24,7 @@ class NodeDataset(UrbansimDataset):
     pya = PyAccess()
     pya.createGraph(1,d['nodeids'],d['nodes'],d['edges'],d['edgeweights'])
     pya.precomputeRange(500)
+    pya.initializePOIs(1,.5*1.6*1000,1)
 
     def __init__(self, **kwargs):
         UrbansimDataset.__init__(self, **kwargs)
@@ -39,7 +40,7 @@ class NodeDataset(UrbansimDataset):
         x = numpy.load((os.path.join(paths.get_opus_data_path_path('bay_area_parcel','base_year_data','2010','transit_stations','x.lf4'))))
         y = numpy.load((os.path.join(paths.get_opus_data_path_path('bay_area_parcel','base_year_data','2010','transit_stations','y.lf4'))))
         xys = numpy.column_stack((x,y))
-        self.pya.initializePOIs(1,.5*1.6*1000,1)
+        #self.pya.initializePOIs(1,.5*1.6*1000,1)
         self.pya.initializeCategory(0,xys)
    
     # def design_variable_query(self, distance):
