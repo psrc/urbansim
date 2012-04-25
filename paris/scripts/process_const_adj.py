f111be17f15ac1fc80506b903036061a03574ee1
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-25 23:31:21 +0000

    added GPL header
    git-svn-id: https://svn.urbansim.org/src/trunk@18154 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- paris/scripts/process_const_adj.py
+++ paris/scripts/process_const_adj.py
@@ -1,3 +1,7 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
+
 from pandas import read_csv, read_clipboard, DataFrame
 from itertools import product
 import numpy as np
--- paris/scripts/totemp_diff.py
+++ paris/scripts/totemp_diff.py
@@ -1,3 +1,6 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE
 
 import sys
 import numpy as np
