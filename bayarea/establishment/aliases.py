e56798f95ee6db83ff2a0d1233018ce83a478883
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-27 06:18:18 +0000

    blcm variables
    git-svn-id: https://svn.urbansim.org/src/trunk@18158 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ bayarea/establishment/aliases.py
@@ -0,0 +1,7 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+aliases = [
+    "employment_submarket_id = establishment.disaggregate(bayarea.building.employment_submarket_id)",
+           ]
