9d48955872ac3642754b9bb4b31fe6e1ea9d6fe4
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-04-29 21:56:42 +0000

    first working script exporting opus_data to travel model
    git-svn-id: https://svn.urbansim.org/src/trunk@18195 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/travel_model/export_opus_data.py
+++ bayarea/travel_model/export_opus_data.py
@@ -2,7 +2,13 @@
 # Copyright (C) 2012 University of California, Berkeley, 2005-2009 University of Washington
 # See opus_core/LICENSE
 
+import os
 from opus_core.logger import logger, block, log_block
+from opus_core.session_configuration import SessionConfiguration
+from opus_core.simulation_state import SimulationState
+from opus_core.store.attribute_cache import AttributeCache
+from opus_core.store.csv_storage import csv_storage
+from opus_core.variables.variable_name import VariableName
 
 @log_block()
 def export_opus_data(config, year):
@@ -14,27 +20,49 @@ def export_opus_data(config, year):
     # http://mtcgis.mtc.ca.gov/foswiki/Main/DataDictionary
 
     """
-    
+
+    cache_directory = config['cache_directory']
+    simulation_state = SimulationState()
+    simulation_state.set_cache_directory(cache_directory)
+    simulation_state.set_current_time(year)
+    attribute_cache = AttributeCache()
+    package_order=config['dataset_pool_configuration'].package_order
+    dataset_pool = SessionConfiguration(new_instance=True,
+                                        package_order=package_order,
+                                        in_storage=attribute_cache
+                                       ).get_dataset_pool()
+
     tm_config = config['travel_model_configuration']
-    data_to_export = tm_config[''] 
+    data_to_export = tm_config['urbansim_to_tm_variable_mapping'] 
+    base_dir = tm_config['travel_model_base_directory']
     data_exchange_dir = tm_config[year]['data_exchange_dir']
-    for data_file, variables in data_to_export.iteritems():
-        data = {} #ordered_dict?
-        data_file = os.path.join(data_exchange_dir, data_file)
-        dataset_name = variables[0].split(".")[0]
+    data_exchange_dir = os.path.join(base_dir, data_exchange_dir)
+    out_storage = csv_storage(storage_location=data_exchange_dir)
+    for data_fname, variable_mapping in data_to_export.iteritems():
+        col_names = variable_mapping.values()
+        variables_aliases = ["=".join(mapping[::-1]) for mapping in \
+                             variable_mapping.iteritems()]
+        dataset_name = VariableName(variables_aliases[0]).get_dataset_name()
         dataset = dataset_pool.get_dataset(dataset_name)
-        dataset.compute_variables(variables)
-        for alias, variable in zip(aliases, variables):
-            data[alias] = dataset[variable]
-        with block("Writing data file {} for travel model".format(data_file)):
-            to_csv(data, data_file)
+        dataset.compute_variables(variables_aliases)
+        #data = {} 
+        #data_file = os.path.join(data_exchange_dir, data_fname)
+        #for col_name, variable in zip(col_names, variables):
+            #    data[col_name] = dataset[variable]
+        block_msg = "Writing {} for travel model to {}".format(data_fname,
+                                                         data_exchange_dir)
+        with block(block_msg):
+            dataset.write_dataset(attributes=col_names,
+                                out_storage=out_storage,
+                                out_table_name=data_fname)
 
 if __name__ == "__main__":
     from optparse import OptionParser
     from opus_core.resources import Resources
     from opus_core.file_utilities import get_resources_from_file
     parser = OptionParser()
-    parser.add_option("-r", "--resources", dest="resources_file_name", action="store", type="string",
+    parser.add_option("-r", "--resources", dest="resources_file_name", 
+                      action="store", type="string",
                       help="Name of file containing resources")
     parser.add_option("-y", "--year", dest="year", action="store", type="int",
                       help="Year in which to 'run' the travel model")
