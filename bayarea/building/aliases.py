6857a0879813dc2c75c6c689737c2326714c981c
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-02 17:06:16 +0000

    changes to building aliases
    git-svn-id: https://svn.urbansim.org/src/trunk@18258 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/building/aliases.py
+++ bayarea/building/aliases.py
@@ -4,13 +4,12 @@
 
 aliases = [
     "zone_id = building.disaggregate(parcel.zone_id)",
-    "superdistrict_id = building.disaggregate((parcel.superdistrict_id)*(parcel.superdistrict_id>0)+(parcel.superdistrict_id<1))",
-    "schooldistrict = building.disaggregate((parcel.schooldistrict_id)*(parcel.schooldistrict_id>0) + 99*(parcel.schooldistrict_id<1))",
-    "neighborhood_type = building.disaggregate((parcel.neighborhood_type + 1)*(parcel.neighborhood_type>-1)+ 6*(parcel.neighborhood_type<0))",
+    "superdistrict_id = building.disaggregate(parcel.superdistrict_id)",
+    "schooldistrict = building.disaggregate(bayarea.parcel.schooldistrict)",
+    "neighborhood_type = building.disaggregate(bayarea.parcel.neighborhood_type)",
     "building_type = 1*(building.building_type_id<2) + 2*(building.building_type_id==2) + 3*(building.building_type_id>2)",
     "jurisdiction_id = building.disaggregate(parcel.jurisdiction_id)",
     "tenure_id = 1*(building.tenure<2) + 2*(building.tenure==2)",
     "building_type_id = building.building_type_id",
-    "within_half_mile_transit = building.disaggregate(bayarea.node.transit_within_800_meters,intermediates=[parcel])"
+    "within_half_mile_transit = building.disaggregate(bayarea.parcel.within_half_mile_transit)"
            ]
-#"building_type = 1*(building.building_type_id==1) + 2*(building.building_type_id==2) + 3*(building.building_type_id>2)"
