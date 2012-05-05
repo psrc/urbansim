ef1f84c3a167d2e7953c8f80260b54ca0abb7aef
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-05 17:31:24 +0000

    changed output filename
    git-svn-id: https://svn.urbansim.org/src/trunk@18307 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/travel_model/export_opus_data.py
+++ bayarea/travel_model/export_opus_data.py
@@ -34,8 +34,8 @@ def export_opus_data(config, year):
 
     tm_config = config['travel_model_configuration']
     data_to_export = tm_config['urbansim_to_tm_variable_mapping'] 
-    base_dir = tm_config['travel_model_base_directory']
-    #base_dir = os.path.join(cache_directory, 'travel_model')
+    #base_dir = tm_config['travel_model_base_directory']
+    base_dir = os.path.join(cache_directory, 'travel_model')
     data_exchange_dir = tm_config[year]['data_exchange_dir']
     data_exchange_dir = os.path.join(base_dir, data_exchange_dir)
     out_storage = csv_storage(storage_location=data_exchange_dir)
@@ -50,14 +50,16 @@ def export_opus_data(config, year):
         #data_file = os.path.join(data_exchange_dir, data_fname)
         #for col_name, variable in zip(col_names, variables):
             #    data[col_name] = dataset[variable]
+
+        org_fname = os.path.join(data_exchange_dir, "%s.computed.csv" % data_fname)
+        new_fname = os.path.join(data_exchange_dir, "%s%s.csv" % (year,data_fname))
         block_msg = "Writing {} for travel model to {}".format(data_fname,
-                                                         data_exchange_dir)
+                                                               new_fname)
         with block(block_msg):
             dataset.write_dataset(attributes=col_names,
                                 out_storage=out_storage,
                                 out_table_name=data_fname)
-            org_fname = os.path.join(data_exchange_dir, "%s.computed.csv" % data_fname)
-            new_fname = os.path.join(data_exchange_dir, "%s.csv" % data_fname)
+            #rename & process header
             shutil.move(org_fname, new_fname)
             os.system("sed 's/:[a-z][0-9]//g' -i %s" % new_fname)
 
