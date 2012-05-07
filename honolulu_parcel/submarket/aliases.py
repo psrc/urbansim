81ce659931e9c97b4e713fac3f13cf400ff179e0
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-07 00:11:10 +0000

    change attribute name in honolulu zone table
    git-svn-id: https://svn.urbansim.org/src/trunk@18322 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- honolulu_parcel/submarket/aliases.py
+++ honolulu_parcel/submarket/aliases.py
@@ -5,5 +5,5 @@
 aliases = [
    "avg_price_per_unit_in_submarket = safe_array_divide(submarket.aggregate(building.improvement_value),submarket.aggregate(building.residential_units))",
    "avg_price_per_unit_in_district = submarket.disaggregate(district.aggregate(honolulu_parcel.submarket.avg_price_per_unit_in_submarket, function=sum)/district.number_of_agents(submarket))",
-   "district_id = submarket.disaggregate(zone.area_type_id)"
+   "district_id = submarket.disaggregate(zone.council_district_id)"
            ]
