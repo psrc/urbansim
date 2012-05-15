25cf13d139fe9839b4da1acb4b19d4a09b3b21ab
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-15 22:23:19 +0000

    new variables
    git-svn-id: https://svn.urbansim.org/src/trunk@18350 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/household/aliases.py
+++ bayarea/household/aliases.py
@@ -15,6 +15,7 @@ aliases = [
                          " 2 * numpy.logical_and(household.income >= 25000, household.income < 45000) +" + \
                          " 3 * numpy.logical_and(household.income >= 45000, household.income < 75000) +" + \
                          " 4 * (household.income >= 75000)",
+    "income_category = bayarea.household.income_4",
     "income_4_person_3 = 1 * (household.income < 25000)*(household.persons < 2) +" + \
                          " 2 * numpy.logical_and(household.income >= 25000, household.income < 45000)*(household.persons < 2) +" + \
                          " 3 * numpy.logical_and(household.income >= 45000, household.income < 75000)*(household.persons < 2) +" + \
--- bayarea/parcel/aliases.py
+++ bayarea/parcel/aliases.py
@@ -5,5 +5,6 @@
 aliases = [
     "schooldistrict = (parcel.schooldistrict_id)*(parcel.schooldistrict_id>0) + 99*(parcel.schooldistrict_id<1)",
     "neighborhood_type = (parcel.neighborhood_type + 1)*(parcel.neighborhood_type>-1)+ 6*(parcel.neighborhood_type<0)",
-    "within_half_mile_transit = parcel.disaggregate(bayarea.node.transit_within_800_meters)"
+    "within_half_mile_transit = parcel.disaggregate(bayarea.node.transit_within_800_meters)",
+    "parcel_acre = parcel.shape_area * 0.000247105381",
            ]
