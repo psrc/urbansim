708d7ba0311d7a9dc9b353b36e15e9000ca64a53
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-16 21:31:03 +0000

    prompt usage with incorrect arguments
    git-svn-id: https://svn.urbansim.org/src/trunk@18358 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/scripts/explore_run_cache.py
+++ bayarea/scripts/explore_run_cache.py
@@ -53,8 +53,12 @@ if __name__ == "__main__":
         else:
             cache_directory = _cache_directory
 
-
-    year = int(args[0])
+    try:
+        year = int(args[0])
+    except IndexError:
+        parser.error("year must be provided.")
+        parser.print_help()
+        sys.exit(1)
 
     if package_order is None:
         package_order = eval(options.package_order)
