3505a900f0254219e4ca500fd07f59b78ae82ffe
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-05-31 16:16:18 +0000

    Adding alias to filter agents that enter workplace location choice model.
    git-svn-id: https://svn.urbansim.org/src/trunk@18396 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- zurich_parcel/person/aliases.py
+++ zurich_parcel/person/aliases.py
@@ -4,7 +4,8 @@
 
 aliases = ["is_worker = person.worker > 0",
            "is_non_home_based_worker = numpy.logical_and(zurich_parcel.person.is_worker, person.work_at_home!=1)",
-           "is_non_home_based_worker_with_job = numpy.logical_and(zurich_parcel.person.is_non_home_based_worker, person.job_id > 0)"
+           "is_non_home_based_worker_with_job = numpy.logical_and(zurich_parcel.person.is_non_home_based_worker, person.job_id > 0)",
+           "is_in_labour_market_without_job = numpy.logical_and(person.workstat==1, person.job_id <= 0)"
    #copy of urbansim_parcel       
 #           'is_worker_without_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id <= 0)',
  #          'is_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_worker, person.job_id > 0)',
