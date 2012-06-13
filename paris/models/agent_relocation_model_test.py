0ed1c027def19c5a9f51cac14a564db992457375
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-13 23:18:36 +0000

    hlcm sampling test
    git-svn-id: https://svn.urbansim.org/src/trunk@18462 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ paris/models/agent_relocation_model_test.py
@@ -0,0 +1,50 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from opus_core.misc import DebugPrinter, unique
+from opus_core.upc_factory import UPCFactory
+from opus_core.logger import logger
+from opus_core.resources import Resources, merge_resources_if_not_None
+from opus_core.model import Model
+from numpy import where, array, concatenate, asarray, resize
+from urbansim.models.rate_based_model import RateBasedModel
+from opus_core.variables.variable_name import VariableName
+
+class AgentRelocationModel(RateBasedModel):
+    """Chooses agents for relocation (according to probabilities computed by the probabilities class),
+    such as employment relocation model, household relocation model. If probabilities is set to None, only unplaced agents are chosen.
+    The run method returns indices of the chosen agents.
+    """
+
+    def __init__(self,
+                 model_name = "Agent Relocation Model",
+                 location_id_name="grid_id",
+                 **kwargs
+                 ):
+        
+        self.location_id_name = location_id_name
+        RateBasedModel.__init__(self, model_name=model_name, 
+                                **kwargs)
+        
+    def run(self, agent_set, 
+            append_unplaced_agents_index=True,
+            **kwargs):
+        movers_indices = RateBasedModel.run(self, agent_set, **kwargs)
+        agent_set.modify_attribute(self.location_id_name, array(movers_indices.size*[-1]), index=movers_indices)
+        if (agent_set.size() > 0) and append_unplaced_agents_index:
+            # add unplaced agents
+            vn = VariableName(self.location_id_name).get_alias()
+            if vn not in agent_set.get_known_attribute_names():
+                agent_set.compute_variables(self.location_id_name)
+                self.location_id_name = vn
+            unplaced_agents = where(agent_set.get_attribute(self.location_id_name) <= 0)[0]
+            movers_indices = unique(concatenate((movers_indices, unplaced_agents)))
+        
+        logger.log_status("Number of total movers: " + str(movers_indices.size))
+        return movers_indices
+
+
+### In order to remove a circular dependency between this file and
+### household_location_choice_model_creator, these unit tests were moved into
+### urbansim.tests.test_agent_relocation_model.
