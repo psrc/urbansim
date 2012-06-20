fbf8ed702bcd9a5733cae8c01288c0206354cc7f
jmbarajas <jmbarajas@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-20 23:10:56 +0000

    Travel model backup script.
    git-svn-id: https://svn.urbansim.org/src/trunk@18522 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ bayarea/scripts/travel_model_backup.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Simple stupid backup for travel model on detroit
+# Expects detroit to be mounted in /mnt
+DATE_STRING=$(date +%Y%m%d)
+
+cd /var/backups
+tar czf travel_model_$DATE_STRING.tar.gz "/mnt"
+
+#Remove backup older than 4 weeks
+OLD_DATE=$(date --date="4 weeks ago" +%Y%m%d)
+if [ `ls | grep travel_model_$OLD_DATE.tar.gz | wc -l` > 0 ]; then
+	rm travel_model_$OLD_DATE.tar.gz
+fi
