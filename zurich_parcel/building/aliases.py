c1dd2ae61ef0ebe538678726e5fb8084af034b53
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-22 11:12:26 +0000

    git-svn-id: https://svn.urbansim.org/src/trunk@18537 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- zurich_parcel/building/aliases.py
+++ zurich_parcel/building/aliases.py
@@ -5,5 +5,21 @@
 
 
 aliases = [
-            "building_cost = building.improvement_value"
+           "building_cost = building.improvement_value",
+           "SC_total_spaces_1 = building.residential_units",
+           "SC_total_spaces_0 = building.non_residential_sqft",
+           "s1 = urbansim_parcel.building.number_of_jobs_of_sector_1 * 50",
+           "s2 = urbansim_parcel.building.number_of_jobs_of_sector_2 * 100",
+           "s3 = urbansim_parcel.building.number_of_jobs_of_sector_3 * 50",
+           "s4 = urbansim_parcel.building.number_of_jobs_of_sector_4 * 50",
+           "s5 = urbansim_parcel.building.number_of_jobs_of_sector_5 * 100",
+           "s6 = urbansim_parcel.building.number_of_jobs_of_sector_6 * 50",
+           "s7 = urbansim_parcel.building.number_of_jobs_of_sector_7 * 15",
+           "s8 = urbansim_parcel.building.number_of_jobs_of_sector_8 * 25",
+           "s49 = urbansim_parcel.building.number_of_jobs_of_sector_49 * 50",
+           "s99 = urbansim_parcel.building.number_of_jobs_of_sector_99 * 100",
+           "occupied_non_residential_spaces = zurich_parcel.building.s1 + zurich_parcel.building.s2 + zurich_parcel.building.s3+ zurich_parcel.building.s4 + zurich_parcel.building.s5 + zurich_parcel.building.s6 + zurich_parcel.building.s7 + zurich_parcel.building.s8 + zurich_parcel.building.s49 + zurich_parcel.building.s99",
+           "SC_occupied_spaces_1 = urbansim_parcel.building.occupied_residential_units * 1.2",
+           "SC_occupied_spaces_0 = zurich_parcel.building.occupied_non_residential_spaces * 2.4",
+           "building_test = building.land_area",
            ]
