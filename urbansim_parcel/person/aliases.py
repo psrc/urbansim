518281be1387b64e097bac06074aa2c767fbff8c
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-27 00:49:46 +0000

    parcel_id added to aliases
    git-svn-id: https://svn.urbansim.org/src/trunk@18586 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/person/aliases.py
+++ urbansim_parcel/person/aliases.py
@@ -10,5 +10,6 @@ aliases = ['is_worker = person.employment_status > 0',
            'zone_id=person.disaggregate(urbansim_parcel.household.zone_id)',
            'workplace_zone_id=person.disaggregate(urbansim_parcel.job.zone_id)',
            'building_id = person.disaggregate(household.building_id)',
-           'is_placed_non_home_based_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_non_home_based_worker_with_job, urbansim_parcel.person.building_id > 0)'
+           'is_placed_non_home_based_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_non_home_based_worker_with_job, urbansim_parcel.person.building_id > 0)',
+           'parcel_id = person.disaggregate(urbansim_parcel.household.parcel_id)'
            ]
