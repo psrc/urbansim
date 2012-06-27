7f1eb431512a96d284b033138878e382d1ade088
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-27 09:55:34 +0000

    Modifying id_name_default. scheduled_year -> year_built
    git-svn-id: https://svn.urbansim.org/src/trunk@18592 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/datasets/development_event_history_dataset.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 17, 2012
+
+@author: zoelligc
+'''
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from urbansim.datasets.development_event_dataset import DevelopmentEventDataset
+
+class DevelopmentEventHistoryDataset(DevelopmentEventDataset):
+    """Set of development events from the history.
+    """
+    in_table_name_default = "development_event_history"
+    out_table_name_default = "development_event_history"
+    dataset_name = "development_event_history"
+    id_name_default = ["building_id", "year_built"]
