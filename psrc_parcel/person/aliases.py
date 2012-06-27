0bcd73dcd456ea393eb47ecb38e13f9d1ce9cba5
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-06-27 00:51:25 +0000

    a few variables added
    git-svn-id: https://svn.urbansim.org/src/trunk@18587 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/person/aliases.py
+++ psrc_parcel/person/aliases.py
@@ -18,7 +18,8 @@ aliases = [
            "edu_college = numpy.in1d(person.edu, (3,6,7))",
            "edu_pg = numpy.in1d(person.edu, (7,))",
            "edu_adv = numpy.in1d(person.edu, (6, 7))",
-           "school_district_id = person.disaggregate(parcel.school_district_id, intermediates=[building, household])", 
+           "student = numpy.logical_or(person.stype == 2, person.stype == 6)", # to get a value of 1 in persons_for_estimation that corresponds to student=1 in the persons table
+           "school_district_id = person.disaggregate(parcel.school_district_id)", 
            "is_in_private_or_public_k12_school = numpy.logical_and(person.stype == 2, numpy.logical_or(psrc_parcel.person.is_in_private_school, psrc_parcel.person.is_in_public_school))",
            "is_in_private_school = numpy.logical_and(person.is_in_school, person.school_type == 2)",
            "is_in_private_k12_school = numpy.logical_and(psrc_parcel.person.is_in_private_school, person.stype == 2)",
@@ -28,5 +29,6 @@ aliases = [
            "has_older_sibling_in_home_school = person.disaggregate(psrc_parcel.household.age_of_oldest_child_in_home_school) > person.age",
            "has_older_sibling_in_private_or_home_school = numpy.logical_or(psrc_parcel.person.has_older_sibling_in_private_school, psrc_parcel.person.has_older_sibling_in_home_school)",
            "has_older_sibling = person.disaggregate(psrc_parcel.household.age_of_oldest_child) > person.age",
+           "has_older_sibling_in_school = person.disaggregate(psrc_parcel.household.age_of_oldest_child_in_school) > person.age",
            "private_schools_in_neighborhood = person.disaggregate(psrc_parcel.parcel.number_of_private_schools_within_1000_of_parcel, intermediates=[building, household])"
            ]
