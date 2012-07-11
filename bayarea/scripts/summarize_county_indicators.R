d58ed3c6300108dadb706ee4568c5e6633c9e329
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-07-11 08:36:27 +0000

    chart margins better with viewport
    git-svn-id: https://svn.urbansim.org/src/trunk@18675 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/scripts/summarize_county_indicators.R
+++ bayarea/scripts/summarize_county_indicators.R
@@ -7,7 +7,6 @@
 #5) --plots chart, writes to pdf file
 
 #USAGE: Rscript /home/aksel/Documents/Scripts/r/county_indicator_plots_index_ggplotfinal_arg.R "/home/aksel/Documents/Data/Urbansim/run_139.2012_05_15_21_23/indicators/2010_2035" 2010 2035
-#TODO: clean so that select counties can be toggled on/off. Now this is MANUALLY set. 
 
 args <- commandArgs(TRUE)
 options(warn=-1) 
@@ -23,7 +22,7 @@ indx <- function(dat, baseRow = 1)
 {
   require(plyr)   
   divisors <- dat[baseRow ,]
-  adply(dat, 1, function(x) x / divisors)
+  adply(dat, 1, function(x) x / divisors*100)
 }
 
 #wrap cmd-line arguments to assign to proper names
@@ -37,17 +36,16 @@ cmdArgs <- function(a="/home/aksel/Documents/Data/Urbansim/run_139.2012_05_15_21
 cmdArgs(a=args[1],b=args[2],c=args[3])
 setwd(pth)
 
-#viewport function--not needed right now
-my.vp <- function(just="topright"){
- 
-switch(just,
-        "topright" = viewport(x=1,y=1,width=1/2,height=1/2,just=c("right","top")),
-        "botleft" = viewport(x=0,y=0,width=1/2,height=1/2,just=c("left","bottom")),
-        viewport(x=1,y=1,width=1/2,height=1/2,just=c("right","top")))
- 
+#for formating chart titles
+TitleCase <- function(string="test string")
+{
+  first <- toupper(substring(string, 1, 1))
+  rest <- tolower(substring(string, 2))
+  fString <- sprintf("%s%s",first,rest)
+  return(fString)
 }
 
-#parse path to get runid
+#parse path to get runid, run date
 split <- strsplit(args[1],"/")[[1]]
 pos <- length(split)-2  #this assumes we are two up from the indicators dir--a regex would be better.
 runid <- split[pos]
@@ -83,7 +81,9 @@ for(i in 1:length(dat))
   sim_start_end <- sim_start_end[sim_start_end$county_id.i8 %in% c(49,48,43,41,38,28,21,7,1),  ]
   
   #convert id key to factor levels for more meaningful labeling. Labels are assigned based on factor numerical order.
-  sim_start_end$county_id.i8 <-factor(sim_start_end$county_id.i8, labels=c('ala','cnc','mar','nap','sfr','smt','scl','sol','son'))
+  #cnty <- c('ala','cnc','mar','nap','sfr','smt','scl','sol','son')
+  cnty <-  c('Alameda','Contra Costa','Marin','Napa','San Francisco','San Mateo','Santa Clara','Solano','Sonoma')
+  sim_start_end$county_id.i8 <-factor(sim_start_end$county_id.i8, labels=cnty)
   
   #rename col names, retaining only year part, extract column name to use in chart name later. Each column has year embedded
   regexp <- "([[:digit:]]{4})"  
@@ -94,9 +94,12 @@ for(i in 1:length(dat))
   yrStart <- min(as.integer(years))
   
   #extract field name representing variable for use in chart
-  title <- substr(names(sim_start_end)[2],1,pos-2)
-  fileNameOutChart=sprintf("%s/plot_%s_indexChart_%s.pdf",fp,runid,title)
-  fileNameOutTable=sprintf("%s/plot_%s_indexTable_%s.pdf",fp,runid,title)
+  title_prelim <- substr(names(sim_start_end)[2],1,pos-2)
+  title_split <- strsplit(title_prelim,"_")
+  title_proper <- lapply(title_split,TitleCase)
+  title <- paste(title_proper[[1]], sep=" ", collapse = " ")
+  #fileNameOutChart=sprintf("%s/plot_%s_indexChart_%s.pdf",fp,runid,title)
+  #fileNameOutTable=sprintf("%s/plot_%s_indexTable_%s.pdf",fp,runid,title)
   
   yrNames <- c('county',years)
   #assign new colunn names
@@ -110,7 +113,7 @@ for(i in 1:length(dat))
   sim_start_end.t <-as.data.frame(sim_start_end.t)
   
   #add regional total
-  sim_start_end.t$region <- rowSums(sim_start_end.t,na.rm = FALSE, dims = 1)
+  sim_start_end.t$Region <- rowSums(sim_start_end.t,na.rm = FALSE, dims = 1)
   
   # call index function to convert absolutes to indices (2010= index 100), replace NAs
   sim_start_end.i <-indx(sim_start_end.t, 1)
@@ -125,64 +128,63 @@ for(i in 1:length(dat))
   sim_start_end_long <- melt(sim_start_end.i,id="year",variable_name = "county")
   
   #plotting object pdf target
-  #pdf(fileNameOutChart,height=8.5, width=11)
-  #pdf("/home/aksel/allinonechart.pdf",height=8.5, width=11,onefile=TRUE)
   ticks <- as.factor(seq(yrStart,yrEnd,1)) 
-  
-  #determine range for plot
-  #g_range <- range(min(ala,cnc,mar,nap,sfr,smt,scl,sol,son), ala,cnc,mar,nap,sfr,smt,scl,sol,son)
-  #rangeMean <- tapply(g_range, c(1,1),mean)
-  
-  #make table object for use as annotation on plot
-  #tb <- tableGrob(sim_start_end[1,1:9],  show.rownames = TRUE,gpar.corefill = gpar(fill="white"))
   
   #table object prep for chart
   end <- yrEnd - yrStart + 1
+  if (end > 26 ) {
+    step <- 2 
+  }
+  else {
+    step <- 1
+  }
+  
   g1 <- tableGrob(
             format(
-                   sim_start_end.t[seq(1,end,1),1:10], 
+                   sim_start_end.t[seq(1,end,step),1:10], 
                    digits = 2,big.mark = ","), 
                    gpar.colfill = gpar(fill=NA,col=NA), 
                    gpar.rowfill = gpar(fill=NA,col=NA), 
-                   h.even.alpha = 0
+                   h.even.alpha = 0,
+                   gpar.rowtext = gpar(col="black", cex=0.8,
+                   equal.width = TRUE,
+                   show.vlines = TRUE, show.hlines = TRUE, separator="grey")                     
                    )
+            )
+                   
   #string <- "
   #placeholder for possible annotation
   #"
   #g2 <- splitTextGrob(string)
-  #theme_set(theme_bw()); 
-  
+  theme_set(theme_grey()); 
+  #theme_set(theme_bw());
+
   #plot object
   g3 <- ggplot(data=sim_start_end_long,
                aes(x=as.factor(year), y=value, group=county,colour=county)) +
-                 #geom_line()+
-                 #scale_x_discrete(breaks=seq(2010,2035,5), labels=seq(2010,2035,5)) + 
-                 geom_line(aes(linetype=county), # Line type depends on county
-                           size = .5) +       # Thin line
-                             geom_point(aes(shape=county) ,   # Shape depends on county
-                                        size = 1)   +       
-                                          opts(title=title)+
-                                          xlab("Year") + 
-                                          ylab(paste("Indexed Value (Rel. to ",yrStart,")")) + opts(axis.text.x=theme_text(angle=90, hjust=0))
+               #geom_line()+
+               #scale_x_discrete(breaks=seq(2010,2035,5), labels=seq(2010,2035,5)) + 
+               geom_line(aes(linetype=county), # Line type depends on county
+               size = .5) +       # Thin line
+               geom_point(aes(shape=county) ,   # Shape depends on county
+               size = 2)   +       
+               opts(title=title)+
+               xlab("Year") + 
+               ylab(paste("Indexed Value (Rel. to ",yrStart,")")) + 
+               opts(axis.text.x=theme_text(angle=90, hjust=0))
 
-  
   #combine in grid, send to print() function, chart
   stamp <- sprintf("Simulation #%s run on %s\nReport generated on %s",id,format(runDate, "%a %b %d %T"),format(Sys.time(), "%a %b %d %T"))
   
   grid.newpage()
-  pushViewport(viewport(height=unit(0.85, "npc"), width=unit(0.85, "npc"), x=0.5, y=0.5, name="vp")) 
+  pushViewport(viewport(height=unit(0.9, "npc"), width=unit(0.95, "npc"), x=0.5, y=0.5, name="vp")) 
   upViewport() 
   print(g3, vp="vp")
   #out <- grid.arrange(g3, ncol=1, main=textGrob(paste("\n",title),gp=gpar(fontsize=14,fontface="bold")))
   print(sprintf("Outputting Chart %s to PDF",title))
   grid.text(stamp,x=unit(0.85,"npc"),y=unit(0.95,"npc"),gp=gpar(fontsize=7,fontface="italic"))
-  #print(out)
-  #dev.off()  
   
-  #combine in grid, send to print() function, table
-  #plotting object pdf target
-  #pdf(fileNameOutTable,height=8.5, width=11)
-  
+  #combine in grid, send to print() function, table  
   out <- grid.arrange(g1, ncol=1, main=textGrob(paste("\n",title),gp=gpar(fontsize=14,fontface="bold"))) #,sub=stamp)
   print(sprintf("Outputting Table %s to PDF",title))
   grid.text(stamp,x=unit(0.85,"npc"),y=unit(0.95,"npc"),gp=gpar(fontsize=7,fontface="italic")) 
@@ -190,10 +192,9 @@ for(i in 1:length(dat))
   #dev.off()  
   
 }
-#pstamp(pwd=TRUE, time=TRUE)
 
 dev.off()
-warnings()
+#warnings()
 ###
 #1  Alameda  ala
 #7  Contra Costa  cnc
@@ -205,5 +206,3 @@ warnings()
 #48	Solano	sol
 #49	Sonoma	son
 ###
-
-
