334940d9352552c35542f706077d102732f75251
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-07-24 19:16:27 +0000

    (ejanowicz) emp_submarket county_id
    git-svn-id: https://svn.urbansim.org/src/trunk@18725 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/employment_submarket/aliases.py
+++ bayarea/employment_submarket/aliases.py
@@ -8,4 +8,5 @@ aliases = [
    "occupied_non_residential_sqft = employment_submarket.aggregate(establishment.employees) * employment_submarket.disaggregate(building_sqft_per_employee.building_sqft_per_employee)",
    "vacant_non_residential_sqft = clip_to_zero(bayarea.employment_submarket.non_residential_sqft - bayarea.employment_submarket.occupied_non_residential_sqft)",
    "vacancy_rates = safe_array_divide( bayarea.employment_submarket.vacant_non_residential_sqft, (bayarea.employment_submarket.non_residential_sqft).astype('f') )",
+   "county_id = employment_submarket.aggregate(building.disaggregate(parcel.county_id),function=median)",
            ]
