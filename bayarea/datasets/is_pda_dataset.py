37f1591acd727430fed485637d6dd1088b9a1543
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-07-26 20:52:18 +0000

    git-svn-id: https://svn.urbansim.org/src/trunk@18754 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/datasets/is_pda_dataset.py
+++ bayarea/datasets/is_pda_dataset.py
@@ -8,9 +8,26 @@ from urbansim.datasets.dataset import Dataset as UrbansimDataset
 from opus_core.resources import Resources
 from opus_core.variables.variable_name import VariableName
 
-class IsPDADataset(UrbansimDataset):
+class IsPdaDataset(UrbansimDataset):
     
     id_name_default = "is_pda_id"
     in_table_name_default = "is_pdas"
     out_table_name_default = "is_pdas"
     dataset_name = "is_pda"
+
+    def __init__(self, id_values=1, **kwargs):
+        storage = StorageFactory().get_storage('dict_storage')
+
+        storage.write_table(table_name='is_pdas',
+                            table_data= {
+                                         'is_pda_id'        :array([0,1]),
+                                         'is_pda'    :array([True,False]),
+                                         }
+                            )
+
+        resources = Resources({
+            'in_storage':storage,
+            'in_table_name':'is_pdas'
+            })
+
+        UrbansimDataset.__init__(self, resources=resources, **kwargs)
