16a4d790258385bddca6e0bc9e8dbe48a439ccb6
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-07-26 21:36:29 +0000

    fixed is_pda_dataset
    git-svn-id: https://svn.urbansim.org/src/trunk@18755 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/datasets/is_pda_dataset.py
+++ bayarea/datasets/is_pda_dataset.py
@@ -2,32 +2,25 @@
 # Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
 # See opus_core/LICENSE 
 
-import os
-
+from numpy import array
+from opus_core.storage_factory import StorageFactory
 from urbansim.datasets.dataset import Dataset as UrbansimDataset
-from opus_core.resources import Resources
-from opus_core.variables.variable_name import VariableName
 
 class IsPdaDataset(UrbansimDataset):
-    
-    id_name_default = "is_pda_id"
-    in_table_name_default = "is_pdas"
-    out_table_name_default = "is_pdas"
+    id_name_default = "is_pda"
+    in_table_name_default = "is_pda"
+    out_table_name_default = "is_pda"
     dataset_name = "is_pda"
 
-    def __init__(self, id_values=1, **kwargs):
+    def __init__(self, id_values=array([0,1],dtype='i4'), **kwargs):
         storage = StorageFactory().get_storage('dict_storage')
 
-        storage.write_table(table_name='is_pdas',
+        storage.write_table(table_name=self.in_table_name_default,
                             table_data= {
-                                         'is_pda_id'        :array([0,1]),
-                                         'is_pda'    :array([True,False]),
+                                         self.id_name_default: id_values,
                                          }
                             )
 
-        resources = Resources({
-            'in_storage':storage,
-            'in_table_name':'is_pdas'
-            })
+        kwargs.update({'in_storage': storage})
+        UrbansimDataset.__init__(self, **kwargs)
 
-        UrbansimDataset.__init__(self, resources=resources, **kwargs)
