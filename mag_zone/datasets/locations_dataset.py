d62f7a293f1c37cde50b694ac3d2f45a81f31a9b
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-07-27 16:15:15 +0000

    new locations dataset for simtravel
    git-svn-id: https://svn.urbansim.org/src/trunk@18765 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ mag_zone/datasets/locations_dataset.py
@@ -0,0 +1,22 @@
+#
+# UrbanSim software. Copyright (C) 2005-2008 University of Washington
+# 
+# You can redistribute this program and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation
+# (http://www.gnu.org/copyleft/gpl.html).
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
+# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
+# other acknowledgments.
+# 
+
+from urbansim.datasets.dataset import Dataset
+
+class LocationsDataset(Dataset):
+    
+    id_name_default = "locationid"
+    in_table_name_default = "locations"
+    out_table_name_default = "locations"
+    dataset_name = "locations"
