8c3aa8b9d3be0414d353da21a1f2e970c16997ba
bcavagnolo <bcavagnolo@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-16 00:59:59 +0000

    fix bug in start_run
    git-svn-id: https://svn.urbansim.org/src/trunk@18881 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/tools/start_run.py
+++ opus_core/tools/start_run.py
@@ -81,7 +81,6 @@ def prepare_run_manager(option_group=None):
  
     run_manager.setup_new_run(cache_directory = config['cache_directory'],
                               configuration = config)
-    run_id, cache_directory = run_manager.run_id, run_manager.get_current_cache_directory() 
 
     return options, config, run_manager
 
@@ -90,6 +89,8 @@ def main(option_group=None):
     run_manager.run_run(config, 
                         scenario_name=options.scenario_name,
                         run_as_multiprocess=not options.run_as_single_process)
+
+    run_id, cache_directory = run_manager.run_id, run_manager.get_current_cache_directory() 
 
     return run_id, cache_directory
 
