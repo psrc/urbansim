2a7d11b36c5ee80df8d0485e029b0c2c79c3db98
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-19 09:12:46 +0000

    split topsheet into panels
    git-svn-id: https://svn.urbansim.org/src/trunk@18894 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/scripts/data_frame_from_binary.R
+++ bayarea/scripts/data_frame_from_binary.R
@@ -9,61 +9,101 @@ library(RColorBrewer)
 
 #pth <-args[1]
 #outPth <- args[2]
-pth <-"/home/aksel/Documents/Data/Urbansim/run_134/2010/scheduled_development_events"
-setwd(pth)
-fileList = list.files(path=pth)#, pattern=ptrn)
-
-##get list with file types to enable proper loading of binary.
-##then transform unwieldy list to get filetypes
+readSingleColumn <- function(pth) {
+  setwd(pth)
+  fileList = list.files(path=pth)#, pattern=ptrn)
+  
+  ##get list with file types to enable proper loading of binary.
+  ##then transform unwieldy list to get filetypes
   fileAndTypes<- lapply(fileList,function(x) strsplit(x,"\\."))
   fileAndTypes2 = lapply(fileAndTypes, ldply)
   fileTypesClean = ldply(fileAndTypes2)[,1:2]
   fileTypesClean[,2] <- ldply(lapply(fileTypesClean[,2], gsub, pattern="\\d",replacement=""))
   names(fileTypesClean) <- c("variable","binarytype")
-
-##lookup between binary files and R's categories
+  
+  ##lookup between binary files and R's categories
   typeMapping <- list("li"= "integer", "lf"= "double", "iS"="character")
   fileTypesClean$filename <- fileList
   
-##grab binary files
+  ##grab binary file
+  column <-fileTypesClean$filename[2]
+  dat<-readBin(column,endian = "little", 
+               what=typeMapping[gsub(pattern="\\d",replacement="",strsplit(column,"\\.")[[1]][[2]])][[1]],
+               n=1984716,size=ifelse(sub(".*?(\\d)", "\\1", column, perl=TRUE)<=4,sub(".*?(\\d)", "\\1", column, perl=TRUE),4)) #last argument gets byte-size from filename
+  ##throw in data frame, shape properly for ggplot
+  dt <-as.data.frame(ldply(dat))
+  colnames(dt) <- column
+  return(dt)
+}
+
+readBinary <- function(pth){
+  setwd(pth)
+  fileList = list.files(path=pth)#, pattern=ptrn)
+  
+  ##get list with file types to enable proper loading of binary.
+  ##then transform unwieldy list to get filetypes
+  fileAndTypes<- lapply(fileList,function(x) strsplit(x,"\\."))
+  fileAndTypes2 = lapply(fileAndTypes, ldply)
+  fileTypesClean = ldply(fileAndTypes2)[,1:2]
+  fileTypesClean[,2] <- ldply(lapply(fileTypesClean[,2], gsub, pattern="\\d",replacement=""))
+  names(fileTypesClean) <- c("variable","binarytype")
+  
+  ##lookup between binary files and R's categories
+  typeMapping <- list("li"= "integer", "lf"= "double", "iS"="character")
+  fileTypesClean$filename <- fileList
+  
+  ##grab binary files
   dat<-lapply(fileTypesClean[2:nrow(fileTypesClean),3],
               function(x) {
-              readBin(x, 
-                      endian = "little",                    
-                      what=typeMapping[gsub(pattern="\\d",
-                                               replacement="",strsplit
-                                           (fileList[2],"\\.")[[1]][[2]])][[1]],n=100)})#, 
-                      #size=sub(".*?(\\d)", "\\1", x, perl=TRUE))}) #last argument gets byte-size from filename
-##throw in data frame, shape properly for ggplot
+                readBin(x, 
+                        endian = "little",                    
+                        what=typeMapping[gsub(pattern="\\d",
+                                              replacement="",strsplit
+                                              (fileList[2],"\\.")[[1]][[2]])][[1]],n=2e5, 
+                        size=ifelse(sub(".*?(\\d)", "\\1", x, perl=TRUE)<=4,sub(".*?(\\d)", "\\1", x, perl=TRUE),4)) }) #last argument gets byte-size from filename
+  ##throw in data frame, shape properly for ggplot
   dt <-as.data.frame(t(ldply(dat)))
   colnames(dt) <- fileTypesClean[2:nrow(fileTypesClean),1]
-  names(dt)
-  dt.m <- melt(dt[,c(1,2,12)], id=c("scheduled_year","building_type_id"),direction = "long")
-  dt.m <- cast(dt.m,scheduled_year + building_type_id ~ variable, sum)
-  dt.m2 <- melt(dt[,c(1,2)], id=c("building_type_id"),direction = "long")
+  return(dt)
+}
+
+################################################################################
+##call function#################################################################
+################################################################################
+pth <-"/home/aksel/Documents/Data/Urbansim/run_139/2040/buildings"
+dt<-readBinary(pth)
+dtSingle <- readSingleColumn(pth)
+names(dt)
+names(dtSingle)
+length((dtSingle))
+dt.m <- melt(dt[,c(1,2,12)], id=c("year_built","building_type_id"),direction = "long")
+dt.m <- cast(dt.m,year_built + building_type_id ~ variable, sum)
+dt.m2 <- melt(dt[,c(1,2)], id=c("building_type_id"),direction = "long")
 
 
 ##generate histogram w density
-  setwd(outPth)
-  pdf("testChart.pdf",height=8.5, width=11,onefile=TRUE)
-  ggplot(dt.m2, aes(x=value)) + 
-    geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
-                   binwidth=125000,
-                   colour="black", fill="white") +
-                     geom_density(alpha=.2, fill="orange") +
-                     xlab("square feet") +
-                     opts(title="Dev Events Size Distribution") +
-                     opts(legend.position=c(.24, .95), legend.justification = c(1, 1))
-                     #scale_y_continuous(labels=percent) 
-                     #scale_x_continuous(limits=c(0, 3000))
+#setwd(outPth)
+pdf("/home/aksel/Downloads/testChart.pdf",height=8.5, width=11,onefile=TRUE)
+ggplot(dt.m2, aes(x=value)) + 
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=10000,
+                 colour="black", fill="white") +
+                   geom_density(alpha=.2, fill="orange") +
+                   xlab("square feet") +
+                   opts(title="Dev Events Size Distribution") +
+                   opts(legend.position=c(.24, .95), legend.justification = c(1, 1)) +
+                   scale_x_continuous(limits=c(0, 1e6))
+#scale_y_continuous(labels=percent) 
+
 
 ##generate line plot
-  ggplot(dt.m, aes(x=scheduled_year,y=building_sqft,
-                   fill=as.factor(building_type_id),
-                   group=as.factor(building_type_id),
-                   )) + 
-                  geom_bar(stat = "identity", position = "stack") + scale_fill_brewer(palette = "Set1") + 
-                  opts(title = "Sqft by year by type") +
-                  labs(x = "Year", y = "Square Feet",
-                       fill = NULL)
-  dev.off()
+ggplot(dt.m, aes(x=year_built,y=building_sqft,
+                 fill=as.factor(building_type_id),
+                 group=as.factor(building_type_id),
+                 )) + 
+                   geom_bar(stat = "identity", position = "stack") + scale_fill_brewer(palette = "Set1") + 
+                   opts(title = "Sqft by year by type") +
+                   labs(x = "Year", y = "Square Feet",
+                        fill = NULL)+
+                          scale_x_continuous(limits=c(1900, 2040))
+dev.off()
--- bayarea/scripts/regional_indicators.R
+++ bayarea/scripts/regional_indicators.R
@@ -3,6 +3,68 @@ require("ggplot2")
 require("reshape")
 require("gridExtra")
 require("RGraphics")
+require("scales")
+library(directlabels)
+library(RColorBrewer)
+
+manyColors <- colorRampPalette(brewer.pal(name = 'Set3',n=10))
+
+##generic line plot function
+linePlot <- function(data,name){
+  gOut <- ggplot(data=data,
+       aes(
+         x=variable, 
+         y=value, 
+         colour=geography,
+         group=geography,
+         linetype = geography))+
+           geom_line(size=1.75) + 
+           scale_colour_manual(values=manyColors(10)) +
+           geom_point(size=1.8) +
+           opts(title=name)+
+           xlab("Year") + 
+           ylab(name) +
+           opts(axis.text.x=theme_text(angle=90, hjust=0)) +
+           opts(legend.key.width = unit(1, "cm")) +
+           scale_y_continuous(labels=comma) +
+          #scale_linetype_manual(values = lty)      +
+           opts(legend.position="none")
+  return(gOut)
+}
+
+stackedBarPlot <- function(data,name){
+  gOut <- ggplot(data=data,
+                 aes(
+                   x=variable, 
+                   y=value, 
+                   fill=geography
+                   ))+
+                     geom_bar(alpha =.65) + 
+                     #scale_colour_manual(values=manyColors(12)) +
+                     scale_fill_brewer(palette="Set3") +
+                     opts(title=name)+
+                     xlab("Year") + 
+                     ylab(name) +
+                     opts(axis.text.x=theme_text(angle=90, hjust=0)) +
+                     opts(legend.key.width = unit(.6, "cm")) +
+                     scale_y_continuous(labels=comma) 
+                     #scale_linetype_manual(values = lty)      +
+                     #opts(legend.position="none")
+  return(gOut)
+}
+
+# define function to create multi-plot setup (nrow, ncol)
+vp.setup <- function(x,y){
+  # create a new layout with grid
+  grid.newpage()
+  # define viewports and assign it to grid layout
+  pushViewport(viewport(layout = grid.layout(x,y)))  
+}
+# define function to easily access layout (row, col)
+vp.layout <- function(x,y){
+  viewport(layout.pos.row=x, layout.pos.col=y)
+}
+
 ## function for formating chart titles
 TitleCase <- function(string="test string")
 {
@@ -19,7 +81,9 @@ makeTable <- function(data){
            digits = 2,big.mark = ","), 
     gpar.colfill = gpar(fill=NA,col=NA), 
     gpar.rowfill = gpar(fill=NA,col=NA), 
+    show.rownames = F,    
     #gpar.corefill = gpar(fill="slateblue",alpha=0.5, col=NA), h.even.alpha = 0.5),
+    col.just = "left", core.just = "center",
     h.even.alpha = 0,
     gpar.rowtext = gpar(col="black", cex=0.8,
                         equal.width = TRUE,
@@ -49,7 +113,7 @@ regionalProcessor <- function(pth,yrStart,yrEnd){
     fileNameClean <- ldply(lapply(fileNameClean, gsub, pattern="alldata.+\\d{4}-\\d{4}_",replacement=""))
     title_split <- lapply(fileNameClean,strsplit, "_")
     title_concat <- ldply(lapply(title_split[[1]][1:length(title_split[[1]])],paste, sep=" ", collapse = " "))
-    title_concat_short <- ldply(lapply(title_concat,gsub, pattern="alldata  region ",replacement=""))
+    title_concat_short <- ldply(lapply(title_concat,gsub, pattern="alldata|region",replacement=""))
     #title_proper <- lapply(title_concat,TitleCase)
     #title <- paste(title_concat[[1]], sep=" ", collapse = " ")
     
@@ -64,17 +128,61 @@ regionalProcessor <- function(pth,yrStart,yrEnd){
     names(df.t)[1] <- "Indicator"
   
   ## grob time    
-    g1 <- makeTable(df.t[c(1,2,ncol(df.t))])
-  ##plot it
-    grid.arrange(g1, nrow=1, main=textGrob(paste("\n","TEST RUN ", "Regional Summary"),gp=gpar(fontsize=11,fontface="bold")))
+    containTest <- '2035' %in% names(df.t)
+    extra=F
+    if (T %in% containTest){
+      g1 <- makeTable(df.t[c("Indicator","2010","2018","2025","2035")])
+      extra=T
+    } else {
+      g1 <- makeTable(df.t[c(1,2,ncol(df.t))])
+    }
+    ############################################################################
+    ##process EMFAC MTC data
+    travelYears <- c('2018','2025','2035')
+    mtcDataFiles <- ldply(lapply(travelYears,function(x) sprintf("/home/aksel/Downloads/EMFAC2011-SG Summary - Year_%s - Group 1.csv",x)))                      
+    mtcData<-lapply(mtcDataFiles[[1]],read.csv,header = TRUE, sep = ",", quote="\"")
+    VMT <- data.frame(mtcData[[1]][,4], mtcData[[1]][,9],mtcData[[2]][,9],mtcData[[3]][,9])
+    names(VMT) <- c("geography",2018,2025,2035)
+    #VMT$geography <- c("region",as.vector(substr(x=VMT$geography[2:10],start=1,last=length(VMT$geography[2:10])-5)))
+    VMT.m <- melt(VMT[2:10,],id="geography")
     
+    g2 <- makeTable(VMT[2:10,])
+    ############################################################################
     ## start pdf object to store all charts
     fp <- file.path(pth, fsep = .Platform$file.sep)
-    fileNameOut=sprintf("%s/%s_plot_%s_indexChart.pdf",fp,"region","134")
+    fileNameOut=sprintf("%s_plot_%s_indexChart.pdf","region","134")
     sprintf("Preparing file %s for charts and figures...", fileNameOut)
+    ##plot it
     pdf(fileNameOut,height=8.5, width=11,onefile=TRUE)
-}    
+    #multiplot(tb1, tb1, cols=1)
+    set.seed(2)
+    #q1 <- ggplot(data.frame(x=rnorm(50)), aes(x)) + geom_density()
+    #q2 <- ggplot(data.frame(x=rnorm(50)), aes(x)) + geom_density()
+    
+    g6 <- linePlot(VMT.m,"Vehicle Miles Traveled")
+    g6 <- direct.label(g6, list(last.points, hjust = 0.7, vjust = 1,fontsize=12))
+    
+    g7 <- stackedBarPlot(VMT.m,"Vehicle Miles Traveled")
+    
+    tb1 <- grid.arrange(g1,g2,g6,g7, nrow=2, 
+                        widths = c(1/2,1/2),
+                        heights = c(1/3,2/3), 
+                        main=textGrob(paste("\n","Topsheet Example"),
+                                      gp=gpar(fontsize=11,fontface="bold")))
+    #grid.text("test",x=unit(0.85,"npc"),y=unit(0.95,"npc"),gp=gpar(fontsize=7,fontface="italic")) 
+    sub=textGrob("test sub", gp=gpar(font=2))
+
+    #multiplot(q1, q2, q3, q4, q5, q6, cols=3)
+#     vp.setup(2,2)
+#     print(q1, vp=vp.layout(1, 1:2))
+#     print(q2, vp=vp.layout(2,2))
+#     print(tb1, vp=vp.layout(2,1))
+#     
+    }   
+
 yrStart<-2010
 yrEnd<-2018
 pth <- "/home/aksel/Documents/Data/Urbansim/run_134/indicators"
+#mtcData <- "/var/www/MTC_Model/No_Project/run_139"
 regionalProcessor(pth,yrStart,yrEnd)
+garbage <- dev.off()
