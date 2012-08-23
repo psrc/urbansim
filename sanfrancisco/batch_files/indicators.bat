0b0c869f1dd8e7a054b292f718c3d4de23f75b19
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-23 23:52:37 +0000

    batch file holding indicator call
    git-svn-id: https://svn.urbansim.org/src/trunk@18942 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ sanfrancisco/batch_files/indicators.bat
@@ -0,0 +1,1 @@
+python -m urbansim.tools.make_indicators -x c:\opus\project_configs\sanfrancisco.xml -c G:\opus\run_123.sanfrancisco_bsyr2009_20120812_1129_Sim2010to2040 -i pdist_summary_short -y "range(2009,2041,1)" 
