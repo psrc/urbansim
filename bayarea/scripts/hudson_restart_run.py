5961ba2f9839c20f8c6816a334327879f168d972
bcavagnolo <bcavagnolo@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-24 18:50:35 +0000

    bayarea: make hudson tolerate restart without knowing year
    In this case, we just guess the year based on the contents of the
    cache directory.  This saves us some UI in hudson.
    
    git-svn-id: https://svn.urbansim.org/src/trunk@18956 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/scripts/hudson_restart_run.py
+++ bayarea/scripts/hudson_restart_run.py
@@ -2,7 +2,7 @@ import os, sys
 from opus_core.logger import logger
 import opus_core.tools.restart_run as restart_run
 from opus_core.services.run_server.run_manager import RunManager
-import shutil
+import shutil, glob
 
 def main(option_group=None, args=None):
 
@@ -24,15 +24,20 @@ def main(option_group=None, args=None):
     run_manager = RunManager(option_group.get_services_database_configuration(options))
     run_as_multiprocess = not options.run_as_single_process
 
-    if len(args) < 2:
+    if len(args) < 1:
         parser.print_help()
         sys.exit(1)
-    run_id, year = (int(args[0]), int(args[1]))
+    run_id = int(args[0])
+    year = None
+    if len(args) > 1:
+        year = int(args[1])
 
     # Get configuration from DB to check where the run was originally
-    # performed.
+    # performed.  Note that we pass a dummy value for restart_year because we
+    # might not know it yet.  But when we actually restart the run, we will
+    # pass the correct year.
     run_resources = run_manager.create_run_resources_from_history(run_id=run_id,
-                                                                  restart_year=year)
+                                                                  restart_year=2010)
     try:
         original_node = run_resources['hudson_details']['node']
         if not original_node:
@@ -64,6 +69,12 @@ def main(option_group=None, args=None):
             rc = os.system(cmd)
             if (rc != 0):
                 raise IOError("Failed to '" + cmd + "'")
+
+    if not year:
+        # guess the year based on how the cache dir is populated
+        years = map(lambda y : int(os.path.basename(y)),
+                    glob.glob(os.path.join(cache_dir, "2*")))
+        year = max(years)
 
     end_year = int(options.end_year) if options.end_year is not None else None
     run_manager.restart_run(run_id, 
