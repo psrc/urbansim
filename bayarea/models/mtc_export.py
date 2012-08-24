501c0cbcab93ba84247ac947035a59ecd0f3d783
bcavagnolo <bcavagnolo@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-24 00:16:52 +0000

    bayarea: apply ABAG age adjustments to mtc exporter
    git-svn-id: https://svn.urbansim.org/src/trunk@18945 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/models/mtc_export.py
+++ bayarea/models/mtc_export.py
@@ -11,6 +11,8 @@ from opus_core.store.attribute_cache import AttributeCache
 from opus_core.store.csv_storage import csv_storage
 from opus_core.variables.variable_name import VariableName
 import bayarea.travel_model.mtc_common as mtc_common
+from opus_core import paths
+import numpy as np
 
 flip_urbansim_to_tm_variable_mappling = True
 
@@ -45,7 +47,29 @@ class MTCExport(Model):
         out_dir = os.path.join(cache_directory, "mtc_data")
 
         out_storage = csv_storage(storage_location=out_dir)
+
+        # Adjust the age distribution per ABAG/MTC's specifications
+        age_control_dir = os.path.join(paths.OPUS_DATA_PATH, configuration['project_name'], "ageControl")
+        age_control_storage = csv_storage(storage_location=age_control_dir)
+        age_control_files = os.listdir(age_control_dir)
+        years = np.array(map(lambda x:
+                             int(os.path.basename(x).replace("tazData", "").replace(".csv", "")),
+                             glob.glob(os.path.join(age_control_dir, "tazData*.csv"))))
+        closest_year = years[np.argmin(np.abs(years - [year]*len(years)))]
+        if closest_year != year:
+            logger.log_warning("Could not find age control data for " + str(year) +
+                               ".  Choosing nearest year " + str(closest_year) + ".")
+
+        age_control_table = age_control_storage.load_table("tazData" + str(closest_year), lowercase=False)
+
+        # Calculate the ABAG shares of person by age
+        age_categories = ['AGE0004', 'AGE0519', 'AGE2044', 'AGE4564', 'AGE65P']
+        age_category_sums = dict((k, age_control_table[k].sum()) for k in age_categories)
+        total = sum(age_category_sums.values())
+        abag_age_category_shares = dict((k, age_category_sums[k]/total) for k in age_categories)
+
         for data_fname, variable_mapping in self.data_to_export.iteritems():
+
             if not flip_urbansim_to_tm_variable_mappling:
                 col_names = variable_mapping.values()
                 variables_aliases = ["=".join(mapping[::-1]) for mapping in \
@@ -59,6 +83,22 @@ class MTCExport(Model):
             dataset = dataset_pool.get_dataset(dataset_name)
             dataset.compute_variables(variables_aliases)
 
+            if data_fname == "ABAGData":
+                logger.log_status("Adjusting ABAGData to match age controls")
+                age_category_sums = dict((k, dataset[k].sum()) for k in age_categories)
+                total = sum(age_category_sums.values())
+                us_age_category_shares = dict((k, age_category_sums[k]/total) for k in age_categories)
+                adjustments = dict((k, abag_age_category_shares[k]/us_age_category_shares[k]) for k in age_categories)
+                diff = np.zeros(dataset.n)
+                for k in age_categories:
+                    before = dataset[k]
+                    dataset[k] = np.array(map(lambda v : round(v*adjustments[k]), dataset.get_attribute(k)))
+                    diff += (dataset[k] - before)
+                dataset["TOTPOP"] += diff
+                dataset["HHPOP"] += diff
+                logger.log_status("NOTE: Adjusted total population by %d (%2.3f%%) due to rounding error." %
+                                  (int(diff.sum()), diff.sum()*100/total))
+
             org_fname = os.path.join(out_dir, "%s.computed.csv" % data_fname)
             new_fname = os.path.join(out_dir, "%s%s.csv" % (year,data_fname))
             block_msg = "Writing {} for travel model to {}".format(data_fname,
@@ -70,5 +110,4 @@ class MTCExport(Model):
                 #rename & process header
                 shutil.move(org_fname, new_fname)
                 os.system("sed 's/:[a-z][0-9]//g' -i %s" % new_fname)
-            return
 
