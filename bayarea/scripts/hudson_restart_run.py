33e4455f147ea9b28e9025d6062710bd20c0564c
bcavagnolo <bcavagnolo@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-24 18:32:05 +0000

    bayarea: fix hudson's ability to restart runs
    The problem is that if we want to restart a run, we must have
    access to the run directory.  But because hudson may launch the
    model on any one of his slaves, the run directory may not be
    available.  The solution is to mount the directory over sshfs.  I
    chose this over rsync'ing the run directory because the latter
    implies a problematic coherency issue.
    
    git-svn-id: https://svn.urbansim.org/src/trunk@18955 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ bayarea/scripts/hudson_restart_run.py
@@ -0,0 +1,80 @@
+import os, sys
+from opus_core.logger import logger
+import opus_core.tools.restart_run as restart_run
+from opus_core.services.run_server.run_manager import RunManager
+import shutil
+
+def main(option_group=None, args=None):
+
+    # When we restart a run, it's critical that we either do it on the same
+    # hudson slave, or that we somehow access the run directory on the hudson
+    # slave where the job was originally run.
+    node_name = os.getenv("NODE_NAME")
+    if not node_name:
+        logger.log_error("NODE_NAME environment variable not set.  Are you running within hudson?")
+        sys.exit(1)
+
+    if option_group is None:
+        option_group = restart_run.RestartRunOptionGroup()
+    parser = option_group.parser
+    if args is None:
+        options, args = option_group.parse()
+    else: 
+        options, _args = option_group.parse()
+    run_manager = RunManager(option_group.get_services_database_configuration(options))
+    run_as_multiprocess = not options.run_as_single_process
+
+    if len(args) < 2:
+        parser.print_help()
+        sys.exit(1)
+    run_id, year = (int(args[0]), int(args[1]))
+
+    # Get configuration from DB to check where the run was originally
+    # performed.
+    run_resources = run_manager.create_run_resources_from_history(run_id=run_id,
+                                                                  restart_year=year)
+    try:
+        original_node = run_resources['hudson_details']['node']
+        if not original_node:
+            raise KeyError
+    except KeyError:
+        logger.log_error("Faied to determine original hudson node.")
+        raise
+
+    if node_name != original_node:
+        logger.log_warning("Mounting %s's run directory for access to run." %
+                           original_node)
+        # We assume that the workspace layout for all hudson slaves is the
+        # same.  This allows us to assume that the paths on the slaves are
+        # identical.
+        cache_dir = run_resources['cache_directory']
+        if os.path.exists(cache_dir) and os.path.ismount(cache_dir):
+            # Here we assume that if the cache_directory is a mounted fs, then
+            # it must be the one that we're expecting.
+            pass
+        else:
+            if os.path.exists(cache_dir):
+                # The local cache dir should be empty.  But sometimes it's not.
+                # Like if sshfs fails during a model run or something.  So we
+                # blow away the contents.
+                shutil.rmtree(cache_dir)
+            os.makedirs(cache_dir)
+            cmd = 'sshfs hudson@' + original_node + ':' + cache_dir + ' ' + cache_dir
+            logger.log_status("mount command: " + cmd)
+            rc = os.system(cmd)
+            if (rc != 0):
+                raise IOError("Failed to '" + cmd + "'")
+
+    end_year = int(options.end_year) if options.end_year is not None else None
+    run_manager.restart_run(run_id, 
+                            year,
+                            options.project_name,
+                            end_year=end_year,
+                            skip_urbansim=options.skip_urbansim,
+                            create_baseyear_cache_if_not_exists=options.create_baseyear_cache_if_not_exists,
+                            skip_cache_cleanup=options.skip_cache_cleanup,
+                            run_as_multiprocess=run_as_multiprocess
+                            )
+
+if __name__ == "__main__":
+    main()
