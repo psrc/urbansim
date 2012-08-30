4f905dd14575da7c20f9cf4a05b7744b38cb42e7
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-30 22:26:42 +0000

    fixed an error in aliases
    git-svn-id: https://svn.urbansim.org/src/trunk@19008 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/parcel/aliases.py
+++ bayarea/parcel/aliases.py
@@ -9,6 +9,7 @@ aliases = [
     "parcel_acre = parcel.shape_area * 0.000247105381",
     "is_pda_id = 1+ (parcel.pda_id > 0).astype('i')" ,
     "is_pda_x_county = (parcel.county_id * (parcel.pda_id > 0)).astype('i')" ,
-    "is_tpp =  = 1+ (parcel.tpp_id > 0).astype('i')",
+    "is_tpp = 1+ (parcel.tpp_id > 0).astype('i')",
+    "region_id = 9*ones_like(parcel.parcel_id)",
            ]
     
