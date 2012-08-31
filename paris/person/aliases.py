89c72c2e4a40f80be9197f4e35c1496303bf64b8
lmwang <lmwang@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-08-31 19:31:15 +0000

    added a person.is_head_of_household alias
    git-svn-id: https://svn.urbansim.org/src/trunk@19017 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ paris/person/aliases.py
@@ -0,0 +1,6 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+aliases = [
+        "is_head_of_household = person.person_id == person.disaggregate(household.ref_pers_id)",
+           ]
