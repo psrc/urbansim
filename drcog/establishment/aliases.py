b454850ccb88cc127b68229a44db37cbf4717a5e
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-09-27 07:18:58 +0000

    establishment aliases updated to have a 6-sector categorization
    git-svn-id: https://svn.urbansim.org/src/trunk@19137 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- drcog/establishment/aliases.py
+++ drcog/establishment/aliases.py
@@ -4,4 +4,5 @@
 
 aliases = [
     "employment_submarket_id = establishment.disaggregate(drcog.building.employment_submarket_id)",
+    "sector_id_six = 1*(establishment.sector_id==61) + 2*(establishment.sector_id==71) + 3*numpy.in1d(establishment.sector_id,[11,21,22,23,31,32,33,42,48,49]) + 4*numpy.in1d(establishment.sector_id,[7221,7222,7224]) + 5*numpy.in1d(establishment.sector_id,[44,45,7211,7212,7213,7223]) + 6*numpy.in1d(establishment.sector_id,[51,52,53,54,55,56,62,81,92])",
            ]
