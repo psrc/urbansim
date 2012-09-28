cdb52b5246ddaeff277460338916dbd79ddffbad
bcavagnolo <bcavagnolo@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-09-28 22:50:18 +0000

    bayarea: make REST API serve up run configuration
    git-svn-id: https://svn.urbansim.org/src/trunk@19147 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/www/opus_rest/rest/views.py
+++ bayarea/www/opus_rest/rest/views.py
@@ -1,10 +1,42 @@
 from django.http import HttpResponse
+from rest.models import RunActivity
+import simplejson as json
+from django.shortcuts import get_object_or_404
+import zlib, pickle
+
+def run_to_dict(r):
+    d = {
+        "run_id": r.run_id,
+        "run_name": r.run_name,
+        "run_description": r.run_description,
+        "scenario_name": r.scenario_name,
+        "cache_directory": r.cache_directory,
+        "processor_name": r.processor_name,
+        "date_time": str(r.date_time),
+        "status": r.status,
+        "project_name": r.project_name,
+    }
+    # Augment this with some data from resources.  Note that resources may or
+    # may not be compressed.  The run manager determines this based on his
+    # config file.  We just guess.
+    try:
+        c = pickle.loads(zlib.decompress(str(r.resources)))
+    except zlib.error:
+        c = pickle.loads(str(r.resources))
+
+    # we have to be careful only to add serializable elements to the dictionary
+    print c.keys()
+    for k in ['project_name', 'hudson_details', 'models', 'years', 'base_year',
+              'models_configuration', 'travel_model_configuration', 'models_in_year']:
+        d[k] = c[k]
+    return d
 
 def index(request):
-    return HttpResponse("Hello.  You have reached the opus rest interface.")
+    recent_runs = RunActivity.objects.all().order_by('-date_time')
+    recent_run_jsons = map(run_to_dict, recent_runs)
+    return HttpResponse(json.dumps(recent_run_jsons), mimetype="application/json")
 
 def run(request, run_id):
-    return HttpResponse("You're looking at run %s." % run_id)
-
-def config(request, run_id):
-    return HttpResponse("You're looking at the configuration of run %s." % run_id)
+    r = get_object_or_404(RunActivity, pk=run_id)
+    j = run_to_dict(r)
+    return HttpResponse(json.dumps(j), mimetype="application/json")
--- bayarea/www/opus_rest/urls.py
+++ bayarea/www/opus_rest/urls.py
@@ -3,5 +3,4 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^rest/$', 'rest.views.index'),
     url(r'^rest/(?P<run_id>\d+)/$', 'rest.views.run'),
-    url(r'^rest/(?P<run_id>\d+)/config/$', 'rest.views.config'),
 )
