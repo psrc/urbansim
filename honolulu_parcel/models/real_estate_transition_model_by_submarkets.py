78aa6047a77d09dc616bfd7a757c54a21903d130
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-10-09 08:16:24 +0000

    loop through zones, run retm at the zonal level
    git-svn-id: https://svn.urbansim.org/src/trunk@19201 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- honolulu_parcel/models/real_estate_transition_model_by_submarkets.py
+++ honolulu_parcel/models/real_estate_transition_model_by_submarkets.py
@@ -8,6 +8,8 @@ from opus_core.logger import logger
 from urbansim.models.real_estate_transition_model import RealEstateTransitionModel
 from opus_core.session_configuration import SessionConfiguration
 from opus_core.simulation_state import SimulationState
+from opus_core.storage_factory import StorageFactory
+from urbansim.datasets.building_dataset import BuildingDataset
 
 class RealEstateTransitionModelBySubmarkets(RealEstateTransitionModel):
 
@@ -27,15 +29,27 @@ class RealEstateTransitionModelBySubmarkets(RealEstateTransitionModel):
         for name in attribs:
             table_data["%s"%(name)]=building_set.get_attribute("%s" %(name))[idx_in_zone]
         logger.log_status("table_data: %s" % (table_data))
+        storage = StorageFactory().get_storage('dict_storage')
+        table_name = 'buildings_zone'
+        storage.write_table(
+            table_name=table_name,
+            table_data=table_data
+            )
+        buildings_zone = BuildingDataset(in_storage=storage, in_table_name=table_name)
+        dataset_pool.add_datasets_if_not_included({
+                                            "buildings_zone":buildings_zone})
+        logger.log_status("Number of buildings: %s" % (buildings_zone.size()))
+        
+        #remove dataset from pool at end
         dptm = RealEstateTransitionModel(target_vancy_dataset=dataset_pool.get_dataset('target_vacancy'))
-        results, index = dptm.run(realestate_dataset = dataset_pool.get_dataset('building'),
+        results, index = dptm.run(realestate_dataset = buildings_zone,#dataset_pool.get_dataset('building'),
                            year = current_year,
                            occupied_spaces_variable = 'occupied_spaces',
                            total_spaces_variable = 'total_spaces',
                            target_attribute_name = 'target_vacancy_rate',
                            sample_from_dataset = dataset_pool.get_dataset('building'),
                            dataset_pool=dataset_pool,
-                           append_to_realestate_dataset = True,
+                           append_to_realestate_dataset = False,
                            reset_attribute_value={'parcel_id':-1},
                            sample_filter="(building.building_type_id==1)*(building.year_built>1989) +  (building.building_type_id==3)*(building.year_built>1979) +  (building.building_type_id==2)*(building.year_built>1989)+ (building.building_type_id>3)*(building.building_type_id<12)"
                            #year_built = 2001,
@@ -43,49 +57,65 @@ class RealEstateTransitionModelBySubmarkets(RealEstateTransitionModel):
                            #dataset_name = 'building',
                            #id_name = 'building_id'
                            )
-        #This is where, now that we know the demand for sqft, I'd want to insert the appropriate amount of permitted/proposed projects.
-        building_set.modify_attribute('year_built', array(index.size*[current_year]), index)
-        minimum_new_building_id = (building_set.get_attribute('building_id'))[index].min()
-        max_old_building_id = minimum_new_building_id - 1
-        index_new_sf_units = where(logical_and(building_set['building_id']>max_old_building_id, building_set['building_type_id']==1))[0]
-        #Come up with a random list of eligible parcel_id's of length index_new_sf_units, then assign these parcel_id's to this index of buildings
-        has_available_sf_capacity = parcel_set.compute_variables('_has_available_sf_capacity = parcel.disaggregate(zoning.allow_sf==1) * (((safe_array_divide(parcel.parcel_sqft,parcel.disaggregate(zoning.min_lot_size)).round().astype(int32)) - (parcel.number_of_agents(building)))>0)')
-        idx_has_available_sf_capacity = where(has_available_sf_capacity)[0]
-        parcel_ids_available_sf_capacity=(parcel_set.get_attribute('parcel_id'))[idx_has_available_sf_capacity]
-        shuffle(parcel_ids_available_sf_capacity)
-        parcel_ids_to_assign = parcel_ids_available_sf_capacity[:index_new_sf_units.size]
-        building_set.modify_attribute('parcel_id', parcel_ids_to_assign, index_new_sf_units)
-        #remember at the end of this program to delete all buildings with parcel_id of -1.  These buildings should not be placed because zoning is maxed out (e.g. sf detached will prolly be maxed after 10 yr...)
-        index_new_mf_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==2)+(building_set['building_type_id']==3)))[0]
-        index_new_industrial_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==6)+(building_set['building_type_id']==7)))[0]
-        index_new_commercial_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==9)+(building_set['building_type_id']==8)))[0]
-        for building in index_new_commercial_units:
-            available_commercial_capacity = parcel_set.compute_variables('_available_commercial_capacity = parcel.disaggregate(zoning.allow_comm==1) * clip_to_zero((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)-(parcel.aggregate(1000*building.residential_units))) - (parcel.aggregate(building.non_residential_sqft)))')
-            building_sqft = building_set['non_residential_sqft'][building]
-            idx_building_would_fit = where(available_commercial_capacity>=building_sqft)[0]
-            parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
-            shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
-            parcel_id_to_assign = parcel_ids_with_enough_capacity[:1] #WHAT TO DO ONCE THERE ARE NO PARCELS WITH CAPACITY?!?
-            building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
-        for building in index_new_industrial_units:
-            available_industrial_capacity = parcel_set.compute_variables('_available_industrial_capacity = parcel.disaggregate(zoning.allow_indust==1) * clip_to_zero((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)-(parcel.aggregate(1000*building.residential_units))) - (parcel.aggregate(building.non_residential_sqft)))')
-            building_sqft = building_set['non_residential_sqft'][building]
-            idx_building_would_fit = where(available_industrial_capacity>=building_sqft)[0]
-            parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
-            shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
-            parcel_id_to_assign = parcel_ids_with_enough_capacity[:1]
-            building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
-        for building in index_new_mf_units:
-            available_mf_capacity = parcel_set.compute_variables('_available_mf_capacity = parcel.disaggregate(zoning.allow_mf==1) * clip_to_zero(((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)) - (parcel.aggregate(building.non_residential_sqft)))/1000 - (parcel.aggregate(building.residential_units)))')
-            building_resunits = building_set['residential_units'][building]
-            idx_building_would_fit = where(available_mf_capacity>=building_resunits)[0]
-            parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
-            shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
-            parcel_id_to_assign = parcel_ids_with_enough_capacity[:1]
-            building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
-        index_unplaced_buildings = where(logical_and(building_set['building_id']>max_old_building_id, building_set['parcel_id']<=0))[0]
-        logger.log_status("Number of unplaced buildings to be removed: %s" % (index_unplaced_buildings.size))
-        building_set.remove_elements(index_unplaced_buildings)
+        logger.log_status("Number of buildings: %s" % (buildings_zone.size()))
+        logger.log_status("results_size: %s" % (results.size()))
+        #del results['development_project_id']
+        attribs2 = results.get_known_attribute_names()
+        table_data2={}
+        for name in attribs2:
+            logger.log_status("attrib: %s" % (name)) 
+            logger.log_status("attrib: %s" % (results.get_attribute("%s" %(name)) ))
+            if name in attribs:
+                table_data2["%s"%(name)]=results.get_attribute("%s" %(name))
+        logger.log_status("table_data2: %s" % (table_data2)) 
+        building_set.add_elements(data=table_data2, require_all_attributes=False, change_ids_if_not_unique=True) 
+        ##########Ok, at this point, I can loop through zones and add all sampled buildings to the buildings table with parcel_id of -1.  At this point, move to placing the building within the appropriate zone, similar to how its done below
+        #####Now connect the two parts of the code!!  My zonal RETM implementation above, with my parcel-placement code below.  Tweak the parcel placement code so that it works at the zone level too.
+
+
+        # #This is where, now that we know the demand for sqft, I'd want to insert the appropriate amount of permitted/proposed projects.
+        # building_set.modify_attribute('year_built', array(index.size*[current_year]), index)
+        # minimum_new_building_id = (building_set.get_attribute('building_id'))[index].min()
+        # max_old_building_id = minimum_new_building_id - 1
+        # index_new_sf_units = where(logical_and(building_set['building_id']>max_old_building_id, building_set['building_type_id']==1))[0]
+        # #Come up with a random list of eligible parcel_id's of length index_new_sf_units, then assign these parcel_id's to this index of buildings
+        # has_available_sf_capacity = parcel_set.compute_variables('_has_available_sf_capacity = parcel.disaggregate(zoning.allow_sf==1) * (((safe_array_divide(parcel.parcel_sqft,parcel.disaggregate(zoning.min_lot_size)).round().astype(int32)) - (parcel.number_of_agents(building)))>0)')
+        # idx_has_available_sf_capacity = where(has_available_sf_capacity)[0]
+        # parcel_ids_available_sf_capacity=(parcel_set.get_attribute('parcel_id'))[idx_has_available_sf_capacity]
+        # shuffle(parcel_ids_available_sf_capacity)
+        # parcel_ids_to_assign = parcel_ids_available_sf_capacity[:index_new_sf_units.size]
+        # building_set.modify_attribute('parcel_id', parcel_ids_to_assign, index_new_sf_units)
+        # #remember at the end of this program to delete all buildings with parcel_id of -1.  These buildings should not be placed because zoning is maxed out (e.g. sf detached will prolly be maxed after 10 yr...)
+        # index_new_mf_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==2)+(building_set['building_type_id']==3)))[0]
+        # index_new_industrial_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==6)+(building_set['building_type_id']==7)))[0]
+        # index_new_commercial_units = where(logical_and(building_set['building_id']>max_old_building_id, (building_set['building_type_id']==9)+(building_set['building_type_id']==8)))[0]
+        # for building in index_new_commercial_units:
+            # available_commercial_capacity = parcel_set.compute_variables('_available_commercial_capacity = parcel.disaggregate(zoning.allow_comm==1) * clip_to_zero((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)-(parcel.aggregate(1000*building.residential_units))) - (parcel.aggregate(building.non_residential_sqft)))')
+            # building_sqft = building_set['non_residential_sqft'][building]
+            # idx_building_would_fit = where(available_commercial_capacity>=building_sqft)[0]
+            # parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
+            # shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
+            # parcel_id_to_assign = parcel_ids_with_enough_capacity[:1] #WHAT TO DO ONCE THERE ARE NO PARCELS WITH CAPACITY?!?
+            # building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
+        # for building in index_new_industrial_units:
+            # available_industrial_capacity = parcel_set.compute_variables('_available_industrial_capacity = parcel.disaggregate(zoning.allow_indust==1) * clip_to_zero((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)-(parcel.aggregate(1000*building.residential_units))) - (parcel.aggregate(building.non_residential_sqft)))')
+            # building_sqft = building_set['non_residential_sqft'][building]
+            # idx_building_would_fit = where(available_industrial_capacity>=building_sqft)[0]
+            # parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
+            # shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
+            # parcel_id_to_assign = parcel_ids_with_enough_capacity[:1]
+            # building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
+        # for building in index_new_mf_units:
+            # available_mf_capacity = parcel_set.compute_variables('_available_mf_capacity = parcel.disaggregate(zoning.allow_mf==1) * clip_to_zero(((((parcel.parcel_sqft)*parcel.disaggregate(zoning.max_far)).round().astype(int32)) - (parcel.aggregate(building.non_residential_sqft)))/1000 - (parcel.aggregate(building.residential_units)))')
+            # building_resunits = building_set['residential_units'][building]
+            # idx_building_would_fit = where(available_mf_capacity>=building_resunits)[0]
+            # parcel_ids_with_enough_capacity = (parcel_set.get_attribute('parcel_id'))[idx_building_would_fit]
+            # shuffle(parcel_ids_with_enough_capacity) #replace with code involving random/uniform/cumprob/searchsorted etc...  I think it would be faster
+            # parcel_id_to_assign = parcel_ids_with_enough_capacity[:1]
+            # building_set.modify_attribute('parcel_id', parcel_id_to_assign, building)
+        # index_unplaced_buildings = where(logical_and(building_set['building_id']>max_old_building_id, building_set['parcel_id']<=0))[0]
+        # logger.log_status("Number of unplaced buildings to be removed: %s" % (index_unplaced_buildings.size))
+        # building_set.remove_elements(index_unplaced_buildings)
 
 
 
