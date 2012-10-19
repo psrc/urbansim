b019046003054bf773bb8ad7b7e02450e635b0d9
fsfoti <fsfoti@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-10-19 18:40:16 +0000

    only sample pdas not in san fran - were getting too much dev in san fran
    git-svn-id: https://svn.urbansim.org/src/trunk@19242 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/proposal/developer_model.py
+++ urbansim_parcel/proposal/developer_model.py
@@ -144,8 +144,8 @@ class DeveloperModel(Model):
     res_parcels = parcel_set.compute_variables("(parcel.number_of_agents(building)>0)*(parcel.node_id>0)*(parcel.shape_area>80)")
     bart_parcels = parcel_set.compute_variables("(parcel.disaggregate(bayarea.node.transit_type_1_within_800_meters))")
     caltrain_parcels = parcel_set.compute_variables("(parcel.disaggregate(bayarea.node.transit_type_2_within_800_meters))")
-    pda_parcels = parcel_set.compute_variables("(parcel.pda_id > -1)")
-    SAMPLE_RATE = 0.01
+    pda_parcels = parcel_set.compute_variables("((parcel.pda_id > -1) * (parcel.county_id <> 38))")
+    SAMPLE_RATE = 0
     from opus_core.sampling_toolbox import sample_noreplace
     from numpy import concatenate, where
     sampled_res_parcels_index = sample_noreplace(where(res_parcels)[0], int(SAMPLE_RATE * parcel_set.size()))
