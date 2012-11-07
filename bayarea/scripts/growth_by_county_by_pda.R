34114609c98fa1b95f46931fbcaffc63d4631259
akselkolsen <akselkolsen@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-11-07 00:21:09 +0000

    path hardcode. bad. must fix later
    git-svn-id: https://svn.urbansim.org/src/trunk@19343 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- bayarea/scripts/growth_by_county_by_pda.R
+++ bayarea/scripts/growth_by_county_by_pda.R
@@ -20,8 +20,8 @@ scenario <- args[5]
 
 ##load stuff
 #filist<-list.files(path=pth,pattern='area_permutation_table-1_2010_2015_2020_2025_2030_2035_2040.tab')
-#summary <- file.path(pth,filist[1],fsep = .Platform$file.sep)
-summary<-'area_permutation_table-1_2010_2015_2020_2025_2030_2035_2040.tab'
+file<-'area_permutation_table-1_2010_2015_2020_2025_2030_2035_2040.tab'
+summary <- file.path(pth,file,fsep = .Platform$file.sep)
 abag <- read.csv(summary,header = TRUE, sep="\t")
 
 ##discard extraneous identifier columns
