1fd7efc2b8609099b34cff5bcc30f8cef1909311
schirpat <schirpat@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-11-29 16:38:02 +0000

    REPM and HLCM on level of living_unit
    git-svn-id: https://svn.urbansim.org/src/trunk@19424 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/parcel/households_from_age_DDD_to_DDD_within_DDD_of_parcel.py
@@ -0,0 +1,87 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+
+
+#this module is based on the jobs_of_sector_DDD_within_DDD_of_parcel module
+#testing has not been adapted to changes and is still original
+
+from abstract_variable_within_radius_DDD_of_parcel import abstract_variable_within_radius_DDD_of_parcel
+
+class households_from_age_DDD_to_DDD_within_DDD_of_parcel(abstract_variable_within_radius_DDD_of_parcel):
+    """total number of households in agegroup min_age to max_age within radius DDD of parcel"""
+    _return_type = "int32"
+
+    def __init__(self, min_age, max_age, radius):
+        self.quantity = "_nhouseholds_from_age_%s_to_%s = parcel.aggregate(numpy.logical_and(household.age_of_head>%s ,household.age_of_head<%s) , intermediates=[living_unit, building])" % (min_age, max_age, min_age, max_age)
+        abstract_variable_within_radius_DDD_of_parcel.__init__(self, radius)
+
+    def post_check(self, values, dataset_pool):
+        self.do_check("x >= 0", values)
+
+from opus_core.tests import opus_unittest
+from numpy import array, int32
+from opus_core.tests.utils.variable_tester import VariableTester
+
+
+class Tests(opus_unittest.OpusTestCase):
+    
+    def test_my_inputs_sector_1(self):
+        tester = VariableTester(
+            __file__,
+            package_order=['urbansim_parcel', 'urbansim'],
+            test_data={
+            'parcel':
+            {
+                "parcel_id":        array([1,   2,    3]),
+                "x_coord_sp": array([1,   2,    3 ]),
+                "y_coord_sp": array([1,   1,    1 ]),
+            },
+            'job':
+            {
+                "job_id":array([1,2,3,4,5,6,7]),
+                "building_id":array([1,2,3,4,5,6,7]),
+                "sector_id":array([1,2,2,1,2,1,2]),
+             },
+            'building':
+            {
+                "building_id":array([1,2,3,4,5,6,7]),
+                "parcel_id":array([1,2,2,2,2,1,3]),
+             },
+        })
+        should_be = array([3, 3, 1])
+
+        instance_name = 'urbansim_parcel.parcel.jobs_of_sector_1_within_1_of_parcel'
+        tester.test_is_equal_for_family_variable(self, should_be, instance_name)
+    
+    def test_my_inputs_sector_2(self):
+        tester = VariableTester(
+            __file__,
+            package_order=['urbansim_parcel', 'urbansim'],
+            test_data={
+            'parcel':
+            {
+                "parcel_id":        array([1,   2,    3]),
+                "x_coord_sp": array([1,   2,    3 ]),
+                "y_coord_sp": array([1,   1,    1 ]),
+            },
+            'job':
+            {
+                "job_id":array([1,2,3,4,5,6,7]),
+                "building_id":array([1,2,3,4,5,6,7]),
+                "sector_id":array([1,2,2,1,2,1,2]),
+             },
+            'building':
+            {
+                "building_id":array([1,2,3,4,5,6,7]),
+                "parcel_id":array([1,2,2,2,2,1,3]),
+             },
+        })
+        should_be = array([3, 4, 4])
+
+        instance_name = 'urbansim_parcel.parcel.jobs_of_sector_2_within_1_of_parcel'
+        tester.test_is_equal_for_family_variable(self, should_be, instance_name)
+
+if __name__=='__main__':
+    opus_unittest.main()
