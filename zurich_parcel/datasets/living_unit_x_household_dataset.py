1bdc822869c5398d1d374fa9bc9fb7be8267c5d4
schirpat <schirpat@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-11-29 16:38:24 +0000

    REPM and HLCM on level of living_unit
    git-svn-id: https://svn.urbansim.org/src/trunk@19425 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/datasets/living_unit_x_household_dataset.py
@@ -0,0 +1,9 @@
+# Opus/UrbanSim urban simulation software.
+# Extension of SustainCity for the Zurich Case Study
+
+from urbansim.datasets.interactions import InteractionDataset
+
+class LivingUnitXHouseholdDataset(InteractionDataset):
+    """Dataset used by Household Location Choice Models
+    """
+    pass
