8d645f16b7078295a003e394439f4f3da0908136
schirpat <schirpat@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2012-12-17 13:21:53 +0000

    add alias for building_id; needed forn "within_SSS_of_parcel" when building_id removed from households;
    git-svn-id: https://svn.urbansim.org/src/trunk@19504 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/household/aliases.py
@@ -0,0 +1,8 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2012 ETH Zurich, Switzerland
+# See opus_core/LICENSE 
+
+aliases = [
+           "building_id = household.disaggregate(building.building_id, intermediates=[living_unit])",
+           
+           ]
