b4e59910196991eed0423ed219d2106f0a6a5f7b
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-01-15 01:34:35 +0000

    hdf5 storage classes implemented (hdf5, hdf5g) - one for a flat- and one for a grouped file structure.
    git-svn-id: https://svn.urbansim.org/src/trunk@19534 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_core/store/hdf5_storage.py
@@ -0,0 +1,140 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+import h5py
+import os
+from glob import glob
+from numpy import array, dtype
+from opus_core.misc import unique
+from opus_core.logger import logger
+from opus_core.store.storage import Storage
+import re
+
+class hdf5_storage(Storage):
+    """Storage class for hdf5 I/O. It supports a structure where every table attribute is 
+            its own hdf5 'dataset'. For a structure using groups, see hdf5g_storage.
+        """
+    def __init__(self,  storage_location):
+        self._base_directory = storage_location
+        
+    def get_storage_location(self):
+        return self._base_directory
+      
+    def _get_file_path(self, file_name=None):
+        dataset_path = self.get_storage_location()
+        if not os.path.exists(dataset_path):
+            raise StandardError("Path '%s' does not exist!" % dataset_path)
+        if file_name is None:
+            return dataset_path
+        return os.path.join(dataset_path, file_name)
+    
+    def _get_file_path_for_table(self, table_name):
+        return self._get_file_path('%s.hdf5' % table_name)
+    
+    def _load_columns(self, f, column_names, lowercase=True):
+        result = {}
+        for column_name in f.keys():
+            if lowercase:
+                column_name = column_name.lower()
+            if column_names == Storage.ALL_COLUMNS or column_name in column_names:
+                result[column_name] = f[column_name][...]
+        return result
+    
+    def load_table(self, table_name, column_names=Storage.ALL_COLUMNS, lowercase=True):
+        """
+        The table is loaded from a file called {table_name}.hdf5 assuming to have a 'flat' structure
+        (i.e. each table column is a hdf5 dataset on the most upper level).
+        """        
+        full_file_name = self._get_file_path_for_table(table_name)   
+        f = h5py.File(full_file_name, 'r')
+        result = self._load_columns(f, column_names, lowercase)               
+        f.close()
+        return result
+
+    def _get_hdf5mode(self, mode):
+        h5mode = mode
+        if mode == Storage.APPEND:
+            h5mode = 'a'
+        elif mode == Storage.OVERWRITE:
+            h5mode = 'w'
+        return h5mode
+    
+    def write_table(self, table_name, table_data, mode = Storage.OVERWRITE, driver=None, **kwargs):
+        """
+        Argument table_name specifies the file name in the base storage directory (without the suffix '.hdf5'). 
+        table_data is a dictionary where keys are the column names and values 
+            are value arrays of the corresponding columns.
+        Each column is stored as an hdf5 dataset. 
+        Argument driver is passed to the h5py.File. kwargs are passed to the h5py create_dataset function, 
+        e.g. compression. 
+        """
+        dir = self.get_storage_location()        
+        if not os.path.exists(dir):
+            logger.log_status("%s doesn't exist and is created" % dir)
+            os.makedirs(dir)
+        unused_column_size, column_names = self._get_column_size_and_names(table_data)
+        f = h5py.File(os.path.join(dir, '%s.hdf5' % table_name), self._get_hdf5mode(mode), driver=driver)
+        for column_name in column_names:                    
+            column_ds = f.create_dataset(column_name, data=table_data[column_name], **kwargs)
+        f.close()
+
+    def get_column_names(self, table_name, lowercase=True):
+        full_file_name = self._get_file_path_for_table(table_name) 
+        f = h5py.File(full_file_name, 'r')
+        result = f.keys()
+        if lowercase:
+            result = self._lower_case(result)
+        f.close()
+        return result
+
+    def get_table_names(self):
+        file_names = glob(os.path.join(self.get_storage_location(), '*.hdf5'))
+        return [os.path.splitext(os.path.basename(file_name))[0] for file_name in file_names]
+    
+    def has_table(self, table_name):
+        return os.path.exists(self._get_file_path_for_table(table_name))
+    
+
+    
+from opus_core.tests import opus_unittest
+from opus_core.store.storage import TestStorageInterface
+from shutil import rmtree
+from tempfile import mkdtemp
+
+from numpy import array
+
+class TestHDF5Storage(TestStorageInterface):
+    def setUp(self):
+        self.temp_dir = mkdtemp(prefix='opus_core_test_hdf5_storage')            
+        self.storage = hdf5_storage(storage_location=self.temp_dir)
+        
+    def tearDown(self):
+        if os.path.exists(self.temp_dir):
+            rmtree(self.temp_dir)
+        
+    def test_hdf5_storage(self):
+        data = {
+            'bar': array([1,2,3]),
+            'baz': array(['one', 'two', 'three']),
+            }
+        self.storage.write_table(
+            table_name = 'foo',
+            table_data = data
+            )          
+        filename = os.path.join(self.temp_dir, 'foo.hdf5')
+        self.assert_(os.path.exists(filename))
+              
+        actual = self.storage.load_table(
+            table_name = 'foo', 
+            column_names = ['bar', 'baz']
+            )           
+        self.assertDictsEqual(data, actual)
+        self.assertEqual(self.storage.get_column_names('foo'), ['bar', 'baz'])
+        self.assertEqual(self.storage.get_table_names(), ['foo'])
+        self.assertEqual(self.storage.has_table('foo'), True)
+        self.assertEqual(self.storage.has_table('bar'), False)
+        
+if __name__ == '__main__':
+    opus_unittest.main()   
+
---
+++ opus_core/store/hdf5g_storage.py
@@ -0,0 +1,129 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+import h5py
+import os
+from numpy import array, dtype
+from opus_core.misc import unique
+from opus_core.logger import logger
+from opus_core.store.hdf5_storage import hdf5_storage
+from opus_core.store.storage import Storage
+import re
+
+class hdf5g_storage(hdf5_storage):
+    """Like hdf5_storage, but it supports a grouped file structure where every table is a group and 
+       its columns are stored on the second level of that group. 
+       An object of this class is associated with a file name (passed to the init method with its full path), 
+       since all datasets are stored in one file.
+    """
+      
+    def load_table(self, table_name, column_names=Storage.ALL_COLUMNS, lowercase=True):
+        """
+        It is assumed that the table is stored as a group called table_name.
+        """
+        full_file_name = self._get_file_path()      
+        f = h5py.File(full_file_name, 'r')
+        if table_name not in f.keys():
+            raise KeyError, 'Table %s not found in %s.' % (table_name, full_file_name)
+        result = self._load_columns(f[table_name], column_names, lowercase) 
+        f.close()
+        return result
+
+    def write_table(self, table_name, table_data, mode = Storage.APPEND, driver=None, **kwargs):
+        """
+        table_data is a dictionary where keys are the column names and values 
+            are value arrays of the corresponding columns.
+        Each table is stored as a group called table_name. Each column is stored as an hdf5 dataset.
+        Set the argument mode to 'a' if data should be appended to an existing file, otherwise the file is overwritten.       
+        Argument driver is passed to the h5py.File. Other arguments can be passed to the h5py create_dataset function, 
+        e.g. compression. 
+        """
+        
+        file_name = self.get_storage_location()  
+        dir = os.path.split(file_name)[0]     
+        if not os.path.exists(dir):
+            logger.log_status("%s doesn't exist and is created" % dir)
+            os.makedirs(dir)
+        unused_column_size, column_names = self._get_column_size_and_names(table_data)
+        h5mode = self._get_hdf5mode(mode)
+        f = h5py.File(file_name, h5mode, driver=driver)
+        if h5mode == 'a' and table_name in f.keys():
+            raise StandardError, 'File %s already contains table %s. Use mode="w" to overwrite the table.' % (file_name, table_name)
+        group = f.create_group(table_name)
+        for column_name in column_names:                    
+            column_ds = group.create_dataset(column_name, data=table_data[column_name], **kwargs)
+        f.close()
+
+
+    def get_column_names(self, table_name, lowercase=True):
+        full_file_name = self._get_file_path()      
+        f = h5py.File(full_file_name, 'r')
+        if table_name not in f.keys():
+            raise KeyError, 'Table %s not found in %s.' % (table_name, full_file_name)
+        result = f[table_name].keys()
+        if lowercase:
+            result = [file.lower() for file in result]
+        f.close()
+        return result
+
+    def get_table_names(self):
+        """
+        Returns a list of the names of the tables in storage. 
+        """    
+        f = h5py.File(self._get_file_path() , 'r')
+        result = f.keys()
+        f.close()
+        return result
+    
+    def has_table(self, table_name):
+        f = h5py.File(self._get_file_path() , 'r')
+        res = table_name in f.keys()
+        f.close()
+        return res
+    
+from opus_core.tests import opus_unittest
+from opus_core.store.storage import TestStorageInterface
+from shutil import rmtree
+from tempfile import mkdtemp
+
+from numpy import array
+
+class TestHDF5Storage(TestStorageInterface):
+    def setUp(self):
+        self.temp_dir = mkdtemp(prefix='opus_core_test_hdf5g_storage')
+        self.temp_file = os.path.join(self.temp_dir, 'xxx.hdf5')         
+        self.storage = hdf5g_storage(storage_location=self.temp_file)
+        
+    def tearDown(self):
+        if os.path.exists(self.temp_file):
+            os.remove(self.temp_file)
+        if os.path.exists(self.temp_dir):
+            rmtree(self.temp_dir)
+        
+    def test_hdf5_storage_with_groups(self):
+        data1 = {
+            'bar': array([1,2,3]),
+            'baz': array(['one', 'two', 'three']),
+            }
+        data2 = {
+            'aaa': array([1,2,3,4,5]),
+            'baz': array(['one', 'two', 'three', 'four', 'five']),
+            }
+        self.storage.write_table(
+            table_name = 'foo1', table_data = data1) 
+        self.storage.write_table(
+            table_name = 'foo2', table_data = data2)          
+              
+        actual1 = self.storage.load_table(table_name = 'foo1', column_names = ['bar', 'baz'])           
+        self.assertDictsEqual(data1, actual1)
+        actual2 = self.storage.load_table(table_name = 'foo2')           
+        self.assertDictsEqual(data2, actual2)
+        self.assertEqual(self.storage.get_column_names('foo1'), ['bar', 'baz'])
+        self.assertEqual(self.storage.get_table_names(), ['foo1', 'foo2'])
+        self.assertEqual(self.storage.has_table('foo1'), True)
+        self.assertEqual(self.storage.has_table('bar'), False)
+
+if __name__ == '__main__':
+    opus_unittest.main()   
+
