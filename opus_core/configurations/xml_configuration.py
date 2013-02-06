psrc/urbansim

commit 1ca0ec2f1259e41ba9103cef7bb30fe1f140a518

improve handling of inherited comments in XML configuration: parent comments are not shown in shadowing nodes (no way to distinguish if they are inherited or not)
git-svn-id: https://svn.urbansim.org/src/trunk@19561 1513fa5d-83ce-422e-ae47-6967bb0fb594

opus_core/configurations/xml_configuration.py
@@ -4,7 +4,7 @@
 
 import copy, os, pprint
 from numpy import array
-from lxml.etree import ElementTree, tostring, fromstring, Element, _Comment
+from lxml.etree import ElementTree, tostring, fromstring, Element, _Comment, Comment
 from opus_core.configuration import Configuration
 from opus_core.configurations.xml_version import XMLVersion
 from opus_core.version_numbers import minimum_xml_version, maximum_xml_version
@@ -599,11 +599,11 @@ class XMLConfiguration(object):
             if name != 'inherited' and not name in local_node.attrib:
                 local_node.set(name, value)
         # pair up nodes with their id's
-        parent_child_nodes = dict((node_identity_string(n), n) for n in parent_node.getchildren())
-        local_child_nodes = dict((node_identity_string(n), n) for n in local_node.getchildren())
+        parent_child_nodes = dict((node_identity_string(n), n) for n in parent_node.iterchildren(tag=Element))
+        local_child_nodes = dict((node_identity_string(n), n) for n in local_node.iterchildren(tag=Element))
         # decide what to do with each child node of the parent tree
         node_index = 0
-        for n in parent_node.getchildren():
+        for n in parent_node.iterchildren(tag=Element):
             id_ = node_identity_string(n)
             parent_child_node = parent_child_nodes[id_]
             if id_ in local_child_nodes:
@@ -1739,6 +1739,16 @@ class XMLConfigurationTests(opus_unittest.OpusTestCase):
             self.assert_(re.match(r'.*child2\.xml$', config.get_first_writable_parent_file()), 'Second parent')
         finally:
             os.access = old_os_access
+            
+    def test_parent_comments_are_removed(self):
+        f = os.path.join(self.test_configs, 'child1.xml')
+        config = XMLConfiguration(f)
+
+        print(tostring(config.full_tree.getroot()))
+        for n in config.full_tree.getroot().getiterator(tag=Comment):
+            self.assert_(not re.match('.* parent .*', n.text),
+                          'Parent comments (except parent-only comments) have been removed')
+        
 
 if __name__ == '__main__':
     opus_unittest.main()

opus_core/configurations/test_configurations/parent1.xml
@@ -2,14 +2,15 @@
   <xml_version>2.0</xml_version>
   <general>
     <expression_library type="dictionary">
+      <!--some parent comment-->
       <variable dataset="parcel" source="expression" type="variable_definition" use="model variable" name="ln_cost">ln(psrc.parcel.cost)</variable>
-      <variable dataset="parcel" source="Python class" type="variable_definition" use="model variable" name="existing_units">urbansim_parcel.parcel.existing_units<!--some comment--></variable>
+      <variable dataset="parcel" source="Python class" type="variable_definition" use="model variable" name="existing_units">urbansim_parcel.parcel.existing_units<!--some parent-only comment--></variable>
     </expression_library>
-    <!--some comment-->
-    <project_name type="string">test_project<!--some comment--></project_name>
+    <!--some parent comment-->
+    <project_name type="string">test_project<!--some parent-only comment--></project_name>
     <test_node oceanic_attribute="squid" test_attribute="parent1_value" type="string">this is just to test attribute inheritance</test_node>
     <years_to_run type="tuple">
-      <firstyear type="integer">1981<!--some comment--></firstyear>
+      <firstyear type="integer">1981<!--some parent-only comment--></firstyear>
       <lastyear type="integer">1984</lastyear>
     </years_to_run>
   </general>

commit 1f9544129ae26875d065ed66dd2b26c841a21fc8

fix regression introduced in r19561: odd handling of parent configurations with lists containing duplicates; test included
git-svn-id: https://svn.urbansim.org/src/trunk@19606 1513fa5d-83ce-422e-ae47-6967bb0fb594

opus_core/configurations/test_configurations/dupname_child.xml
@@ -1,0 +1,9 @@
+<opus_project>
+  <xml_version>2.0</xml_version>
+  <general>
+    <parent type="file">dupname_parent.xml</parent>
+    <expression_library type="dictionary">
+       <variable name="parcel.lns_bldgage" source="expression" type="variable_definition" use="model variable">1</variable>
+    </expression_library>
+   </general>
+ </opus_project>

opus_core/configurations/test_configurations/dupname_parent.xml
@@ -1,0 +1,10 @@
+<opus_project>
+  <xml_version>2.0</xml_version>
+  <general>
+    <expression_library type="dictionary">
+      <variable use="model variable" source="Python class" name="person_x_job.network_distance_from_home_to_work" type="variable_definition">0</variable>
+      <variable use="model variable" source="Python class" name="person_x_job.network_distance_from_home_to_work" type="variable_definition">0</variable>
+  </expression_library>
+  </general>
+</opus_project>
+

opus_core/configurations/xml_configuration.py
@@ -603,7 +603,7 @@ class XMLConfiguration(object):
         local_child_nodes = dict((node_identity_string(n), n) for n in local_node.iterchildren(tag=Element))
         # decide what to do with each child node of the parent tree
         node_index = 0
-        for n in parent_node.iterchildren(tag=Element):
+        for n in tuple(parent_node.iterchildren(tag=Element)):
             id_ = node_identity_string(n)
             parent_child_node = parent_child_nodes[id_]
             if id_ in local_child_nodes:
@@ -1742,6 +1742,15 @@ class XMLConfigurationTests(opus_unittest.OpusTestCase):
             
     def test_parent_comments_are_removed(self):
         f = os.path.join(self.test_configs, 'child1.xml')
+        config = XMLConfiguration(f)
+
+        print(tostring(config.full_tree.getroot()))
+        for n in config.full_tree.getroot().getiterator(tag=Comment):
+            self.assert_(not re.match('.* parent .*', n.text),
+                          'Parent comments (except parent-only comments) have been removed')
+            
+    def test_dup_names_in_parents_do_not_crash_merge_configs(self):
+        f = os.path.join(self.test_configs, 'dupname_child.xml')
         config = XMLConfiguration(f)
 
         print(tostring(config.full_tree.getroot()))