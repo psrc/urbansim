644858d01af8834fb745928da8a749b99b93f4af
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-02-13 02:28:05 +0000

    generic script for converting tables from one format to another
    git-svn-id: https://svn.urbansim.org/src/trunk@19664 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_core/tools/convert_table.py
@@ -0,0 +1,90 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+import os
+import sys
+
+from optparse import OptionParser
+
+from opus_core.logger import logger
+from opus_core.storage_factory import StorageFactory
+from opus_core.export_storage import ExportStorage
+from opus_core.database_management.database_server import DatabaseServer
+from opus_core.database_management.configurations.database_configuration import DatabaseConfiguration
+
+if __name__ == '__main__':
+    parser = OptionParser(usage="python %prog from to [options]",
+                          description='Converts a dataset from one format to another. "from" and "to" can be one of the following: '
+                          'csv, tab, tsv, flt, sql, dbf, esri, hdf5, hdf5g')
+    
+    parser.add_option('-d', '--directory', dest='directory',
+        type='string', help='The filesystem path containing the input data. If converting a database table, this is the database name (required).')
+    parser.add_option('-o', '--output', dest='output', 
+        type='string', help='Full name of the output directory or file or output database name (required).')
+    parser.add_option('-t', '--table_name', dest='table_name', 
+        type='string', help='The name of the table (required). If exporting from a file, it must correspond to the filename without the suffix.')
+    parser.add_option('--no-type-info', dest='append_type_info', action="store_false", default=True,
+                      help='No type info is added to column names. Available for tab and comma delimited output types.')
+    parser.add_option('--compression', dest='compression', type='choice', default=None, choices=['gzip', 'lzf'],
+                      help='Compression type for hdf5 and hdf5g output types. Available: gzip, lzf. Default is no compression.')
+    parser.add_option("--database_configuration", dest="database_configuration", default = "indicators_database_server",
+            action="store", help="Name of the database server configuration in database_server_configurations.xml where the output database is to be created. Defaults to 'indicators_database_server'. Used only with sql output type.")
+    (options, args) = parser.parse_args()
+
+    directory = options.directory
+    output_file = options.output
+    table_name = options.table_name
+    
+    arg_list = {'hdf5': ['compression'],
+                'hdf5g': ['compression'],
+                'tab': ['append_type_info'],
+                'tsv': ['append_type_info'],
+                'csv': ['append_type_info'],
+                }
+    
+    if (directory is None or output_file is None or table_name is None):      
+        parser.print_help()
+        sys.exit(1)
+    storage_intype = args[0]
+    storage_outtype = args[1]
+        
+    if storage_intype == 'sql':
+        database_name = directory
+        db_server = DatabaseServer(DatabaseConfiguration(
+            database_name = database_name,
+            database_configuration = options.database_configuration
+            )
+        )                
+        db = db_server.get_database(database_name)
+        directory = db
+        
+    if storage_outtype == 'sql':
+        database_name = output_file
+        db_server = DatabaseServer(DatabaseConfiguration(
+            database_name = database_name,
+            database_configuration = options.database_configuration
+            )
+        )
+        if not db_server.has_database(database_name):
+            db_server.create_database(database_name)                          
+        db = db_server.get_database(database_name)
+        output_file = db
+        
+    input_storage = StorageFactory().get_storage('%s_storage' % storage_intype, storage_location = directory)
+    output_storage = StorageFactory().get_storage('%s_storage' % storage_outtype, storage_location = output_file)
+    
+    logger.start_block("Converting table '%s' from %s into %s ..." % (table_name, storage_intype, storage_outtype))
+    kwargs = {}
+    for arg in arg_list.get(storage_outtype, []):
+        kwargs[arg] = getattr(options, arg, None)
+    try:
+        if hasattr(output_storage,"drop_table"):
+            output_storage.drop_table(table_name)
+        ExportStorage().export_dataset(
+            dataset_name = table_name,
+            in_storage = input_storage, 
+            out_storage = output_storage, **kwargs)
+    finally:
+        logger.end_block()
+    
