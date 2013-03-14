336e21284d0b23b61d1b16bd142e80d4f38ea5fc
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-03-14 06:43:09 +0000

    add gpl
    git-svn-id: https://svn.urbansim.org/src/trunk@19722 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/proforma/isr.py
+++ urbansim_parcel/proforma/isr.py
@@ -1,3 +1,7 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
 import psycopg2
 import os
 from opus_core import paths
