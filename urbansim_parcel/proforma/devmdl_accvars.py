e5fdc488d3ea11893fa12020dd7709b16aa46d2f
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-03-14 06:42:29 +0000

    add gpl
    git-svn-id: https://svn.urbansim.org/src/trunk@19720 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/proforma/devmdl_accvars.py
+++ urbansim_parcel/proforma/devmdl_accvars.py
@@ -1,4 +1,6 @@
-################Need a better way to handle residential building types.  In xml, we should have a place to define which building types fall into sf-res, mf-res
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
 
 from numpy import array
 import numpy
