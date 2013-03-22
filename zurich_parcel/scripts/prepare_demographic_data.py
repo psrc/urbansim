54c6856ebd0eec2ca4bc587f8e11be7bf773a165
muelleki <muelleki@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-03-22 02:51:02 +0000

    experimental reading of chunks, with ID recoding
    git-svn-id: https://svn.urbansim.org/src/trunk@19769 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- zurich_parcel/scripts/prepare_demographic_data.py
+++ zurich_parcel/scripts/prepare_demographic_data.py
@@ -57,50 +57,67 @@ def read_csv_with_numpy(in_fname):
                       skiprows=1, delimiter=",")
     return data
 
-def read_native_write_h5py(in_fname, out_fname, dataset_name, 
+def read_native_write_h5py(in_fnamel, out_fname, dataset_name, 
                            skiprows=1, delimiter=",", comments="#",
                           rename_and_fix_attrs=None):
     logger.log_note('Importing %s' % dataset_name)
-    names, dtype = read_header(in_fname, rename_attrs=rename_and_fix_attrs)
+    names, dtype = read_header(in_fnamel[0], rename_attrs=rename_and_fix_attrs)
 
-    shape = determine_dims(in_fname)
+    shape = determine_dims(in_fnamel[0])
 
     out_fh = h5py.File(out_fname)
     h5data = out_fh.create_dataset(dataset_name, shape=(shape[0],), dtype=dtype, 
                                    compression='gzip', compression_opts=5)
     
-    with open(in_fname, 'U') as fh:
-        for irow, row in enumerate(fh):
-            if irow < skiprows: continue
-            
-            if irow % 1e4 == 0:
-                logger.log_note('Processed %d/%d rows' % (irow, shape[0]))
+    nextdelta = None
+    for i, in_fname in enumerate(in_fnamel):
+        with open(in_fname, 'U') as fh:
+            logger.log_note('Processing %s' % in_fnamel)
+            for irow, row in enumerate(fh):
+                if irow < skiprows: continue
+                
+                if irow % 1e4 == 0:
+                    logger.log_note('Processed %d/%d rows' % (irow, shape[0]))
+    
+                row = row.split(comments)[0].strip()
+                if row == '': continue
+                vals = [int(val) for val in row.split(delimiter)]
+                
+                maxdelta = dict( (names.index(n), vals[names.index(n)]) for n in rename_and_fix_attrs.values())
 
-            row = row.split(comments)[0].strip()
-            if row == '': continue
-            vals = [int(val) for val in row.split(delimiter)]
-            
-            # Adjust those attributes in rename_and_fix_attrs
-            # by the respective value of the first record
-            if irow == skiprows:
-                delta = dict( (names.index(n), vals[names.index(n)]) for n in rename_and_fix_attrs.values())
-                logger.log_note('Adjusting IDs: %s' % delta)
-            for i, d in delta.iteritems():
-                vals[i] -= d
-            
-            h5data[irow-1] = np.array([tuple(vals)], dtype=dtype)
-            
-        logger.log_note('Processed %d rows in total' % (irow + 1))
+                # Adjust those attributes in rename_and_fix_attrs
+                # by the respective value of the first record
+                if irow == skiprows:
+                    delta = dict( (n, nextdelta[n] + 1 - maxdelta[n]) for n in maxdelta.keys())
+                    logger.log_note('Adjusting IDs: %s' % delta)
+                for i, d in delta.iteritems():
+                    vals[i] += d
+                
+                h5data[irow-1] = np.array([tuple(vals)], dtype=dtype)
+                
+            logger.log_note('Processed %d rows in total' % (irow + 1))
+        
+        nextdelta = maxdelta
     out_fh.close()
     return h5data
 
+def usage():
+    print "Usage: python %s.py chunks in_fname_hh [...] in_fname_person [...] out_fname" % sys.argv[0]
+    sys.exit(0)
+
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print "Usage: python %s.py in_fname_hh in_fname_person out_fname" % sys.argv[0]
-        sys.exit(0)
+    if len(sys.argv) < 2:
+        usage()
+    
+    chunks = sys.argv[1]
+
+    if len(sys.argv) != 3 + 2 * chunks:
+        usage()
         
     in_fnames = {}
-    in_fnames['household'], in_fnames['person'], out_fname = sys.argv[1:4]
+    in_fnames['household'] = sys.argv[2:(2+chunks)]
+    in_fnames['person'] = sys.argv[(2+chunks):(2+2*chunks)]
+    out_fname = sys.argv[2+2*chunks]
 
     try:
         os.unlink(out_fname)
@@ -117,8 +134,8 @@ if __name__ == '__main__':
         ## read
         #data = read_csv_with_numpy(in_fname) 
         #data = read_csv_native(in_fname) 
-        in_fname = in_fnames[dataset_name]
-        data = read_native_write_h5py(in_fname, out_fname, dataset_name,
+        in_fnamel = in_fnames[dataset_name]
+        data = read_native_write_h5py(in_fnamel, out_fname, dataset_name,
                                       rename_and_fix_attrs=rename_attrs[dataset_name])
         
         logger.log_note('Reorganizing %s' % dataset_name)
