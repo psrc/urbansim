ff53d2a11e3d39702a2295e791f19e87c3f746c9
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-04-02 21:44:53 +0000

    added support for cases when there are agents with no available choices
    git-svn-id: https://svn.urbansim.org/src/trunk@19795 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- opus_core/upc/random_choices_from_index.py
+++ opus_core/upc/random_choices_from_index.py
@@ -5,6 +5,7 @@
 from opus_core.choices import Choices
 from opus_core.random_choices import random_choices
 from opus_core.misc import take_choices
+from numpy import where
 
 class random_choices_from_index(Choices):
     """ Like random_choices, but uses an index (see docs for the compute method)."""
@@ -16,8 +17,16 @@ class random_choices_from_index(Choices):
         The returned value is a 1D array of choice indices [0, nequations-1] of length nobservations.
         If the entry 'index' is missing, the returned value is the returned value of 'random_choices'.
         """
+        allow_no_choice_availability = resources.get("allow_no_choice_availability", True)
+        if allow_no_choice_availability:
+            zero_prob_sum = where(probability.sum(axis=1)==0)[0]
+            if zero_prob_sum.size > 0:            # agents with no availability of choices
+                probability[zero_prob_sum, 0] = 1 # to pass the random choices call
+ 
         choice_idx = random_choices().run(probability, resources)
         index = resources.get("index", None)
         if index <> None:
-            return take_choices(index, choice_idx) # transfer random choices to indices
+            choice_idx = take_choices(index, choice_idx) # transfer random choices to indices
+        if allow_no_choice_availability and zero_prob_sum.size > 0:
+            choice_idx[zero_prob_sum] = resources.get("choice_value_for_no_availability", -1)
         return choice_idx
