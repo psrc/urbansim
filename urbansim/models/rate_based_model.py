5145311d8b02239d3a1df984bfa929880dd6b276
ejanowicz <ejanowicz@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-04-17 22:32:29 +0000

    revert previous change
    git-svn-id: https://svn.urbansim.org/src/trunk@19841 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim/models/rate_based_model.py
+++ urbansim/models/rate_based_model.py
@@ -72,9 +72,7 @@ class RateBasedModel(Model):
                         sample_rates=False, 
                         n=100, 
                         multiplicator=1, 
-                        flush_rates=True,
-                        future_probabilities=False,
-                        transition_years = None):
+                        flush_rates=True):
         """
         what - unused, argument kept to be compatible with old code 
         """
@@ -93,19 +91,6 @@ class RateBasedModel(Model):
                                                            }
                                                     )
         if probability_attribute is not None:
-            rates.probability_attribute = probability_attribute
-        if (future_probabilities is True) and (transition_years is not None):
-            current_year = SimulationState().get_current_time()
-            firstyear,lastyear = transition_years
-            if current_year >= firstyear:
-                base_rates = rates.get_attribute(probability_attribute)
-                future_rates = rates.get_attribute('future_' + probability_attribute)
-                if current_year < lastyear:
-                    number_of_interpolation_steps = lastyear - firstyear + 1
-                    step_size = (future_rates - base_rates) / number_of_interpolation_steps
-                    current_step_number = current_year - firstyear + 1
-                    future_rates = base_rates + step_size * current_step_number
-                rates.modify_attribute(probability_attribute, future_rates)
             rates.probability_attribute = probability_attribute
         if sample_rates:
             cache_storage=None
