611009f124ca5bce6abeebbae8217b875eeaab9f
zoelligc <zoelligc@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2013-05-07 09:40:14 +0000

    stub
    git-svn-id: https://svn.urbansim.org/src/trunk@19867 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ zurich_parcel/development_event_history/sector_id.py
@@ -0,0 +1,35 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from opus_core.variables.variable import Variable
+from numpy import ma, clip, where
+from opus_core.logger import logger, log_block
+
+class sector_id(Variable):
+    """ number of job spaces that is vacant/unoccupied"""
+    
+    _return_type = "int32"
+    
+    def dependencies(self):
+        return [
+                ]
+
+    @log_block(name='sector_id.compute')
+    def compute(self, dataset_pool):
+        pass
+
+    def post_check(self, values, dataset_pool=None):
+        self.do_check("x >= 0", values)
+
+from opus_core.tests import opus_unittest
+from opus_core.datasets.dataset_pool import DatasetPool
+from opus_core.storage_factory import StorageFactory
+from numpy import array
+from opus_core.tests.utils.variable_tester import VariableTester
+
+class Tests(opus_unittest.OpusTestCase):
+    pass
+
+if __name__=='__main__':
+    opus_unittest.main()
