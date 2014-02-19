eaf6db32f308b1c0c505b9cefcbc453d096a7e9a
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2014-02-19 23:55:05 +0000

    variable added
    git-svn-id: https://svn.urbansim.org/src/trunk@20109 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/person/aliases.py
+++ urbansim_parcel/person/aliases.py
@@ -11,6 +11,7 @@ aliases = ['is_worker = person.employment_status > 0',
            'workplace_zone_id=person.disaggregate(urbansim_parcel.job.zone_id)',
            'building_id = person.disaggregate(household.building_id)',
            'is_placed_non_home_based_worker_with_job = numpy.logical_and(urbansim_parcel.person.is_non_home_based_worker_with_job, urbansim_parcel.person.building_id > 0)',
+           'is_non_home_based_worker_with_placed_job = numpy.logical_and(urbansim_parcel.person.is_non_home_based_worker_with_job, urbansim_parcel.person.workplace_zone_id > 0)',
            'parcel_id = person.disaggregate(urbansim_parcel.household.parcel_id)',
            'large_area_id = person.disaggregate(urbansim_parcel.household.large_area_id)',
            ]
