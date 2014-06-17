e409a7c9a10d1a0905faadbc62c67b091b9f544c
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2014-06-17 11:49:31 +0000

    4k variables added/updated
    git-svn-id: https://svn.urbansim.org/src/trunk@20157 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/person_x_job/logsum_hbw_am_from_home_to_work_4k.py
+++ psrc_parcel/person_x_job/logsum_hbw_am_from_home_to_work_4k.py
@@ -2,9 +2,9 @@
 # Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
 # See opus_core/LICENSE
 
-from psrc_parcel.household_x_building.workerDDD_logsum_hbw_am_from_home_to_work import workerDDD_logsum_hbw_am_from_home_to_work
+from psrc_parcel.person_x_job.logsum_hbw_am_from_home_to_work import logsum_hbw_am_from_home_to_work
 
-class workerDDD_logsum_hbw_am_from_home_to_work_4k(workerDDD_logsum_hbw_am_from_home_to_work):
+class logsum_hbw_am_from_home_to_work_4k(logsum_hbw_am_from_home_to_work):
     """logsum_hbw_am_time_from_home_to_work (4k model)
        logsum breaks by income:
            Less than $34K;
@@ -13,5 +13,5 @@ class workerDDD_logsum_hbw_am_from_home_to_work_4k(workerDDD_logsum_hbw_am_from_
            More than $102K.
     """
     default_value = -20
-    agent_category_attribute = "(psrc_parcel.household.income_breaks_34000_64000_102000).astype(int32)"
+    agent_category_attribute = "logsum_income_break = (person.disaggregate(psrc_parcel.household.income_breaks_34000_64000_102000)).astype(int32)"
 
