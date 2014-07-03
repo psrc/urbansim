b0f4dbbca658e2abbb7bab2c184e1f6ca270f253
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2014-07-03 11:54:32 +0000

    job.county_id variable added
    git-svn-id: https://svn.urbansim.org/src/trunk@20165 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- urbansim_parcel/job/aliases.py
+++ urbansim_parcel/job/aliases.py
@@ -11,6 +11,7 @@ aliases = [
    "faz_id = job.disaggregate(urbansim_parcel.building.faz_id)",
    "dummy_id = urbansim_parcel.job.faz_id * 100 + job.sector_id",
    "large_area_id = job.disaggregate(parcel.large_area_id, intermediates=[building])",
+   "county_id = job.disaggregate(parcel.county_id, intermediates=[building])",
    "is_home_based = job.home_based_status==1",
    "is_non_home_based = numpy.logical_not(urbansim_parcel.job.is_home_based)",
    'residence_type = job.disaggregate(building.building_type_id)',
