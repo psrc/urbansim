16b2b8fc7eee1ffd25c2a7a744efbff75c120762
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2014-08-04 18:09:43 +0000

    model for deleting attributes
    git-svn-id: https://svn.urbansim.org/src/trunk@20174 1513fa5d-83ce-422e-ae47-6967bb0fb594

---
+++ opus_core/models/delete_attribute_model.py
@@ -0,0 +1,52 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2005-2009 University of Washington
+# See opus_core/LICENSE 
+
+from opus_core.model import Model
+from opus_core.variables.variable_name import VariableName
+from numpy import ndarray, ones_like, where
+from opus_core.logger import logger
+
+class DeleteAttributeModel(Model):
+    """
+    The model deletes given attribute of the given dataset.
+    """
+
+    def __init__(self, model_name=None, *args, **kwargs):
+        Model.__init__(self, *args, **kwargs)
+        if model_name is not None:
+            self.model_name = model_name
+
+    def run(self, dataset, attribute):
+        """
+        If 'attribute' is not a known attribute of 'dataset', the model does nothing.
+        Otherwise the attribute is deleted.
+        """
+        if attribute in dataset.get_known_attribute_names():
+            dataset.delete_one_attribute(attribute)
+        return
+    
+from opus_core.tests import opus_unittest
+from opus_core.storage_factory import StorageFactory
+from opus_core.datasets.dataset import Dataset
+from numpy import arange, array, ma, sqrt, log, zeros
+
+class DeleteAttributeModelTest(opus_unittest.OpusTestCase):
+    def setUp(self):
+        self.data = {
+            'id': arange(10)+1,
+            'attribute':  array([3000,2800,1000,550,600,1000,2000,500,100,1000]),
+        }
+        storage = StorageFactory().get_storage('dict_storage')
+        storage.write_table(table_name = 'dataset', table_data = self.data)
+        self.dataset = Dataset(in_storage=storage, in_table_name='dataset', id_name=['id'])
+
+    def test_delete_attribute_model(self):
+        m = DeleteAttributeModel()
+        m.run(self.dataset, 'attr')
+        self.assertEqual('attribute' in self.dataset.get_primary_attribute_names(), True)
+        m.run(self.dataset, 'attribute')
+        self.assertEqual('attribute' in self.dataset.get_primary_attribute_names(), False)
+
+if __name__=="__main__":
+    opus_unittest.main()
