0f346f2d2d742fad4014a5b8f33dc4048039bcba
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2014-12-09 16:25:32 +0000

    travel time to cbd fixed
    git-svn-id: https://svn.urbansim.org/src/trunk@20228 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc/zone/travel_time_hbw_am_drive_alone_to_cbd.py
+++ psrc/zone/travel_time_hbw_am_drive_alone_to_cbd.py
@@ -2,18 +2,21 @@
 # Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
 # See opus_core/LICENSE
 
+from numpy import minimum
 from opus_core.variables.variable import Variable
 from variable_functions import my_attribute_label
 
 class travel_time_hbw_am_drive_alone_to_cbd(Variable):
-    """Travel time to the CBD zone whose ID is the 129.
+    """Minimum travel time to CBD zones.
     The travel time used is for the home-based-work am trips by auto with 
     drive-alone.
     """
-    cbd_zone = "129"
-    variable_name = "travel_time_hbw_am_drive_alone_to_"+cbd_zone
+
     def dependencies(self):
-        return [my_attribute_label(self.variable_name)]
+        return [my_attribute_label("travel_time_hbw_am_drive_alone_to_seattle_cbd"),
+                my_attribute_label("travel_time_hbw_am_drive_alone_to_bellevue_cbd")]
 
     def compute(self, dataset_pool):
-        return self.get_dataset().get_attribute(self.variable_name)
+        ds = self.get_dataset()
+        return minimum(ds["travel_time_hbw_am_drive_alone_to_seattle_cbd"], 
+                       ds["travel_time_hbw_am_drive_alone_to_bellevue_cbd"])
