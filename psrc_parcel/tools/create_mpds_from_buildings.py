0a55be66cc74d7d698a42c913fe665daeac661fb
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2015-01-20 19:37:39 +0000

    debugging
    git-svn-id: https://svn.urbansim.org/src/trunk@20248 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/tools/create_mpds_from_buildings.py
+++ psrc_parcel/tools/create_mpds_from_buildings.py
@@ -1,4 +1,5 @@
-from numpy import where, all, unique, array, zeros, logical_not, round, arange, in1d
+from numpy import where, all, unique, array, zeros, logical_not, logical_and, round, arange
+from numpy import in1d, argmin, abs
 from opus_core.storage_factory import StorageFactory
 from opus_core.datasets.dataset_pool import DatasetPool
 
@@ -21,14 +22,17 @@ class InverseMPDs:
         else:
             raise InputError, "Either input_table or filter must be given."
         self.parcels_not_processed = []
+        self.original_templates = None
         
     def compute_building_variables(self):
         self.input_buildings.compute_variables(["land_use_type_id = building.disaggregate(parcel.land_use_type_id)",
                                                 "urbansim_parcel.building.is_residential"], 
                                                dataset_pool=self.dataset_pool)
                        
-    def is_residential_building(self, idx):
-        return self.input_buildings["is_residential"][idx] and self.input_buildings["residential_units"][idx] > 0
+    def get_units(self, idx):
+        if self.input_buildings["is_residential"][idx] and self.input_buildings["residential_units"][idx] > 0:
+            return "residential_units"
+        return "non_residential_sqft"
                       
     def preprocess_datasets(self):
         self.compute_building_variables()
@@ -47,10 +51,7 @@ class InverseMPDs:
                 cons_idx = bidx[0]
                 for attr in ["non_residential_sqft", "land_area", "residential_units"]:
                     self.input_buildings[attr][cons_idx] = self.input_buildings[attr][bidx].sum()
-                if self.is_residential_building(cons_idx):
-                    unitattr = "residential_units"
-                else:
-                    unitattr = "non_residential_sqft"
+                unitattr = self.get_units(cons_idx)
                 totsqft = self.input_buildings[unitattr][bidx] * self.input_buildings["sqft_per_unit"][bidx]
                 if self.input_buildings[unitattr][cons_idx] > 0:
                     self.input_buildings["sqft_per_unit"][cons_idx] = round(totsqft.sum()/self.input_buildings[unitattr][cons_idx].astype("float32"))
@@ -60,37 +61,65 @@ class InverseMPDs:
             bldgs_to_remove[bidx] = True
             self.parcels_not_processed = self.parcels_not_processed + [parcels["parcel_id"][i]]
         self.input_buildings.subset_by_index(where(logical_not(bldgs_to_remove))[0])
-        
+        if "template_id" in self.input_buildings.get_known_attribute_names():
+            self.original_templates = self.input_buildings["template_id"].copy()
+        self.input_buildings.add_attribute(zeros(self.input_buildings.size(), dtype="int32"), name="template_id",
+                                           metadata=1)
         print "%s buildings consolidated into %s." % (consolidated[1], consolidated[0])
         print "%s parcels were not processed." % len(self.parcels_not_processed)
         print "Updated total: %s buildings." % self.input_buildings.size()
         
-    def run(self):
+    def run(self, outstorage, output_table=None):
         templates = self.dataset_pool.get_dataset("development_template")
         template_comps = self.dataset_pool.get_dataset("development_template_component")
-        land_sqft = self.input_buildings["land_area"]*43560.00
+        templates.compute_variables("urbansim_parcel.development_template.density_converter", 
+                                               dataset_pool=self.dataset_pool)
+        land_sqft = self.input_buildings["land_area"]
+        results = self.input_buildings["template_id"]
+        no_template_found = []
         for bidx in arange(self.input_buildings.size()):
             # match by land use type and building type
-            templ_match = templates["land_use_type_id"] == self.input_buildings["land_use_type_id"][bidx]
+            #templ_match = templates["land_use_type_id"] == self.input_buildings["land_use_type_id"][bidx]
             comp_match = template_comps["building_type_id"] == self.input_buildings["building_type_id"][bidx]
-            templ_match = logical_and(templ_match, in1d(templates["template_id"], template_comps["template_id"]))
-            # match land area
-            
+            #templ_match = logical_and(templ_match, in1d(templates["template_id"], template_comps["template_id"]))
+            templ_match = in1d(templates["template_id"], template_comps["template_id"])
+            # match land area            
             templ_match = logical_and(templ_match, 
                                       logical_and(land_sqft[bidx] <= templates["land_sqft_max"],
-                                                  land_sqft[bidx] >= templates["land_sqft_min"])) 
-        
+                                                  land_sqft[bidx] >= templates["land_sqft_min"]))
+            templ_idx = where(templ_match)[0]
+            if templ_idx.size == 0:
+                results[bidx] = -1
+                no_template_found = no_template_found + [bidx]
+                continue
+            units = zeros(templ_idx.size, dtype='float32')
+            for i in arange(templ_idx.size):
+                units[i] = self.input_buildings["land_area"][bidx]*(1-templates["percent_land_overhead"][templ_idx[i]])/100.0*templates["density"][templ_idx[i]]*templates["density_converter"][templ_idx[i]]
+            unitattr = self.get_units(bidx)
+            winner_templ = argmin(abs(units - self.input_buildings[unitattr][bidx]))
+            results[bidx] = templates["template_id"][templ_idx[winner_templ]]
+        # write results
+        if self.original_templates is not None:
+            self.input_buildings.add_attribute(self.original_templates, name="original_template_id", metadata=1)
+        self.input_buildings.write_dataset(out_storage=outstorage, out_table_name=output_table, attributes=1)
+        print "No template found for %s buildings." % len(no_template_found)
+            
 if __name__ == '__main__':
     ### User's settings:
     #input_cache =  "/Users/hana/workspace/data/psrc_parcel/base_year_data/2000"
     input_cache =  "/Users/hana/workspace/data/psrc_parcel/MPDs/inverse_templates"
-    output_dir =  "/Users/hana/workspace/data/psrc_parcel/MPDs/inverse_templates"
-    input_buildings_table = "buildings1999"
-    buildings_filter = None # if input_buildings_table is None, use this to filter out buildings from the input cache 
+    #input_buildings_table = "buildings1999"
+    input_buildings_table = None
+    #buildings_filter = None # if input_buildings_table is None, use this to filter out buildings from the input cache 
+    buildings_filter = "building.year_built==2005"
+    output_cache =  "/Users/hana/workspace/data/psrc_parcel/MPDs/inverse_templates"
+    output_buildings_table = "buildings1999out"
+    output_buildings_table = "buildings2005out"
     ### End of user's settings
     instorage = FltStorage().get(input_cache)
+    outstorage = FltStorage().get(output_cache)
     model = InverseMPDs(instorage, input_buildings_table, buildings_filter)
     model.preprocess_datasets()
-    model.run()
-    
+    model.run(outstorage, output_table=output_buildings_table)
+    #TODO: Compare templates id (2004 buildings)
     
