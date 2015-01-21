34a1726801ea76b1838e650f32c81f24e08d7f73
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2015-01-21 01:29:25 +0000

    handling multiple buildings on a parcel
    git-svn-id: https://svn.urbansim.org/src/trunk@20253 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/tools/create_mpds_from_buildings.py
+++ psrc_parcel/tools/create_mpds_from_buildings.py
@@ -58,8 +58,8 @@ class InverseMPDs:
                 bldgs_to_remove[bidx[1:]] = True
                 consolidated = consolidated + array([1, bidx.size])
                 continue
-            bldgs_to_remove[bidx] = True
-            self.parcels_not_processed = self.parcels_not_processed + [parcels["parcel_id"][i]]
+#            bldgs_to_remove[bidx] = True
+#            self.parcels_not_processed = self.parcels_not_processed + [parcels["parcel_id"][i]]
         self.input_buildings.subset_by_index(where(logical_not(bldgs_to_remove))[0])
         if "template_id" in self.input_buildings.get_known_attribute_names():
             self.original_templates = self.input_buildings["template_id"].copy()
@@ -74,46 +74,65 @@ class InverseMPDs:
         template_comps = self.dataset_pool.get_dataset("development_template_component")
         templates.compute_variables(["urbansim_parcel.development_template.density_converter", 
                                      "number_of_components = (development_template.number_of_agents(development_template_component)).astype(int32)",
-                                     # careful with the construction cost if there are more than one component (here we consider only one component)
-                                     "construction_cost = development_template.aggregate(development_template_component.construction_cost_per_unit * development_template_component.building_sqft_per_unit)",
                                      ], 
                                             dataset_pool=self.dataset_pool)
         land_sqft = self.input_buildings["land_area"]
         results = self.input_buildings["template_id"]
         no_template_found = []
-        for bidx in arange(self.input_buildings.size()):
+        parcel_ids = unique(self.input_buildings["parcel_id"])
+        for pcl in parcel_ids:
+        #for bidx in arange(self.input_buildings.size()):
+            bidx = where(self.input_buildings['parcel_id'] == pcl)[0]
             # match by land use type and building type
             #templ_match = templates["land_use_type_id"] == self.input_buildings["land_use_type_id"][bidx]
-            templ_match = templates["number_of_components"] == 1
-            comp_match = template_comps["building_type_id"] == self.input_buildings["building_type_id"][bidx]
+            templ_match = templates["number_of_components"] == bidx.size
+            comp_match = in1d(template_comps["building_type_id"], self.input_buildings["building_type_id"][bidx])
             templ_match = logical_and(templ_match, in1d(templates["template_id"], template_comps["template_id"][where(comp_match)]))
             # match land area            
             templ_match = logical_and(templ_match, 
-                                      logical_and(land_sqft[bidx] <= templates["land_sqft_max"],
-                                                  land_sqft[bidx] >= templates["land_sqft_min"]))
+                                      logical_and(land_sqft[bidx].sum() <= templates["land_sqft_max"],
+                                                  land_sqft[bidx].sum() >= templates["land_sqft_min"]))
+            templ_idx = where(templ_match)[0]
+            for templ in templ_idx: # accept only templates where ALL components match in building type
+                ctmpl = where(template_comps["template_id"] == templates["template_id"][templ])[0]
+                if not (in1d(template_comps["building_type_id"][ctmpl], self.input_buildings["building_type_id"][bidx])).all():
+                    templ_match[templ] = False
+                    
             templ_idx = where(templ_match)[0]
             if templ_idx.size == 0:
                 results[bidx] = -1
                 no_template_found = no_template_found + [bidx]
                 continue
             units = zeros(templ_idx.size, dtype='float32')
+            improvement_value = zeros(templ_idx.size, dtype='float32')
             for i in arange(templ_idx.size):
-                units[i] = self.input_buildings["land_area"][bidx]*(1-templates["percent_land_overhead"][templ_idx[i]]/100.0)*templates["density"][templ_idx[i]]*templates["density_converter"][templ_idx[i]]
-            units = maximum(1, round_(units, 0))    
-            improvement_value = templates["construction_cost"][templ_idx] * units
-            unitattr = self.get_units(bidx)
-            winner_templ = argmin(abs(units - self.input_buildings[unitattr][bidx]))
+                units[i] = self.input_buildings["land_area"][bidx].sum()*(1-templates["percent_land_overhead"][templ_idx[i]]/100.0)*templates["density"][templ_idx[i]]*templates["density_converter"][templ_idx[i]]
+                ctmpl = where(template_comps["template_id"] == templates["template_id"][templ_idx[i]])[0]
+                improvement_value[i] = (template_comps["construction_cost_per_unit"][ctmpl] * template_comps["percent_building_sqft"][ctmpl]/100. * template_comps["building_sqft_per_unit"][ctmpl] * maximum(1, round(units[i],0))).sum()                
+            units = maximum(1, round_(units, 0))
+            
+            btotalunits = 0
+            mixed = self.input_buildings["is_residential"][bidx]
+            mixed = not (mixed == mixed[0]).all()
+            factor = 1      
+            for i in arange(bidx.size):
+                unitattr = self.get_units(bidx[i])
+                if mixed:
+                    factor = self.input_buildings["sqft_per_unit"][bidx][i]                  
+                btotalunits = btotalunits + self.input_buildings[unitattr][bidx][i] * factor
+            winner_templ = argmin(abs(units - btotalunits))
             all_winners = units == units[winner_templ]
             if all_winners.sum() > 1: # more than 1 winner
                 # choose the one with matching land use type
-                lut_match = templates["land_use_type_id"] == self.input_buildings["land_use_type_id"][bidx]
+                lut_match = in1d(templates["land_use_type_id"], self.input_buildings["land_use_type_id"][bidx])
                 winners = logical_and(all_winners, lut_match[templ_idx])
                 if winners.any():
                     winner_templ = where(winners)[0]
-                if winners.sum() > 1:
+                    all_winners = winners
+                if all_winners.sum() > 1:
                     # if still multiple winners choose one that matches improvement value
-                    impr_winner = argmin(abs(improvement_value[winner_templ] - self.input_buildings["improvement_value"][bidx]))
-                    winner_templ = winner_templ[impr_winner]
+                    impr_winner = argmin(abs(improvement_value[where(all_winners)] - self.input_buildings["improvement_value"][bidx].sum()))
+                    winner_templ = where(all_winners)[0][impr_winner]
             results[bidx] = templates["template_id"][templ_idx[winner_templ]]
         # write results
         if self.original_templates is not None:
