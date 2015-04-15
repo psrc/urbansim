da24008b5bf6be496fbeb959b7423a05fde17e7d
hana <hana@1513fa5d-83ce-422e-ae47-6967bb0fb594>
2015-04-15 01:13:50 +0000

    changes in the weighting of zones
    git-svn-id: https://svn.urbansim.org/src/trunk@20291 1513fa5d-83ce-422e-ae47-6967bb0fb594

--- psrc_parcel/development_project_proposal/expected_sales_price_per_sqft_luv.py
+++ psrc_parcel/development_project_proposal/expected_sales_price_per_sqft_luv.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2005-2009 University of Washington
 # See opus_core/LICENSE
 
-from numpy import where, logical_not, logical_or, logical_and, zeros
+from numpy import where, logical_not, logical_or, logical_and, zeros, ones
 from opus_core.variables.variable import Variable
 from opus_core.variables.functions import expfin
 from opus_core.logger import logger
@@ -15,23 +15,67 @@ class expected_sales_price_per_sqft_luv(Variable):
     _return_type="float32"
         
     zone_dict = {
-            "res-increase": {
-                    "zone_id": [2151, 2286, 2293, 2481, 2475, 2657, 2116, 2598, 6316],
+        "15": { # 15% revenue
+                "residential": {
+                    "zone_id": [2598, 2657],
+                    "faz_id": []
+                                },
+                 "nonresidential": {
+                    "zone_id": [2145, 2309, ],
+                    "faz_id": []
+                            }
+                },        
+        "18": { # double increase
+                "residential": {
+                    "zone_id": [2151, 2286, 2293, 2481, 2475,  2116, 6316],
                     "faz_id": [705]
-                   },
-            "res-decrease": {
+                                },
+                 "nonresidential": {
+                    "zone_id": [2172, 2127, 2284, 2570, 2520, 2487, 2554, 2249, 2169],
+                    "faz_id": [505, 705, 6216]
+                             }
+                },
+        "27": { # 3x increase
+                "residential": {
+                    "zone_id": [],
+                    "faz_id": []
+                                },
+                 "nonresidential": {
+                    "zone_id": [2147,],
+                    "faz_id": []
+                            }
+                }, 
+        "36": { # 4x increase
+                "residential": {
+                    "zone_id": [ ],
+                    "faz_id": []
+                                },
+                 "nonresidential": {
+                    "zone_id": [2145, 2309, ],
+                    "faz_id": [5925]
+                            }
+                }, 
+        "4.5": { # half decrease
+                "residential": {
+                    "zone_id": [],
+                    "faz_id": []
+                                },
+                 "nonresidential": {
+                    "zone_id": [2311, 2310, 2312, 2121, 2282, 2257, 2409, 2171, 2597],
+                    "faz_id": []                            }
+                }, 
+        "1": { #only 1% revenue 
+                "residential": {
                     "zone_id": [2214],
                     "faz_id": []
-                   },
-            "nonres-increase": {
-                    "zone_id": [2309, 2172, 2255, 2127, 2624, 2147, 2284, 2570, 2520, 2145, 2487, 2554, 2249, 2169, 2403],
-                    "faz_id": [505, 705, 5925, 6216]
-                   },
-            "nonres-decrease": {
-                    "zone_id": [2311, 2310, 2312, 2121, 2282, 2257, 2252, 2118, 2251, 2409, 2171, 2597, 2564],
-                    "faz_id": [5825, 6125, 6115, 6114, 5915]
-                   }
+                                },
+                 "nonresidential": {
+                    "zone_id": [2118, 2252, 2251, 2564],
+                    "faz_id": [5825, 5915, 6114, 6115, 6125]
+                            }
+                }
     }
+
     
     def dependencies(self):
         return ["development_project_proposal.expected_sales_price_per_sqft", 
@@ -42,23 +86,22 @@ class expected_sales_price_per_sqft_luv(Variable):
     
     def compute(self,  dataset_pool):
         dpp = self.get_dataset()
-        price = 0.09 * dpp["expected_sales_price_per_sqft"]
         is_res = dpp['is_res']
         is_non_res = logical_not(is_res)
-        idx = self._get_indicator("res-increase", is_res)
-        price[idx] = 2*price[idx]
-        idx = self._get_indicator("res-decrease", is_res)
-        price[idx] = 0.5*price[idx]
-        idx = self._get_indicator("nonres-increase", is_non_res)
-        price[idx] = 2*price[idx]
-        idx = self._get_indicator("nonres-decrease", is_non_res)
-        price[idx] = 0.5*price[idx]
+        multipl = 9*ones(dpp.size(), dtype="float32")
+        for m, set in self.zone_dict.iteritems():
+            idx = self._get_indicator("residential", set, is_res)
+            multipl[idx] = float(m)
+            idx = self._get_indicator("nonresidential", set, is_non_res)
+            multipl[idx] = float(m)
+            
+        price = multipl/100. * dpp["expected_sales_price_per_sqft"]
         return expfin(price)
     
-    def _get_indicator(self, what, res_ind):
+    def _get_indicator(self, what, set, res_ind):
         ds = self.get_dataset()
         indicator = zeros(ds.size(), dtype='bool8')
-        for geoid in self.zone_dict[what].keys():            
-            for zone in self.zone_dict[what][geoid]:
+        for geoid in set[what].keys():            
+            for zone in set[what][geoid]:
                 indicator = logical_or(indicator,ds[geoid] == zone)
         return where(logical_and(res_ind, indicator))
