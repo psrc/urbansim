832a267a6d4921acea73fd7c83d774726ab22e91
Hana Sevcikova <hsevcikova@psrc.org>
2016-08-16 17:42:38 -0700

    test code

--- psrc_parcel/travel_model_input_file_writer_emme4.py
+++ psrc_parcel/travel_model_input_file_writer_emme4.py
@@ -18,7 +18,8 @@ class TravelModelInputFileWriterEmme4(ParentTravelModelInputFileWriter):
         """
         input_dir = config['travel_model_configuration'].get('emme_input_directory', 
                                 os.path.join('tripgen', current_year_emme2_dir, 'landuse'))
-        print "input dir: ", input_dir
+        logger.log_status("input dir: ", input_dir, config['travel_model_configuration'].get('emme_input_directory'),
+                        os.path.join('tripgen', current_year_emme2_dir, 'landuse'))
         tm_input_file_1 = self._write_input_file_1(current_year_emme2_dir, input_dir, current_year, dataset_pool, config) # writes tazdata.in
         return [tm_input_file_1]
 
