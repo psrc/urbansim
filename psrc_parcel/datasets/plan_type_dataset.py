47c9845c8c62c4f50b201b7db72e7b6dd06db842
Modeller <Modeller@psrc.local>
2018-11-13 16:14:44 -0800

    adding plan types dataset to classify zoning for indicators

---
+++ psrc_parcel/datasets/plan_type_dataset.py
@@ -0,0 +1,15 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from urbansim.datasets.dataset import Dataset as UrbansimDataset
+
+class PlanTypeDataset(UrbansimDataset):
+
+    id_name_default = "plan_type_id"
+    in_table_name_default = "plan_types"
+    out_table_name_default = "plan_types"
+    dataset_name = "plan_type"
+
+    def __init__(self, id_values=None, **kwargs):
+        UrbansimDataset.__init__(self, **kwargs)
