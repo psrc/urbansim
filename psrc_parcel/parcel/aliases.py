fc271963603000736e8d41187f0257cc933c86a3
Hana Sevcikova <hanas@uw.edu>
2025-07-07 17:36:30 -0700

    variables moved to urbansim_parcel

--- psrc_parcel/parcel/aliases.py
+++ psrc_parcel/parcel/aliases.py
@@ -51,8 +51,7 @@ aliases = [
        "sqft_per_du_hb1110 = 1400 * (%s == 2)  + 1200 * (%s == 4) + 1000 * (%s == 6) + 1553 * (%s == 0)" % (
            "psrc_parcel.parcel.max_units_for_hb1110", "psrc_parcel.parcel.max_units_for_hb1110",
            "psrc_parcel.parcel.max_units_for_hb1110", "psrc_parcel.parcel.max_units_for_hb1110"
-       ), 
-       "is_zoned_res_or_mix = numpy.logical_or(numpy.logical_or(psrc_parcel.parcel.is_zoned_for_glu_1, psrc_parcel.parcel.is_zoned_for_glu_2), psrc_parcel.parcel.is_zoned_for_glu_6)"
+       )
        ]
 
 
---
+++ urbansim_parcel/parcel/is_zoned_for_glu_DDD.py
@@ -0,0 +1,18 @@
+# Opus/UrbanSim urban simulation software.
+# Copyright (C) 2010-2011 University of California, Berkeley, 2005-2009 University of Washington
+# See opus_core/LICENSE
+
+from opus_core.variables.variable import Variable
+import numpy as np
+
+class is_zoned_for_glu_DDD(Variable):
+    
+    def __init__(self, generic_land_use_type):
+        Variable.__init__(self)
+        self.glu = generic_land_use_type
+        
+    def compute(self,  dataset_pool):
+        parcels = self.get_dataset()
+        constraints = dataset_pool.get_dataset("development_constraint")         
+        plan_types_for_glu = np.unique(constraints["plan_type_id"][constraints["generic_land_use_type"] ==  self.glu])
+        return np.in1d(parcels["plan_type_id"], plan_types_for_glu)
