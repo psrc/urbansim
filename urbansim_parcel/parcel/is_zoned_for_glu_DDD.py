de6cf7f977825872d757181f56120a67b138f7da
Hana Sevcikova <hsevcikova@psrc.org>
2025-07-08 09:50:05 -0700

    variable fix

--- urbansim_parcel/parcel/is_zoned_for_glu_DDD.py
+++ urbansim_parcel/parcel/is_zoned_for_glu_DDD.py
@@ -14,5 +14,7 @@ class is_zoned_for_glu_DDD(Variable):
     def compute(self,  dataset_pool):
         parcels = self.get_dataset()
         constraints = dataset_pool.get_dataset("development_constraint")         
-        plan_types_for_glu = np.unique(constraints["plan_type_id"][constraints["generic_land_use_type"] ==  self.glu])
+        plan_types_for_glu = np.unique(constraints["plan_type_id"][np.logical_and(constraints["plan_type_id"] > 0,
+                                                                                  np.logical_and(constraints["generic_land_use_type_id"] ==  self.glu,
+                                                                                                 constraints["maximum"] > 0))])
         return np.in1d(parcels["plan_type_id"], plan_types_for_glu)
